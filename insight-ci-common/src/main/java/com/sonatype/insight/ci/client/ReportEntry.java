0d595b7e6f65966573f9aa82b68a6369086d837f
Stuart McCulloch <mcculls@gmail.com>
2012-05-29 14:28:30 +0100

    Extract common report embedding code

---
+++ insight-ci-common/src/main/java/com/sonatype/insight/ci/client/ReportEntry.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2011-2012 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/insight/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.ci.client;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.codehaus.plexus.util.IOUtil;
+
+public final class ReportEntry
+{
+    public final String name;
+
+    public final long time;
+
+    public final byte[] buf;
+
+    private ReportEntry( final String name, final long time, final byte[] buf )
+    {
+        this.name = name;
+        this.time = time;
+        this.buf = buf;
+    }
+
+    public static ReportEntry extract( final File report, final String path )
+        throws IOException
+    {
+        ZipFile archive = null;
+        try
+        {
+            archive = new ZipFile( report );
+            final ZipEntry entry = archive.getEntry( normalizePath( path ) );
+            if ( entry != null )
+            {
+                final byte[] buf = IOUtil.toByteArray( archive.getInputStream( entry ) );
+                return new ReportEntry( entry.getName(), entry.getTime(), buf );
+            }
+        }
+        finally
+        {
+            if ( archive != null )
+            {
+                archive.close(); // closes all InputStreams retrieved from this archive
+            }
+        }
+        return null;
+    }
+
+    private static String normalizePath( final String path )
+    {
+        if ( null == path || path.length() == 0 )
+        {
+            return "index.html";
+        }
+        boolean seenSlash = true;
+        StringBuilder buf = null;
+        for ( int i = 0, len = path.length(); i < len; i++ )
+        {
+            final char c = path.charAt( i );
+            final boolean isSlash = '/' == c;
+            if ( seenSlash && isSlash )
+            {
+                if ( buf == null )
+                {
+                    buf = new StringBuilder( path.subSequence( 0, i ) );
+                }
+            }
+            else if ( buf != null )
+            {
+                buf.append( c );
+            }
+            seenSlash = isSlash;
+        }
+        if ( seenSlash && buf != null )
+        {
+            buf.append( "index.html" );
+        }
+        return buf != null ? buf.toString() : path;
+    }
+}

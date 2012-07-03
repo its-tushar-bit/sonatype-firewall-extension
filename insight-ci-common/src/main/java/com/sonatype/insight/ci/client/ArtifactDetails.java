aad6ddecdb986e96971cf7c02639de124bd7ee33
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2012-07-03 22:33:45 +0200

    Added REST endpoint to CI plugin that acts as proxy for artifact detail requests INSIGHT-3070 INSIGHT-2986

---
+++ insight-ci-common/src/main/java/com/sonatype/insight/ci/client/ArtifactDetails.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2011-2012 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/insight/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.ci.client;
+
+import com.sonatype.insight.scan.upload.ReportDataResult;
+
+public final class ArtifactDetails
+{
+    public final int statusCode;
+
+    public final String contentType;
+
+    public final byte[] data;
+
+    ArtifactDetails( final ReportDataResult result )
+    {
+        statusCode = result.getStatusCode();
+        contentType = result.getContentType();
+        data = result.getData();
+    }
+}

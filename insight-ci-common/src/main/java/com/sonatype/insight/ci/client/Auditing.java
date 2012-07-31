43f569f793424edc3eb74f7612e9bb250be9e6b5
Stuart McCulloch <mcculls@gmail.com>
2012-07-31 16:36:22 +0100

    INSIGHT-3253: remove old RSS audit feed (not as flexible as new approach)

--- insight-ci-common/src/main/java/com/sonatype/insight/ci/client/Auditing.java
+++ insight-ci-common/src/main/java/com/sonatype/insight/ci/client/Auditing.java
@@ -7,7 +7,6 @@ package com.sonatype.insight.ci.client;
 
 import static com.sonatype.insight.ci.client.DataStore.augmentTable;
 import static com.sonatype.insight.ci.client.DataStore.filterDataLog;
-import static com.sonatype.insight.ci.client.DataStore.loadData;
 import static com.sonatype.insight.ci.client.DataStore.logData;
 import static com.sonatype.insight.ci.client.DataStore.parseData;
 import static com.sonatype.insight.ci.client.DataStore.streamData;
@@ -21,11 +20,6 @@ import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -37,16 +31,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.codehaus.plexus.util.IOUtil;
 import org.codehaus.plexus.util.StringUtils;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ContainerNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.sun.syndication.feed.synd.SyndContent;
-import com.sun.syndication.feed.synd.SyndContentImpl;
-import com.sun.syndication.feed.synd.SyndEntry;
-import com.sun.syndication.feed.synd.SyndEntryImpl;
-import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.feed.synd.SyndFeedImpl;
 
 public final class Auditing
 {
@@ -82,42 +68,6 @@ public final class Auditing
             ip = resolveIp( xff.split( "," ) );
         }
         return ip != null ? ip : request.getRemoteAddr();
-    }
-
-    public static SyndFeed getAuditFeed( final File auditDir )
-        throws IOException
-    {
-        final SyndFeedImpl feed = new SyndFeedImpl();
-
-        feed.setFeedType( "rss_2.0" );
-        feed.setPublishedDate( new Date() );
-        feed.setAuthor( "Insight CI" );
-        feed.setTitle( "Insight" );
-
-        feed.setDescription( "Insight Audit Log" );
-
-        final AuditLock lock = lockFor( auditDir );
-
-        lock.sharedLock();
-        try
-        {
-            final List<SyndEntry> entries = new ArrayList<SyndEntry>();
-            final String[] auditNames = auditDir.list( JSON_FILES );
-            if ( auditNames != null )
-            {
-                for ( final String name : auditNames )
-                {
-                    entries.addAll( getAuditEntries( auditDir, name ) );
-                }
-            }
-            feed.setEntries( entries );
-        }
-        finally
-        {
-            lock.sharedUnlock();
-        }
-
-        return feed;
     }
 
     public static byte[] filterAuditLog( final File auditDir, final byte[] key )
@@ -202,110 +152,6 @@ public final class Auditing
         {
             lock.sharedUnlock();
         }
-    }
-
-    private static List<SyndEntry> getAuditEntries( final File auditDir, final String name )
-        throws IOException
-    {
-        final File auditFile = new File( auditDir, name );
-        if ( !auditFile.canRead() )
-        {
-            return Collections.emptyList();
-        }
-
-        final String kind = StringUtils.chompLast( StringUtils.chompLast( name, ".json" ), "s" );
-
-        final ArrayNode dataLog = loadData( auditFile );
-        final List<SyndEntry> entries = new ArrayList<SyndEntry>( dataLog.size() );
-        for ( final JsonNode event : dataLog )
-        {
-            final JsonNode data = event.get( "data" );
-            if ( data.size() > 0 )
-            {
-                final SyndEntryImpl entry = new SyndEntryImpl();
-
-                entry.setPublishedDate( new Date( event.get( "time" ).asLong() ) );
-                final String user = event.get( "user" ).asText();
-                final String ip = event.get( "ip" ).asText();
-                entry.setAuthor( user + ":" + ip );
-
-                final List<String> summary = summarize( data, kind );
-
-                String title = summary.get( 0 );
-                if ( event.has( "where" ) )
-                {
-                    title += " in build " + event.get( "where" ).asText();
-                }
-                entry.setTitle( title );
-
-                final StringBuilder buf = new StringBuilder();
-                for ( int i = 1; i < summary.size(); i++ )
-                {
-                    buf.append( summary.get( i ) ).append( "<br>" );
-                }
-
-                final SyndContent description = new SyndContentImpl();
-                description.setValue( buf.toString() );
-                entry.setDescription( description );
-
-                entries.add( entry );
-            }
-        }
-
-        return entries;
-    }
-
-    private static List<String> summarize( final JsonNode data, final String kind )
-    {
-        final JsonNode status = data.get( 0 ).get( "status" );
-        final JsonNode overriden = data.get( 0 ).get( "overriddenLicenses" );
-        final JsonNode comment = data.get( 0 ).get( "comment" );
-
-        final StringBuilder title = new StringBuilder();
-        if ( status != null )
-        {
-            String label = status.asText();
-            label = label.replace( "Open", "Re-opened" );
-            label = label.replace( "Not Applicable", "Ignored" );
-            label = label.replace( "Overridden", "Overrode" );
-            title.append( label ).append( ' ' );
-        }
-
-        final int rows = data.size();
-        title.append( rows ).append( ' ' ).append( kind ).append( rows != 1 ? " alerts" : " alert" );
-
-        if ( overriden != null && overriden.size() > 0 )
-        {
-            title.append( " as " ).append( overriden.get( 0 ).asText() );
-        }
-
-        final List<String> summary = new ArrayList<String>();
-
-        summary.add( title.toString() );
-        if ( comment != null && StringUtils.isNotBlank( comment.asText() ) )
-        {
-            summary.add( comment.asText() );
-            summary.add( "" );
-        }
-        for ( int i = 0; i < rows; i++ )
-        {
-            final JsonNode artifact = data.get( i );
-            final StringBuilder text = new StringBuilder( Report.gav( artifact ) );
-            if ( artifact.has( "reference" ) )
-            {
-                text.append( " (" );
-                final String reference = artifact.get( "reference" ).asText();
-                final String source = artifact.get( "source" ).asText();
-                if ( !reference.toUpperCase( Locale.ENGLISH ).startsWith( source.toUpperCase( Locale.ENGLISH ) ) )
-                {
-                    text.append( source ).append( '-' );
-                }
-                text.append( reference ).append( ')' );
-            }
-            summary.add( text.toString() );
-        }
-
-        return summary;
     }
 
     private static AuditLock lockFor( final File auditDir )

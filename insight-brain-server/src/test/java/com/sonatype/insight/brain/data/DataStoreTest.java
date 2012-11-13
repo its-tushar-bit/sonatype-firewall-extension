6d95c7f11d8a4ca5b47a32aa40b6fe15582a7bb0
Stuart McCulloch <mcculls@gmail.com>
2012-11-13 19:11:41 +0000

    Add placeholder test

---
+++ insight-brain-server/src/test/java/com/sonatype/insight/brain/data/DataStoreTest.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2011-2012 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/insight/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.data;
+
+import org.junit.Test;
+
+public class DataStoreTest
+{
+    @Test
+    public void placeholder()
+    {
+    }
+}

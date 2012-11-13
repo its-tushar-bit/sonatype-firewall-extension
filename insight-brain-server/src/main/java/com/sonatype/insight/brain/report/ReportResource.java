d3ebd4a46faebe364e1adf4823e593c622ffc102
Stuart McCulloch <mcculls@gmail.com>
2012-11-13 19:05:03 +0000

    license:format

--- insight-brain-server/src/main/java/com/sonatype/insight/brain/report/ReportResource.java
+++ insight-brain-server/src/main/java/com/sonatype/insight/brain/report/ReportResource.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2011-2012 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/insight/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
 package com.sonatype.insight.brain.report;
 
 import java.io.File;

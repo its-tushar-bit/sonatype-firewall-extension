f2b8a4987d50d47d5b60d300c80a20a00aaf8570
Stuart McCulloch <mcculls@gmail.com>
2012-11-19 13:17:38 +0000

    Add quick workaround for DW logger restart issue in UTs/ITs

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/service/AbstractResourceTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/service/AbstractResourceTest.java
@@ -6,10 +6,14 @@
 package com.sonatype.insight.brain.service;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Iterator;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.ning.http.client.Response;
 
@@ -21,7 +25,6 @@ public abstract class AbstractResourceTest
     public void startService()
         throws Exception
     {
-        // The dropwizard's logging configuration code hangs if the service is stopped and re-started :(
         if ( service == null )
         {
             service = new TestInsightBrainService();
@@ -33,11 +36,13 @@ public abstract class AbstractResourceTest
     public void stopService()
         throws Exception
     {
-        // The dropwizard's logging configuration code hangs if the service is stopped and re-started :(
-        // if ( service != null )
-        // {
-        // service.stop();
-        // }
+        if ( service != null )
+        {
+            stopAsyncAppenders();
+
+            service.stop();
+            service = null;
+        }
     }
 
     protected static void assertResponseStatus( int expectedStatus, Response response )
@@ -46,5 +51,47 @@ public abstract class AbstractResourceTest
         int actualStatus = response.getStatusCode();
         Assert.assertEquals( "URI:" + response.getUri() + ", StatusText:" + response.getStatusText()
             + ", ResponseBody:" + response.getResponseBody(), expectedStatus, actualStatus );
+    }
+
+    /*
+     * Workaround for DropWizard 0.5.1 restart bug... (fixed in latest DW snapshot)
+     */
+    private void stopAsyncAppenders()
+    {
+        try
+        {
+            final ch.qos.logback.classic.Logger log =
+                (ch.qos.logback.classic.Logger) LoggerFactory.getLogger( Logger.ROOT_LOGGER_NAME );
+
+            final Field running = com.yammer.dropwizard.logging.AsyncAppender.class.getDeclaredField( "running" );
+            final Field dispatcher = com.yammer.dropwizard.logging.AsyncAppender.class.getDeclaredField( "dispatcher" );
+
+            running.setAccessible( true );
+            dispatcher.setAccessible( true );
+
+            final Iterator<? extends ch.qos.logback.core.Appender<?>> itr = log.iteratorForAppenders();
+            while ( itr.hasNext() )
+            {
+                final ch.qos.logback.core.Appender<?> appender = itr.next();
+                final Thread t = ( (Thread) dispatcher.get( appender ) );
+                try
+                {
+                    running.setBoolean( appender, false );
+                    t.interrupt();
+                    t.interrupt();
+                    t.interrupt();
+                }
+                catch ( final Exception e )
+                {
+                    continue;
+                }
+            }
+
+            log.getLoggerContext().stop();
+        }
+        catch ( final Exception e )
+        {
+            LoggerFactory.getLogger( getClass() ).warn( "Cannot interrupt AsyncAppender", e );
+        }
     }
 }

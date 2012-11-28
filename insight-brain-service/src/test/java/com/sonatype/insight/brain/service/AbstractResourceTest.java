3e0f033bcc7bbba9449b378992d9f8c712e85d01
vladt <vladt@sonatype.com>
2012-11-28 15:44:19 -0500

    Cleanup

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/service/AbstractResourceTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/service/AbstractResourceTest.java
@@ -6,14 +6,10 @@
 package com.sonatype.insight.brain.service;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.Iterator;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.ning.http.client.Response;
 
@@ -40,8 +36,6 @@ public abstract class AbstractResourceTest
         {
             service.stop();
             service = null;
-
-            stopAsyncAppenders();
         }
     }
 
@@ -51,39 +45,5 @@ public abstract class AbstractResourceTest
         int actualStatus = response.getStatusCode();
         Assert.assertEquals( "URI:" + response.getUri() + ", StatusText:" + response.getStatusText()
             + ", ResponseBody:" + response.getResponseBody(), expectedStatus, actualStatus );
-    }
-
-    /*
-     * Workaround for DropWizard 0.5.1 restart bug... (fixed in latest DW snapshot)
-     */
-    private void stopAsyncAppenders()
-    {
-        try
-        {
-            final ch.qos.logback.classic.Logger log =
-                (ch.qos.logback.classic.Logger) LoggerFactory.getLogger( Logger.ROOT_LOGGER_NAME );
-
-            final Field running = com.yammer.dropwizard.logging.AsyncAppender.class.getDeclaredField( "running" );
-            final Field dispatcher = com.yammer.dropwizard.logging.AsyncAppender.class.getDeclaredField( "dispatcher" );
-
-            running.setAccessible( true );
-            dispatcher.setAccessible( true );
-
-            final Iterator<? extends ch.qos.logback.core.Appender<?>> itr = log.iteratorForAppenders();
-            while ( itr.hasNext() )
-            {
-                final ch.qos.logback.core.Appender<?> appender = itr.next();
-                if ( appender instanceof com.yammer.dropwizard.logging.AsyncAppender )
-                {
-                    running.setBoolean( appender, false );
-                    final Thread t = ( (Thread) dispatcher.get( appender ) );
-                    t.interrupt();
-                }
-            }
-        }
-        catch ( final Exception e )
-        {
-            LoggerFactory.getLogger( getClass() ).warn( "Cannot interrupt AsyncAppender", e );
-        }
     }
 }

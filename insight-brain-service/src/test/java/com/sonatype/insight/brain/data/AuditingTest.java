b2dd108f4f3119a32e42d2250c56325f65a49980
vladt <vladt@sonatype.com>
2012-11-29 13:52:46 -0500

    Use TemporaryFolder to create/delete temp audit dirs INSIGHT-3872

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/data/AuditingTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/data/AuditingTest.java
@@ -20,233 +20,191 @@ import java.io.File;
 import java.io.IOException;
 
 import org.codehaus.plexus.util.FileUtils;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 @SuppressWarnings( "boxing" )
 public class AuditingTest
 {
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder( new File( "target" ) );
+
+    private File auditDir;
+
+    @Before
+    public void setUp()
+    {
+        auditDir = FileUtils.createTempFile( "audit", "test", temporaryFolder.getRoot() );
+    }
+
     @Test
     public void testNoAugmentedData()
         throws IOException
     {
-        final File auditDir = FileUtils.createTempFile( "audit", "test", new File( "target" ) );
-        try
-        {
-            final String table = "{ \"aaData\" : [ { \"id\" : \"A\" }, { \"id\" : \"B\" }, { \"id\" : \"C\" } ] }";
+        final String table = "{ \"aaData\" : [ { \"id\" : \"A\" }, { \"id\" : \"B\" }, { \"id\" : \"C\" } ] }";
 
-            final byte[] buf =
-                streamData( applyAugmentedData( parseData( table.getBytes( "UTF-8" ) ), auditDir, "sample.json" ) );
+        final byte[] buf =
+            streamData( applyAugmentedData( parseData( table.getBytes( "UTF-8" ) ), auditDir, "sample.json" ) );
 
-            assertThat( new String( buf, "UTF-8" ), equalToIgnoringWhiteSpace( table ) );
+        assertThat( new String( buf, "UTF-8" ), equalToIgnoringWhiteSpace( table ) );
 
-            assertThat( 0, equalTo( getModificationCount( auditDir ) ) );
-        }
-        finally
-        {
-            FileUtils.deleteDirectory( auditDir );
-        }
+        assertThat( 0, equalTo( getModificationCount( auditDir ) ) );
     }
 
     @Test
     public void testSingleAugmentedData()
         throws IOException
     {
-        final File auditDir = FileUtils.createTempFile( "audit", "test", new File( "target" ) );
-        try
-        {
-            assertThat( 0, equalTo( getModificationCount( auditDir ) ) );
+        assertThat( 0, equalTo( getModificationCount( auditDir ) ) );
 
-            final String table = "{ \"aaData\" : [ { \"id\" : \"A\" }, { \"id\" : \"B\" }, { \"id\" : \"C\" } ] }";
+        final String table = "{ \"aaData\" : [ { \"id\" : \"A\" }, { \"id\" : \"B\" }, { \"id\" : \"C\" } ] }";
 
-            final String addition = "[ { \"id\" : \"B\", \"override\" : \"EPL\", \"comment\" : \"Testing...\" } ]";
+        final String addition = "[ { \"id\" : \"B\", \"override\" : \"EPL\", \"comment\" : \"Testing...\" } ]";
 
-            final String result =
-                "{ \"aaData\" : [ { \"id\" : \"A\" }, { \"id\" : \"B\", \"override\" : \"EPL\", \"comment\" : \"Testing...\" }, { \"id\" : \"C\" } ] }";
+        final String result =
+            "{ \"aaData\" : [ { \"id\" : \"A\" }, { \"id\" : \"B\", \"override\" : \"EPL\", \"comment\" : \"Testing...\" }, { \"id\" : \"C\" } ] }";
 
-            saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition.getBytes( "UTF-8" ) ),
-                               "anon", "127.0.0.1", "office" );
+        saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition.getBytes( "UTF-8" ) ), "anon",
+                           "127.0.0.1", "office" );
 
-            final byte[] buf =
-                streamData( applyAugmentedData( parseData( table.getBytes( "UTF-8" ) ), auditDir, "sample.json" ) );
+        final byte[] buf =
+            streamData( applyAugmentedData( parseData( table.getBytes( "UTF-8" ) ), auditDir, "sample.json" ) );
 
-            assertThat( new String( buf, "UTF-8" ), equalToIgnoringWhiteSpace( result ) );
+        assertThat( new String( buf, "UTF-8" ), equalToIgnoringWhiteSpace( result ) );
 
-            assertThat( 1, equalTo( getModificationCount( auditDir ) ) );
-        }
-        finally
-        {
-            FileUtils.deleteDirectory( auditDir );
-        }
+        assertThat( 1, equalTo( getModificationCount( auditDir ) ) );
     }
 
     @Test
     public void testMultipleAugmentedData()
         throws IOException
     {
-        final File auditDir = FileUtils.createTempFile( "audit", "test", new File( "target" ) );
-        try
-        {
-            assertThat( 0, equalTo( getModificationCount( auditDir ) ) );
+        assertThat( 0, equalTo( getModificationCount( auditDir ) ) );
 
-            final String table = "{ \"aaData\" : [ { \"id\" : \"A\" }, { \"id\" : \"B\" }, { \"id\" : \"C\" } ] }";
+        final String table = "{ \"aaData\" : [ { \"id\" : \"A\" }, { \"id\" : \"B\" }, { \"id\" : \"C\" } ] }";
 
-            final String addition1 =
-                "[ { \"id\" : \"A\", \"override\" : \"EPL\" }, { \"id\" : \"B\", \"override\" : \"APL\" } ]";
+        final String addition1 =
+            "[ { \"id\" : \"A\", \"override\" : \"EPL\" }, { \"id\" : \"B\", \"override\" : \"APL\" } ]";
 
-            final String addition2 = "[ { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\" } ]";
+        final String addition2 = "[ { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\" } ]";
 
-            final String result =
-                "{ \"aaData\" : [ { \"id\" : \"A\", \"override\" : \"EPL\" }, { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\" }, { \"id\" : \"C\" } ] }";
+        final String result =
+            "{ \"aaData\" : [ { \"id\" : \"A\", \"override\" : \"EPL\" }, { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\" }, { \"id\" : \"C\" } ] }";
 
-            saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition1.getBytes( "UTF-8" ) ),
-                               "anon", "127.0.0.1", "office" );
+        saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition1.getBytes( "UTF-8" ) ), "anon",
+                           "127.0.0.1", "office" );
 
-            assertThat( 1, equalTo( getModificationCount( auditDir ) ) );
+        assertThat( 1, equalTo( getModificationCount( auditDir ) ) );
 
-            saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition2.getBytes( "UTF-8" ) ),
-                               "anon", "127.0.0.1", "office" );
+        saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition2.getBytes( "UTF-8" ) ), "anon",
+                           "127.0.0.1", "office" );
 
-            assertThat( 2, equalTo( getModificationCount( auditDir ) ) );
+        assertThat( 2, equalTo( getModificationCount( auditDir ) ) );
 
-            final byte[] buf =
-                streamData( applyAugmentedData( parseData( table.getBytes( "UTF-8" ) ), auditDir, "sample.json" ) );
+        final byte[] buf =
+            streamData( applyAugmentedData( parseData( table.getBytes( "UTF-8" ) ), auditDir, "sample.json" ) );
 
-            assertThat( new String( buf, "UTF-8" ), equalToIgnoringWhiteSpace( result ) );
-        }
-        finally
-        {
-            FileUtils.deleteDirectory( auditDir );
-        }
+        assertThat( new String( buf, "UTF-8" ), equalToIgnoringWhiteSpace( result ) );
     }
 
     @Test
     public void testFilteredNamedAuditFeed()
         throws IOException
     {
-        final File auditDir = FileUtils.createTempFile( "audit", "test", new File( "target" ) );
-        try
-        {
-            final String addition1 =
-                "[ { \"id\" : \"A\", \"override\" : \"EPL\" }, { \"id\" : \"B\", \"override\" : \"APL\" } ]";
+        final String addition1 =
+            "[ { \"id\" : \"A\", \"override\" : \"EPL\" }, { \"id\" : \"B\", \"override\" : \"APL\" } ]";
 
-            final String addition2 = "[ { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\" } ]";
+        final String addition2 = "[ { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\" } ]";
 
-            final String result =
-                "{ \"aaData\" : [ { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\", \"time\" : 0, \"user\" : \"test\", \"ip\" : \"192.168.1.8\", \"where\" : \"home\", \"filename\" : \"sample.json\" }, "
-                    + "{ \"id\" : \"B\", \"override\" : \"APL\", \"time\" : 0, \"user\" : \"anon\", \"ip\" : \"127.0.0.1\", \"where\" : \"office\", \"filename\" : \"sample.json\" } ] }";
+        final String result =
+            "{ \"aaData\" : [ { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\", \"time\" : 0, \"user\" : \"test\", \"ip\" : \"192.168.1.8\", \"where\" : \"home\", \"filename\" : \"sample.json\" }, "
+                + "{ \"id\" : \"B\", \"override\" : \"APL\", \"time\" : 0, \"user\" : \"anon\", \"ip\" : \"127.0.0.1\", \"where\" : \"office\", \"filename\" : \"sample.json\" } ] }";
 
-            saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition1.getBytes( "UTF-8" ) ),
-                               "anon", "127.0.0.1", "office" );
+        saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition1.getBytes( "UTF-8" ) ), "anon",
+                           "127.0.0.1", "office" );
 
-            saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition2.getBytes( "UTF-8" ) ),
-                               "test", "192.168.1.8", "home" );
+        saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition2.getBytes( "UTF-8" ) ), "test",
+                           "192.168.1.8", "home" );
 
-            final byte[] buf = filterAuditLog( auditDir, "{\"id\":\"B\"}".getBytes( "UTF-8" ), "sample.json" );
+        final byte[] buf = filterAuditLog( auditDir, "{\"id\":\"B\"}".getBytes( "UTF-8" ), "sample.json" );
 
-            assertThat( new String( buf, "UTF-8" ).replaceAll( "\"time\" : [0-9]+", "\"time\" : 0" ),
-                        equalToIgnoringWhiteSpace( result ) );
-        }
-        finally
-        {
-            FileUtils.deleteDirectory( auditDir );
-        }
+        assertThat( new String( buf, "UTF-8" ).replaceAll( "\"time\" : [0-9]+", "\"time\" : 0" ),
+                    equalToIgnoringWhiteSpace( result ) );
     }
 
     @Test
     public void testFilteredAuditFeed()
         throws IOException
     {
-        final File auditDir = FileUtils.createTempFile( "audit", "test", new File( "target" ) );
-        try
-        {
-            final String addition1 =
-                "[ { \"id\" : \"A\", \"override\" : \"EPL\" }, { \"id\" : \"B\", \"override\" : \"APL\" } ]";
+        final String addition1 =
+            "[ { \"id\" : \"A\", \"override\" : \"EPL\" }, { \"id\" : \"B\", \"override\" : \"APL\" } ]";
 
-            final String addition2 = "[ { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\" } ]";
+        final String addition2 = "[ { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\" } ]";
 
-            final String addition3 = "[ { \"id\" : \"B\", \"confirmed\" : true, \"comment\" : \"Must fix\" } ]";
+        final String addition3 = "[ { \"id\" : \"B\", \"confirmed\" : true, \"comment\" : \"Must fix\" } ]";
 
-            final String result =
-                "{ \"aaData\" : [ "
-                    + "{ \"id\" : \"B\", \"confirmed\" : true, \"comment\" : \"Must fix\", \"time\" : 0, \"user\" : \"test\", \"ip\" : \"127.0.0.1\", \"where\" : \"cafe\", \"filename\" : \"another.json\" }, "
-                    + "{ \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\", \"time\" : 0, \"user\" : \"test\", \"ip\" : \"192.168.1.8\", \"where\" : \"home\", \"filename\" : \"sample.json\" }, "
-                    + "{ \"id\" : \"B\", \"override\" : \"APL\", \"time\" : 0, \"user\" : \"anon\", \"ip\" : \"127.0.0.1\", \"where\" : \"office\", \"filename\" : \"sample.json\" }"
-                    + " ] }";
+        final String result =
+            "{ \"aaData\" : [ "
+                + "{ \"id\" : \"B\", \"confirmed\" : true, \"comment\" : \"Must fix\", \"time\" : 0, \"user\" : \"test\", \"ip\" : \"127.0.0.1\", \"where\" : \"cafe\", \"filename\" : \"another.json\" }, "
+                + "{ \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\", \"time\" : 0, \"user\" : \"test\", \"ip\" : \"192.168.1.8\", \"where\" : \"home\", \"filename\" : \"sample.json\" }, "
+                + "{ \"id\" : \"B\", \"override\" : \"APL\", \"time\" : 0, \"user\" : \"anon\", \"ip\" : \"127.0.0.1\", \"where\" : \"office\", \"filename\" : \"sample.json\" }"
+                + " ] }";
 
-            saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition1.getBytes( "UTF-8" ) ),
-                               "anon", "127.0.0.1", "office" );
+        saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition1.getBytes( "UTF-8" ) ), "anon",
+                           "127.0.0.1", "office" );
 
-            saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition2.getBytes( "UTF-8" ) ),
-                               "test", "192.168.1.8", "home" );
+        saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition2.getBytes( "UTF-8" ) ), "test",
+                           "192.168.1.8", "home" );
 
-            saveAugmentedData( auditDir, "another.json", new ByteArrayInputStream( addition3.getBytes( "UTF-8" ) ),
-                               "test", "127.0.0.1", "cafe" );
+        saveAugmentedData( auditDir, "another.json", new ByteArrayInputStream( addition3.getBytes( "UTF-8" ) ), "test",
+                           "127.0.0.1", "cafe" );
 
-            final byte[] buf = filterAuditLog( auditDir, "{\"id\":\"B\"}".getBytes( "UTF-8" ) );
+        final byte[] buf = filterAuditLog( auditDir, "{\"id\":\"B\"}".getBytes( "UTF-8" ) );
 
-            assertThat( new String( buf, "UTF-8" ).replaceAll( "\"time\" : [0-9]+", "\"time\" : 0" ),
-                        equalToIgnoringWhiteSpace( result ) );
-        }
-        finally
-        {
-            FileUtils.deleteDirectory( auditDir );
-        }
+        assertThat( new String( buf, "UTF-8" ).replaceAll( "\"time\" : [0-9]+", "\"time\" : 0" ),
+                    equalToIgnoringWhiteSpace( result ) );
     }
 
     @Test
     public void testAuditFeed()
         throws IOException
     {
-        final File auditDir = FileUtils.createTempFile( "audit", "test", new File( "target" ) );
-        try
-        {
-            final String addition1 =
-                "[ { \"id\" : \"A\", \"override\" : \"EPL\" }, { \"id\" : \"B\", \"override\" : \"APL\" } ]";
+        final String addition1 =
+            "[ { \"id\" : \"A\", \"override\" : \"EPL\" }, { \"id\" : \"B\", \"override\" : \"APL\" } ]";
 
-            final String addition2 = "[ { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\" } ]";
+        final String addition2 = "[ { \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\" } ]";
 
-            final String addition3 = "[ { \"id\" : \"B\", \"confirmed\" : true, \"comment\" : \"Must fix\" } ]";
+        final String addition3 = "[ { \"id\" : \"B\", \"confirmed\" : true, \"comment\" : \"Must fix\" } ]";
 
-            final String result =
-                "{ \"aaData\" : [ "
-                    + "{ \"id\" : \"B\", \"confirmed\" : true, \"comment\" : \"Must fix\", \"time\" : 0, \"user\" : \"test\", \"ip\" : \"127.0.0.1\", \"where\" : \"cafe\", \"filename\" : \"another.json\" }, "
-                    + "{ \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\", \"time\" : 0, \"user\" : \"test\", \"ip\" : \"192.168.1.8\", \"where\" : \"home\", \"filename\" : \"sample.json\" }, "
-                    + "{ \"id\" : \"A\", \"override\" : \"EPL\", \"time\" : 0, \"user\" : \"anon\", \"ip\" : \"127.0.0.1\", \"where\" : \"office\", \"filename\" : \"sample.json\" }, "
-                    + "{ \"id\" : \"B\", \"override\" : \"APL\", \"time\" : 0, \"user\" : \"anon\", \"ip\" : \"127.0.0.1\", \"where\" : \"office\", \"filename\" : \"sample.json\" }"
-                    + " ] }";
+        final String result =
+            "{ \"aaData\" : [ "
+                + "{ \"id\" : \"B\", \"confirmed\" : true, \"comment\" : \"Must fix\", \"time\" : 0, \"user\" : \"test\", \"ip\" : \"127.0.0.1\", \"where\" : \"cafe\", \"filename\" : \"another.json\" }, "
+                + "{ \"id\" : \"B\", \"override\" : \"ASL\", \"comment\" : \"Fix typo\", \"time\" : 0, \"user\" : \"test\", \"ip\" : \"192.168.1.8\", \"where\" : \"home\", \"filename\" : \"sample.json\" }, "
+                + "{ \"id\" : \"A\", \"override\" : \"EPL\", \"time\" : 0, \"user\" : \"anon\", \"ip\" : \"127.0.0.1\", \"where\" : \"office\", \"filename\" : \"sample.json\" }, "
+                + "{ \"id\" : \"B\", \"override\" : \"APL\", \"time\" : 0, \"user\" : \"anon\", \"ip\" : \"127.0.0.1\", \"where\" : \"office\", \"filename\" : \"sample.json\" }"
+                + " ] }";
 
-            saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition1.getBytes( "UTF-8" ) ),
-                               "anon", "127.0.0.1", "office" );
+        saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition1.getBytes( "UTF-8" ) ), "anon",
+                           "127.0.0.1", "office" );
 
-            saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition2.getBytes( "UTF-8" ) ),
-                               "test", "192.168.1.8", "home" );
+        saveAugmentedData( auditDir, "sample.json", new ByteArrayInputStream( addition2.getBytes( "UTF-8" ) ), "test",
+                           "192.168.1.8", "home" );
 
-            saveAugmentedData( auditDir, "another.json", new ByteArrayInputStream( addition3.getBytes( "UTF-8" ) ),
-                               "test", "127.0.0.1", "cafe" );
+        saveAugmentedData( auditDir, "another.json", new ByteArrayInputStream( addition3.getBytes( "UTF-8" ) ), "test",
+                           "127.0.0.1", "cafe" );
 
-            final byte[] buf = filterAuditLog( auditDir, null );
+        final byte[] buf = filterAuditLog( auditDir, null );
 
-            assertThat( new String( buf, "UTF-8" ).replaceAll( "\"time\" : [0-9]+", "\"time\" : 0" ),
-                        equalToIgnoringWhiteSpace( result ) );
-        }
-        finally
-        {
-            FileUtils.deleteDirectory( auditDir );
-        }
+        assertThat( new String( buf, "UTF-8" ).replaceAll( "\"time\" : [0-9]+", "\"time\" : 0" ),
+                    equalToIgnoringWhiteSpace( result ) );
     }
 
     @Test
     public void testEmptyAuditFeed()
         throws IOException
     {
-        final File auditDir = FileUtils.createTempFile( "audit", "test", new File( "target" ) );
-        try
-        {
-            assertThat( null, equalTo( filterAuditLog( auditDir, null, "" ) ) );
-        }
-        finally
-        {
-            FileUtils.deleteDirectory( auditDir );
-        }
+        assertThat( null, equalTo( filterAuditLog( auditDir, null, "" ) ) );
     }
 }

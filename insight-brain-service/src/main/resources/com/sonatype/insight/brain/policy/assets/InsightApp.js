952db7ed5806012436b1e94ab298ed6e5b28aa48
Damian Bradicich <dbradicich@sonatype.com>
2012-12-06 20:36:30 -0500

    - added in the constraint table and actions - added some sample data - add constraint modal is shown, however the content isn't yet populated - INSIGHT-3898 INSIGHT-3901

--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/InsightApp.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/InsightApp.js
@@ -3,7 +3,70 @@ var insightApp = angular.module('insightApp', [], function($locationProvider){
 });
 
 insightApp.factory('global', function($rootScope) {
-    var state = {};
+    var checkboxRulesSelector = new Slick.CheckboxSelectColumn({
+        cssClass: "slick-cell-checkboxsel"
+	}), state = {
+    	//TODO: this will be removed when REST request for this data is in place
+    	policyConstraintCount : 1,
+    	//TODO: this will be removed when REST request for this data is in place
+    	policyConstraintList : [{
+    		name: 'myname',
+    		status: 'status'
+    	},{
+    		name: 'myname2',
+    		status: 'status2'
+    	},{
+    		name: 'myname3',
+    		status: 'status3'
+    	},{
+    		name: 'myname4',
+    		status: 'status4'
+    	},{
+    		name: 'myname5',
+    		status: 'status5'
+    	},{
+    		name: 'myname6',
+    		status: 'status6'
+    	},{
+    		name: 'myname7',
+    		status: 'status7'
+    	},{
+    		name: 'myname8',
+    		status: 'status8'
+    	}],
+    	policyConstraintTableDefinition : {
+    		columns : [ checkboxRulesSelector.getColumnDefinition(), {
+    			id : "name",
+    			name : "Constraint Name",
+    			field : "name",
+    			width : 400,
+    			cssClass : 'edit-click'
+    		}, {
+    			id : "enabled",
+    			name : "Status",
+    			field : "enabled",
+    			width : 100,
+    			formatter : function(row, cell, value, columnDef, dataContext) {
+    				return '<table><tr><td style="padding: 0px;width: 99%;">' + (value ? 'enabled' : 'disabled') + '</td>'
+    				    + '<td style="padding: 0px;padding-right:2px;"><button id="' + dataContext.id + '" class="btn btn-mini btn-edit slick-row-hover-button" title="Edit Rule"><i class="icon-pencil"></i></button></td>'
+    				    + '<td style="padding: 0px;padding-right:2px;"><button id="' + dataContext.id + '" class="btn btn-mini btn-enable slick-row-hover-button" title="Enable Rule"><i class="icon-ok-circle"></i></button></td>'
+    				    + '<td style="padding: 0px;padding-right:2px;"><button id="' + dataContext.id + '" class="btn btn-mini btn-disable slick-row-hover-button" title="Disable Rule"><i class="icon-remove-circle"></i></button></td>'
+    				    + '<td style="padding: 0px;padding-right:2px;"><button id="' + dataContext.id + '" class="btn btn-mini btn-delete slick-row-hover-button" title="Delete Rule"><i class="icon-trash"></i></button></td>'
+    				    + '</tr></table>';
+    			}
+    		} ],
+    		options : {
+    			height : 200,
+    			forceFitColumns : true,
+    			fullWidthRows : true
+    		},
+    		plugins : [checkboxRulesSelector],
+    		selectionModel : new Slick.RowSelectionModel({selectActiveRow: false}),
+    		emptyMessage : "No Constraints have been defined.<br><a href='#newConstraintModal' data-toggle='modal'>Create</a> a new Constraint?"
+    	}
+    };
         
     return state;
 });
+
+insightApp.directive('slickgrid', SlickGridComponent);
--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/InsightPolicyController.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/InsightPolicyController.js
@@ -1,7 +1,59 @@
 function InsightPolicyController($scope, global, $http, $location) {
 	$scope.state = global;
 	
-	$scope.createClick = function(){
-		//TODO: show the create screen
+	$scope.createPolicyClick = function(){
+		$scope.state.showAddPolicyScreen = true;
+	}
+	
+	$scope.savePolicyClick = function(){
+		//TODO: save the policy
+	}
+	
+	$scope.cancelPolicyClick = function(){
+		//TODO: process the cancel
+		delete $scope.state.showAddPolicyScreen;
+	}
+	
+	$scope.validatePolicy = function() {
+		delete $scope.state.policyValid;
+		//TODO: put validations in place in place
+		if ($scope.state.policyName) {
+			$scope.state.policyValid = true;
+		}
+	}
+	
+	$scope.enableConstraint = function() {
+		$scope.updateStatus(true);
+	}
+	
+	$scope.disableConstraint = function() {
+		$scope.updateStatus(false);
+	}
+	
+	$scope.updateStatus = function(enabled) {
+		var rows = $scope.policyConstraintGrid.getSelectedRows();
+		
+		if (!rows.length > 0){
+			delete rows;
+			return;
+		}
+		
+		angular.forEach(rows, function(value, key){
+			$scope.policyConstraintGrid.getDataItem(value).enabled = enabled;
+		});
+	}
+	
+	$scope.removeConstraint = function() {		
+		var rows = $scope.policyConstraintGrid.getSelectedRows();
+		
+		if (!rows.length > 0){
+			return;
+		}
+		
+		rows.reverse();
+		
+		angular.forEach(rows, function(value, key){
+			$scope.policyConstraintGrid.dataView.deleteItem($scope.policyConstraintGrid.getDataItem(value).id);
+		});
 	}
 }

d15a608fbbc11ebffcc3881345d2b60501558683
vladt <vladt@sonatype.com>
2012-12-27 15:43:30 -0500

    Code copied from nexus-procurement-plugin for component GAV coordinates matching INSIGHT-3964 INSIGHT-3985

---
+++ insight-brain-model/src/main/java/com/sonatype/insight/brain/model/policy/conditions/ArtifactCoordinate.java
@@ -0,0 +1,333 @@
+/**
+ * Copyright (c) 2011-2012 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/insight/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.model.policy.conditions;
+
+import org.codehaus.plexus.util.StringUtils;
+import org.springframework.util.AntPathMatcher;
+
+/**
+ * Copied from com.sonatype.nexus.procurement.ArtifactCoordinate.
+ * <p>
+ * The coordinates/conditions used to address one artifact. If we want to expand the rules, this is the place to do so.
+ * The coordinates may be "fixed" (isFixed() returns TRUE), or "wildcarded". Examples (presented as G:A:V triplets):
+ * 
+ * <pre>
+ * org.sonatype.nexus : nexus-indexer : 1.0 - is a fixed coordinate that points exactly to what is says
+ * org.sonatype* : nexus-indexer : 1.* - is a wildcard coordinate, that points to (inclusive) group 'org.sonatype' and below (ie. 'org.sonatypefoo' or  'org.sonatype.blah'), and artifact named named 'nexus-indexer' in these groups, and any version that starts with '1.' 
+ * org.sonatype.* : nexus-indexer : 1.0 - is a wildcard coordinate, that is like the above one, except it matches this group and its subgroups ONLY ('org.sonatypefoo' is NOT matched), and matches for version '1.0' only.
+ * </pre>
+ * 
+ * The other fields (A, V) also are able to make use of '*' (wildcard), but it will be interpreted obly as "starts with"
+ * (ie. in field A, 'nexus*' will be inerpreted as artifactId.startsWith("nexus"). Same stands for V field). In case of
+ * <b>groups</b> (G field), the things are a little different: blah* means group starts with 'blah' (hence, and and
+ * below), blah.* means only group 'blah' and groups below 'blah' like 'blah.foo', but not 'blahfoo.foo'.
+ * <p>
+ * An ArtifactCoordinate is matchable only against "fixed" coordinate, hence, two wildarcded coordinate cannot be
+ * matched.
+ * 
+ * @author cstamas
+ */
+public class ArtifactCoordinate
+    implements Comparable<ArtifactCoordinate>
+{
+    private static final String PLACEHOLDER = "*";
+
+    private String groupId;
+
+    private String artifactId;
+
+    private String version;
+
+    private transient AntPathMatcher pathMatcher = new AntPathMatcher();
+
+    /**
+     * Constructs an ArtifactCoordinate.
+     */
+    public ArtifactCoordinate()
+    {
+    }
+
+    /**
+     * Constructs an ArtifactCoordinate.
+     * 
+     * @param groupId
+     * @param artifactId
+     * @param version
+     */
+    public ArtifactCoordinate( String groupId, String artifactId, String version )
+    {
+        this.groupId = groupId;
+
+        this.artifactId = artifactId;
+
+        this.version = version;
+    }
+
+    /**
+     * Returns true when all GAV coordinates are present, and none of them contains a '*' joker character.
+     * 
+     * @return
+     */
+    public boolean isFixed()
+    {
+        // fixed is when all GAV is given and none of those contains placeholder
+        // when fixed, it identifies exactly one artifact
+        return !StringUtils.isEmpty( getGroupId() ) && !StringUtils.isEmpty( getArtifactId() )
+            && !StringUtils.isEmpty( getVersion() ) && !getGroupId().contains( PLACEHOLDER )
+            && !getArtifactId().contains( PLACEHOLDER ) && !getVersion().contains( PLACEHOLDER );
+    }
+
+    /**
+     * Returns true if this ArtifactCoordinate matches the passed path.
+     * 
+     * @param groupId
+     * @param artifactId
+     * @param version
+     * @return
+     */
+    public boolean matches( String path )
+    {
+        StringBuffer pathBuf = new StringBuffer( "/" );
+
+        String gid = getGroupId();
+
+        // a* -> a*/**/
+        // a.* -> a/**
+        if ( gid.endsWith( ".*" ) )
+        {
+            gid = gid.substring( 0, gid.length() - 2 ) + "/**";
+        }
+        else if ( gid.endsWith( "*" ) )
+        {
+            gid = gid.substring( 0, gid.length() - 1 ) + "*/**";
+        }
+
+        pathBuf.append( gid.replace( '.', '/' ) );
+
+        if ( !PLACEHOLDER.equals( getArtifactId() ) || !PLACEHOLDER.equals( getVersion() ) )
+        {
+            pathBuf.append( "/" );
+
+            pathBuf.append( getArtifactId() );
+
+            if ( !PLACEHOLDER.equals( getVersion() ) )
+            {
+                pathBuf.append( "/" );
+
+                pathBuf.append( getVersion() );
+            }
+            else
+            {
+                pathBuf.append( "/" );
+
+                pathBuf.append( "**" );
+            }
+        }
+        else
+        {
+            pathBuf.append( "/*/*" );
+        }
+
+        return pathMatcher.match( pathBuf.toString(), path );
+    }
+
+    /**
+     * Returns true if this ArtifactCoordinate matches the passed coordinates.
+     * 
+     * @param groupId
+     * @param artifactId
+     * @param version
+     * @return
+     */
+    public boolean matches( String groupId, String artifactId, String version )
+    {
+        return matchesGroup( getGroupId(), groupId ) && matches( getArtifactId(), artifactId )
+            && matches( getVersion(), version );
+    }
+
+    /**
+     * Returns true if this ArtifactCoordinate matches the passed in fixed ArtifactCoordinate coordinates.
+     * 
+     * @param groupId
+     * @param artifactId
+     * @param version
+     * @return
+     */
+    public boolean matches( ArtifactCoordinate coordinate )
+    {
+        return coordinate.isFixed()
+            && matches( coordinate.getGroupId(), coordinate.getArtifactId(), coordinate.getVersion() );
+    }
+
+    public String getGroupId()
+    {
+        return groupId;
+    }
+
+    public void setGroupId( String groupId )
+    {
+        this.groupId = groupId;
+    }
+
+    public String getArtifactId()
+    {
+        return artifactId;
+    }
+
+    public void setArtifactId( String artifactId )
+    {
+        this.artifactId = artifactId;
+    }
+
+    public String getVersion()
+    {
+        return version;
+    }
+
+    public void setVersion( String version )
+    {
+        this.version = version;
+    }
+
+    // internal
+
+    private int getMatchableCharacters()
+    {
+        return cleanseCoordinate( getGroupId() ).length() + cleanseCoordinate( getArtifactId() ).length()
+            + cleanseCoordinate( getVersion() ).length();
+    }
+
+    private String cleanseCoordinate( String coordinate )
+    {
+        if ( coordinate.endsWith( PLACEHOLDER ) )
+        {
+            coordinate = coordinate.substring( 0, coordinate.indexOf( PLACEHOLDER ) );
+
+            if ( coordinate.endsWith( "." ) )
+            {
+                return coordinate.substring( 0, coordinate.length() - 1 );
+            }
+            else
+            {
+                return coordinate;
+            }
+        }
+
+        return coordinate;
+    }
+
+    /**
+     * A utility method that handles group coordinates as matchable target. The meaning of them are:
+     * 
+     * <pre>
+     * * (or null) - matches all
+     * some.value - matches exactly 'some.value'
+     * some.value* - matches by prefix, so 'some.value', 'some.value.more' are all ok
+     * some.value.* - matches only subgroups, so 'some.value.more1', 'some.value.more2' is ok only
+     * </pre>
+     * 
+     * @param coordinate
+     * @param value
+     * @return
+     */
+    private boolean matchesGroup( String coordinate, String value )
+    {
+        if ( StringUtils.isEmpty( coordinate ) || PLACEHOLDER.equals( coordinate ) )
+        {
+            // empty: not specified, it matches all
+            return true;
+        }
+        else if ( StringUtils.isEmpty( value ) )
+        {
+            // cordinate not empty, value empty, no match
+            return false;
+        }
+        else if ( coordinate.endsWith( "." + PLACEHOLDER ) )
+        {
+            if ( value.length() <= coordinate.length() - 2 )
+            {
+                // coordinate ends with a joker, matches if it is prefix of value
+                return value.startsWith( coordinate.substring( 0, coordinate.length() - 2 ) );
+            }
+            else
+            {
+                // coordinate ends with a joker, matches if it is prefix of value
+                return value.startsWith( coordinate.substring( 0, coordinate.length() - 1 ) );
+            }
+        }
+        else if ( coordinate.endsWith( PLACEHOLDER ) )
+        {
+            // coordinate ends with a joker, matches if it is prefix of value
+            return value.startsWith( coordinate.substring( 0, coordinate.length() - 1 ) );
+        }
+        else
+        {
+            // coordinate has no joker, matches if equals
+            return coordinate.equals( value );
+        }
+    }
+
+    /**
+     * A utility method that handles A and V coordinates as matchable target. These are handled a bit differently that G
+     * coordinates! The meaning of them are:
+     * 
+     * <pre>
+     * null - matches all
+     * some.value - matches exactly 'some.value'
+     * somevalue* - matches by prefix just before the '*'
+     * </pre>
+     * 
+     * @param coordinate
+     * @param value
+     * @return
+     */
+    private boolean matches( String coordinate, String value )
+    {
+        if ( StringUtils.isEmpty( coordinate ) || PLACEHOLDER.equals( coordinate ) )
+        {
+            // empty: not specified, it matches all
+            return true;
+        }
+        else if ( StringUtils.isEmpty( value ) )
+        {
+            // cordinate not empty, value empty, no match
+            return false;
+        }
+        else if ( coordinate.endsWith( PLACEHOLDER ) )
+        {
+            // coordinate ends with a joker, matches if it is prefix of value
+            return value.startsWith( coordinate.substring( 0, coordinate.length() - 1 ) );
+        }
+        else
+        {
+            // coordinate has no joker, matches if equals
+            return coordinate.equals( value );
+        }
+    }
+
+    // Object
+
+    @Override
+    public int compareTo( ArtifactCoordinate o )
+    {
+        return getMatchableCharacters() - o.getMatchableCharacters();
+    }
+
+    @Override
+    public String toString()
+    {
+        return gavToString( getGroupId(), getArtifactId(), getVersion() );
+    }
+
+    public static String gavToString( String g, String a, String v )
+    {
+        StringBuffer sb = new StringBuffer();
+
+        sb.append( g ).append( ":" ).append( a ).append( ":" ).append( v );
+
+        return sb.toString();
+    }
+}
---
+++ insight-brain-model/src/test/java/com/sonatype/insight/brain/model/policy/conditions/ArtifactCoordinateTest.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2011-2012 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/insight/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.model.policy.conditions;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+// Copied from com.sonatype.nexus.procurement.ArtifactCoordinateTest
+public class ArtifactCoordinateTest
+{
+
+    @Test
+    public void testSimple()
+        throws Exception
+    {
+        // one fixed coord
+        ArtifactCoordinate coordF1 = new ArtifactCoordinate( "org.group", "artifact", "1.0" );
+        assertTrue( coordF1.isFixed() );
+
+        // one fixed coord
+        ArtifactCoordinate coordF2 = new ArtifactCoordinate( "com.group", "artifact", "2.0" );
+        assertTrue( coordF2.isFixed() );
+
+        // one non-fixed coord
+        ArtifactCoordinate coordN1 = new ArtifactCoordinate( "org.*", "artifact", "1.*" );
+        assertFalse( coordN1.isFixed() );
+
+        // one non-fixed coord
+        ArtifactCoordinate coordN2 = new ArtifactCoordinate( "com.*", "artifact", "2.*" );
+        assertFalse( coordN2.isFixed() );
+
+        // one non-fixed coord
+        ArtifactCoordinate coordN3 = new ArtifactCoordinate( "org*", "artifact", "1.*" );
+        assertFalse( coordN3.isFixed() );
+
+        // one non-fixed coord
+        ArtifactCoordinate coordN4 = new ArtifactCoordinate( "com*", "artifact", "2.*" );
+        assertFalse( coordN4.isFixed() );
+
+        // pair matching
+        // coordF1 is fixed, hence coordN1 is matchable against it and IS matching
+        assertTrue( coordN1.matches( coordF1 ) );
+        // coordN1 is not fixed, hence coordF1 is not matchable against it
+        assertFalse( coordF1.matches( coordN1 ) );
+        // coordF2 is fixed, hence coordN2 is matchable against it and IS matching
+        assertTrue( coordN2.matches( coordF2 ) );
+        // coordN2 is not fixed, hence coordF2 is not matchable against it
+        assertFalse( coordF2.matches( coordN2 ) );
+
+        // cross matching
+        // coordF2 is fixed, hence coordN1 is matchable against it and IS NOT matching
+        assertFalse( coordN1.matches( coordF2 ) );
+        // coordN1 is not fixed, hence coordF2 is not matchable against it
+        assertFalse( coordF2.matches( coordN1 ) );
+        // coordF1 is fixed, hence coordN2 is matchable against it and IS NOT matching
+        assertFalse( coordN2.matches( coordF1 ) );
+        // coordN1 is not fixed, hence coordF2 is not matchable against it
+        assertFalse( coordF1.matches( coordN2 ) );
+
+        // only subgroups
+        assertTrue( coordN1.matches( "org", "artifact", "1.2" ) );
+        assertTrue( coordN1.matches( "org", "artifact", "1.3" ) );
+        assertFalse( coordN1.matches( "organ", "artifact", "1.2" ) );
+        assertFalse( coordN1.matches( "organic", "artifact", "1.3" ) );
+        assertFalse( coordN1.matches( "organic.group", "artifact", "1.2" ) );
+        assertTrue( coordN1.matches( "org.group", "artifact", "1.2" ) );
+        assertTrue( coordN1.matches( "org.group.where", "artifact", "1.3" ) );
+        assertFalse( coordN1.matches( "org.group.something.boo", "artifact", "2.0" ) );
+        assertFalse( coordN1.matches( "org.group.something.boo", "non-artifact", "1.0" ) );
+
+        // given group and subgroups
+        assertTrue( coordN3.matches( "org", "artifact", "1.2" ) );
+        assertTrue( coordN3.matches( "org", "artifact", "1.3" ) );
+        assertTrue( coordN3.matches( "organ", "artifact", "1.2" ) );
+        assertTrue( coordN3.matches( "organic", "artifact", "1.3" ) );
+        assertTrue( coordN3.matches( "organic.group", "artifact", "1.2" ) );
+        assertTrue( coordN3.matches( "org.group.where", "artifact", "1.3" ) );
+        assertFalse( coordN3.matches( "org.group.something.boo", "artifact", "2.0" ) );
+        assertFalse( coordN3.matches( "org.group.something.boo", "non-artifact", "1.0" ) );
+    }
+
+    @Test
+    public void testNXCM195()
+    {
+        // path in question:
+        // org/apache/maven/shared/maven-repository-builder/1.0-alpha-1/maven-repository-builder-1.0-alpha-1.jar
+        // GAV: org.apache.maven.shared : maven-repository-builder : 1.0-alpha-1
+        // one fixed coord
+        ArtifactCoordinate coord1 = new ArtifactCoordinate( "org.apache.maven", "*", "*" );
+
+        ArtifactCoordinate coord2 = new ArtifactCoordinate( "org.apache.maven.*", "*", "*" );
+
+        assertFalse( "Should not match!",
+                     coord1.matches( "org.apache.maven.shared", "maven-repository-builder", "1.0-alpha-1" ) );
+        assertTrue( "Should match!",
+                    coord2.matches( "org.apache.maven.shared", "maven-repository-builder", "1.0-alpha-1" ) );
+
+        assertFalse( "Should not match!", coord1.matches( "/org/apache/maven/kuku/a/v/" ) );
+        assertTrue( "Should match!", coord2.matches( "/org/apache/maven/kuku/a/v/" ) );
+    }
+
+}

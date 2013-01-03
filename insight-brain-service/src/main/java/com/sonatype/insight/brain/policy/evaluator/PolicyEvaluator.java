ad5fd99b89cef850e21419924d7aea50c442dd60
vladt <vladt@sonatype.com>
2013-01-03 16:41:13 -0500

    Cleanup

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/policy/evaluator/PolicyEvaluator.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/policy/evaluator/PolicyEvaluator.java
@@ -25,7 +25,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.sonatype.insight.brain.model.component.Component;
-import com.sonatype.insight.brain.model.policy.Action;
 import com.sonatype.insight.brain.model.policy.Constraint;
 import com.sonatype.insight.brain.model.policy.Policy;
 import com.sonatype.insight.brain.model.policy.PolicyAlert;

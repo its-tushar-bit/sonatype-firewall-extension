bfd410856bb14473c81963626c1d1b28de724d8c
Jamie Whitehouse <basil.whitehouse@gmail.com>
2013-01-04 15:59:20 -0500

    Update source license header.

--- insight-brain-data-access/src/test/java/com/sonatype/insight/brain/dataaccess/policy/PolicyDAOTest.java
+++ insight-brain-data-access/src/test/java/com/sonatype/insight/brain/dataaccess/policy/PolicyDAOTest.java
@@ -1,6 +1,6 @@
-/**
- * Copyright (c) 2011-2012 Sonatype, Inc. All rights reserved.
- * Includes the third-party code listed at http://links.sonatype.com/products/insight/attributions.
+/*
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
 package com.sonatype.insight.brain.dataaccess.policy;

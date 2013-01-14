598bb8ef2a96fdaf5eea18bf3dbee5f2fba6f965
Matthew Piggott <mpiggott@sonatype.com>
2013-01-14 13:46:24 -0500

    Add service to retrieve labels associated with a hash  - INSIGHT-3927 INSIGHT-3914

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/label/ComponentLabelResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/label/ComponentLabelResource.java
@@ -5,17 +5,24 @@
  */
 package com.sonatype.insight.brain.label;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.sonatype.insight.brain.dataaccess.ApplicationDAO;
 import com.sonatype.insight.brain.dataaccess.label.ComponentLabelDAO;
+import com.sonatype.insight.brain.dataaccess.label.LabelDAO;
 import com.sonatype.insight.brain.model.Application;
+import com.sonatype.insight.brain.model.label.ComponentLabel;
+import com.sonatype.insight.brain.model.label.Label;
 
 @Path( ComponentLabelResource.SERVICE_PATH )
 public class ComponentLabelResource
@@ -23,6 +30,8 @@ public class ComponentLabelResource
     public static final String SERVICE_PATH = "rest/label/component/{appId}/{hash}";
 
     private ApplicationDAO applicationDAO = new ApplicationDAO();
+
+    private LabelDAO labelDAO = new LabelDAO();
 
     private ComponentLabelDAO componentLabelDAO = new ComponentLabelDAO();
 
@@ -34,5 +43,24 @@ public class ComponentLabelResource
         Application application = applicationDAO.getOrInsertByPublicId( applicationPublicId );
 
         componentLabelDAO.setComponentLabels( application.getId(), hash, stringLabels );
+    }
+
+    @GET
+    @Produces( MediaType.APPLICATION_JSON )
+    public List<Label> getComponentLabels( @PathParam( "appId" ) String applicationPublicId,
+                                           @PathParam( "hash" ) String hash )
+    {
+        Application application = applicationDAO.getOrInsertByPublicId( applicationPublicId );
+        List<ComponentLabel> labelIds = componentLabelDAO.getByApplicationIdAndHash( application.getId(), hash );
+        List<Label> labels = new ArrayList<Label>( labelIds.size() );
+        for ( ComponentLabel labelId : labelIds )
+        {
+            Label label = labelDAO.getById( labelId.getLabelId() );
+            if ( label != null )
+            {
+                labels.add( label );
+            }
+        }
+        return labels;
     }
 }

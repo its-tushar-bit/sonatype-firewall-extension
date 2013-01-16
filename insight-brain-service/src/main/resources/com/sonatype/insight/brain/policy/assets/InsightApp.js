1d62efcd911fafbcd116f5ad8278cf3f83abfcab
Matthew Piggott <mpiggott@sonatype.com>
2013-01-16 14:32:44 -0500

    Fix AppID detection code

--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/InsightApp.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/InsightApp.js
@@ -53,8 +53,9 @@ insightApp.factory('global', function($rootScope) {
 });
 
 insightApp.getQueryString = function(key) {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    var vars = [],
+        hash,
+        hashes = window.location.search.slice(1).split('&');
     for(var i = 0; i < hashes.length; i++)
     {
         hash = hashes[i].split('=');

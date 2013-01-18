52ae1d19e1b6af886757408fdf6d475549ad95f0
vladt <vladt@sonatype.com>
2013-01-18 11:20:34 -0500

    Fixed package for test class INSIGHT-4062

--- insight-brain-data-access/src/test/java/com/sonatype/insight/brain/dataaccess/DatabaseAccessTest.java
+++ insight-brain-data-access/src/test/java/com/sonatype/insight/brain/dataaccess/DatabaseAccessTest.java
@@ -3,7 +3,7 @@
  * Includes the third-party code listed at http://links.sonatype.com/products/insight/attributions.
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
-package com.sonatype.insight.brain.dataaccess.label;
+package com.sonatype.insight.brain.dataaccess;
 
 import java.io.File;
 import java.util.ArrayList;

b393e502aae8142dae14955fb9fcc320546b3a0b
vladt <vladt@sonatype.com>
2013-02-01 16:16:33 -0500

    Added db schema incremental script INSIGHT-4116 INSIGHT-4117

---
+++ insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0001.sql
@@ -0,0 +1,19 @@
+CREATE TABLE license_threat_group (
+  license_threat_group_id varchar(50) NOT NULL,
+  application_id varchar(50) NOT NULL,
+  name varchar(50) NOT NULL,
+  threat_level smallint(2) NOT NULL,
+  CONSTRAINT license_threat_group_pk PRIMARY KEY (license_threat_group_id),
+  CONSTRAINT license_threat_group_application_fk FOREIGN KEY (application_id) REFERENCES application(application_id),
+  CONSTRAINT license_threat_group_uk UNIQUE KEY (application_id, name)
+);
+
+CREATE TABLE license_threat_group_license (
+  license_threat_group_license_id varchar(50) NOT NULL,
+  application_id varchar(50) NOT NULL,
+  license_threat_group_id varchar(50) NOT NULL,
+  multi_license_id varchar(50) NOT NULL,
+  CONSTRAINT license_threat_group_license_pk PRIMARY KEY (license_threat_group_license_id),
+  CONSTRAINT license_threat_group_license_application_fk FOREIGN KEY (application_id) REFERENCES application(application_id),
+  CONSTRAINT license_threat_group_license_uk UNIQUE KEY (application_id, multi_license_id)
+);

70edd789c58de6b9eacb0568d4b2b32afe99ba22
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2013-02-14 00:08:04 +0100

    Introduced another brain client tailored to validating the CLM configuration of a client INSIGHT-4163 INSIGHT-4160

---
+++ insight-brain-client/src/main/java/com/sonatype/insight/brain/client/ValidationClient.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.client;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Map;
+
+import com.sonatype.insight.client.utils.AbstractClient;
+import com.sonatype.insight.client.utils.HttpClientUtils.Configuration;
+import com.sonatype.insight.client.utils.Result;
+import com.sonatype.insight.json.store.JsonUtils;
+
+public final class ValidationClient
+    extends AbstractClient
+{
+    public ValidationClient( final Configuration config )
+    {
+        super( config );
+    }
+
+    public void validateConfiguration()
+        throws IOException
+    {
+        final Result result;
+        try
+        {
+            result = path( "rest/version" ).get();
+        }
+        catch ( UnknownHostException e )
+        {
+            // improve error msg
+            throw (IOException) new UnknownHostException( "Unknown host: " + e.getMessage() ).initCause( e );
+        }
+        final int status = result.status();
+        final String text = result.text();
+        // at this point, the network connection appears fine, now let's just check we actually talked to a CLM server
+        if ( status >= 300 )
+        {
+            throw new IOException( "Error code " + status + ": " + text );
+        }
+        try
+        {
+            Map<?, ?> versionInfo = JsonUtils.parse( text, Map.class );
+            if ( versionInfo.get( "version" ) == null && versionInfo.get( "name" ) == null )
+            {
+                throw new Exception( "No CLM version information present" );
+            }
+        }
+        catch ( Exception e )
+        {
+            throw new IOException( "Server is not compatible with this Sonatype CLM integration", e );
+        }
+    }
+
+    public void validateApplicationId( String applicationId )
+        throws IOException
+    {
+        final Result result;
+        try
+        {
+            result = path( "rest/application/validate", applicationId ).get();
+        }
+        catch ( UnknownHostException e )
+        {
+            // improve error msg
+            throw (IOException) new UnknownHostException( "Unknown host: " + e.getMessage() ).initCause( e );
+        }
+        final int status = result.status();
+        final String text = result.text();
+        if ( status >= 300 )
+        {
+            throw new IOException( "Error code " + status + ": " + text );
+        }
+        if ( !"OK".equals( text ) )
+        {
+            throw new IOException( "Invalid application ID: " + text );
+        }
+    }
+}
---
+++ insight-brain-client/src/test/java/com/sonatype/insight/brain/client/ValidationClientTest.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.client;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.sonatype.insight.brain.service.AbstractBrainServiceTest;
+import com.sonatype.insight.client.utils.HttpClientUtils.Configuration;
+
+public class ValidationClientTest
+    extends AbstractBrainServiceTest
+{
+
+    private void assertMatch( String pattern, String text )
+    {
+        assertTrue( text + " does not match pattern " + pattern, text != null && text.matches( pattern ) );
+    }
+
+    @Test
+    public void testValidateConfiguration_AllGood()
+        throws Exception
+    {
+        Configuration config = brain.getClientConfiguration();
+        new ValidationClient( config ).validateConfiguration();
+    }
+
+    @Test
+    public void testValidateConfiguration_BadContextRoot()
+        throws Exception
+    {
+        Configuration config = brain.getClientConfiguration();
+        config.setServerUrl( config.getServerUrl() + "/bad" );
+        try
+        {
+            new ValidationClient( config ).validateConfiguration();
+            fail( "Validation should have failed due to bad context root" );
+        }
+        catch ( IOException e )
+        {
+            assertMatch( "(?i).*404.*not found.*", e.getMessage() );
+        }
+    }
+
+    @Test
+    public void testValidateConfiguration_BadHost()
+        throws Exception
+    {
+        Configuration config = brain.getClientConfiguration();
+        config.setServerUrl( "http://bad.host/" );
+        try
+        {
+            new ValidationClient( config ).validateConfiguration();
+            fail( "Validation should have failed due to bad host" );
+        }
+        catch ( IOException e )
+        {
+            assertEquals( "Unknown host: bad.host", e.getMessage() );
+        }
+    }
+
+    @Test
+    public void testValidateConfiguration_BadPort()
+        throws Exception
+    {
+        Configuration config = brain.getClientConfiguration();
+        config.setServerUrl( "http://localhost:65535/" );
+        try
+        {
+            new ValidationClient( config ).validateConfiguration();
+            fail( "Validation should have failed due to bad port" );
+        }
+        catch ( IOException e )
+        {
+            assertMatch( "(?i).*Connection.* refused.*", e.getMessage() );
+        }
+    }
+
+    @Test
+    public void testValidateConfiguration_BadProxyHost()
+        throws Exception
+    {
+        Configuration config = brain.getClientConfiguration();
+        config.setProxy( "bad.host" );
+        try
+        {
+            new ValidationClient( config ).validateConfiguration();
+            fail( "Validation should have failed due to bad proxy host" );
+        }
+        catch ( IOException e )
+        {
+            assertEquals( "Unknown host: bad.host", e.getMessage() );
+        }
+    }
+
+    @Test
+    public void testValidateConfiguration_BadProxyPort()
+        throws Exception
+    {
+        Configuration config = brain.getClientConfiguration();
+        config.setProxy( "localhost:65535" );
+        try
+        {
+            new ValidationClient( config ).validateConfiguration();
+            fail( "Validation should have failed due to bad proxy port" );
+        }
+        catch ( IOException e )
+        {
+            assertMatch( "(?i).*Connection.* refused.*", e.getMessage() );
+        }
+    }
+
+    @Test
+    public void testValidateApplicationId_AllGood()
+        throws Exception
+    {
+        new ValidationClient( brain.getClientConfiguration() ).validateApplicationId( "valid-id" );
+    }
+
+    @Test
+    public void testValidateApplicationId_UnknownId()
+        throws Exception
+    {
+        invalidateAppId( "unknown-id", "Not Found" );
+        try
+        {
+            new ValidationClient( brain.getClientConfiguration() ).validateApplicationId( "unknown-id" );
+            fail( "Validation should have failed due to bad app id" );
+        }
+        catch ( IOException e )
+        {
+            assertMatch( "(?i).*invalid.*not found.*", e.getMessage() );
+        }
+    }
+
+}

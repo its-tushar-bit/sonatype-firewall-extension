86bb97f46f018354bab7beb4e994128a5369cd1e
Damian Bradicich <dbradicich@sonatype.com>
2013-02-22 15:56:44 -0500

    - cancel will now clear the last added email address - INSIGHT-3988 INSIGHT-4159

--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/js/notification-management.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/js/notification-management.js
@@ -7,75 +7,70 @@
 /* global angular, $ */
 /* jslint plusplus: true */
 (function() {
-  'use strict';
+    'use strict';
 
-  var module = angular.module('NotificationManagement', []);
+    var module = angular.module('NotificationManagement', []);
 
-  module.controller('NotificationManagementController', ['$scope', 'global', function($scope, global) {
-            $scope.state = global;
+    module.controller('NotificationManagementController', [ '$scope', 'global', function($scope, global) {
+        $scope.state = global;
 
-            $scope.editNotifications = function(actionData) {
-              $scope.state.notificationEmailList = [];
-              $scope.state.currentActionStep = actionData;
-              if ($scope.state.currentActionStep.target)
-              {
+        $scope.editNotifications = function(actionData) {
+            delete $scope.state.currentNotificationEmail;
+            $scope.state.notificationEmailList = [];
+            $scope.state.currentActionStep = actionData;
+            if ($scope.state.currentActionStep.target) {
                 $scope.state.notificationEmailList = $scope.state.currentActionStep.target.split(',');
-              }
-              $('#editNotificationsModal').modal('show');
             }
-
-            $scope.addNotificationEmail = function() {
-              $scope.state.notificationEmailList.push($scope.state.currentNotificationEmail);
-              item.targetCount = item.target.split(/,/g).length;
-            }
-
-            $scope.cancelNotificationEmail = function() {
-              $('#editNotificationsModal').modal('hide');
-            }
-
-            $scope.doneNotificationEmail = function() {
-              $('#editNotificationsModal').modal('hide');
-              $scope.state.currentActionStep.target = $scope.state.notificationEmailList.join();
-              $scope.state.currentActionStep.targetCount = $scope.state.notificationEmailList.length;
-            }
-
-            $scope.removeNotificationEmail = function(index) {
-              $scope.state.notificationEmailList.splice(index, 1);
-            }
-          }]);
-
-  var EMAIL_REGEXP = /^\S+@\S+\.\S+$/;
-  module.directive('emailInput', function() {
-        return {
-          require : 'ngModel',
-          link : function(scope, elm, attrs, ctrl) {
-            ctrl.$parsers.unshift(function(viewValue) {
-                  if (!viewValue)
-                  {
-                    scope.state.notificationValidationMsg = "Enter an email address";
-                    return undefined;
-                  }
-                  else if (!EMAIL_REGEXP.test(viewValue))
-                  {
-                    scope.state.notificationValidationMsg = "Enter a valid email address";
-                    return undefined;
-                  }
-                  else
-                  {
-                    delete scope.state.notificationValidationMsg;
-                    return viewValue;
-                  }
-                });
-          }
-        };
-      });
-
-  module.directive('notificationManagement', function() {
-        return {
-          restrict : 'A',
-          replace : true,
-          transclude : true,
-          templateUrl : 'components/notification-management.html'
+            $('#editNotificationsModal').modal('show');
         }
-      });
+
+        $scope.addNotificationEmail = function() {
+            $scope.state.notificationEmailList.push($scope.state.currentNotificationEmail);
+            item.targetCount = item.target.split(/,/g).length;
+        }
+
+        $scope.cancelNotificationEmail = function() {
+            $('#editNotificationsModal').modal('hide');
+        }
+
+        $scope.doneNotificationEmail = function() {
+            $('#editNotificationsModal').modal('hide');
+            $scope.state.currentActionStep.target = $scope.state.notificationEmailList.join();
+            $scope.state.currentActionStep.targetCount = $scope.state.notificationEmailList.length;
+        }
+
+        $scope.removeNotificationEmail = function(index) {
+            $scope.state.notificationEmailList.splice(index, 1);
+        }
+    } ]);
+
+    var EMAIL_REGEXP = /^\S+@\S+\.\S+$/;
+    module.directive('emailInput', function() {
+        return {
+            require : 'ngModel',
+            link : function(scope, elm, attrs, ctrl) {
+                ctrl.$parsers.unshift(function(viewValue) {
+                    if (!viewValue) {
+                        scope.state.notificationValidationMsg = "Enter an email address";
+                        return undefined;
+                    } else if (!EMAIL_REGEXP.test(viewValue)) {
+                        scope.state.notificationValidationMsg = "Enter a valid email address";
+                        return undefined;
+                    } else {
+                        delete scope.state.notificationValidationMsg;
+                        return viewValue;
+                    }
+                });
+            }
+        };
+    });
+
+    module.directive('notificationManagement', function() {
+        return {
+            restrict : 'A',
+            replace : true,
+            transclude : true,
+            templateUrl : 'components/notification-management.html'
+        }
+    });
 }());

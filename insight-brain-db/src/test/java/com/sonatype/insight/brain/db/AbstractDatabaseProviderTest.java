9b320ae002fbc6f6e4197e1956d673418c0f7214
vladt <vladt@sonatype.com>
2013-03-01 12:40:57 -0500

    Code reuse

---
+++ insight-brain-db/src/test/java/com/sonatype/insight/brain/db/AbstractDatabaseProviderTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.db;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.codehaus.plexus.util.FileUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+
+import com.sonatype.insight.db.DatabaseConfig;
+
+public abstract class AbstractDatabaseProviderTest
+{
+    @Before
+    public void setUp()
+        throws Exception
+    {
+        DataSourceFactory.clear_ForTestsOnly();
+    }
+
+    @After
+    public void tearDown()
+    {
+        DataSourceFactory.clear_ForTestsOnly();
+    }
+
+    protected void verifyDatabaseCreation( DatabaseConfig databaseConfig )
+        throws Exception
+    {
+        OperationalDataStoreProvider.init( databaseConfig );
+        DataSource dataSource = OperationalDataStoreProvider.getDataSource();
+        Assert.assertNotNull( dataSource );
+        Connection conn = dataSource.getConnection();
+        try
+        {
+            exec( conn, "SELECT * FROM test_table" );
+
+            String databaseURL = conn.getMetaData().getURL();
+            Assert.assertNotNull( databaseURL );
+            if ( databaseConfig != null )
+            {
+                Assert.assertTrue( databaseConfig.getUrl().startsWith( databaseURL + ";" ) );
+            }
+            else
+            {
+                Assert.assertEquals( "jdbc:h2:mem:inMemoryDatabase", databaseURL );
+            }
+        }
+        finally
+        {
+            conn.close();
+        }
+    }
+
+    protected void verifyDatabaseCreation_OnDisk( DatabaseConfig databaseConfig, File databaseDir )
+        throws Exception
+    {
+        FileUtils.deleteDirectory( databaseDir );
+        Assert.assertFalse( databaseDir.exists() );
+
+        // New database
+        verifyDatabaseCreation( databaseConfig );
+        Assert.assertTrue( databaseDir.exists() );
+        Assert.assertTrue( new File( databaseDir, "test.h2.db" ).exists() );
+
+        // Existing database
+        DataSourceFactory.clear_ForTestsOnly();
+        verifyDatabaseCreation( databaseConfig );
+        Assert.assertTrue( databaseDir.exists() );
+        Assert.assertTrue( new File( databaseDir, "test.h2.db" ).exists() );
+    }
+
+    private void exec( Connection conn, String sql )
+        throws SQLException
+    {
+        Statement stmt = conn.createStatement();
+        try
+        {
+            stmt.execute( sql );
+        }
+        finally
+        {
+            if ( stmt != null )
+            {
+                stmt.close();
+            }
+        }
+    }
+}
--- insight-brain-db/src/test/java/com/sonatype/insight/brain/db/DatamartDataStoreProviderTest.java
+++ insight-brain-db/src/test/java/com/sonatype/insight/brain/db/DatamartDataStoreProviderTest.java
@@ -6,35 +6,14 @@
 package com.sonatype.insight.brain.db;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-import javax.sql.DataSource;
-
-import org.codehaus.plexus.util.FileUtils;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import com.sonatype.insight.db.DatabaseConfig;
 
 public class DatamartDataStoreProviderTest
+    extends AbstractDatabaseProviderTest
 {
-    @Before
-    public void setUp()
-        throws Exception
-    {
-        DataSourceFactory.clear_ForTestsOnly();
-    }
-
-    @After
-    public void tearDown()
-    {
-        DataSourceFactory.clear_ForTestsOnly();
-    }
-
     @Test
     public void verifyDatabaseCreation_InMemory()
         throws Exception
@@ -53,62 +32,7 @@ public class DatamartDataStoreProviderTest
         databaseConfig.setPassword( "" );
         databaseConfig.setMaxConnections( 50 );
         File databaseDir = new File( "target/DatamartDataStoreProviderTest" );
-        FileUtils.deleteDirectory( databaseDir );
 
-        // New database
-        verifyDatabaseCreation( databaseConfig );
-        Assert.assertTrue( databaseDir.exists() );
-        Assert.assertTrue( new File( databaseDir, "test.h2.db" ).exists() );
-
-        // Existing database
-        DataSourceFactory.clear_ForTestsOnly();
-        verifyDatabaseCreation( databaseConfig );
-        Assert.assertTrue( databaseDir.exists() );
-        Assert.assertTrue( new File( databaseDir, "test.h2.db" ).exists() );
-    }
-
-    private void verifyDatabaseCreation( DatabaseConfig databaseConfig )
-        throws Exception
-    {
-        DatamartProvider.init( databaseConfig );
-        DataSource dataSource = DatamartProvider.getDataSource();
-        Assert.assertNotNull( dataSource );
-        Connection conn = dataSource.getConnection();
-        try
-        {
-            exec( conn, "SELECT * FROM test_table" );
-
-            String databaseURL = conn.getMetaData().getURL();
-            Assert.assertNotNull( databaseURL );
-            if ( databaseConfig != null )
-            {
-                Assert.assertTrue( databaseConfig.getUrl().startsWith( databaseURL + ";" ) );
-            }
-            else
-            {
-                Assert.assertEquals( "jdbc:h2:mem:inMemoryDatabase", databaseURL );
-            }
-        }
-        finally
-        {
-            conn.close();
-        }
-    }
-
-    private void exec( Connection conn, String sql )
-        throws SQLException
-    {
-        Statement stmt = conn.createStatement();
-        try
-        {
-            stmt.execute( sql );
-        }
-        finally
-        {
-            if ( stmt != null )
-            {
-                stmt.close();
-            }
-        }
+        verifyDatabaseCreation_OnDisk( databaseConfig, databaseDir );
     }
 }

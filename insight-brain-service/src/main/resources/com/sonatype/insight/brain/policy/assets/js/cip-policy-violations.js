ebe9b1c2296bcf7144bb699b45437237e4cac85c
Damian Bradicich <dbradicich@sonatype.com>
2013-03-15 10:50:10 -0400

    - cleaned up the cip policy violation tab quite a bit - INSIGHT-4310 INSIGHT-4311

--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/js/cip-policy-violations.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/js/cip-policy-violations.js
@@ -1,90 +1,100 @@
 /**
- * @license Copyright (c) 2013 Sonatype, Inc. All rights reserved.
- * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
- * "Sonatype" is a trademark of Sonatype, Inc.
+ * @license Copyright (c) 2013 Sonatype, Inc. All rights reserved. Includes the
+ *          third-party code listed at
+ *          http://links.sonatype.com/products/clm/attributions. "Sonatype" is a
+ *          trademark of Sonatype, Inc.
  */
-/*global angular, $, window, CLM */
-(function () {
+/* global angular, $, window, CLM */
+(function() {
     'use strict';
 
-	$.extend(true, window, {
-	    'Insight' : {
-			'PolicyViolations' : function (node, appId, hash) {
-				var timestamp = (new Date()).getTime(),
-					container = $('<div ng-include src="\'' + CLM.path + 'policy-assets/components/cip-policy-violations.html\'"></div>');
-				node.empty();
-				container.appendTo(node);
-				
-				angular.module('policyViolations' + timestamp, []).service('PolicyViolationData', function () {
+    $.extend(true, window, {
+        'Insight' : {
+            'PolicyViolations' : function(node, appId, hash) {
+                var timestamp = (new Date()).getTime(), container = $('<div ng-include src="\'' + CLM.path + 'policy-assets/components/cip-policy-violations.html\'"></div>');
+                node.empty();
+                container.appendTo(node);
+
+                angular.module('policyViolations' + timestamp, []).service('PolicyViolationData', function() {
                     return {
                         hash : hash
                     };
                 });
-				
-				angular.bootstrap(container[0], ['PolicyViolations', 'policyViolations' + timestamp]);
-	        }
-	    }
-	});
 
-	var policyViolationApp = angular.module('PolicyViolations', []);
+                angular.bootstrap(container[0], [ 'PolicyViolations', 'policyViolations' + timestamp ]);
+            }
+        }
+    });
 
-	policyViolationApp.controller('PolicyViolationsController', ['$http', '$scope', 'PolicyViolationData', function ($http, $scope, policyViolationData) {
-		function errorFn(data, status, headersFn, config) {
-		    var header = headersFn();
+    var policyViolationApp = angular.module('PolicyViolations', []);
+
+    policyViolationApp.controller('PolicyViolationsController', [ '$http', '$scope', '$timeout', 'PolicyViolationData', function($http, $scope, $timeout, policyViolationData) {
+        function errorFn(data, status, headersFn, config) {
+            var header = headersFn();
             if (header['content-type'] && header['content-type'].indexOf('text/html') === 0) {
                 $scope.errorResponse = 'Server Error';
             } else {
                 $scope.errorResponse = data;
             }
-		}
-		
-		function startIfReady() {
-		    if ($scope.policyAlerts !== undefined
-		        && $scope.actionTypes !== undefined) {
-		        $scope.processedPolicyAlerts = [];
-	            angular.forEach($scope.policyAlerts, function(policyAlert,policyAlertIndex){
-	                var actions = [];
-	                angular.forEach(policyAlert.actions, function(action, actionIndex){
-                        angular.forEach($scope.actionTypes, function(actionType, actionTypeIndex){
-                            if (actionType.id === action.actionTypeId) {
+        }
+
+        function startIfReady() {
+            if ($scope.policyAlerts !== undefined && $scope.actionTypes !== undefined) {
+                $scope.processedPolicyAlerts = [];
+                angular.forEach($scope.policyAlerts, function(policyAlert, policyAlertIndex) {
+                    var actions = [];
+                    angular.forEach(policyAlert.actions, function(action, actionIndex) {
+                        angular.forEach($scope.actionTypes, function(actionType, actionTypeIndex) {
+                            if (actionType.id === action.actionTypeId && actions.indexOf(actionType.summary) == -1) {
                                 actions.push(actionType.summary);
                                 return false;
                             }
                         });
                     });
-	                angular.forEach(policyAlert.trigger.componentFacts, function(componentFact, componentFactIndex){
-	                    if (componentFact.hash === policyViolationData.hash){
-	                        var tLvl = policyAlert.trigger.threatLevel;
-	                        $scope.processedPolicyAlerts.push({
-	                            id: policyAlert.trigger.policyId,
-	                            name: policyAlert.trigger.policyName,
-	                            threatLevel: tLvl,
-	                            groupId: componentFact.groupId,
-	                            artifactId: componentFact.artifactId,
-	                            version: componentFact.version,
-	                            hash: componentFact.hash,
-	                            color: tLvl > 7 ? 'red' : tLvl > 3 ? 'orange' : tLvl > 0 ? 'yellow' : 'blue',
-	                            constraints: componentFact.constraintFacts,
-	                            actions: actions
-	                        });
-	                    }
-	                });
-	            });  
-	            
-	            $scope.processedPolicyAlerts.sort(function(policyA, policyB){
-	                return policyA.threatLevel > policyB.threatLevel ? -11 : policyA.threatLevel < policyB.threatLevel ? 1 : 0;
-	            });
-		    }
-		}
-		
-	    $http.get('policyalerts.json', { params : { timestamp : new Date().getTime() } }).success(function (data) {
-			$scope.policyAlerts = data.aaData || [];
-			startIfReady();
-		}).error(errorFn);
-		
-		$http.get(CLM.path + 'rest/policy/actionType').success(function (data) {
-		    $scope.actionTypes = data;
-		    startIfReady();
-		}).error(errorFn);
-	}]);
+                    angular.forEach(policyAlert.trigger.componentFacts, function(componentFact, componentFactIndex) {
+                        if (componentFact.hash === policyViolationData.hash) {
+                            var tLvl = policyAlert.trigger.threatLevel;
+                            $scope.processedPolicyAlerts.push({
+                                id : policyAlert.trigger.policyId,
+                                name : policyAlert.trigger.policyName,
+                                threatLevel : tLvl,
+                                groupId : componentFact.groupId,
+                                artifactId : componentFact.artifactId,
+                                version : componentFact.version,
+                                hash : componentFact.hash,
+                                color : tLvl > 7 ? 'red' : tLvl > 3 ? 'orange' : tLvl > 0 ? 'yellow' : 'blue',
+                                constraints : componentFact.constraintFacts,
+                                actions : actions
+                            });
+                        }
+                    });
+                });
+
+                $scope.processedPolicyAlerts.sort(function(policyA, policyB) {
+                    return policyA.threatLevel > policyB.threatLevel ? -11 : policyA.threatLevel < policyB.threatLevel ? 1 : 0;
+                });
+
+                $timeout(function() {
+                    $('.cip-policy').each(function() {
+                        $(this).height($(this).closest('td').height());
+                    });
+                }, 0, false);
+
+            }
+        }
+
+        $http.get('policyalerts.json', {
+            params : {
+                timestamp : new Date().getTime()
+            }
+        }).success(function(data) {
+            $scope.policyAlerts = data.aaData || [];
+            startIfReady();
+        }).error(errorFn);
+
+        $http.get(CLM.path + 'rest/policy/actionType').success(function(data) {
+            $scope.actionTypes = data;
+            startIfReady();
+        }).error(errorFn);
+    } ]);
 }());

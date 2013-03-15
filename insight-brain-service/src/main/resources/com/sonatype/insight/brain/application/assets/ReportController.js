1582badd199abad1e98d4a215d8e5dfaf347a677
Justin Young <jyoung@sonatype.com>
2013-03-15 12:39:13 -0700

    INSIGHT-4235 INSIGHT-4232 Programatically fit iframe to report page

--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/application/assets/ReportController.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/application/assets/ReportController.js
@@ -12,4 +12,30 @@
     reportModule.controller('ReportController', ['$scope', '$routeParams', function ($scope, $routeParams) {
         $scope.reportUrl = '/rest/report/' + $routeParams.applicationId + '/' + $routeParams.scanId + '/embedReport/index.html?readonly=true';
     }]);
+
+    reportModule.directive('expandableIframe', function () {
+        return {
+            template: "<iframe ng-src='{{reportUrl}}' width='100%' height='1000px' border='0' frameborder='0' scrolling='yes' style='overflow:auto;'/>",
+            compile: function () {
+                "use strict";
+
+                function setDimensions() {
+                    var iframe = $('iframe');
+                    if (!iframe) {
+                        clearTimeout(setDimensions);
+                        return;
+                    }
+                    var windowHeight = (window.innerHeight) ? window.innerHeight : $(document.body).getHeight(),
+                        containerTop = iframe.offset().top,
+                        bottomPadding = 20,
+                        height = Math.max(400, windowHeight - containerTop - bottomPadding);
+
+                    iframe.css({ 'height': height + 'px' });
+                }
+
+                setTimeout(setDimensions, 100);
+                $('.container').css({ 'width': '955px' });
+            }
+        };
+    });
 }());

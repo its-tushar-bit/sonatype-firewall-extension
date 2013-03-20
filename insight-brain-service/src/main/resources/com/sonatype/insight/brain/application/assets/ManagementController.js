270ad45cd1a00d093b3ac85f38b3255ebb008148
Justin Young <jyoung@sonatype.com>
2013-03-20 12:07:05 -0700

    INSIGHT-4299 Hudson JQuery AJAX Proxy

--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/application/assets/ManagementController.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/application/assets/ManagementController.js
@@ -5,179 +5,185 @@
  */
 /*global angular, $, clmBuildTimestamp */
 (function () {
-    'use strict';
+	'use strict';
 
-    var managementModule = angular.module('Management', ['AngularCommon', 'Hudson', 'CLMLocation']);
+	var managementModule = angular.module('Management', ['AngularCommon', 'Hudson', 'CLMLocation']);
 
-    managementModule.controller('ManagementController', ['$scope', '$location', '$http', 'hudson', 'CLMLocations', function ($scope, $location, $http, hudson, clmLocations) {
-        $scope.orderColumn = 'publicId';
-        $scope.orderDirection = true;
+	managementModule.controller('ManagementController', ['$scope', '$location', '$http', 'hudson', 'CLMLocations', function ($scope, $location, $http, hudson, clmLocations) {
+		$scope.orderColumn = 'publicId';
+		$scope.orderDirection = true;
 
-        function onAddApplicationError(data, status, headersFn, config) {
-            var header = headersFn();
-            if (header['content-type'] && header['content-type'].indexOf('text/html') === 0) {
-                $scope.addApplicationError = 'Server Error';
-            } else {
-                $scope.addApplicationError = data;
-            }
-        }
+		function onAddApplicationError(data, status, headersFn, config) {
+			var header = headersFn();
+			if (header['content-type'] && header['content-type'].indexOf('text/html') === 0) {
+				$scope.addApplicationError = 'Server Error';
+			} else {
+				$scope.addApplicationError = data;
+			}
+		}
 
-        $http.get(clmLocations.getActionStageUrl(), {
-            params: { timestamp: new Date().getTime() }
-        }).success(function (data) {
-                $scope.stages = data;
-            }).error($scope.showServerError);
+		$http.get(clmLocations.getActionStageUrl(), {
+			params: { timestamp: new Date().getTime() }
+		}).success(function (data) {
+				$scope.stages = data;
+			}).error($scope.showServerError);
 
-        $http.get(clmLocations.getApplicationsUrl(), {
-            params: { timestamp: new Date().getTime() }
-        }).success(function (data) {
-                $scope.applications = data;
-            }).error($scope.showServerError);
+		$http.get(clmLocations.getApplicationsUrl(), {
+			params: { timestamp: new Date().getTime() }
+		}).success(function (data) {
+				$scope.applications = data;
+			}).error($scope.showServerError);
 
-        $scope.getApplicationNames = function () {
-            var names = [];
-            if ($scope.applications) {
-                for (var i = 0; i < $scope.applications.length; i++) {
-                    names.push($scope.applications[i].publicId);
-                }
-            }
-            return names;
-        };
+		$scope.getApplicationNames = function () {
+			var names = [];
+			if ($scope.applications) {
+				for (var i = 0; i < $scope.applications.length; i++) {
+					names.push($scope.applications[i].publicId);
+				}
+			}
+			return names;
+		};
 
-        $scope.registerNewApplication = function () {
-            $scope.newApplicationUrl = 'components/new-application-editor.html?' + clmBuildTimestamp;
+		$scope.registerNewApplication = function () {
+			$scope.newApplicationUrl = 'components/new-application-editor.html?' + clmBuildTimestamp;
 
-            $scope.selectedApplication = { id: null, publicId: null };
+			$scope.selectedApplication = { id: null, publicId: null };
 
-            $('#newApplicationModal').modal('show');
-        };
+			$('#newApplicationModal').modal('show');
+		};
 
-        $scope.addApplication = function () {
-            if (!$scope.applicationPublicId) {
-                $scope.addApplicationError = 'Please enter a value for the Application Id';
-            }
-            hudson.post(clmLocations.getApplicationsUrl(), $scope.applicationPublicId).success(function (application) {
-                $scope.applications.push(application);
-                $scope.clearAddApplicationError();
-                $('#addApplicationModal').modal('hide');
-            }).error(onAddApplicationError);
-        };
+		$scope.addApplication = function () {
+			if (!$scope.applicationPublicId) {
+				$scope.addApplicationError = 'Please enter a value for the Application Id';
+			}
+			hudson.post(clmLocations.getApplicationsUrl(), $scope.applicationPublicId).success(function (application) {
+				$scope.applications.push(application);
+				$scope.clearAddApplicationError();
+				$('#addApplicationModal').modal('hide');
+			}).error(onAddApplicationError);
+		};
 
-        $scope.clearAddApplicationError = function () {
-            $scope.addApplicationError = null;
-        };
+		$scope.clearAddApplicationError = function () {
+			$scope.addApplicationError = null;
+		};
 
-        $scope.order = function (column) {
-            if ($scope.orderColumn === column) {
-                $scope.orderDirection = !$scope.orderDirection;
-            } else {
-                $scope.orderColumn = column;
-                $scope.orderDirection = true;
-            }
-        };
+		$scope.order = function (column) {
+			if ($scope.orderColumn === column) {
+				$scope.orderDirection = !$scope.orderDirection;
+			} else {
+				$scope.orderColumn = column;
+				$scope.orderDirection = true;
+			}
+		};
 
-        $scope.orderBy = function (application) {
-            if ($scope.orderColumn === 'publicId') {
-                return application.publicId;
-            } else if ($scope.orderColumn === application.policyEvaluation.stage.stageTypeId) {
-                return application.policyEvaluation.time;
-            }
-            // return max value to prevent empty values showing up as low
-            return Number.MAX_VALUE;
-        };
+		$scope.orderBy = function (application) {
+			if ($scope.orderColumn === 'publicId') {
+				return application.publicId;
+			} else if ($scope.orderColumn === application.policyEvaluation.stage.stageTypeId) {
+				return application.policyEvaluation.time;
+			}
+			// return max value to prevent empty values showing up as low
+			return Number.MAX_VALUE;
+		};
 
-        $scope.encodeURIComponent = function (value) {
-            return encodeURIComponent(value);
-        }
-    }]);
+		$scope.encodeURIComponent = function (value) {
+			return encodeURIComponent(value);
+		}
+	}]);
 
-    managementModule.filter('filterReportColumns', function () {
-        return function (items) {
-            var arrayToReturn = [];
-            if (items) {
-                var validReportColumns = ['Build', 'Stage Release', 'Release'];
-                for (var i = 0; i < items.length; i++) {
-                    if (validReportColumns.indexOf(items[i].name) > -1) {
-                        arrayToReturn.push(items[i]);
-                    }
-                }
-            }
-            return arrayToReturn;
-        };
-    });
+	managementModule.filter('filterReportColumns', function () {
+		return function (items) {
+			var arrayToReturn = [];
+			if (items) {
+				var validReportColumns = ['Build', 'Stage Release', 'Release'];
+				for (var i = 0; i < items.length; i++) {
+					if (validReportColumns.indexOf(items[i].name) > -1) {
+						arrayToReturn.push(items[i]);
+					}
+				}
+			}
+			return arrayToReturn;
+		};
+	});
 
-    managementModule.controller('NewApplicationController', ['$scope', 'hudson', 'CLMLocations', function ($scope, hudson, clmLocations) {
-        $scope.submitActive = false;
+	managementModule.controller('NewApplicationController', ['$scope', 'hudson', 'CLMLocations', function ($scope, hudson, clmLocations) {
+		$scope.submitActive = false;
 
-        $scope.addApplication = clmLocations.getApplicationsUrl();
+		$scope.addApplication = clmLocations.getApplicationsUrl();
 
-        $scope.fileChanged = function (element) {
-            if (element.files.length > 0) {
-                var file = element.files[0],
-                    src;
-                if (window.URL) {
-                    src = window.URL.createObjectURL(file);
-                } else if (window.webkitURL) {
-                    src = window.webkitURL.createObjectURL(file);
-                }
-                if (src) {
-                    $scope.$apply(function () {
-                        $scope.hasIconSource = true;
-                        $scope.applicationIconSource = src;
-                    });
-                } else {
-                    $scope.$apply(function () {
-                        $scope.hasIconSource = false;
-                    });
-                }
-            } else {
-                $scope.$apply(function () {
-                    $scope.hasIconSource = false;
-                });
-            }
-        };
+		$scope.fileChanged = function (element) {
+			if (element.files.length > 0) {
+				var file = element.files[0],
+					src;
+				if (window.URL) {
+					src = window.URL.createObjectURL(file);
+				} else if (window.webkitURL) {
+					src = window.webkitURL.createObjectURL(file);
+				}
+				if (src) {
+					$scope.$apply(function () {
+						$scope.hasIconSource = true;
+						$scope.applicationIconSource = src;
+					});
+				} else {
+					$scope.$apply(function () {
+						$scope.hasIconSource = false;
+					});
+				}
+			} else {
+				$scope.$apply(function () {
+					$scope.hasIconSource = false;
+				});
+			}
+		};
 
-        $scope.canSaveEdit = function (valid) {
-            return valid && !$scope.submitActive && $scope.selectedApplication != null && $scope.selectedApplication.publicId.length > 0;
-        };
+		$scope.canSaveEdit = function (valid) {
+			return valid && !$scope.submitActive && $scope.selectedApplication != null && $scope.selectedApplication.publicId.length > 0;
+		};
 
-        // This needs to be invoked by onsubmit rather than ng-submit to suppress submit when necessary
-        $scope.saveClick = function (e) {
-            if (window.FormData) {
-                $scope.uploadInProgress = true;
-                var formData = new FormData(angular.element('#applicationEditor')[0]);
-                var icon = angular.element('#uploadFile')[0];
-                if (icon.files.length > 0) {
-                    formData.append('file', icon.files[0]);
-                }
+		// This needs to be invoked by onsubmit rather than ng-submit to suppress submit when necessary
+		$scope.saveClick = function (e) {
+			if (window.FormData) {
+				$scope.uploadInProgress = true;
+				var formData = new FormData(angular.element('#applicationEditor')[0]);
+				var icon = angular.element('#uploadFile')[0];
+				if (icon.files.length > 0) {
+					formData.append('file', icon.files[0]);
+				}
 
-                var xhrWrapper = hudson.xhrPost();
-                xhrWrapper.xhr.upload.addEventListener("progress", uploadProgress, false);
-                xhrWrapper.xhr.addEventListener("load", uploadComplete, false);
-                xhrWrapper.xhr.addEventListener("error", uploadFailed, false);
-                xhrWrapper.xhr.addEventListener("abort", uploadCanceled, false);
-                xhrWrapper.xhr.open("POST", clmLocations.getApplicationsUrl());
-                xhrWrapper.post(formData);
-                return false;
-            } else {
-                return true;
-            }
-        };
+				hudson.ajaxPost({
+					url: clmLocations.getApplicationsUrl(),
+					data: formData
+				});
+				return false;
+
+				var xhrWrapper = hudson.xhrPost();
+				xhrWrapper.xhr.upload.addEventListener("progress", uploadProgress, false);
+				xhrWrapper.xhr.addEventListener("load", uploadComplete, false);
+				xhrWrapper.xhr.addEventListener("error", uploadFailed, false);
+				xhrWrapper.xhr.addEventListener("abort", uploadCanceled, false);
+				xhrWrapper.xhr.open("POST", clmLocations.getApplicationsUrl());
+				xhrWrapper.post(formData);
+				return false;
+			} else {
+				return true;
+			}
+		};
 
 
-        function uploadProgress(evt) {
-            if (evt.lengthComputable) {
-                $scope.progress = Math.round(evt.loaded * 100 / evt.total)
-            }
-        }
+		function uploadProgress(evt) {
+			if (evt.lengthComputable) {
+				$scope.progress = Math.round(evt.loaded * 100 / evt.total)
+			}
+		}
 
-        function uploadComplete(evt) {
-        }
+		function uploadComplete(evt) {
+		}
 
-        function uploadFailed(evt) {
-        }
+		function uploadFailed(evt) {
+		}
 
-        function uploadCanceled(evt) {
-        }
-    }]);
+		function uploadCanceled(evt) {
+		}
+	}]);
 }());
--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/assets/js/Hudson.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/assets/js/Hudson.js
@@ -5,123 +5,141 @@
  */
 /*global angular, console */
 (function () {
-    'use strict';
+	'use strict';
 
-    var hudson = false,
-        tested = null,
-        outstanding = [];
+	var hudson = false,
+		tested = null,
+		outstanding = [];
 
-    function startTest($http, clmLocations) {
-        function complete() {
-            angular.forEach(outstanding, function (fn, key) {
-                fn();
-            });
-        }
+	function startTest($http, clmLocations) {
+		function complete() {
+			angular.forEach(outstanding, function (fn, key) {
+				fn();
+			});
+		}
 
-        tested = $http.get(clmLocations.getBaseUrl() + '/../../../crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)').success(function () {
-            hudson = true;
-            tested = true;
-            complete();
-        }).error(function (xhr, msg) {
-                tested = true;
-                if (xhr && xhr.status !== 404 && console) {
-                    console.log('Got (' + xhr.status + ') while checking for Hudson API');
-                }
-                complete();
-            });
-    }
+		tested = $http.get(clmLocations.getBaseUrl() + '/../../../crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)').success(function () {
+			hudson = true;
+			tested = true;
+			complete();
+		}).error(function (xhr, msg) {
+				tested = true;
+				if (xhr && xhr.status !== 404 && console) {
+					console.log('Got (' + xhr.status + ') while checking for Hudson API');
+				}
+				complete();
+			});
+	}
 
-    function wrap($http, method, args, clmLocations) {
-        var success = [],
-            error = [],
-            result = {
-                success: function (fn) {
-                    success.push(fn);
-                    return this;
-                },
-                error: function (fn) {
-                    error.push(fn);
-                    return this;
-                }
-            },
-            iter = function (arr, me, args) {
-                angular.forEach(arr, function (fn) {
-                    fn.apply(me, args);
-                });
-            },
-            successFn = function (data) {
-                var header = data !== null ? data.split(':') : null,
-                    config = { headers: {  } };
+	function wrap($http, method, args, clmLocations) {
+		var success = [],
+			error = [],
+			result = {
+				success: function (fn) {
+					success.push(fn);
+					return this;
+				},
+				error: function (fn) {
+					error.push(fn);
+					return this;
+				}
+			},
+			iter = function (arr, me, args) {
+				angular.forEach(arr, function (fn) {
+					fn.apply(me, args);
+				});
+			},
+			successFn = function (data) {
+				var header = data !== null ? data.split(':') : null,
+					config = { headers: {  } };
 
-                if (header !== null && header.length === 2) {
-                    config.headers[header[0]] = header[1];
-                }
-                if (args.length < 3) {
-                    args.push(config);
-                } else {
-                    args[2] = angular.extend(config, args[2]);
-                }
+				if (header !== null && header.length === 2) {
+					config.headers[header[0]] = header[1];
+				}
+				if (args.length < 3) {
+					args.push(config);
+				} else {
+					args[2] = angular.extend(config, args[2]);
+				}
 
-                method(args[0], args[1], args[2]).success(function () {
-                    iter(success, this, arguments);
-                }).error(function () {
-                        iter(error, this, arguments);
-                    });
-            },
-            request = function () {
-                if (hudson === true) {
-                    $http.get(clmLocations.getBaseUrl() + '/../../../crumbIssuer/api/xml', {
-                        params: {
-                            timestamp: new Date().getTime(),
-                            xpath: 'concat(//crumbRequestField,":",//crumb)'
-                        }
-                    }).success(successFn).error(function () {
-                            iter(error, this, arguments);
-                        });
-                } else {
-                    successFn(null);
-                }
-            };
-        if (tested === true) {
-            request();
-        } else {
-            outstanding.push(request);
-        }
-        return result;
-    }
+				method(args[0], args[1], args[2]).success(function () {
+					iter(success, this, arguments);
+				}).error(function () {
+						iter(error, this, arguments);
+					});
+			},
+			request = function () {
+				if (hudson === true) {
+					$http.get(clmLocations.getBaseUrl() + '/../../../crumbIssuer/api/xml', {
+						params: {
+							timestamp: new Date().getTime(),
+							xpath: 'concat(//crumbRequestField,":",//crumb)'
+						}
+					}).success(successFn).error(function () {
+							iter(error, this, arguments);
+						});
+				} else {
+					successFn(null);
+				}
+			};
+		if (tested === true) {
+			request();
+		} else {
+			outstanding.push(request);
+		}
+		return result;
+	}
 
-    angular.module('Hudson', ['CLMLocation']).service('hudson', ['$http', 'CLMLocations', function ($http, clmLocations) {
-        if (tested === null) {
-            startTest($http, clmLocations);
-        }
-        return {
-            post: function () {
-                //note that we are not using angular.copy here, as the special arguments object is not iterable properly in IE8
-                var argArray = [], i;
-                for (i = 0; i < arguments.length; i++) {
-                    argArray.push(arguments[i]);
-                }
-                if (!hudson && tested === true) {
-                    return $http.post.apply($http, argArray);
-                }
-                return wrap($http, $http.post, argArray, clmLocations);
-            },
-            xhrPost: function () {
-                var xhr = new XMLHttpRequest();
-                return {
-                    xhr: xhr,
-                    post: function () {
-                        var argArray = [], i;
-                        for (i = 0; i < arguments.length; i++) {
-                            argArray.push(arguments[i]);
-                        }
-                        if (!hudson && tested === true) {
-                            return xhr.send.apply(xhr, argArray);
-                        }
-                        return wrap($http, xhr.send, [], clmLocations);
-                    }
-                };
-            }
-        };
-    }]);
+	angular.module('Hudson', ['CLMLocation']).service('hudson', ['$http', 'CLMLocations', function ($http, clmLocations) {
+		if (tested === null) {
+			startTest($http, clmLocations);
+		}
+		return {
+			post: function () {
+				//note that we are not using angular.copy here, as the special arguments object is not iterable properly in IE8
+				var argArray = [], i;
+				for (i = 0; i < arguments.length; i++) {
+					argArray.push(arguments[i]);
+				}
+				if (!hudson && tested === true) {
+					return $http.post.apply($http, argArray);
+				}
+				return wrap($http, $http.post, argArray, clmLocations);
+			},
+			xhrPost: function () {
+				var xhr = new XMLHttpRequest();
+				return {
+					xhr: xhr,
+					post: function () {
+						var argArray = [], i;
+						for (i = 0; i < arguments.length; i++) {
+							argArray.push(arguments[i]);
+						}
+						if (!hudson && tested === true) {
+							return xhr.send.apply(xhr, argArray);
+						}
+						return wrap($http, xhr.send, argArray, clmLocations);
+					}
+				};
+			},
+			ajaxPost: function () {
+				var argArray = [], i;
+				for (i = 0; i < arguments.length; i++) {
+					// apply necessary multi form properties
+					if (i === 0) {
+						var argument = arguments[i];
+						argument.cache = false;
+						argument.contentType = false;
+						argument.processData = false;
+						argument.type = 'POST';
+					}
+					argArray.push(arguments[i]);
+				}
+				if (!hudson && tested === true) {
+					return jQuery.ajax.apply(jQuery, argArray);
+				}
+				return wrap($http, jQuery.ajax, argArray, clmLocations);
+			}
+		};
+	}]);
 }());

c9aa66c9028ed03689ce838ceaea14f2ddef44b8
Damian Bradicich <dbradicich@sonatype.com>
2013-03-21 15:00:25 -0400

    - split out the css into css files so that the loading of icons is consistant across browsers (FF and IE were using different relative paths to the css), ultimate outcome being that you see the severity background on each policy item in IE now - INSIGHT-4311 INSIGHT-4310

--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/js/cip-loader.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/js/cip-loader.js
@@ -1,24 +1,28 @@
 /**
- * @license Copyright (c) 2013 Sonatype, Inc. All rights reserved.
- * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
- * "Sonatype" is a trademark of Sonatype, Inc.
+ * @license Copyright (c) 2013 Sonatype, Inc. All rights reserved. Includes the
+ *          third-party code listed at
+ *          http://links.sonatype.com/products/clm/attributions. "Sonatype" is a
+ *          trademark of Sonatype, Inc.
  */
-/*global $, window, CLM */
-(function ()
-{
+/* global $, window, CLM */
+(function() {
     'use strict';
 
-    $.extend( true, window, {
-        'CLM': {
-            'path': '../brain/'
+    $.extend(true, window, {
+        'CLM' : {
+            'path' : '../brain/'
         }
-    } );
+    });
 
-    var head = $( 'head' ), scripts = ['assets/angular/angular-1.0.5.min.js', 'policy-assets/js/cip-label-editor.js',
-        'policy-assets/js/cip-policy-violations.js'];
+    var head = $('head'), 
+        scripts = [ 'assets/angular/angular-1.0.5.min.js', 'policy-assets/js/cip-label-editor.js', 'policy-assets/js/cip-policy-violations.js' ], 
+        styles = [ 'policy-assets/css/cip-label-editor.css', 'policy-assets/css/cip-policy-violations.css' ];
 
-    $.each( scripts, function ( key, script )
-    {
-        $( '<script></script>' ).attr( 'src', CLM.path + script ).appendTo( head );
-    } );
+    $.each(scripts, function(key, script) {
+        $('<script></script>').attr('type', 'text/javascript').attr('src', CLM.path + script).appendTo(head);
+    });
+
+    $.each(styles, function(key, style) {
+        $('<link></link>').attr('type', 'text/css').attr('rel', 'stylesheet').attr('href', CLM.path + style).appendTo(head);
+    });
 }());

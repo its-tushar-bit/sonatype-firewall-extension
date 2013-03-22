cb390bf986ce40c74edfbe85f324aafd9b0c3295
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2013-03-22 17:31:56 +0100

    Fixed configuration of assets bundles to use correct index file

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/service/InsightBrainService.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/service/InsightBrainService.java
@@ -71,10 +71,11 @@ public class InsightBrainService
     @Override
     public void initialize( final Bootstrap<InsightConfig> bootstrap )
     {
-        bootstrap.addBundle(
-            new AssetsBundle( "/com/sonatype/insight/brain/application/assets/", APPLICATION_ASSET_PATH ) );
+        bootstrap.addBundle( new AssetsBundle( "/com/sonatype/insight/brain/application/assets/",
+                                               APPLICATION_ASSET_PATH, "index.html" ) );
         bootstrap.addBundle( new AssetsBundle( "/com/sonatype/insight/brain/assets/", BRAIN_ASSET_PATH ) );
-        bootstrap.addBundle( new AssetsBundle( "/com/sonatype/insight/brain/policy/assets/", POLICY_ASSET_PATH ) );
+        bootstrap.addBundle( new AssetsBundle( "/com/sonatype/insight/brain/policy/assets/", POLICY_ASSET_PATH,
+                                               "index.html" ) );
 
         // workaround to let us set different defaults in the core HTTP configuration
         bootstrap.getObjectMapperFactory().registerModule( new HttpConfig.Module() );

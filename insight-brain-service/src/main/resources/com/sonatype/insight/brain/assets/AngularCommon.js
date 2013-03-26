c0621b445555499194035e61a801ff0ceee99f8b
Justin Young <jyoung@sonatype.com>
2013-03-26 15:33:59 -0700

    Space -> Tab JS Reformatting

--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/assets/AngularCommon.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/assets/AngularCommon.js
@@ -5,72 +5,72 @@
  */
 var angularCommon;
 (function () {
-    "use strict";
+	"use strict";
 
-    angularCommon = angular.module('AngularCommon', []);
+	angularCommon = angular.module('AngularCommon', []);
 
-    angularCommon.directive('errorModal', function () {
-        return {
-            replace: true,
-            templateUrl: '../assets/components/errorModal.html',
-            link: function ($scope, element) {
-                $scope.showError = function (errorResponse) {
-                    $scope.errorResponse = errorResponse;
-                    element.modal('show');
-                };
-                $scope.showServerError = function (data, status, headersFn, config) {
-                    var header = headersFn();
-                    if (header['content-type'] && header['content-type'].indexOf('text/html') === 0) {
-                        $scope.errorResponse = 'Server Error';
-                    } else {
-                        $scope.errorResponse = data;
-                    }
-                    element.modal('show');
-                };
-                $scope.hideError = function () {
-                    element.modal('hide');
-                };
-            }
-        };
-    });
+	angularCommon.directive('errorModal', function () {
+		return {
+			replace: true,
+			templateUrl: '../assets/components/errorModal.html',
+			link: function ($scope, element) {
+				$scope.showError = function (errorResponse) {
+					$scope.errorResponse = errorResponse;
+					element.modal('show');
+				};
+				$scope.showServerError = function (data, status, headersFn, config) {
+					var header = headersFn();
+					if (header['content-type'] && header['content-type'].indexOf('text/html') === 0) {
+						$scope.errorResponse = 'Server Error';
+					} else {
+						$scope.errorResponse = data;
+					}
+					element.modal('show');
+				};
+				$scope.hideError = function () {
+					element.modal('hide');
+				};
+			}
+		};
+	});
 
-    angularCommon.directive('typeAhead', ['$parse', function ($parse) {
-        'use strict';
+	angularCommon.directive('typeAhead', ['$parse', function ($parse) {
+		'use strict';
 
-        return {
-            restrict: 'A',
-            require: '?ngModel',
-            link: function postLink($scope, element, attrs, controller) {
-                var source = $parse(attrs.typeAhead)($scope);
-                $scope.$watch(attrs.typeAhead, function (newSource, oldSource) {
-                    if (oldSource !== newSource) {
-                        source = newSource;
-                    }
-                });
+		return {
+			restrict: 'A',
+			require: '?ngModel',
+			link: function postLink($scope, element, attrs, controller) {
+				var source = $parse(attrs.typeAhead)($scope);
+				$scope.$watch(attrs.typeAhead, function (newSource, oldSource) {
+					if (oldSource !== newSource) {
+						source = newSource;
+					}
+				});
 
-                element.attr('data-provide', 'typeahead');
-                element.typeahead({
-                    source: function (query) {
-                        return angular.isFunction(source) ? source.apply(this, arguments) : source;
-                    },
-                    updater: function (item) {
-                        if (controller) {
-                            $scope.$apply(function () {
-                                controller.$setViewValue(item);
-                            });
-                        }
-                        return item;
-                    }
-                });
-            }
-        };
-    }]);
+				element.attr('data-provide', 'typeahead');
+				element.typeahead({
+					source: function (query) {
+						return angular.isFunction(source) ? source.apply(this, arguments) : source;
+					},
+					updater: function (item) {
+						if (controller) {
+							$scope.$apply(function () {
+								controller.$setViewValue(item);
+							});
+						}
+						return item;
+					}
+				});
+			}
+		};
+	}]);
 
-    angularCommon.directive('tip', function () {
-        return function (scope, element, attrs) {
-            $(element).tooltip();
-        };
-    });
+	angularCommon.directive('tip', function () {
+		return function (scope, element, attrs) {
+			$(element).tooltip();
+		};
+	});
 
 	angularCommon.factory('regexFactory', function () {
 		return {
--- insight-brain-service/src/test/resources/com/sonatype/insight/brain/assets/AngularCommonSpec.js
+++ insight-brain-service/src/test/resources/com/sonatype/insight/brain/assets/AngularCommonSpec.js
@@ -1,19 +1,19 @@
 describe('AngularCommon', function () {
-    var scope, compile, httpBackend, regex;
+	var scope, compile, httpBackend, regex;
 
-    beforeEach(module('AngularCommon'));
-    beforeEach(inject(function ($httpBackend, $rootScope, $compile, regexFactory) {
-        scope = $rootScope.$new();
-        compile = $compile;
-        httpBackend = $httpBackend;
+	beforeEach(module('AngularCommon'));
+	beforeEach(inject(function ($httpBackend, $rootScope, $compile, regexFactory) {
+		scope = $rootScope.$new();
+		compile = $compile;
+		httpBackend = $httpBackend;
 		regex = regexFactory;
-    }));
+	}));
 
-    it('implements errorModal directive', function () {
-        httpBackend.expectGET('../assets/components/errorModal.html').respond("<div id='errorModal'></div>");
-        var element = compile('<div error-Modal></div>')(scope);
-        expect(element).not.toBeUndefined();
-    });
+	it('implements errorModal directive', function () {
+		httpBackend.expectGET('../assets/components/errorModal.html').respond("<div id='errorModal'></div>");
+		var element = compile('<div error-Modal></div>')(scope);
+		expect(element).not.toBeUndefined();
+	});
 
 	it('provides regex to match unicode characters', function () {
 		var allLettersRegex = regex.allLetters();

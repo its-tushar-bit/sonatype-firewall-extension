1c608df588dd00e68d40a37de953c86d9cd9a13a
Matthew Piggott <mpiggott@sonatype.com>
2013-03-27 17:10:45 -0400

    Ensure CIP panels are nto executed until AngularJS is loaded

--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/js/cip-label-editor.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/js/cip-label-editor.js
@@ -7,211 +7,223 @@
 (function () {
     'use strict';
 
-	$.extend(true, window, {
-	    'Insight' : {
-			'LabelEditor' : function (node, applicationId, hash) {
-				var timestamp = (new Date()).getTime(),
-					container = $('<div ng-include src="\'' + CLM.path + 'policy-assets/components/cip-label-editor.html\'"></div>');
-				node.empty();
-				container.appendTo(node);
+    function doLoad() {
+		$.extend(true, window, {
+		    'Insight' : {
+				'LabelEditor' : function (node, applicationId, hash) {
+					var timestamp = (new Date()).getTime(),
+						container = $('<div ng-include src="\'' + CLM.path + 'policy-assets/components/cip-label-editor.html\'"></div>');
+					node.empty();
+					container.appendTo(node);
 
-				angular.module('labelEditor' + timestamp, []).service('ComponentLabelEditorGAV', function () {
-				    return {
-				        applicationId : applicationId,
-				        hash : hash
-				    };
-				});
-				angular.bootstrap(container[0], ['ComponentLabelEditor', 'labelEditor' + timestamp]);
-	        }
-	    }
-	});
-
-	function locate(needle, haystack, haystackProperty) {
-		var result = null;
-		angular.forEach(haystack, function (candidate, key) {
-			if (candidate !== null && needle === candidate[haystackProperty]) {
-				result = candidate;
-				return false;
-			}
+					angular.module('labelEditor' + timestamp, []).service('ComponentLabelEditorGAV', function () {
+					    return {
+					        applicationId : applicationId,
+					        hash : hash
+					    };
+					});
+					angular.bootstrap(container[0], ['ComponentLabelEditor', 'labelEditor' + timestamp]);
+		        }
+		    }
 		});
-		return result;
-	}
 
-	var labelsApp = angular.module('ComponentLabelEditor', []);
-
-	labelsApp.controller('LabelsController', ['$http', '$scope', 'ComponentLabelEditorGAV', function ($http, $scope, componentLabelEditorGAV) {
-	    var componentLabelsUrl = CLM.path + 'rest/label/component/' + componentLabelEditorGAV.applicationId + '/' + componentLabelEditorGAV.hash;
-
-		function errorFn(data, status, headersFn, config) {
-			var header = headersFn();
-			if (header['content-type'] && header['content-type'].indexOf('text/html') === 0) {
-			    $scope.editErrorResponse = 'Server Error';
-			} else {
-			    $scope.editErrorResponse = data;
-			}
+		function locate(needle, haystack, haystackProperty) {
+			var result = null;
+			angular.forEach(haystack, function (candidate, key) {
+				if (candidate !== null && needle === candidate[haystackProperty]) {
+					result = candidate;
+					return false;
+				}
+			});
+			return result;
 		}
 
-	    function persist(labelArr, color) {
-			$http.put(componentLabelsUrl, { labels : labelArr, color : color}).success(function (data) {
-				$scope.itemLabels = data;
-				$scope.reloadAppLabels(); // Only really necessary if someone adds a brand new label, and removes it in the same session
-			}).error(errorFn);
-		}
+		var labelsApp = angular.module('ComponentLabelEditor', []);
 
-		$scope.reloadLabels = function () {
-			$http.get(componentLabelsUrl, { params : { timestamp : new Date().getTime() } }).success(function (data) {
-				$scope.itemLabels = data;
-			}).error(errorFn);
-		};
-		$scope.reloadLabels();
+		labelsApp.controller('LabelsController', ['$http', '$scope', 'ComponentLabelEditorGAV', function ($http, $scope, componentLabelEditorGAV) {
+		    var componentLabelsUrl = CLM.path + 'rest/label/component/' + componentLabelEditorGAV.applicationId + '/' + componentLabelEditorGAV.hash;
 
-		$scope.reloadAppLabels = function () {
-			$http.get(CLM.path + 'rest/label/application/' + componentLabelEditorGAV.applicationId, { params : { timestamp : new Date().getTime() } }).success(function (data) {
-				$scope.availableLabels = data;
-			}).error(errorFn);
-		};
-		$scope.reloadAppLabels(); // do initial load
-
-		$scope.color = null;
-		$scope.colors = [{ color : null, text : '(Unset)' },
-						{ color : 'white', text : 'White' },
-						{ color : 'grey', text : 'Grey' },
-						{ color : 'black', text : 'Black' },
-						{ color : 'green', text : 'Green' },
-						{ color : 'yellow', text : 'Yellow' },
-						{ color : 'orange', text : 'Orange' },
-						{ color : 'red', text : 'Red' },
-						{ color : 'blue', text : 'Blue' }];
-
-		$scope.removeLabel = function (label) {
-			var updatedLabels = [];
-			angular.forEach($scope.itemLabels, function (candidate, key) {
-				if (candidate.label !== label.label) {
-					updatedLabels.push(candidate.label);
+			function errorFn(data, status, headersFn, config) {
+				var header = headersFn();
+				if (header['content-type'] && header['content-type'].indexOf('text/html') === 0) {
+				    $scope.editErrorResponse = 'Server Error';
+				} else {
+				    $scope.editErrorResponse = data;
 				}
-			});
-			persist(updatedLabels);
-		};
-
-		$scope.addLabels = function () {
-			var newLabels = $scope.labelInput.split(' '),
-			    updatedLabels = [],
-			    bigLabels = [],
-			    hasNewLabel = false;
-
-			if (newLabels.length === 0) {
-				return;
 			}
 
-			$scope.labelInput = '';
-
-			angular.forEach($scope.itemLabels, function (candidate, key) {
-				updatedLabels.push(candidate.label);
-			});
-
-			angular.forEach(newLabels, function (label, key) {
-				label = $.trim(label);
-				if (label.length > 50) {
-					bigLabels.push(label);
-				} else if (label.length > 0) {
-				    updatedLabels.push(label);
-				    hasNewLabel = true;
-				}
-			});
-
-			if (hasNewLabel) {
-				persist(updatedLabels, $scope.color);
+		    function persist(labelArr, color) {
+				$http.put(componentLabelsUrl, { labels : labelArr, color : color}).success(function (data) {
+					$scope.itemLabels = data;
+					$scope.reloadAppLabels(); // Only really necessary if someone adds a brand new label, and removes it in the same session
+				}).error(errorFn);
 			}
-			if (bigLabels.length > 0) {
-				angular.forEach(bigLabels, function (candidate, key) {
-					$scope.labelInput += candidate + ' ';
-				});
-				$scope.labelInput = $.trim($scope.labelInput);
-				$scope.editErrorResponse = '';
-			}
-		};
 
-		$scope.addLabel = function (label) {
-			var updatedLabels = [],
-			    duplicate = false;
+			$scope.reloadLabels = function () {
+				$http.get(componentLabelsUrl, { params : { timestamp : new Date().getTime() } }).success(function (data) {
+					$scope.itemLabels = data;
+				}).error(errorFn);
+			};
+			$scope.reloadLabels();
 
-			angular.forEach($scope.itemLabels, function (candidate, key) {
-				if (candidate.label.toLowerCase() === label.label.toLowerCase()) {
-					duplicate = true;
-				}
-				updatedLabels.push(candidate.label);
-			});
+			$scope.reloadAppLabels = function () {
+				$http.get(CLM.path + 'rest/label/application/' + componentLabelEditorGAV.applicationId, { params : { timestamp : new Date().getTime() } }).success(function (data) {
+					$scope.availableLabels = data;
+				}).error(errorFn);
+			};
+			$scope.reloadAppLabels(); // do initial load
 
-			if (!duplicate) {
-				updatedLabels.push(label.label);
-				persist(updatedLabels);
-			}
-		};
+			$scope.color = null;
+			$scope.colors = [{ color : null, text : '(Unset)' },
+							{ color : 'white', text : 'White' },
+							{ color : 'grey', text : 'Grey' },
+							{ color : 'black', text : 'Black' },
+							{ color : 'green', text : 'Green' },
+							{ color : 'yellow', text : 'Yellow' },
+							{ color : 'orange', text : 'Orange' },
+							{ color : 'red', text : 'Red' },
+							{ color : 'blue', text : 'Blue' }];
 
-		$scope.isWhite = function (label) {
-			return label.color === "green" || label.color === "black" || label.color === "orange" || label.color === "red" || label.color === "blue";
-		};
-
-		$scope.isApplied = function (label) {
-			var duplicate = false;
-			angular.forEach($scope.itemLabels, function (candidate, key) {
-				duplicate = duplicate || (candidate.label === label.label);
-				return !duplicate;
-			});
-			return !duplicate;
-		};
-
-		$scope.checkLength = function () {
-			$scope.oversize = false;
-			angular.forEach($scope.labelInput.split(' '), function (label, key) {
-				if (label.length > 50) {
-				    $scope.oversize = true;
-				}
-			});
-		};
-	}]);
-
-	labelsApp.directive('disablenav', function () {
-		return function (scope, element, attrs) {
-			element.bind("keydown.nav", function (e) {
-				if (e.keyCode >= 37 && e.keyCode <= 40) {
-					e.stopImmediatePropagation();
-				}
-			});
-		};
-	});
-
-	labelsApp.directive('entersubmit', function () {
-		return function(scope, element, attrs) {
-			element.bind('keydown', function(e) {
-				if (e.keyCode === 13) { // Enter
-					e.preventDefault();
-					console.log('triggering submit');
-					element.trigger('submit');
-				}
-			});
-		};
-	});
-
-	labelsApp.directive('spinner', function () {
-		var properties = ['-ms-transform', '-webkit-transform', '-moz-transform', 'transform'];
-
-		function setElement(element, value) {
-			angular.forEach(properties, function (prop, key) {
-				element.css(prop, value);
-			});
-			return element;
-		}
-
-		return function (scope, element, attrs) {
-			element.bind('click', function (e) {
-				setElement(element, '').prop('rotate', null).animate({ rotate : '+360'}, {
-					step : function (now, fx) {
-					    now = now % 360;
-					    setElement(element, 'rotate(' + now + 'deg)');
+			$scope.removeLabel = function (label) {
+				var updatedLabels = [];
+				angular.forEach($scope.itemLabels, function (candidate, key) {
+					if (candidate.label !== label.label) {
+						updatedLabels.push(candidate.label);
 					}
 				});
-			});
-		};
-	});
+				persist(updatedLabels);
+			};
+
+			$scope.addLabels = function () {
+				var newLabels = $scope.labelInput.split(' '),
+				    updatedLabels = [],
+				    bigLabels = [],
+				    hasNewLabel = false;
+
+				if (newLabels.length === 0) {
+					return;
+				}
+
+				$scope.labelInput = '';
+
+				angular.forEach($scope.itemLabels, function (candidate, key) {
+					updatedLabels.push(candidate.label);
+				});
+
+				angular.forEach(newLabels, function (label, key) {
+					label = $.trim(label);
+					if (label.length > 50) {
+						bigLabels.push(label);
+					} else if (label.length > 0) {
+					    updatedLabels.push(label);
+					    hasNewLabel = true;
+					}
+				});
+
+				if (hasNewLabel) {
+					persist(updatedLabels, $scope.color);
+				}
+				if (bigLabels.length > 0) {
+					angular.forEach(bigLabels, function (candidate, key) {
+						$scope.labelInput += candidate + ' ';
+					});
+					$scope.labelInput = $.trim($scope.labelInput);
+					$scope.editErrorResponse = '';
+				}
+			};
+
+			$scope.addLabel = function (label) {
+				var updatedLabels = [],
+				    duplicate = false;
+
+				angular.forEach($scope.itemLabels, function (candidate, key) {
+					if (candidate.label.toLowerCase() === label.label.toLowerCase()) {
+						duplicate = true;
+					}
+					updatedLabels.push(candidate.label);
+				});
+
+				if (!duplicate) {
+					updatedLabels.push(label.label);
+					persist(updatedLabels);
+				}
+			};
+
+			$scope.isWhite = function (label) {
+				return label.color === "green" || label.color === "black" || label.color === "orange" || label.color === "red" || label.color === "blue";
+			};
+
+			$scope.isApplied = function (label) {
+				var duplicate = false;
+				angular.forEach($scope.itemLabels, function (candidate, key) {
+					duplicate = duplicate || (candidate.label === label.label);
+					return !duplicate;
+				});
+				return !duplicate;
+			};
+
+			$scope.checkLength = function () {
+				$scope.oversize = false;
+				angular.forEach($scope.labelInput.split(' '), function (label, key) {
+					if (label.length > 50) {
+					    $scope.oversize = true;
+					}
+				});
+			};
+		}]);
+
+		labelsApp.directive('disablenav', function () {
+			return function (scope, element, attrs) {
+				element.bind("keydown.nav", function (e) {
+					if (e.keyCode >= 37 && e.keyCode <= 40) {
+						e.stopImmediatePropagation();
+					}
+				});
+			};
+		});
+
+		labelsApp.directive('entersubmit', function () {
+			return function(scope, element, attrs) {
+				element.bind('keydown', function(e) {
+					if (e.keyCode === 13) { // Enter
+						e.preventDefault();
+						console.log('triggering submit');
+						element.trigger('submit');
+					}
+				});
+			};
+		});
+
+		labelsApp.directive('spinner', function () {
+			var properties = ['-ms-transform', '-webkit-transform', '-moz-transform', 'transform'];
+
+			function setElement(element, value) {
+				angular.forEach(properties, function (prop, key) {
+					element.css(prop, value);
+				});
+				return element;
+			}
+
+			return function (scope, element, attrs) {
+				element.bind('click', function (e) {
+					setElement(element, '').prop('rotate', null).animate({ rotate : '+360'}, {
+						step : function (now, fx) {
+						    now = now % 360;
+						    setElement(element, 'rotate(' + now + 'deg)');
+						}
+					});
+				});
+			};
+		});
+	}
+
+	function check() {
+		if (window.angular) {
+			doLoad();
+		} else {
+			setTimeout(check, 100);
+		}
+	}
+
+	setTimeout(check, 0);
 }());
--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/js/cip-policy-violations.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/policy/assets/js/cip-policy-violations.js
@@ -8,86 +8,98 @@
 (function() {
     'use strict';
 
-    $.extend(true, window, {
-        'Insight' : {
-            'PolicyViolations' : function(node, appId, hash) {
-                var timestamp = (new Date()).getTime(), container = $('<div ng-include src="\'' + CLM.path + 'policy-assets/components/cip-policy-violations.html\'"></div>');
-                node.empty();
-                container.appendTo(node);
+    function doLoad() {
+	    $.extend(true, window, {
+	        'Insight' : {
+	            'PolicyViolations' : function(node, appId, hash) {
+	                var timestamp = (new Date()).getTime(), container = $('<div ng-include src="\'' + CLM.path + 'policy-assets/components/cip-policy-violations.html\'"></div>');
+	                node.empty();
+	                container.appendTo(node);
 
-                angular.module('policyViolations' + timestamp, []).service('PolicyViolationData', function() {
-                    return {
-                        hash : hash
-                    };
-                });
+	                angular.module('policyViolations' + timestamp, []).service('PolicyViolationData', function() {
+	                    return {
+	                        hash : hash
+	                    };
+	                });
 
-                angular.bootstrap(container[0], [ 'PolicyViolations', 'policyViolations' + timestamp ]);
-            }
-        }
-    });
+	                angular.bootstrap(container[0], [ 'PolicyViolations', 'policyViolations' + timestamp ]);
+	            }
+	        }
+	    });
 
-    var policyViolationApp = angular.module('PolicyViolations', []);
+	    var policyViolationApp = angular.module('PolicyViolations', []);
 
-    policyViolationApp.controller('PolicyViolationsController', [ '$http', '$scope', '$timeout', 'PolicyViolationData', function($http, $scope, $timeout, policyViolationData) {
-        function errorFn(data, status, headersFn, config) {
-            var header = headersFn();
-            if (header['content-type'] && header['content-type'].indexOf('text/html') === 0) {
-                $scope.errorResponse = 'Server Error';
-            } else {
-                $scope.errorResponse = data;
-            }
-        }
+	    policyViolationApp.controller('PolicyViolationsController', [ '$http', '$scope', '$timeout', 'PolicyViolationData', function($http, $scope, $timeout, policyViolationData) {
+	        function errorFn(data, status, headersFn, config) {
+	            var header = headersFn();
+	            if (header['content-type'] && header['content-type'].indexOf('text/html') === 0) {
+	                $scope.errorResponse = 'Server Error';
+	            } else {
+	                $scope.errorResponse = data;
+	            }
+	        }
 
-        function startIfReady() {
-            if ($scope.policyAlerts !== undefined && $scope.actionTypes !== undefined) {
-                $scope.processedPolicyAlerts = [];
-                angular.forEach($scope.policyAlerts, function(policyAlert, policyAlertIndex) {
-                    var actions = [];
-                    angular.forEach(policyAlert.actions, function(action, actionIndex) {
-                        angular.forEach($scope.actionTypes, function(actionType, actionTypeIndex) {
-                            if (actionType.id === action.actionTypeId && jQuery.inArray(actionType.summary, actions) === -1) {
-                                actions.push(actionType.summary);
-                                return false;
-                            }
-                        });
-                    });
-                    angular.forEach(policyAlert.trigger.componentFacts, function(componentFact, componentFactIndex) {
-                        if (componentFact.hash === policyViolationData.hash) {
-                            var tLvl = policyAlert.trigger.threatLevel;
-                            $scope.processedPolicyAlerts.push({
-                                id : policyAlert.trigger.policyId,
-                                name : policyAlert.trigger.policyName,
-                                threatLevel : tLvl,
-                                groupId : componentFact.groupId,
-                                artifactId : componentFact.artifactId,
-                                version : componentFact.version,
-                                hash : componentFact.hash,
-                                color : tLvl > 7 ? 'red' : tLvl > 3 ? 'orange' : tLvl > 0 ? 'yellow' : 'blue',
-                                constraints : componentFact.constraintFacts,
-                                actions : actions
-                            });
-                        }
-                    });
-                });
+	        function startIfReady() {
+	            if ($scope.policyAlerts !== undefined && $scope.actionTypes !== undefined) {
+	                $scope.processedPolicyAlerts = [];
+	                angular.forEach($scope.policyAlerts, function(policyAlert, policyAlertIndex) {
+	                    var actions = [];
+	                    angular.forEach(policyAlert.actions, function(action, actionIndex) {
+	                        angular.forEach($scope.actionTypes, function(actionType, actionTypeIndex) {
+	                            if (actionType.id === action.actionTypeId && jQuery.inArray(actionType.summary, actions) === -1) {
+	                                actions.push(actionType.summary);
+	                                return false;
+	                            }
+	                        });
+	                    });
+	                    angular.forEach(policyAlert.trigger.componentFacts, function(componentFact, componentFactIndex) {
+	                        if (componentFact.hash === policyViolationData.hash) {
+	                            var tLvl = policyAlert.trigger.threatLevel;
+	                            $scope.processedPolicyAlerts.push({
+	                                id : policyAlert.trigger.policyId,
+	                                name : policyAlert.trigger.policyName,
+	                                threatLevel : tLvl,
+	                                groupId : componentFact.groupId,
+	                                artifactId : componentFact.artifactId,
+	                                version : componentFact.version,
+	                                hash : componentFact.hash,
+	                                color : tLvl > 7 ? 'red' : tLvl > 3 ? 'orange' : tLvl > 0 ? 'yellow' : 'blue',
+	                                constraints : componentFact.constraintFacts,
+	                                actions : actions
+	                            });
+	                        }
+	                    });
+	                });
 
-                $scope.processedPolicyAlerts.sort(function(policyA, policyB) {
-                    return policyA.threatLevel > policyB.threatLevel ? -11 : policyA.threatLevel < policyB.threatLevel ? 1 : 0;
-                });
-            }
-        }
+	                $scope.processedPolicyAlerts.sort(function(policyA, policyB) {
+	                    return policyA.threatLevel > policyB.threatLevel ? -11 : policyA.threatLevel < policyB.threatLevel ? 1 : 0;
+	                });
+	            }
+	        }
 
-        $http.get('policyalerts.json', {
-            params : {
-                timestamp : new Date().getTime()
-            }
-        }).success(function(data) {
-            $scope.policyAlerts = data.aaData || [];
-            startIfReady();
-        }).error(errorFn);
+	        $http.get('policyalerts.json', {
+	            params : {
+	                timestamp : new Date().getTime()
+	            }
+	        }).success(function(data) {
+	            $scope.policyAlerts = data.aaData || [];
+	            startIfReady();
+	        }).error(errorFn);
 
-        $http.get(CLM.path + 'rest/policy/actionType').success(function(data) {
-            $scope.actionTypes = data;
-            startIfReady();
-        }).error(errorFn);
-    } ]);
+	        $http.get(CLM.path + 'rest/policy/actionType').success(function(data) {
+	            $scope.actionTypes = data;
+	            startIfReady();
+	        }).error(errorFn);
+	    } ]);
+    }
+
+	function check() {
+		if (window.angular) {
+			doLoad();
+		} else {
+			setTimeout(check, 100);
+		}
+	}
+
+	setTimeout(check, 0);
 }());

8f01c8f7e4385a8f99c7ff9525afbcfb5eb4239a
Justin Young <jyoung@sonatype.com>
2013-03-27 15:45:49 -0700

    INSIGHT-4299 update application name in db, set not null

--- insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0007.sql
+++ insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0007.sql
@@ -5,3 +5,14 @@ ALTER TABLE application
 
 ALTER TABLE application
   ADD COLUMN name_lowercase_no_whitespace VARCHAR(60) NULL;
+
+UPDATE application SET name = public_id;
+
+UPDATE application SET name_lower_no_whitespace = REPLACE(public_id_lowercase, ' ', '')
+
+ALTER TABLE application
+  ALTER COLUMN name varchar(60) NOT NULL;
+
+ALTER TABLE application
+  ALTER COLUMN name_lower_no_whitespace varchar(60) NOT NULL;
+

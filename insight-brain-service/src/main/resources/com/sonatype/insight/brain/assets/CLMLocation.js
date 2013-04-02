6ee9b85b61ab236c4ff8851f6a9bac4a986594d9
Justin Young <jyoung@sonatype.com>
2013-04-02 09:45:02 -0700

    INSIGHT-4399 CLMLocation.js Restore changes overwritten by branch merge

--- insight-brain-service/src/main/resources/com/sonatype/insight/brain/assets/CLMLocation.js
+++ insight-brain-service/src/main/resources/com/sonatype/insight/brain/assets/CLMLocation.js
@@ -4,9 +4,9 @@
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
 (function () {
-    "use strict";
+	"use strict";
 
-    // URI Encoded Query Parameter
+	// URI Encoded Query Parameter
 	function getQueryString(key) {
 		var results = new RegExp('[\\?&]' + key + '=([^&#]*)').exec(window.location.search);
 		if (results) {
@@ -14,9 +14,9 @@
 		}
 	}
 
-    angular.module('CLMLocation', []).factory('CLMLocations', function () {
+	angular.module('CLMLocation', []).factory('CLMLocations', function () {
 		// URI encoded ApplicationID
-		var getAppId = (function() {
+		var getAppId = (function () {
 			var appId = null;
 			return function () {
 				if (appId) {
@@ -28,45 +28,47 @@
 			};
 		}());
 
-        return {
-            getBaseUrl: function () {
-                if (this.baseUrl) {
-                    return this.baseUrl;
-                }
+		return {
+			getBaseUrl: function () {
+				if (this.baseUrl) {
+					return this.baseUrl;
+				}
 
 				this.baseUrl = '';
 
-                var idx = window.location.href.indexOf('/policy-assets/');
-                if (idx > -1) {
-                    this.baseUrl = window.location.href.substring(0, idx);
-                }
-                idx = window.location.href.indexOf('/application-assets/');
-                if (idx > -1) {
-                    this.baseUrl = window.location.href.substring(0, idx);
-                }
+				var idx = window.location.href.indexOf('/policy-assets/');
+				if (idx > -1) {
+					this.baseUrl = window.location.href.substring(0, idx);
+				}
+				idx = window.location.href.indexOf('/application-assets/');
+				if (idx > -1) {
+					this.baseUrl = window.location.href.substring(0, idx);
+				}
+
+				return this.baseUrl;
 			},
 
-            getLabelsUrl: function () {
-                return this.getBaseUrl() + '/rest/label/application/' + getAppId();
-            },
+			getLabelsUrl: function () {
+				return this.getBaseUrl() + '/rest/label/application/' + getAppId();
+			},
 
-            getDeleteLabelsUrl: function (label) {
-                return this.getBaseUrl() + '/rest/label/application/' + getAppId() + '/' +  encodeURIComponent(label.id);
-            },
+			getDeleteLabelsUrl: function (label) {
+				return this.getBaseUrl() + '/rest/label/application/' + getAppId() + '/' + encodeURIComponent(label.id);
+			},
 
-            getLicenseGroupsUrl: function () {
-                return this.getBaseUrl() + '/rest/licenseThreatGroup/application/' + getAppId();
-            },
+			getLicenseGroupsUrl: function () {
+				return this.getBaseUrl() + '/rest/licenseThreatGroup/application/' + getAppId();
+			},
 
-            getDeleteLicenseGroupUrl: function (group) {
-                return this.getBaseUrl() + '/rest/licenseThreatGroup/application/' + getAppId() + '/' +  encodeURIComponent(group.id);
-            },
+			getDeleteLicenseGroupUrl: function (group) {
+				return this.getBaseUrl() + '/rest/licenseThreatGroup/application/' + getAppId() + '/' + encodeURIComponent(group.id);
+			},
 
-            getLicenseGroupLicensesUrl: function (group) {
-                return this.getBaseUrl() + '/rest/licenseThreatGroupLicense/application/' + getAppId() + '/'
-                    + group.id;
-            },
-				
+			getLicenseGroupLicensesUrl: function (group) {
+				return this.getBaseUrl() + '/rest/licenseThreatGroupLicense/application/' + getAppId() + '/'
+				+ group.id;
+			},
+
 			getLicensesUrl: function () {
 				return this.getBaseUrl() + '/rest/license';
 			},
@@ -83,18 +85,18 @@
 				return this.getBaseUrl() + '/rest/policy/stageType';
 			},
 
-            getConditionValueTypeUrl: function () {
-                return this.getBaseUrl() + '/rest/conditionValueType/' + getAppId();
-            },
+			getConditionValueTypeUrl: function () {
+				return this.getBaseUrl() + '/rest/conditionValueType/' + getAppId();
+			},
 
-            getPolicyUrl: function () {
-                return this.getBaseUrl() + '/rest/policy/' + getAppId();
-            },
+			getPolicyUrl: function () {
+				return this.getBaseUrl() + '/rest/policy/' + getAppId();
+			},
 
-            getApplicationUrl: function (applicationId) {
-                return this.getBaseUrl() + '/rest/application/' +  encodeURIComponent(applicationId);
-            },
-				
+			getApplicationUrl: function (applicationId) {
+				return this.getBaseUrl() + '/rest/application/' + encodeURIComponent(applicationId);
+			},
+
 			getAddApplicationSyncUrl: function () {
 				return this.getBaseUrl() + '/rest/application/sync';
 			},

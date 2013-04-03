84679803509115b72a6f05cab916f927d2524748
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2013-04-03 21:04:45 +0200

    Polished code

--- insight-brain-model/src/main/java/com/sonatype/insight/brain/model/ApplicationProfile.java
+++ insight-brain-model/src/main/java/com/sonatype/insight/brain/model/ApplicationProfile.java
@@ -66,23 +66,13 @@ public class ApplicationProfile
 
     /**
      * This method is defined here only to trick jackson into "thinking" that it de-serialized the value of the
-     * publicIdLowercase field. If this method is not defined, jackson will set/access the publicIdLowercase field
-     * directly via reflection, possibly setting it to an incorrect value.
-     * 
-     * @deprecated This method should not be used explicitly.
-     */
-    public void setPublicIdLowercase( String publicIdLowercase )
-    {
-    }
-
-    /**
-     * This method is defined here only to trick jackson into "thinking" that it de-serialized the value of the
      * nameLowercaseNoWhitespace field. If this method is not defined, jackson will set/access the
      * nameLowercaseNoWhitespace field directly via reflection, possibly setting it to an incorrect value.
      * 
      * @deprecated This method should not be used explicitly.
      */
-    public void setNameLowercaseNoWhitespace( String nameLowercaseNoWhitespace )
+    @SuppressWarnings( "unused" )
+    private void setNameLowercaseNoWhitespace( String nameLowercaseNoWhitespace )
     {
     }
 }

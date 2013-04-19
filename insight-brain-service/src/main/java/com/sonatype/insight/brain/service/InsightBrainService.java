its-tushar-bit/sonatype-firewall-extension

commit d9880d0d481397a4b4d3016e4dc67677dfa45de4

Turned the last REST resource into a component loaded automatically via Sisu INSIGHT-4455 INSIGHT-4454

insight-brain-service/src/main/java/com/sonatype/insight/brain/service/InsightBrainService.java
@@ -6,6 +6,8 @@
 package com.sonatype.insight.brain.service;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 import javax.inject.Named;
@@ -15,13 +17,13 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.LoadingCache;
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import com.google.inject.TypeLiteral;
 import com.sonatype.insight.brain.db.DatamartProvider;
 import com.sonatype.insight.brain.db.OperationalDataStoreProvider;
 import com.sonatype.insight.brain.releasegraph.ReleaseGraphCacheLoader;
-import com.sonatype.insight.brain.releasegraph.ReleaseGraphHealthCheck;
 import com.sonatype.insight.brain.releasegraph.ReleaseGraphKey;
-import com.sonatype.insight.brain.releasegraph.ReleaseGraphResource;
-import com.sonatype.insight.brain.releasegraph.ReleaseGraphTask;
 import com.sonatype.insight.db.DatabaseConfig;
 import com.sonatype.insight.error.JaxRsExceptionMapper;
 import com.sun.jersey.api.core.ResourceConfig;
@@ -89,13 +91,6 @@ public class InsightBrainService
         DatamartProvider.init( dmDatabaseConfig );
         DatabaseConfig odsDatabaseConfig = getDatabaseConfig( databaseDir, "ods" );
         OperationalDataStoreProvider.init( odsDatabaseConfig );
-
-        LoadingCache<ReleaseGraphKey, byte[]> cache =
-            CacheBuilder.newBuilder().maximumSize( config.getReleaseGraphCacheSize() ).build(
-                new ReleaseGraphCacheLoader() );
-        env.addResource( new ReleaseGraphResource( cache ) );
-        env.addHealthCheck( new ReleaseGraphHealthCheck( cache ) );
-        env.addTask( new ReleaseGraphTask( cache ) );
         
         log.info( "Server base URL: {}", config.getBaseUrl() );
         log.debug( "Saas address: {}", config.getSaasAddress() );
@@ -119,5 +114,22 @@ public class InsightBrainService
 
         // Add our own mapper for exceptions.
         environment.addProvider( new JaxRsExceptionMapper() );
+    }
+
+    @Override
+    protected List<Module> modules( final InsightConfig config )
+    {
+        return Arrays.<Module> asList( new AbstractModule()
+        {
+            @Override
+            protected void configure()
+            {
+                final LoadingCache<ReleaseGraphKey, byte[]> cache =
+                    CacheBuilder.newBuilder().maximumSize( config.getReleaseGraphCacheSize() ).build( new ReleaseGraphCacheLoader() );
+                bind( new TypeLiteral<LoadingCache<ReleaseGraphKey, byte[]>>()
+                {
+                } ).toInstance( cache );
+            }
+        } );
     }
 }

insight-brain-service/src/main/java/com/sonatype/insight/brain/releasegraph/ReleaseGraphHealthCheck.java
@@ -1,13 +1,18 @@
 package com.sonatype.insight.brain.releasegraph;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import com.google.common.cache.LoadingCache;
 import com.yammer.metrics.core.HealthCheck;
 
+@Named
 public class ReleaseGraphHealthCheck
     extends HealthCheck
 {
     private LoadingCache<ReleaseGraphKey, byte[]> cache;
 
+    @Inject
     public ReleaseGraphHealthCheck( LoadingCache<ReleaseGraphKey, byte[]> cache )
     {
         super( "Release Graph" );

insight-brain-service/src/main/java/com/sonatype/insight/brain/releasegraph/ReleaseGraphResource.java
@@ -3,6 +3,7 @@ package com.sonatype.insight.brain.releasegraph;
 import java.util.Date;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -20,6 +21,7 @@ import com.sonatype.insight.brain.service.InsightProxy;
 import com.sonatype.insight.brain.service.InsightWork;
 import com.sonatype.insight.error.HttpStatusCode;
 
+@Named
 @Path( "rest/report/{applicationId}/{scanId}/releaseGraph" )
 public class ReleaseGraphResource
 {
@@ -38,6 +40,7 @@ public class ReleaseGraphResource
     @Inject
     private CLMLicenseManager licenseManager;
 
+    @Inject
     public ReleaseGraphResource( LoadingCache<ReleaseGraphKey, byte[]> cache )
     {
         this.cache = cache;

insight-brain-service/src/main/java/com/sonatype/insight/brain/releasegraph/ReleaseGraphTask.java
@@ -2,15 +2,20 @@ package com.sonatype.insight.brain.releasegraph;
 
 import java.io.PrintWriter;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableMultimap;
 import com.yammer.dropwizard.tasks.Task;
 
+@Named
 public class ReleaseGraphTask
     extends Task
 {
     private LoadingCache<ReleaseGraphKey, byte[]> cache;
 
+    @Inject
     public ReleaseGraphTask( LoadingCache<ReleaseGraphKey, byte[]> cache )
     {
         super( "clearReleaseGraphCache" );

commit 6ab3eb3b265e205b69d63b72f68f194af4ebbb1b

Fixed order of DB initialization and DB access which got broken in d9880d0d481397a4b4d3016e4dc67677dfa45de4

insight-brain-service/src/main/java/com/sonatype/insight/brain/service/InsightBrainService.java
@@ -86,12 +86,6 @@ public class InsightBrainService
         env.enableJerseyFeature( ResourceConfig.FEATURE_CANONICALIZE_URI_PATH );
         env.enableJerseyFeature( ResourceConfig.FEATURE_NORMALIZE_URI );
 
-        File databaseDir = new File( config.getSonatypeWork(), "data" );
-        DatabaseConfig dmDatabaseConfig = getDatabaseConfig( databaseDir, "dm" );
-        DatamartProvider.init( dmDatabaseConfig );
-        DatabaseConfig odsDatabaseConfig = getDatabaseConfig( databaseDir, "ods" );
-        OperationalDataStoreProvider.init( odsDatabaseConfig );
-
         log.info( "Server base URL: {}", config.getBaseUrl() );
         log.debug( "Saas address: {}", config.getSaasAddress() );
     }
@@ -119,6 +113,13 @@ public class InsightBrainService
     @Override
     protected List<Module> modules( final InsightConfig config )
     {
+        // NOTE: The ReleaseGraphCacheLoader indirectly uses the ApplicationDAO so we better setup the DB before
+        File databaseDir = new File( config.getSonatypeWork(), "data" );
+        DatabaseConfig dmDatabaseConfig = getDatabaseConfig( databaseDir, "dm" );
+        DatamartProvider.init( dmDatabaseConfig );
+        DatabaseConfig odsDatabaseConfig = getDatabaseConfig( databaseDir, "ods" );
+        OperationalDataStoreProvider.init( odsDatabaseConfig );
+
         return Arrays.<Module> asList( new AbstractModule()
         {
             @Override
57c297cf0554c9807fbc9ed4b18dffa464f574c8
Justin Young <jyoung@sonatype.com>
2013-05-13 15:22:05 -0700

    Reworked Policy Editor tests to test is duplicate directive

--- insight-brain-service/src/test/resources/assets/policy/PolicyEditorSpec.js
+++ insight-brain-service/src/test/resources/assets/policy/PolicyEditorSpec.js
@@ -3,24 +3,46 @@ var clmBuildTimestamp = '';
 describe('PolicyEditor', function() {
 	var sampleData = [{"id":"c2e1bf404e6d4f5d9458069a04a5cf11","name":"Policy1","enabled":true,"threatLevel":8,"constraints":[{"id":"f52c8ce2958743d5b5e10b176bfce67b","name":"Constraint1","enabled":true,"operator":"OR","conditions":[{"conditionTypeId":"AgeInDays","operator":"older than","value":"365"}]}],"actions":{"procure":[{"actionTypeId":"fail","target":null}],"develop":[{"actionTypeId":"warn","target":null}],"build":[],"stage-release":[{"actionTypeId":"fail","target":null}],"release":[],"operate":[]}}],
 		sampleActions = { build : [ { actionTypeId : 'notify', target : 'test@example.org' } ], develop : [], operate : [], procure : [{ actionTypeId : 'fail' }], release : [], 'stage-release' : []};
-
+	
 	function getController(controllerName) {
 		var controller = null,
-			scope = null;
+			scope = null,
+			compile = null,
+			sniffer = null;
 
-		inject(function ($rootScope, $controller, $httpBackend) {
+		inject(function ($rootScope, $controller, $httpBackend, $compile, $sniffer) {
 			scope = $rootScope.$new();
 			controller = $controller(controllerName, {$scope: scope});
+			compile = $compile;
+			sniffer = $sniffer;
 			$httpBackend.flush();
 		});
-		return { controller : controller, scope : scope };
+		
+		return { controller : controller, scope : scope, compile : compile, sniffer: sniffer };
 	}
 
 	function getPolicyEditorController(policyResponseData) {
 		expectActionRequests();
 		expectPolicyRequest(policyResponseData || sampleData);
+		
+		var modulePackage = getController('PolicyEditorController');
+		
+		modulePackage.isSaveDisabled = function () { return modulePackage.scope.policyEditor.$invalid || !(modulePackage.scope.state.currentPolicy && modulePackage.scope.state.currentPolicy.constraints.length > 0) };
+		
+		var nameInput = angular.element("<input type='text' id='policyName' name='policyName' placeholder='Enter Policy Name...' ng-model='state.currentPolicy.name' required is-Duplicate is-Duplicate-Array='policies' is-Duplicate-Id-Field='id' is-Duplicate-Case-Sensitive='false'>");
+		var body = angular.element('body').append("<form id='policyEditor' name='policyEditor'></form>").find('#policyEditor').append(nameInput);
 
-		return getController('PolicyEditorController');
+		modulePackage.setNameInput = function (val) {
+			nameInput.val(val);
+
+			var inputEvent = document.createEvent('HTMLEvents');
+			inputEvent.initEvent((modulePackage.sniffer.hasEvent('input')) ? 'input' : 'change', false, false);
+			nameInput[0].dispatchEvent(inputEvent);
+		};
+		
+		modulePackage.compile(body)(modulePackage.scope);
+		
+		return modulePackage;
 	}
 
 	function getConstraintEditorController() {
@@ -67,6 +89,10 @@ describe('PolicyEditor', function() {
 	} ]);
 
 	beforeEach(module('ApplicationId', 'PolicyEditor'));
+	
+	afterEach(function() {
+		angular.element('#policyEditor').remove();
+	});
 
 	it('Test Create New Policy', inject(function ($httpBackend, $routeParams) {
 		$routeParams.policyId = 'new';
@@ -78,13 +104,15 @@ describe('PolicyEditor', function() {
 		expect(policy).not.toBeUndefined();
 		expect(policy.name).toBeUndefined();
 		expect(policy.constraints).toEqual([]);
-		expect(controller.scope.isPolicyValid()).toEqual(false);
+		expect(controller.isSaveDisabled()).toEqual(true);
 
 		// Validation Test
-		policy.name = 'Sample Policy';
-		expect(controller.scope.isPolicyValid()).toEqual(false);
+		controller.setNameInput('Sample Policy');
+		expect(policy.name).toEqual('Sample Policy');
+		
+		expect(controller.isSaveDisabled()).toEqual(true);
 		policy.constraints.push({}); // Invalid constraint but PolicyEditor doesn't handle that validation
-		expect(controller.scope.isPolicyValid()).toEqual(true);
+		expect(controller.isSaveDisabled()).toEqual(false);
 
 		// Add an action
 		controller.scope.state.actions.procure.action = 'fail';
@@ -113,11 +141,11 @@ describe('PolicyEditor', function() {
 			policy = controller.scope.state.currentPolicy;
 
 		expect(policy).not.toBeUndefined();
-		expect(controller.scope.isPolicyValid()).toEqual(false);
+		expect(controller.isSaveDisabled()).toEqual(true);
 
 		policy.constraints.push({}); // Invalid constraint but PolicyEditor doesn't handle that validation
-		policy.name = 'Policy1';
-		expect(controller.scope.isPolicyValid()).toEqual(false);
+		controller.setNameInput('Policy1');
+		expect(controller.isSaveDisabled()).toEqual(true);
 	}));
 
 	it('Test Store Not Modified', inject(function ($routeParams) {
@@ -127,7 +155,7 @@ describe('PolicyEditor', function() {
 			policy = controller.scope.state.currentPolicy;
 
 		expect(policy).not.toBeUndefined();
-		expect(controller.scope.isPolicyValid()).toEqual(true);
+		expect(controller.isSaveDisabled()).toEqual(false);
 		policy.name = 'An Entirely New Policy Name';
 
 		expect(controller.scope.policies[0].name).toEqual('Policy1');

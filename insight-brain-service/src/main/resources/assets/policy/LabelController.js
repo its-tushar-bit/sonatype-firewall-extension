6760bfa5ec43bf4e1a10f29cad3f620327386f0e
Justin Young <jyoung@sonatype.com>
2013-06-12 13:24:45 -0400

    Fixed showing of error modal in label editor

--- insight-brain-service/src/main/resources/assets/policy/LabelController.js
+++ insight-brain-service/src/main/resources/assets/policy/LabelController.js
@@ -62,7 +62,7 @@
                 $('#deleteLabelModal').modal('hide');
             }).error(function () {
                 $('#deleteLabelModal').modal('hide');
-                $scope.showServerError.apply(this, arguments);
+                $scope.$broadcast('showServerError', arguments);
             });
         };
 

1c8351c64427e85f4486f0c96a3f1333ed88c591
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2013-06-17 23:27:34 +0200

    Copied sonatype-clm-scanner over from insight-scanner repository to break cyclic dependency between insight-scanner and insight-brain repos CLM-219 CLM-22

---
+++ sonatype-clm-scanner/src/main/java/com/sonatype/insight/scan/cli/RestClientFactory.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.scan.cli;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import javax.inject.Named;
+
+import com.sonatype.clm.dto.model.ScanReceipt;
+import com.sonatype.clm.dto.model.policy.PolicyEvaluationResult;
+import com.sonatype.clm.dto.model.policy.Stage;
+import com.sonatype.insight.brain.client.ConfigurationClient;
+import com.sonatype.insight.brain.client.PolicyClient;
+import com.sonatype.insight.brain.client.ScanClient;
+import com.sonatype.insight.client.utils.HttpClientUtils.Configuration;
+
+@Named
+public class RestClientFactory
+{
+
+    public RestClient newRestClient( Configuration config )
+    {
+        return new RestClient( config );
+    }
+
+    public static class RestClient
+    {
+
+        private final Configuration config;
+
+        RestClient( Configuration config )
+        {
+            this.config = config;
+        }
+
+        public Map<String, String> getApplications()
+            throws IOException
+        {
+            return new ConfigurationClient( config ).getApplicationIdNameMap();
+        }
+
+        public ScanReceipt uploadScan( String appId, File scanFile )
+            throws IOException
+        {
+            return new ScanClient( config, appId ).uploadCiScan( scanFile );
+        }
+
+        public PolicyEvaluationResult evaluatePolicy( String appId, String scanId, String stageId )
+            throws IOException
+        {
+            return new PolicyClient( config, appId ).evaluate( scanId, new Stage( stageId ) );
+        }
+
+    }
+
+}

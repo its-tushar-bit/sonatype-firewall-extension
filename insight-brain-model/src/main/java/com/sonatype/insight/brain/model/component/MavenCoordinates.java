2f2ab58be1577ca7bd702c0b77ce899341d90dcd
Jamie Whitehouse <basil.whitehouse@gmail.com>
2013-06-18 10:00:30 -0400

    New resource oriented SaaS client
    Initial use is to access the SaaS resource for ComponentSummary
    information.
    
    CLM-162 CLM-200

---
+++ insight-brain-model/src/main/java/com/sonatype/insight/brain/model/component/MavenCoordinates.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.model.component;
+
+/**
+ * Identity of a component in the Maven coordinate system.
+ * 
+ * Refer to http://maven.apache.org/pom.html#Maven_Coordinates
+ */
+public class MavenCoordinates
+{
+    private String groupId;
+
+    private String artifactId;
+
+    private String version;
+
+    private String extension;
+
+    private String classifier;
+
+    public MavenCoordinates( String groupId, String artifactId, String version, String extension, String classifier )
+    {
+        this.groupId = groupId;
+        this.artifactId = artifactId;
+        this.version = version;
+        this.extension = extension;
+        this.classifier = classifier;
+    }
+    
+    /**
+     * Creates a new copy of the given coordinate.
+     */
+    public static MavenCoordinates copy( MavenCoordinates coordinate )
+    {
+        return new MavenCoordinates( coordinate.getGroupId(), coordinate.getArtifactId(), coordinate.getVersion(), 
+              coordinate.getExtension(), coordinate.getClassifier() );
+    }
+
+    public String getGroupId()
+    {
+        return groupId;
+    }
+
+    public void setGroupId( String groupId )
+    {
+        this.groupId = groupId;
+    }
+
+    public String getArtifactId()
+    {
+        return artifactId;
+    }
+
+    public void setArtifactId( String artifactId )
+    {
+        this.artifactId = artifactId;
+    }
+
+    public String getVersion()
+    {
+        return version;
+    }
+
+    public void setVersion( String version )
+    {
+        this.version = version;
+    }
+
+    public String getExtension()
+    {
+        return extension;
+    }
+
+    public void setExtension( String extension )
+    {
+        if ( extension != null && extension.trim().isEmpty() )
+        {
+            extension = null;
+        }
+        this.extension = extension;
+    }
+
+    public String getClassifier()
+    {
+        return classifier;
+    }
+
+    public void setClassifier( String classifier )
+    {
+        if ( classifier != null && classifier.trim().isEmpty() )
+        {
+            classifier = null;
+        }
+        this.classifier = classifier;
+    }
+
+    public String getGAVECString()
+    {
+        return groupId + ':' + artifactId + ':' + version + ':' + extension + ':' + classifier;
+    }
+}

fb7f0887dac74fc2dbafb14bdde8b91de31d8455
Damian Bradicich <dbradicich@sonatype.com>
2013-06-25 11:43:03 -0400

    - put missing css back into cip resource loader - CLM-162 CLM-255

--- insight-brain-service/src/main/resources/assets/policy/js/cip-loader.js
+++ insight-brain-service/src/main/resources/assets/policy/js/cip-loader.js
@@ -24,7 +24,7 @@
 
     var head = $('head'),
         scripts = [ 'assets/bootstrap/bootstrap-datepicker.js', 'policy-assets/js/cip-label-editor.js', 'policy-assets/js/cip-policy-violations.js', 'policy-assets/js/cip-claim-component.js', 'assets/js/Hudson.js', 'assets/CLMLocation.js' ],
-        styles = [ 'policy-assets/css/cip-label-editor.css', 'policy-assets/css/cip-policy-violations.css', 'policy-assets/css/cip-claim-component.css' ];
+        styles = [ 'assets/bootstrap/datepicker.css', 'policy-assets/css/cip-label-editor.css', 'policy-assets/css/cip-policy-violations.css', 'policy-assets/css/cip-claim-component.css' ];
 
 	if (!window.angular) {
 		loadScript(null, 'assets/angular/angular-1.0.6.min.js', function () {

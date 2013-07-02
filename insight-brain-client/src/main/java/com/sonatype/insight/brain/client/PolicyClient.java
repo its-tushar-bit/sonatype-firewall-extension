f6565a38f7cd8ef8ff161bc578910b05c5df744c
Damian Bradicich <dbradicich@sonatype.com>
2013-07-02 14:30:57 -0400

    - policy client now using the new management ui url

--- insight-brain-client/src/main/java/com/sonatype/insight/brain/client/PolicyClient.java
+++ insight-brain-client/src/main/java/com/sonatype/insight/brain/client/PolicyClient.java
@@ -49,12 +49,12 @@ public class PolicyClient
         {
             // implicit redirect from initial top-level request to the actual management asset
             final HttpResponse redirect = new BasicHttpResponse( HttpVersion.HTTP_1_1, 302, null );
-            redirect.setHeader( HttpHeaders.LOCATION, "policy-assets/index.html?appId=" + appId );
+            redirect.setHeader( HttpHeaders.LOCATION, "assets/index.html#/management/application/" + appId + "/policies" );
             return result( redirect );
         }
 
         // workaround for DropWizard directory->index redirect bug
-        if ( path.contains( "-assets/" ) && path.endsWith( "/" ) )
+        if ( path.contains( "assets/" ) && path.endsWith( "/" ) )
         {
             return path( path, "index.html" ).get();
         }

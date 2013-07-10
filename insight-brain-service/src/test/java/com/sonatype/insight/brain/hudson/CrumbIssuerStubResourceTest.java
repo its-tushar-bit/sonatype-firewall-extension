8483091422e09b398e04130752dcd5af2524f339
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2013-07-10 14:13:01 +0200

    Added resource to handle requests for Hudson's XSRF API such that the server log remains free of errors regarding missing resource CLM-448

---
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/hudson/CrumbIssuerStubResourceTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.hudson;
+
+import org.junit.Test;
+
+import com.ning.http.client.Response;
+import com.sonatype.insight.brain.service.AbstractResourceTest;
+import com.sonatype.insight.test.RestAccess;
+
+public class CrumbIssuerStubResourceTest
+    extends AbstractResourceTest
+{
+
+    @Test
+    public void testGet()
+        throws Exception
+    {
+        uninstallLicense();
+        Response response = RestAccess.get( getRestBaseUrl() + CrumbIssuerStubResource.SERVICE_PATH );
+        assertResponseStatus( 404, response );
+    }
+
+}

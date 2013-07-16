its-tushar-bit/sonatype-firewall-extension

commit 7e92d307c13d328c8ee6c7638b97d409bcaf3068

Start moving tests - CLM-454 CLM-391

insight-brain-service/src/main/resources/assets/assets/components/policy-editor/PolicyEditor.js
@@ -159,14 +159,14 @@
 				if (angular.isUndefined($scope.policy.id)) {
 					changed = $scope.policy.constraints.length > 0 || $scope.policy.name;
 					if (!changed) {
-						angular.forEach(policyStore.serializeActions($scope.state.actions), function (actions, stage) {
+						angular.forEach(policyStore.serializeActions($scope.actions), function (actions, stage) {
 							changed = changed || actions.length > 0;
 						});
 					}
 				} else {
 					angular.forEach($scope.policies, function (policy, index) {
 						if (policy.id === $scope.policy.id) {
-							changed = $scope.policy.isDirty() || policyStore.isActionDirty($scope.policy, $scope.state.actions);
+							changed = $scope.policy.isDirty() || policyStore.isActionDirty($scope.policy, $scope.actions);
 						}
 					});
 				}
@@ -451,7 +451,7 @@
 		};
 	}]);
 
-	module.directive('inlinePolicyEditor', ['$dialog', 'Messages', function ($dialog, messages) {
+	module.directive('inlinePolicyCreator', ['$dialog', 'Messages', function ($dialog, messages) {
 		return {
 			restrict : 'A',
 			templateUrl : "../assets/components/policy-editor/policy-inline-editor.html",

insight-brain-service/src/main/resources/assets/policy/components/policy/policy.html
@@ -1,6 +1,6 @@
 <div id="policy">
 	<div class='add-section'>
-		<div inline-policy-editor='createPolicy()'></div>
+		<div inline-policy-creator='createPolicy()'></div>
 	</div>
 	<div class="content-section">
 		<div load-error='error' reload='doLoad()'></div>

commit 82962fbcdd06bfe602d489ad6d9c8d24a9c017f1

fix for naming breakage introduced in 7e92d30

insight-brain-service/src/main/resources/assets/assets/components/policy-editor/PolicyEditor.js
@@ -456,7 +456,7 @@
 			restrict : 'A',
 			templateUrl : "../assets/components/policy-editor/policy-inline-editor.html",
 			scope : {
-				createPolicy : '&inlinePolicyEditor'
+				createPolicy : '&inlinePolicyCreator'
 			},
 			link : function (scope, element, attrs) {
 				scope.click = function () {
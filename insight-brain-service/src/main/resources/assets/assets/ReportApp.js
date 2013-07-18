87a7cd07f826205741573c184abdc3f72da5afe3
Matthew Piggott <mpiggott@sonatype.com>
2013-07-18 15:58:38 -0400

    Create initial application report list - CLM-462 CLM-440

---
+++ insight-brain-service/src/main/resources/assets/assets/ReportApp.js
@@ -0,0 +1,39 @@
+/**
+ * @license Copyright (c) 2012-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+(function () {
+	"use strict";
+	var reportApp = angular.module('reportApp', ['DashboardModule', 'ReportList'],
+			['$stateProvider', '$routeProvider', '$urlRouterProvider', function ($stateProvider, $routeProvider, $urlRouterProvider) {
+				$routeProvider.when('', { redirectTo : '/reports' });
+			}]);
+}());
+
+(function () {
+	'use strict';
+
+	var reportModule = angular.module('ReportList', ['ui.compat', 'AngularCommon'], ['$stateProvider', function ($stateProvider) {
+		$stateProvider.state('reports', {
+			url : '/reports',
+			templateUrl : '../assets/components/report-list.html',
+			controller : 'ReportListController'
+		});
+	}]);
+
+	reportModule.controller('ReportListController', ['$scope', '$http', '$q', 'CLMLocations', function ($scope, $http, $q, clmLocations) {
+		$scope.doLoad = function () {
+			var promises = [$http.get(clmLocations.getActionStageUrl()), $http.get(clmLocations.getApplicationsUrl())];
+			$q.all(promises).then(function (results) {
+				$scope.stages = results[0].data;
+				$scope.applications = results[1].data;
+			}, function () {
+				// TODO 
+				window.alert('hi');
+			});
+		};
+		$scope.encodeURIComponent = window.encodeURIComponent;
+		$scope.doLoad();
+	}]);
+}());

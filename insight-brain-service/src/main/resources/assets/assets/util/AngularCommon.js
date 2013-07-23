2c35a26fef7bdc8fa4131dfa694c72f2ca6b60e1
Matthew Piggott <mpiggott@sonatype.com>
2013-07-23 14:09:53 -0400

    Simplify DOM, remove unnecessary non-selectable dropdown - CLM-391 CLM-456

--- insight-brain-service/src/main/resources/assets/assets/util/AngularCommon.js
+++ insight-brain-service/src/main/resources/assets/assets/util/AngularCommon.js
@@ -500,7 +500,7 @@
 			scope : {
 				selected : '='
 			},
-			template : '<span ng-show="!noSelect" class="threat-level-dropdown dropdown">' +
+			template : '<span class="threat-level-dropdown dropdown">' +
 					'<a class="btn dropdown-toggle threat-level-{{selected}}" data-toggle="dropdown" href="#">' +
 					'{{selected}} <span class="caret"></span>' +
 					'</a>' +
@@ -509,13 +509,8 @@
 						'<a ng-click="select(10 - $index)" class="threat-level-{{10 - $index}}">{{ threatLevel }}</a>' +
 						'</li>' +
 					'</ul>' +
-				'</span>' +
-				'<span ng-show="noSelect" class="threat-chiclet threat-level-{{selected}}">&nbsp</span>',
+				'</span>',
 			link : function (scope, element, attrs) {
-				// TODO Dark Blue for some usages
-			    if (attrs.noselect) {
-			        scope.noSelect = true;
-			    }
 				scope.threatLevels = [10, 9, 8, 7, 6, 5, 4, 3, 2, attrs.one || 1, attrs.zero || 0];
 				scope.select = function (threat) {
 					scope.selected = threat;

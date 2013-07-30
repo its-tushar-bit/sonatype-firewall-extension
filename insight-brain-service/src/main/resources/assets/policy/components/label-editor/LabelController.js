its-tushar-bit/sonatype-firewall-extension

commit 535839004dbd8b0dc73842bc010dcb77b1c5c10f

Backend service to provide applicable labels. Front end to display. Jasmine tests broken.

insight-brain-service/src/main/resources/assets/policy/components/label-editor/LabelController.js
@@ -44,8 +44,8 @@
     };
   }]);
 
-  labelModule.controller('LabelController', ['$scope', '$http', '$dialog', 'CLMAppLocations', 'Messages', 'CLMResource', 'LabelStore',
-                                             function ($scope, $http, $dialog, clmAppLocations, messages, clmResource, LabelStore) {
+  labelModule.controller('LabelController', ['$scope', '$http', '$dialog', '$q', 'CLMAppLocations', 'Messages', 'CLMResource', 'LabelStore',
+                                             function ($scope, $http, $dialog, $q, clmAppLocations, messages, clmResource, LabelStore) {
       function deselect() {
         if($scope.selectedLabel)
         {
@@ -62,14 +62,27 @@
 
       $scope.doLoad = function () {
         $scope.error = null;
-        LabelStore.get().then(function(labels){
-          $scope.labels = labels;
+        var promises = [LabelStore.get(), $http.get(clmAppLocations.getApplicableLabelsUrl(), {
+          params: { timestamp: new Date().getTime() }
+        })];
+
+        $q.all(promises).then(function(results){
+          $scope.applicableLabels = results[1].data.labelsByOwner;
+          angular.forEach($scope.applicableLabels, function (applicableLabel, index) {
+            applicableLabel.editable = index === 0;
+            if (index === 0) {
+              applicableLabel.labels = results[0];
+            }
+          });
         }, function(error){
           $scope.error = error;
         });
       };
 
-      $scope.editLabel = function (label) {
+      $scope.editLabel = function (isEditable, label) {
+        if (!isEditable) {
+          return;
+        }
         deselect();
         $scope.selectedLabel = angular.copy(label || labelTemplate);
         $scope.editorUrl = '../policy-assets/components/label-editor/label-editor.html?' + clmBuildTimestamp;

insight-brain-service/src/main/java/com/sonatype/insight/brain/label/LabelResource.java
@@ -5,6 +5,7 @@
  */
 package com.sonatype.insight.brain.label;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Named;
@@ -21,8 +22,15 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sonatype.insight.brain.dataaccess.ApplicationDAO;
+import com.sonatype.insight.brain.dataaccess.OrganizationDAO;
 import com.sonatype.insight.brain.dataaccess.label.LabelDAO;
 import com.sonatype.insight.brain.dataaccess.policy.PolicyDAO;
+import com.sonatype.insight.brain.model.Application;
+import com.sonatype.insight.brain.model.Organization;
 import com.sonatype.insight.brain.model.label.Label;
 import com.sonatype.insight.brain.model.policy.Condition;
 import com.sonatype.insight.brain.model.policy.Constraint;
@@ -40,6 +48,8 @@ public class LabelResource
     public static final String SERVICE_BASEPATH = "rest/label/";
 
     public static final String SERVICE_PATH = SERVICE_BASEPATH + "{ownerType: application|organization}/{ownerId}";
+
+    private static final Logger log = LoggerFactory.getLogger( LabelResource.class );
 
     @Context
     private InsightWork work;
@@ -59,6 +69,52 @@ public class LabelResource
         ownerId = IdUtils.getInternalOwnerId( ownerType, ownerId );
 
         return labelDAO.getByOwnerId( ownerId, inherit );
+    }
+
+    /**
+     * @since 1.7
+     */
+    @GET
+    @Produces( { MediaType.APPLICATION_JSON } )
+    @Path( "applicable" )
+    public ApplicableLabels getApplicableLabels( @PathParam( "ownerType" ) String ownerType,
+                                                       @PathParam( "ownerId" ) String ownerId )
+    {
+        log.debug( "Received request to get all applicable labels for {} id {}", ownerType, ownerId );
+
+        String internalOwnerId = IdUtils.getInternalOwnerId( ownerType, ownerId );
+
+        ApplicableLabels result = new ApplicableLabels();
+
+        result.labelsByOwner = new ArrayList<LabelsByOwner>();
+        String organizationId;
+        if ( "application".equals( ownerType ) )
+        {
+            Application application = new ApplicationDAO().getByIdNotNull( internalOwnerId );
+            LabelsByOwner labelsByOwner = new LabelsByOwner();
+            labelsByOwner.ownerId = application.getId();
+            labelsByOwner.ownerName = application.getName();
+            labelsByOwner.ownerType = "application";
+            labelsByOwner.labels = labelDAO.getByOwnerId( application.getId() );
+            result.labelsByOwner.add( labelsByOwner );
+            organizationId = application.getOrganizationId();
+        }
+        else
+        {
+            organizationId = internalOwnerId;
+        }
+        if ( organizationId != null )
+        {
+            Organization organization = new OrganizationDAO().getByIdNotNull( organizationId );
+            LabelsByOwner labelsByOwner = new LabelsByOwner();
+            labelsByOwner.ownerId = organization.getId();
+            labelsByOwner.ownerName = organization.getName();
+            labelsByOwner.ownerType = "organization";
+            labelsByOwner.labels = labelDAO.getByOwnerId( organization.getId() );
+            result.labelsByOwner.add( labelsByOwner );
+        }
+
+        return result;
     }
 
     /**
@@ -140,5 +196,21 @@ public class LabelResource
         }
 
         labelDAO.delete( label );
+    }
+
+    public static class ApplicableLabels
+    {
+        public List<LabelsByOwner> labelsByOwner;
+    }
+
+    public static class LabelsByOwner
+    {
+        public String ownerId;
+
+        public String ownerName;
+
+        public String ownerType;
+
+        public List<Label> labels;
     }
 }

insight-brain-service/src/main/java/com/sonatype/insight/brain/policy/evaluator/PolicyEvaluator.java
@@ -220,7 +220,7 @@ public class PolicyEvaluator
                                     ResourceType.DRL );
         if ( droolsKnowledgeBuilder.hasErrors() )
         {
-            throw new RuntimeException( "Failed to load the policies: " + droolsKnowledgeBuilder.getErrors().toString() );
+            throw new RuntimeException( "Failed to load the labels: " + droolsKnowledgeBuilder.getErrors().toString() );
         }
         final Collection<KnowledgePackage> droolsKnowledgePackages = droolsKnowledgeBuilder.getKnowledgePackages();
         final KnowledgeBase droolsKnowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();

insight-brain-service/src/main/resources/assets/assets/util/CLMAppLocation.js
@@ -31,6 +31,10 @@
 				return baseUrl.get() + '/rest/label/' + getServicePathWithId();
 			},
 
+      getApplicableLabelsUrl: function() {
+        return baseUrl.get() + '/rest/label/' + getServicePathWithId() + '/applicable';
+      },
+
 			getDeleteLabelsUrl: function (label) {
 				return baseUrl.get() + '/rest/label/' + getServicePathWithId() + '/' + encodeURIComponent(label.id);
 			},

insight-brain-service/src/main/resources/assets/policy/components/label-editor/labels.html
@@ -1,24 +1,43 @@
 <div>
   <div load-error='error' reload='doLoad()'></div>
-  <div class='content-container' style='text-align:center;width:370px;margin:25px auto' ng-show='labels == null && !error'>
+  <div class='content-container' style='text-align:center;width:370px;margin:25px auto' ng-show='applicableLabels == null && !error'>
     <h5><i class='icon-time'></i> Loading...</h5>
   </div>
-  <div ng-show='!error && labels != null'>
+  <div ng-show='!error && applicableLabels != null'>
     <div class='add-section'>
       <div inline-label-creator></div>
     </div>
-    <div class="content-section">
-      <div class="content-header">
-        <h4>{{selectedApplication.name}} Labels</h4>
-      </div>
-      <div class="label-list">
-        <div ng-repeat='label in labels | orderBy:"label"' style='display:inline-block;'  ng-click='editLabel(label)'>
-          <div class="labelContainer" ng-class="{active: selectedLabel.id == label.id}">
-            <span title="Edit {{label.label}} Label" class="clmLabel {{label.color}}Label"> {{label.label}} </span>
-            <span title="Delete {{label.label}} Label" class="icon-remove-circle deleteHoverTransparent" ng-click="deleteLabel(label)"></span></div>
+    <div id='{{applicableLabel.ownerId}}' ng-repeat='applicableLabel in applicableLabels' ng-show='applicableLabel.editable || applicableLabel.labels.length > 0'>
+      <div class="content-section">
+        <div class="content-header">
+          <h4>
+            <span ng-switch='applicableLabel.editable'>
+							<span ng-switch-when='true'>{{applicableLabel.ownerName}} Labels</span>
+							<span ng-switch-default>Labels Inherited from {{applicableLabel.ownerName}}</span>
+						</span>
+          </h4>
         </div>
-        <em ng-show='labels.length == 0'>No labels available</em></div>
-      <div ng-include='editorUrl' style="margin-left: 25px;"></div>
+        <div class="label-list">
+          <div ng-repeat='label in applicableLabel.labels | orderBy:"label"' style='display:inline-block;'  ng-click='editLabel(applicableLabel.editable, label)'>
+            <div class="labelContainer" ng-class="{active: selectedLabel.id == label.id}">
+              <span ng-switch='applicableLabel.editable'>
+                <span ng-switch-when='true'>
+                  <span title="Edit {{label.label}} Label" class="clmLabel {{label.color}}Label"> {{label.label}} </span>
+                  <span title="Delete {{label.label}} Label" class="icon-remove-circle deleteHoverTransparent" ng-click="deleteLabel(label)"></span>
+                </span>
+                <span ng-switch-default>
+                  <span class="clmLabel {{label.color}}Label"> {{label.label}} </span>
+                </span>
+              </span>
+            </div>
+          </div>
+          <em ng-show='labels.length == 0'>No labels available</em></div>
+        <span ng-switch='applicableLabel.editable'>
+          <span ng-switch-when='true'>
+            <div ng-include='editorUrl' style="margin-left: 25px;"></div>
+          </span>
+        </span>
+      </div>
     </div>
   </div>
   <div error-Modal></div>

insight-brain-service/src/test/java/com/sonatype/insight/brain/label/LabelResourceTest.java
@@ -14,6 +14,9 @@ import org.junit.Test;
 
 import com.ning.http.client.Response;
 import com.sonatype.clm.dto.model.policy.Action;
+import com.sonatype.insight.brain.dataaccess.ApplicationDAO;
+import com.sonatype.insight.brain.label.LabelResource.ApplicableLabels;
+import com.sonatype.insight.brain.label.LabelResource.LabelsByOwner;
 import com.sonatype.insight.brain.model.Application;
 import com.sonatype.insight.brain.model.Organization;
 import com.sonatype.insight.brain.model.label.Color;
@@ -33,6 +36,9 @@ import com.yammer.dropwizard.testing.JsonHelpers;
 public class LabelResourceTest
     extends AbstractResourceTest
 {
+    private static final String APP = "application";
+
+    private static final String ORG = "organization";
 
     @Test
     public void testDeleteLabel_UsedInPolicyCondition()
@@ -184,7 +190,7 @@ public class LabelResourceTest
         Organization organization = createOrganization( orgName );
 
         // Get all labels
-        Response response = RestAccess.get( getServiceURL( "organization", organization.getId() ) );
+        Response response = RestAccess.get( getServiceURL( ORG, organization.getId() ) );
         assertResponseStatus( 200, response );
         Label[] labels = JsonHelpers.fromJson( response.getResponseBody(), Label[].class );
         Assert.assertNotNull( labels );
@@ -193,13 +199,13 @@ public class LabelResourceTest
         // Add a label
         Label label = new Label();
         label.setLabel( "MyLabel" );
-        response = RestAccess.post( getServiceURL( "organization", organization.getId() ), JsonHelpers.asJson( label ) );
+        response = RestAccess.post( getServiceURL( ORG, organization.getId() ), JsonHelpers.asJson( label ) );
         assertResponseStatus( 200, response );
         label = JsonHelpers.fromJson( response.getResponseBody(), Label.class );
         assertLabel( organization.getId(), "MyLabel", null /* color */, label );
 
         // Get all labels
-        response = RestAccess.get( getServiceURL( "organization", organization.getId() ) );
+        response = RestAccess.get( getServiceURL( ORG, organization.getId() ) );
         assertResponseStatus( 200, response );
         labels = JsonHelpers.fromJson( response.getResponseBody(), Label[].class );
         Assert.assertNotNull( labels );
@@ -208,13 +214,13 @@ public class LabelResourceTest
 
         // Update a label
         label.setLabel( "MyUpdatedLabel" );
-        response = RestAccess.put( getServiceURL( "organization", organization.getId() ), JsonHelpers.asJson( label ) );
+        response = RestAccess.put( getServiceURL( ORG, organization.getId() ), JsonHelpers.asJson( label ) );
         assertResponseStatus( 200, response );
         label = JsonHelpers.fromJson( response.getResponseBody(), Label.class );
         assertLabel( organization.getId(), "MyUpdatedLabel", null /* color */, label );
 
         // Get all labels
-        response = RestAccess.get( getServiceURL( "organization", organization.getId() ) );
+        response = RestAccess.get( getServiceURL( ORG, organization.getId() ) );
         assertResponseStatus( 200, response );
         labels = JsonHelpers.fromJson( response.getResponseBody(), Label[].class );
         Assert.assertNotNull( labels );
@@ -222,15 +228,101 @@ public class LabelResourceTest
         assertLabel( organization.getId(), "MyUpdatedLabel", null /* color */, labels[0] );
 
         // Delete a label
-        response = RestAccess.delete( getServiceURL( "organization", organization.getId() ) + "/" + label.getId() );
+        response = RestAccess.delete( getServiceURL( ORG, organization.getId() ) + "/" + label.getId() );
         assertResponseStatus( 204, response );
 
         // Get all labels
-        response = RestAccess.get( getServiceURL( "organization", organization.getId() ) );
+        response = RestAccess.get( getServiceURL( ORG, organization.getId() ) );
         assertResponseStatus( 200, response );
         labels = JsonHelpers.fromJson( response.getResponseBody(), Label[].class );
         Assert.assertNotNull( labels );
         Assert.assertEquals( 0, labels.length );
+    }
+
+    @Test
+    public void testGetApplicablePolicies()
+        throws Exception
+    {
+        // Create an organization and an application
+        String orgName = "testGetApplicableLabelsOrg";
+        String orgId = createOrganization( orgName ).getId();
+        String appName = "testGetApplicableLabelsApp";
+        String appPublicId = appName;
+        Application app = new Application( appPublicId, appName, orgId );
+        ApplicationDAO appDAO = new ApplicationDAO();
+        appDAO.insert( app );
+        applicationsToDelete.add( app );
+        String appId = app.getId();
+
+        // Verify the applicable labels for the application
+        Response response = RestAccess.get( getServiceURL( APP, appPublicId ) + "/applicable" );
+        assertResponseStatus( 200, response );
+        assertResponseStatus( 200, response );
+        ApplicableLabels applicableLabels = JsonHelpers.fromJson( response.getResponseBody(), ApplicableLabels.class );
+        Assert.assertNotNull( applicableLabels );
+        Assert.assertEquals( 2, applicableLabels.labelsByOwner.size() );
+        assertLabelsByOwner( appId, appName, "application", 0, applicableLabels.labelsByOwner.get( 0 ) );
+        assertLabelsByOwner( orgId, orgName, "organization", 0, applicableLabels.labelsByOwner.get( 1 ) );
+
+        // Verify the applicable labels for the organization
+        response = RestAccess.get( getServiceURL( ORG, orgId ) + "/applicable" );
+        assertResponseStatus( 200, response );
+        applicableLabels = JsonHelpers.fromJson( response.getResponseBody(), ApplicableLabels.class );
+        Assert.assertNotNull( applicableLabels );
+        Assert.assertEquals( 1, applicableLabels.labelsByOwner.size() );
+        assertLabelsByOwner( orgId, orgName, "organization", 0, applicableLabels.labelsByOwner.get( 0 ) );
+
+        // Create a label for the application
+        Label appLabel = new Label();
+        appLabel.setLabel( "testGetApplicableLabels_App_label" );
+        response = RestAccess.post( getServiceURL( APP, appPublicId ), JsonHelpers.asJson( appLabel ) );
+        assertResponseStatus( 200, response );
+        appLabel = JsonHelpers.fromJson( response.getResponseBody(), Label.class );
+
+        // Verify the applicable labels for the application
+        response = RestAccess.get( getServiceURL( APP, appPublicId ) + "/applicable" );
+        assertResponseStatus( 200, response );
+        applicableLabels = JsonHelpers.fromJson( response.getResponseBody(), ApplicableLabels.class );
+        Assert.assertNotNull( applicableLabels );
+        Assert.assertEquals( 2, applicableLabels.labelsByOwner.size() );
+        assertLabelsByOwner( appId, appName, "application", 1, applicableLabels.labelsByOwner.get( 0 ) );
+        assertLabelsByOwner( orgId, orgName, "organization", 0, applicableLabels.labelsByOwner.get( 1 ) );
+        Assert.assertEquals( appLabel.getId(), applicableLabels.labelsByOwner.get( 0 ).labels.get( 0 ).getId() );
+
+        // Verify the applicable labels for the organization
+        response = RestAccess.get( getServiceURL( ORG, orgId ) + "/applicable" );
+        assertResponseStatus( 200, response );
+        applicableLabels = JsonHelpers.fromJson( response.getResponseBody(), ApplicableLabels.class );
+        Assert.assertNotNull( applicableLabels );
+        Assert.assertEquals( 1, applicableLabels.labelsByOwner.size() );
+        assertLabelsByOwner( orgId, orgName, "organization", 0, applicableLabels.labelsByOwner.get( 0 ) );
+
+        // Create a label for the organization
+        Label orgLabel = new Label();
+        orgLabel.setLabel( "testGetApplicableLabels_Org_label" );
+        response = RestAccess.post( getServiceURL( ORG, orgId ), JsonHelpers.asJson( orgLabel ) );
+        assertResponseStatus( 200, response );
+        orgLabel = JsonHelpers.fromJson( response.getResponseBody(), Label.class );
+
+        // Verify the applicable labels for the application
+        response = RestAccess.get( getServiceURL( APP, appPublicId ) + "/applicable" );
+        assertResponseStatus( 200, response );
+        applicableLabels = JsonHelpers.fromJson( response.getResponseBody(), ApplicableLabels.class );
+        Assert.assertNotNull( applicableLabels );
+        Assert.assertEquals( 2, applicableLabels.labelsByOwner.size() );
+        assertLabelsByOwner( appId, appName, "application", 1, applicableLabels.labelsByOwner.get( 0 ) );
+        assertLabelsByOwner( orgId, orgName, "organization", 1, applicableLabels.labelsByOwner.get( 1 ) );
+        Assert.assertEquals( appLabel.getId(), applicableLabels.labelsByOwner.get( 0 ).labels.get( 0 ).getId() );
+        Assert.assertEquals( orgLabel.getId(), applicableLabels.labelsByOwner.get( 1 ).labels.get( 0 ).getId() );
+
+        // Verify the applicable labels for the organization
+        response = RestAccess.get( getServiceURL( ORG, orgId ) + "/applicable" );
+        assertResponseStatus( 200, response );
+        applicableLabels = JsonHelpers.fromJson( response.getResponseBody(), ApplicableLabels.class );
+        Assert.assertNotNull( applicableLabels );
+        Assert.assertEquals( 1, applicableLabels.labelsByOwner.size() );
+        assertLabelsByOwner( orgId, orgName, "organization", 1, applicableLabels.labelsByOwner.get( 0 ) );
+        Assert.assertEquals( orgLabel.getId(), applicableLabels.labelsByOwner.get( 0 ).labels.get( 0 ).getId() );
     }
 
     private void assertLabel( String ownerId, String label, Color color, Label actual )
@@ -241,9 +333,18 @@ public class LabelResourceTest
         Assert.assertEquals( color, actual.getColor() );
     }
 
+    private void assertLabelsByOwner( String ownerId, String ownerName, String ownerType, int labelsCount,
+                                        LabelsByOwner actual )
+    {
+        Assert.assertEquals( ownerId, actual.ownerId );
+        Assert.assertEquals( ownerName, actual.ownerName );
+        Assert.assertEquals( ownerType, actual.ownerType );
+        Assert.assertEquals( labelsCount, actual.labels.size() );
+    }
+
     private String getServiceURLForApplication( final String appId )
     {
-        return getServiceURL( "application", appId );
+        return getServiceURL( APP, appId );
     }
 
     private String getServiceURL( final String ownerType, final String ownerId )

insight-brain-service/src/test/resources/assets/policy/LabelControllerSpec.js
@@ -4,6 +4,39 @@ describe('LabelController.js', function() {
   var LabelMockData = {
     getLabels : function(){
       return [angular.copy(labelTemplate)]
+    },
+    getApplicableLabels : function() {
+      return {
+        "labelsByOwner":[
+          {
+            "ownerId":"appownerid",
+            "ownerName":"appname",
+            "ownerType":"application",
+            "labels":[
+              {
+                "id":"applabelid",
+                "ownerId":"appownerid",
+                "label":"AppLabel",
+                "labelLowercase":"applabel",
+                "color":"red"
+              }
+            ]
+          },
+          {
+            "ownerId":"orgownerid",
+            "ownerName":"orgname",
+            "ownerType":"organization",
+            "labels":[
+              {
+                "id":"orglabelid",
+                "ownerId":"orgownerid",
+                "label":"OrgLabel",
+                "labelLowercase":"orglabel",
+                "color":"red"
+              }
+            ]
+          }]
+      };
     }
   }
   //var LabelStore = {
@@ -95,7 +128,8 @@ describe('LabelController.js', function() {
     beforeEach(inject(function ($rootScope, $controller, $httpBackend, CLMAppLocations, $state) {
       scope = testScope;
       scope.alerts = [];
-      $httpBackend.whenGET(SpecUtil.toRegExp(CLMAppLocations.getLabelsUrl())).respond(LabelMockData.getLabels());
+      $httpBackend.whenGET(SpecUtil.toRegExp(CLMAppLocations.getLabels())).respond(LabelMockData.getLabels());
+      $httpBackend.whenGET(SpecUtil.toRegExp(CLMAppLocations.getApplicableLabelsUrl())).respond(LabelMockData.getApplicableLabelsUrl());
       labelController = $controller('LabelController', {$scope: scope});
       labelEditController = $controller('LabelEditorController', {$scope: scope});
       $httpBackend.flush();

commit 7b80399fab8e1b7241fb515086f20477d6f31052

use tooltip-inner to enforce min-width - fix isDirty check broken by 535839004dbd8b0dc73842bc010dcb77b1c5c10f CLM-595 - UI for application labels

insight-brain-service/src/main/resources/assets/policy/components/label-editor/LabelController.js
@@ -133,17 +133,18 @@
             return valid && !$scope.submitActive && label != null && label.label;
         };
 
-		$scope.$on('pageChangeStarted', function (event) {
-		    if ($scope.selectedLabel && $scope.selectedLabel.id) {
-		        angular.forEach($scope.labels, function (candidate) {
-		            if (candidate.id === $scope.selectedLabel.id && !angular.equals(candidate, $scope.selectedLabel)) {
-		                event.preventDefault();
-		            }
-		        });
-		    } else if ($scope.selectedLabel && $scope.selectedLabel.label) {
-		        event.preventDefault();
-		    }
-		});
+        $scope.$on('pageChangeStarted', function (event) {
+            if ($scope.applicableLabel && $scope.selectedLabel && $scope.selectedLabel.id) {
+                angular.forEach($scope.applicableLabel.labels, function (candidate) {
+                    if (candidate.isDirty()) {
+                        event.preventDefault();
+                      return false;
+                    }
+                });
+            } else if ($scope.selectedLabel && $scope.selectedLabel.label) {
+                event.preventDefault();
+            }
+        });
 
       $scope.click = function () {
         if (!$scope.selectedLabel) {

insight-brain-service/src/main/wro4j/policy/labels.css
@@ -115,4 +115,4 @@ form[name="labelEditor"] .clmLabel {
 	padding-right: 25px
 }
 
-.tooltip {min-width:100px;}
+.tooltip-inner {min-width:100px;}
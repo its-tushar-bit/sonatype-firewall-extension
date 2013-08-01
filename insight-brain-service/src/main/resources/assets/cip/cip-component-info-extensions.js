a189defaf2d70974b2644f5433aba9add42f91c4
Damian Bradicich <dbradicich@sonatype.com>
2013-08-01 18:14:37 -0400

    - reworked the layout of the extension class, so things are only loaded when necessary - updated to angular-mocks-1.0.7.js - works manually just fine, test having issues (and not currently running) - CLM-437 CLM-578

--- insight-brain-service/src/main/resources/assets/cip/cip-component-info-extensions.js
+++ insight-brain-service/src/main/resources/assets/cip/cip-component-info-extensions.js
@@ -7,16 +7,37 @@
 /* global angular, $, window, CLM, setTimeout, applicationId */
 (function() {
   'use strict';
-  var compInfoScope = null;
 
-  function loadAngular() {
+  function processResults($scope, results) {
+    $scope.waiverLoading = false;
+    $scope.waivers = results[0].data;
+
+    // process the results to add policy name and owner name
+    $.each($scope.waivers, function(waiverIndex, waiver) {
+      var date = new Date(waiver.createTime);
+      waiver.createTimeStr = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+      $.each(results[1].data.policiesByOwner, function(policyOwnerIndex, policyOwner) {
+        $.each(policyOwner.policies, function(policyIndex, policy) {
+          if (waiver.policyId === policy.id) {
+            waiver.policyName = policy.name;
+            waiver.ownerName = policyOwner.ownerName;
+            return false;
+          }
+        });
+        if (waiver.policyName) { return false; }
+      });
+    });
+  }
+
+  function buildAngularApp() {
     angular.module('ComponentInfo', []).controller(
             'ComponentInfoController',
             [
                 '$scope',
                 '$http',
                 '$q',
-                function($scope, $http, $q) {
+                '$timeout',
+                function($scope, $http, $q, $timeout) {
                   $scope.viewWaivers = function() {
                     // get the waivers and policies from the server
                     var policyWaiverPromise = $http.get(CLM.path + 'rest/policyWaiver/application/'
@@ -24,68 +45,31 @@
                             + 'rest/policy/application/' + $scope.applicationId + '/applicable');
 
                     $q.all([policyWaiverPromise, policyPromise]).then(function(results) {
-                      $scope.waiverLoading = false;
-                      $scope.waivers = results[0].data;
-
-                      // process the results to add policy name and owner name
-                      $.each($scope.waivers, function(waiverIndex, waiver) {
-                        var date = new Date(waiver.createTime);
-                        waiver.createTimeStr = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
-                        $.each(results[1].data.policiesByOwner, function(policyOwnerIndex, policyOwner) {
-                          $.each(policyOwner.policies, function(policyIndex, policy) {
-                            if (waiver.policyId === policy.id) {
-                              waiver.policyName = policy.name;
-                              waiver.ownerName = policyOwner.ownerName;
-                              return false;
-                            }
-                          });
-                          if (waiver.policyName) { return false; }
-                        });
-                      });
-
+                      processResults($scope, results);
                     }, function() {
                       $scope.waiverLoading = false;
                       $scope.appError = arguments[0];
                     });
-                  }
-
-                  compInfoScope = $scope;
+                  };
+                  $scope.close = function() {
+                    $('#componentExistingWaiverModal').modal('hide');
+                    // simply giving the animation enough time to run
+                    $timeout(function() {
+                      $('#componentExistingWaiverModal').remove();
+                    }, 500);
+                  };
                 }]);
+  }
 
-    $('body')
-            .append(
-                    '<div id="componentExistingWaiverModal" ng-controller="ComponentInfoController" class="modal hide fade">'
-                            + '<div class="modal-header">'
-                            + '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>'
-                            + '<h3>Component Waivers</h3>'
-                            + '</div>'
-                            + '<div class="modal-body" ng-show="waiversLoading">'
-                            + '<span>Loading data...</span>'
-                            + '</div>'
-                            + '<div class="modal-body" ng-show="!waiversLoading">'
-                            + '<table class="table table-condensed">'
-                            + '<thead>'
-                            + '<tr>'
-                            + '<th>Policy</th>'
-                            + '<th>Created</th>'
-                            + '<th>Owner</th>'
-                            + '<th>Comment</th>'
-                            + '</tr>'
-                            + '</thead>'
-                            + '<tr ng-repeat="waiver in waivers">'
-                            + '<td>{{waiver.policyName}}</td>'
-                            + '<td>{{waiver.createTimeStr}}</td>'
-                            + '<td>{{waiver.ownerName}}</td>'
-                            + '<td>{{waiver.comment}}</td>'
-                            + '</tr>'
-                            + '</table>'
-                            + '</div>'
-                            + '<div class="modal-footer">'
-                            + '<span class="alert alert-error" ng-show="appError" style="padding-right:200px;">{{appError}}</span>'
-                            + '<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'
-                            + '</div>' + '</div>');
-
+  function startAngularApp(hash, appId) {
     angular.bootstrap($('#componentExistingWaiverModal')[0], ['ComponentInfo']);
+    var scope = angular.element('#componentExistingWaiverModal').scope();
+
+    scope.$apply(function() {
+      scope.hash = hash;
+      scope.applicationId = appId;
+      scope.viewWaivers();
+    });
   }
 
   function panelLoadHandler(event, data) {
@@ -96,16 +80,53 @@
 
     // when the button is clicked, get data in the scope and $apply since we are
     // outside of angular
-    $('button[data-target="#componentExistingWaiverModal"]').bind('click', function() {
-      compInfoScope.hash = data.gav.hash;
-      compInfoScope.applicationId = applicationId;
-      compInfoScope.viewWaivers();
-      compInfoScope.$apply();
-    });
+    $('button[data-target="#componentExistingWaiverModal"]')
+            .bind(
+                    'click',
+                    function() {
+                      $('body')
+                              .append(
+                                      '<div id="componentExistingWaiverModal" ng-controller="ComponentInfoController" data-keyboard="false" data-backdrop="static" class="modal hide fade">'
+                                              + '<div class="modal-header">'
+                                              + '<button type="button" class="close" ng-click="close()" aria-hidden="true">&times;</button>'
+                                              + '<h3>Component Waivers</h3>'
+                                              + '</div>'
+                                              + '<div class="modal-body" ng-show="waiversLoading">'
+                                              + '<span>Loading data...</span>'
+                                              + '</div>'
+                                              + '<div class="modal-body" ng-show="!waiversLoading">'
+                                              + '<table class="table table-condensed">'
+                                              + '<thead>'
+                                              + '<tr>'
+                                              + '<th>Policy</th>'
+                                              + '<th>Created</th>'
+                                              + '<th>Owner</th>'
+                                              + '<th>Comment</th>'
+                                              + '</tr>'
+                                              + '</thead>'
+                                              + '<tr ng-repeat="waiver in waivers">'
+                                              + '<td>{{waiver.policyName}}</td>'
+                                              + '<td>{{waiver.createTimeStr}}</td>'
+                                              + '<td>{{waiver.ownerName}}</td>'
+                                              + '<td>{{waiver.comment}}</td>'
+                                              + '</tr>'
+                                              + '</table>'
+                                              + '</div>'
+                                              + '<div class="modal-footer">'
+                                              + '<span class="alert alert-error" ng-show="appError" style="padding-right:200px;">{{appError}}</span>'
+                                              + '<button type="button" class="btn btn-primary" ng-click="close()">Close</button>'
+                                              + '</div>' + '</div>');
+
+                      startAngularApp(data.gav.hash, applicationId);
+                    });
   }
 
-  $(document).ready(function(){
-    loadAngular();
+  $(document).ready(function() {
+    if (angular) {
+      buildAngularApp(); 
+    } else {
+      setTimeout(buildAngularApp,100);
+    }    
     // setup our stuff when the panel is being built
     $(document).bind('artifactInfoPanelLoading', panelLoadHandler);
   });
--- insight-brain-service/src/test/resources/assets/policy/cip-component-info-extensions-spec.js
+++ insight-brain-service/src/test/resources/assets/policy/cip-component-info-extensions-spec.js
@@ -3,22 +3,22 @@ var applicationId = 'appId', CLM = {
   path: '../brain/'
 };
 describe('CIP Component info extensions tests', function() {
-  var $http, $scope, controller;
-
+  var $scope;
+  
   angular.module('Hudson', []).factory('hudson', ['$http', function($http) {
     return $http;
   }]);
-
+  
   beforeEach(module('ComponentInfo'));
 
-  beforeEach(inject(function($rootScope, $compile, $controller) {
-    var node = $("<table id='infoPanelArtifactTable'><tr></tr></table>");
-    node.appendTo('body');
+  beforeEach(inject(function($rootScope, $compile, $controller, $httpBackend) {
     $scope = $rootScope.$new();
-    $compile(node)($scope);
-    controller = $controller('ComponentInfoController', {
+    $controller('ComponentInfoController', {
       $scope: $scope
     });
+    var node = $("<table id='infoPanelArtifactTable'><tr></tr></table>");
+    node.appendTo('body');
+    $compile(node)($scope);
   }));
 
   afterEach(function() {
@@ -28,9 +28,48 @@ describe('CIP Component info extensions tests', function() {
 
   afterEach(inject(function($httpBackend) {
     $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
   }));
 
-  xit('Validate button and modal injected', inject(function($httpBackend, $controller) {
+  //TODO: commented out because weird stuff is happening, the http requests aren't getting shoveled
+  //through the mock httpBackend...
+  xit('Validate button and modal injected', inject(function($httpBackend) {
+    $httpBackend.expectGET('../brain/rest/policyWaiver/application/appId/component/1234').respond([{
+      id: "id",
+      hash: "1234",
+      policyId: "policyId",
+      constraintId: null,
+      ownerId: "ownerId",
+      comment: "some comment",
+      createTime: 1375366539817
+    }]);
+    $httpBackend.expectGET('../brain/rest/policy/application/appId/applicable').respond({
+      "policiesByOwner": [{
+        "ownerId": "ownerId",
+        "ownerName": "ownerName",
+        "ownerType": "application",
+        "policies": [{
+          "id": "policyId",
+          "name": "policyName",
+          "ownerId": "ownerId",
+          "enabled": true,
+          "threatLevel": 5,
+          "constraints": [{
+            "id": "constraintId",
+            "name": "constraintName",
+            "enabled": true,
+            "operator": "AND",
+            "conditions": [{
+              "conditionTypeId": "AgeInDays",
+              "operator": "older than",
+              "value": "1825"
+            }]
+          }],
+          "actions": {}
+        }]
+      }]
+    });
+
     $.event.trigger("artifactInfoPanelLoading", {
       gav: {
         hash: '1234'
@@ -39,14 +78,23 @@ describe('CIP Component info extensions tests', function() {
 
     expect($('button[data-target="#componentExistingWaiverModal"]').length).toEqual(1);
 
-    expect($('#componentExistingWaiverModal').length).toEqual(1);
-
-    $httpBackend.expectGET(CLM.path + 'rest/policyWaiver/application/appId/component/1234').respond([]);
-    $httpBackend.expectGET(CLM.path + 'rest/policy/application/appId/applicable').respond([]);
+    expect($('#componentExistingWaiverModal').length).toEqual(0);
 
     $('button[data-target="#componentExistingWaiverModal"]').trigger('click');
 
+    $httpBackend.flush();
+
+    expect($('#componentExistingWaiverModal').length).toEqual(1);
     expect($scope.hash).toEqual('1234');
     expect($scope.applicationId).toEqual('appId');
+
+    var values = $('#componentExistingWaiverModal').find('td');
+
+    expect(values.length).toEqual(4);
+
+    expect(values[0].val()).toEqual('policyId');
+    expect(values[1].val()).toEqual('8/1/2013');
+    expect(values[2].val()).toEqual('test');
+    expect(values[3].val()).toEqual('asdfdsa');
   }));
 });

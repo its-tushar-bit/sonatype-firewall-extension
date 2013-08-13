b79648ee23afe8ee24dd4f57fe6f2f44ba7b798a
Damian Bradicich <dbradicich@sonatype.com>
2013-08-13 15:37:56 -0400

    - now using all of the new api for loading labels, adding and deleting - added seperate controllers (and a service to transfer data) for the add/remove modals - still one issue remaining where the add is not using the proper context when assigning to an org scope - also still need to update tests for new changes

--- insight-brain-service/src/main/resources/assets/cip/cip-label-editor.js
+++ insight-brain-service/src/main/resources/assets/cip/cip-label-editor.js
@@ -26,93 +26,164 @@
 		}
 	});
 
-	var labelsApp = angular.module('ComponentLabelEditor', []);
+	var labelsApp = angular.module('ComponentLabelEditor', ['CommonServices', 'Hudson']).service('CurrentLabelData', function() {
+	  var currentLabel = null;
+	  var currentError = null;
+	  return {
+	    get: function(){
+	      return currentLabel;
+	    },
+	    set: function(label){
+	      currentLabel = label;
+	    },
+	    getError: function(){
+	      return currentError;
+	    },
+	    setError: function(error){
+	      currentError = error;
+	    }
+	  }
+	});
+	
+	labelsApp.controller('LabelAddController', ['$scope', 'CurrentLabelData', 'ComponentLabelEditorGAV', 'hudson', 'Messages', function($scope, currentLabelData, componentLabelEditorGAV, hudson, messages){
+	  $scope.decline = function() {
+      $('#labelAssignScopeModal').modal('hide');
+    };
+    
+    $scope.accept = function() {
+      $scope.labelSaving = true;
+      $scope.labelAddError = null;
+      var parts = $scope.selectedOwner.split('$$');
+      hudson.post(CLM.path + 'rest/label/component/' + parts[1] + '/' + parts[0] + '/' + componentLabelEditorGAV.hash, currentLabelData.get()).success(function(responseData){
+        $scope.labelSaving = false;
+        $('#labelAssignScopeModal').modal('hide');
+      }).error(function(data, status, headersFn, config){
+        $scope.labelSaving = false;
+        $scope.labelAddError = messages.getHttpErrorMessage({ status: status,  data: data });
+      });
+    };
+    
+    $('#labelAssignScopeModal').on('shown',function(){
+        $scope.$apply(function(){
+          var label = currentLabelData.get();
+          $scope.selectedOwner = componentLabelEditorGAV.applicationId + '$$application';
+          //if label is owned by an app, simply do the add with no further input from user
+          $scope.labelOwners = [{
+            ownerId: componentLabelEditorGAV.applicationId,
+            ownerName: componentLabelEditorGAV.applicationId,
+            ownerType: 'application'
+          },{
+            ownerId: label.ownerId,
+            ownerName: label.ownerName,
+            ownerType: label.ownerType
+          }];  
+        });
+    });
+    
+    $("body > #labelAssignScopeModal").remove();
+    $("#labelAssignScopeModal").appendTo("body");
+	}]);
+	
+	labelsApp.controller('LabelRemoveController', ['$scope', '$http', 'CurrentLabelData', 'ComponentLabelEditorGAV', 'Messages', function($scope, $http, currentLabelData, componentLabelEditorGAV, messages){
+    $scope.decline = function() {
+      $('#labelRemoveModal').modal('hide');
+    };
+    
+    $scope.accept = function() {
+      $scope.labelDeleting = true;
+      $scope.labelRemoveError = null;
+      
+      var label = currentLabelData.get();
+      
+      $http['delete'](CLM.path + 'rest/label/component/' + label.ownerType + '/' + label.ownerId + '/' + componentLabelEditorGAV.hash + '/' + label.id).success(function(responseData){
+        $scope.labelDeleting = false;
+        $('#labelRemoveModal').modal('hide');
+      }).error(function(data, status, headersFn, config){
+        $scope.labelDeleting = false;
+        $scope.labelRemoveError = messages.getHttpErrorMessage({ status: status,  data: data });
+      });
+    };
+    
+    $("body > #labelRemoveModal").remove();
+    $("#labelRemoveModal").appendTo("body");
+	}]);
 
-	labelsApp.controller('LabelsController', ['$http', '$scope', 'ComponentLabelEditorGAV', function ($http, $scope, componentLabelEditorGAV) {
-		var componentLabelsUrl = CLM.path + 'rest/label/component/application/' + componentLabelEditorGAV.applicationId + '/' + componentLabelEditorGAV.hash;
-
+	labelsApp.controller('LabelsController', ['$http', '$scope', 'CurrentLabelData', 'ComponentLabelEditorGAV', 'hudson', 'Messages', function ($http, $scope, currentLabelData, componentLabelEditorGAV, hudson, messages) {
 		function errorFn(data, status, headersFn, config) {
 			$scope.alerts.push({
 				type : 'error',
 				msg : messages.getHttpErrorMessage({ status: status,  data: data })
 			});
 		}
-
-		function persist(labelArr, color) {
-			$http.put(componentLabelsUrl, { labels : labelArr, color : color}).success(function (data) {
-				$scope.itemLabels = data;
-				$scope.reloadAppLabels(); // Only really necessary if someone adds a brand new label, and removes it in the same session
-			}).error(errorFn);
+		function flattenLabelList(data) {
+		  var list = [];
+      angular.forEach(data.labelsByOwner,function(labelOwner,labelOwnerIndex){
+        angular.forEach(labelOwner.labels,function(label, labelIndex){
+          label.ownerId = labelOwner.ownerId;
+          label.ownerType = labelOwner.ownerType;
+          label.ownerName = labelOwner.ownerName;
+          list.push(label);
+        });
+      });
+      return list;
 		}
 
-		$scope.alerts = [];
-
 		$scope.reloadLabels = function () {
-			$http.get(componentLabelsUrl, { params : { timestamp : new Date().getTime() } }).success(function (data) {
-				$scope.itemLabels = data;
+			$http.get(CLM.path + 'rest/label/component/application/' + componentLabelEditorGAV.applicationId + '/' + componentLabelEditorGAV.hash, { params : { timestamp : new Date().getTime() } }).success(function (data) {
+				$scope.itemLabels = flattenLabelList(data);
 			}).error(errorFn);
 		};
-		$scope.reloadLabels();
 
 		$scope.reloadAppLabels = function () {
-			$http.get(CLM.path + 'rest/label/application/' + componentLabelEditorGAV.applicationId, { params : { inherit : 'true', timestamp : new Date().getTime() } }).success(function (data) {
-				$scope.availableLabels = data;
+			$http.get(CLM.path + 'rest/label/application/' + componentLabelEditorGAV.applicationId + '/applicable', { params : { timestamp : new Date().getTime() } }).success(function (data) {
+			  $scope.availableLabels = flattenLabelList(data);
 			}).error(errorFn);
 		};
-		$scope.reloadAppLabels(); // do initial load
-
-		$scope.color = null;
-		$scope.colors = [{ color : null, text : '(Unset)' },
-						{ color : 'white', text : 'White' },
-						{ color : 'grey', text : 'Grey' },
-						{ color : 'black', text : 'Black' },
-						{ color : 'green', text : 'Green' },
-						{ color : 'yellow', text : 'Yellow' },
-						{ color : 'orange', text : 'Orange' },
-						{ color : 'red', text : 'Red' },
-						{ color : 'blue', text : 'Blue' }];
 
 		$scope.removeLabel = function (label) {
-			var updatedLabels = [];
-			angular.forEach($scope.itemLabels, function (candidate, key) {
-				if (candidate.label !== label.label) {
-					updatedLabels.push(candidate.label);
-				}
-			});
-			persist(updatedLabels);
+		  currentLabelData.set(label);
+		  $('#labelRemoveModal').modal('show');
 		};
 
-		$scope.addLabel = function (label) {
-			var updatedLabels = [],
-				duplicate = false;
-
-			angular.forEach($scope.itemLabels, function (candidate, key) {
-				if (candidate.label.toLowerCase() === label.label.toLowerCase()) {
-					duplicate = true;
-				}
-				updatedLabels.push(candidate.label);
-			});
-
-			if (!duplicate) {
-				updatedLabels.push(label.label);
-				persist(updatedLabels);
-			}
+		$scope.addLabel = function (label) { 
+		  if (label.ownerType === 'application') {
+		    hudson.post(CLM.path + 'rest/label/component/application/' + componentLabelEditorGAV.applicationId + '/' + componentLabelEditorGAV.hash, label).success(function(responseData){
+		      $scope.reloadLabels();
+		      $scope.reloadAppLabels();
+	      }).error(errorFn);  
+		  } else {
+		    currentLabelData.set(label);
+	      $('#labelAssignScopeModal').modal('show');  
+		  }
 		};
 
-		$scope.isWhite = function (label) {
-			return label.color === "green" || label.color === "black" || label.color === "orange" || label.color === "red" || label.color === "blue";
-		};
+    $scope.isWhite = function (label) {
+      return label.color === "green" || label.color === "black" || label.color === "orange" || label.color === "red" || label.color === "blue";
+    };
+    
+    $scope.isApplied = function (label) {
+      var duplicate = false;
+      angular.forEach($scope.itemLabels, function (candidate, key) {
+        duplicate = duplicate || (candidate.label === label.label);
+        return !duplicate;
+      });
+      return !duplicate;
+    };
 
-		$scope.isApplied = function (label) {
-			var duplicate = false;
-			angular.forEach($scope.itemLabels, function (candidate, key) {
-				duplicate = duplicate || (candidate.label === label.label);
-				return !duplicate;
-			});
-			return !duplicate;
-		};
+    $scope.alerts = [];
+    $scope.reloadLabels(); // do initial load
+    $scope.reloadAppLabels(); // do initial load
+    
+    $('#labelAssignScopeModal').on('hide',function(){
+      $scope.reloadLabels();
+      $scope.reloadAppLabels();
+    });
+    $('#labelRemoveModal').on('hide',function(){
+      $scope.reloadLabels();
+      $scope.reloadAppLabels();
+    });
 	}]);
-
+  
   /**
    * Enables tipsy tooltip on an element(with fixed parameters)
    */

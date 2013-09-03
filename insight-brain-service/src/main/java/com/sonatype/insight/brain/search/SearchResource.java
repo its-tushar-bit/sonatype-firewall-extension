c2665d1d6892e0a5f913a9dd48dc60b91594b0f6
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2013-09-03 19:48:02 +0200

    Introduced REST API to search for component across all applications CLM-667 CLM-779 Fixes #78

---
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/search/SearchResource.java
@@ -0,0 +1,226 @@
+/*
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.search;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.sonatype.clm.dto.model.policy.ComponentFact;
+import com.sonatype.clm.dto.model.policy.PolicyAlert;
+import com.sonatype.insight.brain.dataaccess.ApplicationDAO;
+import com.sonatype.insight.brain.model.Application;
+import com.sonatype.insight.brain.model.policy.PolicyEvaluation;
+import com.sonatype.insight.brain.model.policy.conditions.ArtifactCoordinate;
+import com.sonatype.insight.brain.model.policy.stages.StageTypes;
+import com.sonatype.insight.brain.policy.evaluator.PolicyEvaluationLog;
+import com.sonatype.insight.brain.policy.evaluator.PolicyEvaluationUtils;
+import com.sonatype.insight.brain.report.Report;
+import com.sonatype.insight.brain.report.ReportResource;
+import com.sonatype.insight.brain.service.BaseUrl;
+import com.sonatype.insight.brain.service.InsightWork;
+import com.sonatype.insight.error.exception.BadRequestException;
+import com.sonatype.insight.json.store.JsonUtils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.codehaus.plexus.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Enables end users to search for components within their applications. This REST API is exposed directly to users.
+ * 
+ * @since 1.7
+ */
+@Path(SearchResource.SERVICE_PATH)
+@Named
+public class SearchResource
+{
+  public static final String SERVICE_PATH = "api/v1/search/component";
+
+  private static final Logger log = LoggerFactory.getLogger(SearchResource.class);
+
+  private final InsightWork work;
+
+  private final BaseUrl baseUrl;
+
+  private final PolicyEvaluationUtils policyEvaluationUtils;
+
+  @Inject
+  public SearchResource(InsightWork work, BaseUrl baseUrl, PolicyEvaluationUtils policyEvaluationUtils) {
+    this.work = work;
+    this.baseUrl = baseUrl;
+    this.policyEvaluationUtils = policyEvaluationUtils;
+  }
+
+  /**
+   * Searches all currently registered applications for a component matching the given search criteria. A component can
+   * be searched for by its hash or its coordinates, the latter supporting wildcards like the equivalent policy
+   * condition. The mandatory stage parameter restricts which scans/reports of the applications are inspected for the
+   * component.
+   */
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public SearchResults searchComponent(@QueryParam("stageId") String stageId, @QueryParam("hash") String hash,
+      @QueryParam("groupId") String groupId, @QueryParam("artifactId") String artifactId,
+      @QueryParam("version") String version) throws IOException
+  {
+    if (StringUtils.isEmpty(stageId)) {
+      throw new BadRequestException("Stage has not been specified");
+    }
+    if (StageTypes.getById(stageId) == null) {
+      throw new BadRequestException("Invalid stage: " + stageId);
+    }
+
+    ArtifactCoordinate coords = null;
+    if (!StringUtils.isEmpty(groupId) || !StringUtils.isEmpty(artifactId) || !StringUtils.isEmpty(version)) {
+      coords = new ArtifactCoordinate(groupId, artifactId, version);
+    }
+    else if (StringUtils.isEmpty(hash)) {
+      throw new BadRequestException("Neither hash nor coordinates of component to search for have been specified");
+    }
+    if (!StringUtils.isEmpty(hash)) {
+      if (!hash.matches("[0-9a-fA-F]{20,40}")) {
+        throw new BadRequestException("Invalid hash: " + hash);
+      }
+      hash = hash.substring(0, 20);
+    }
+    else {
+      hash = null;
+    }
+
+    log.debug("Searching for component with hash={} and gav={}:{}:{}", hash, groupId, artifactId, version);
+
+    long start = System.currentTimeMillis();
+    SearchResults results = new SearchResults();
+    results.criteria.stageId = stageId;
+    results.criteria.hash = hash;
+    results.criteria.groupId = groupId;
+    results.criteria.artifactId = artifactId;
+    results.criteria.version = version;
+    String baseUrl = this.baseUrl.get();
+    for (Application app : new ApplicationDAO().getAll()) {
+      PolicyEvaluationLog evalLog = new PolicyEvaluationLog(work.getAuditDir(app.getId()));
+      PolicyEvaluation eval = evalLog.lastPrimaryByStage(stageId);
+      if (eval == null) {
+        continue;
+      }
+
+      File reportFile = ReportResource.getReport(work, app.getId(), eval.getScanId());
+      if (reportFile == null) {
+        log.error("Cannot search application {} for component, recent report does not exist", app.getName());
+        continue;
+      }
+      List<PolicyAlert> alerts = null;
+      JsonNode bomNode = JsonUtils.parse(Report.getEntry(reportFile, "bom.json").buf);
+      for (JsonNode componentNode : bomNode.get("aaData")) {
+        String h = componentNode.path("hash").asText();
+        if (hash != null && !hash.equalsIgnoreCase(h)) {
+          continue;
+        }
+
+        String g = componentNode.path("groupId").asText();
+        String a = componentNode.path("artifactId").asText();
+        String v = componentNode.path("version").asText();
+        if (coords != null && !coords.matches(g, a, v)) {
+          continue;
+        }
+
+        SearchResult result = new SearchResult();
+        result.applicationId = app.getPublicId();
+        result.applicationName = app.getName();
+        result.reportUrl = baseUrl + ReportResource.getReportPath(app.getPublicId(), eval.getScanId());
+        result.hash = h;
+        result.groupId = g;
+        result.artifactId = a;
+        result.version = v;
+        results.results.add(result);
+
+        if (alerts == null) {
+          alerts = policyEvaluationUtils.findPolicyAlerts(app.getId(), eval.getScanId());
+        }
+        result.threatLevel = null;
+        for (PolicyAlert alert : alerts) {
+          if (result.threatLevel != null && alert.getTrigger().getThreatLevel() <= result.threatLevel) {
+            continue;
+          }
+          for (ComponentFact fact : alert.getTrigger().getComponentFacts()) {
+            if (result.hash.equalsIgnoreCase(fact.getHash())) {
+              result.threatLevel = alert.getTrigger().getThreatLevel();
+              break;
+            }
+          }
+        }
+
+        if (hash != null) {
+          break;
+        }
+      }
+    }
+
+    log.debug("Searched for component with hash={} and gav={}:{}:{} in {} ms, got {} hits", hash, groupId, artifactId,
+        version, System.currentTimeMillis() - start, results.results.size());
+
+    return results;
+  }
+
+  /**
+   * Aggregates the results of a search.
+   */
+  public static class SearchResults
+  {
+    public SearchCriteria criteria = new SearchCriteria();
+
+    public List<SearchResult> results = new ArrayList<SearchResult>();
+  }
+
+  /**
+   * Describes a single component within an application that matches the search criteria.
+   */
+  public static class SearchResult
+  {
+    public String applicationId;
+
+    public String applicationName;
+
+    public String reportUrl;
+
+    public String hash;
+
+    public String groupId;
+
+    public String artifactId;
+
+    public String version;
+
+    public Integer threatLevel;
+  }
+
+  /**
+   * Describes the search criteria that was processed.
+   */
+  public static class SearchCriteria
+  {
+    public String stageId;
+
+    public String hash;
+
+    public String groupId;
+
+    public String artifactId;
+
+    public String version;
+  }
+}
---
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/search/SearchResourceTest.java
@@ -0,0 +1,262 @@
+/*
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.search;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Locale;
+import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import com.sonatype.clm.dto.model.policy.Stage;
+import com.sonatype.insight.brain.model.Application;
+import com.sonatype.insight.brain.policy.evaluator.PolicyEvaluationLog;
+import com.sonatype.insight.brain.search.SearchResource.SearchResult;
+import com.sonatype.insight.brain.search.SearchResource.SearchResults;
+import com.sonatype.insight.brain.service.AbstractResourceTest;
+import com.sonatype.insight.test.RestAccess;
+
+import com.ning.http.client.Response;
+import com.yammer.dropwizard.testing.JsonHelpers;
+import org.codehaus.plexus.util.FileUtils;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class SearchResourceTest
+    extends AbstractResourceTest
+{
+
+  private String getSearchUrl(String stageId, String hash) {
+    return getRestBaseUrl() + SearchResource.SERVICE_PATH + "?stageId=" + stageId + "&hash=" + hash;
+  }
+
+  private String getSearchUrl(String stageId, String groupId, String artifactId, String version) {
+    return getRestBaseUrl() + SearchResource.SERVICE_PATH + "?stageId=" + stageId + "&groupId=" + groupId
+        + "&artifactId=" + artifactId + "&version=" + version;
+  }
+
+  private String getSearchUrl(String stageId, String hash, String groupId, String artifactId, String version) {
+    return getRestBaseUrl() + SearchResource.SERVICE_PATH + "?stageId=" + stageId + "&hash=" + hash + "&groupId="
+        + groupId + "&artifactId=" + artifactId + "&version=" + version;
+  }
+
+  private File getReportCacheEntry(String appId, String scanId, String name) {
+    return new File(new File(brain.getReportDir(appId, scanId), "report.cache"), name);
+  }
+
+  private void createAppWithScan(String appPublicId, String stageId) throws Exception {
+    String scanId = UUID.randomUUID().toString().replace("-", "");
+    Application app = createApplication(appPublicId, appPublicId.toUpperCase(Locale.ENGLISH));
+    FileUtils.copyURLToFile(getClass().getResource("/SearchResourceTest/" + appPublicId + "/bom.json"),
+        getReportCacheEntry(app.getId(), scanId, "bom.json"));
+    FileUtils.copyURLToFile(getClass().getResource("/SearchResourceTest/" + appPublicId + "/policyalerts.json"),
+        getReportCacheEntry(app.getId(), scanId, "policyalerts.json"));
+    createReport(app.getId(), scanId);
+    PolicyEvaluationLog log = new PolicyEvaluationLog(brain.getAuditDir(app.getId()));
+    log.add(new Stage(stageId), scanId, "nobody", null);
+  }
+
+  private void createReport(String appId, String scanId) throws Exception {
+    ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(new File(brain.getReportDir(appId, scanId),
+        "report.zip")));
+    try {
+      zos.putNextEntry(new ZipEntry("index.html"));
+    }
+    finally {
+      zos.close();
+    }
+  }
+
+  private void assertSearchResult(SearchResult result, String appId, String appName, String hash, String groupId,
+      String artifactId, String version, Integer threatLevel) throws Exception
+  {
+    assertThat(result.applicationId, is(appId));
+    assertThat(result.applicationName, is(appName));
+    assertThat(result.reportUrl, is(notNullValue()));
+    assertResponseStatus(200, RestAccess.get(result.reportUrl));
+    assertThat(result.hash, is(hash));
+    assertThat(result.groupId, is(groupId));
+    assertThat(result.artifactId, is(artifactId));
+    assertThat(result.version, is(version));
+    assertThat(result.threatLevel, is(threatLevel));
+  }
+
+  @Test
+  public void testSearchComponent_MissingStageId() throws Exception {
+    Response response = RestAccess.get(getSearchUrl("", "12345678901234567890"));
+    assertResponseStatus(400, response);
+    assertThat(response.getResponseBody(), is("Stage has not been specified"));
+  }
+
+  @Test
+  public void testSearchComponent_InvalidStageId() throws Exception {
+    Response response = RestAccess.get(getSearchUrl("invalid", "12345678901234567890"));
+    assertResponseStatus(400, response);
+    assertThat(response.getResponseBody(), is("Invalid stage: invalid"));
+  }
+
+  @Test
+  public void testSearchComponent_MissingHashAndGav() throws Exception {
+    Response response = RestAccess.get(getSearchUrl("build", ""));
+    assertResponseStatus(400, response);
+    assertThat(response.getResponseBody(),
+        is("Neither hash nor coordinates of component to search for have been specified"));
+  }
+
+  @Test
+  public void testSearchComponent_InvalidHash() throws Exception {
+    Response response = RestAccess.get(getSearchUrl("build", "invalid-hash"));
+    assertResponseStatus(400, response);
+    assertThat(response.getResponseBody(), is("Invalid hash: invalid-hash"));
+  }
+
+  @Test
+  public void testSearchComponent_TooShortHash() throws Exception {
+    Response response = RestAccess.get(getSearchUrl("build", "1249e25aebb15358bed"));
+    assertResponseStatus(400, response);
+    assertThat(response.getResponseBody(), is("Invalid hash: 1249e25aebb15358bed"));
+  }
+
+  @Test
+  public void testSearchComponent_RestrictedToStage() throws Exception {
+    createAppWithScan("search-app-1", Stage.ID_BUILD);
+    createAppWithScan("search-app-2", Stage.ID_RELEASE);
+
+    Response response = RestAccess.get(getSearchUrl(Stage.ID_BUILD, "1249e25aebb15358bedd"));
+    assertResponseStatus(200, response);
+    SearchResults results = JsonHelpers.fromJson(response.getResponseBody(), SearchResults.class);
+    assertThat(results, is(notNullValue()));
+    assertThat(results.results, is(notNullValue()));
+    assertThat(results.results, hasSize(1));
+    assertSearchResult(results.results.get(0), "search-app-1", "SEARCH-APP-1", "1249e25aebb15358bedd", "tomcat",
+        "tomcat-util", "5.5.23", 8);
+  }
+
+  @Test
+  public void testSearchComponent_ByHash() throws Exception {
+    createAppWithScan("search-app-1", Stage.ID_BUILD);
+    createAppWithScan("search-app-2", Stage.ID_BUILD);
+
+    Response response = RestAccess.get(getSearchUrl(Stage.ID_BUILD, "1249e25aebb15358bedd"));
+    assertResponseStatus(200, response);
+    SearchResults results = JsonHelpers.fromJson(response.getResponseBody(), SearchResults.class);
+    assertThat(results, is(notNullValue()));
+    assertThat(results.results, is(notNullValue()));
+    assertThat(results.results, hasSize(2));
+    assertSearchResult(results.results.get(0), "search-app-1", "SEARCH-APP-1", "1249e25aebb15358bedd", "tomcat",
+        "tomcat-util", "5.5.23", 8);
+    assertSearchResult(results.results.get(1), "search-app-2", "SEARCH-APP-2", "1249e25aebb15358bedd", "tomcat",
+        "tomcat-util", "5.5.23", 4);
+  }
+
+  @Test
+  public void testSearchComponent_ByHash_FullHashString() throws Exception {
+    createAppWithScan("search-app-1", Stage.ID_BUILD);
+
+    Response response = RestAccess.get(getSearchUrl(Stage.ID_BUILD, "1249E25aEbb15358bEdd00000000000000000000"));
+    assertResponseStatus(200, response);
+    SearchResults results = JsonHelpers.fromJson(response.getResponseBody(), SearchResults.class);
+    assertThat(results, is(notNullValue()));
+    assertThat(results.results, is(notNullValue()));
+    assertThat(results.results, hasSize(1));
+    assertSearchResult(results.results.get(0), "search-app-1", "SEARCH-APP-1", "1249e25aebb15358bedd", "tomcat",
+        "tomcat-util", "5.5.23", 8);
+  }
+
+  @Test
+  public void testSearchComponent_ByGav() throws Exception {
+    createAppWithScan("search-app-1", Stage.ID_BUILD);
+    createAppWithScan("search-app-2", Stage.ID_BUILD);
+
+    Response response = RestAccess.get(getSearchUrl(Stage.ID_BUILD, "tomcat", "*", "*"));
+    assertResponseStatus(200, response);
+    SearchResults results = JsonHelpers.fromJson(response.getResponseBody(), SearchResults.class);
+    assertThat(results, is(notNullValue()));
+    assertThat(results.results, is(notNullValue()));
+    assertThat(results.results, hasSize(3));
+    assertSearchResult(results.results.get(0), "search-app-1", "SEARCH-APP-1", "1249e25aebb15358bedd", "tomcat",
+        "tomcat-util", "5.5.23", 8);
+    assertSearchResult(results.results.get(1), "search-app-1", "SEARCH-APP-1", "a397f601582e5ccd4b1a", "tomcat",
+        "servlets-default", "5.5.4", null);
+    assertSearchResult(results.results.get(2), "search-app-2", "SEARCH-APP-2", "1249e25aebb15358bedd", "tomcat",
+        "tomcat-util", "5.5.23", 4);
+  }
+
+  @Test
+  public void testSearchComponent_ByGavAndHash() throws Exception {
+    createAppWithScan("search-app-1", Stage.ID_BUILD);
+    createAppWithScan("search-app-2", Stage.ID_BUILD);
+
+    Response response = RestAccess.get(getSearchUrl(Stage.ID_BUILD, "1249e25aebb15358bedd", "tomcat", "*", "*"));
+    assertResponseStatus(200, response);
+    SearchResults results = JsonHelpers.fromJson(response.getResponseBody(), SearchResults.class);
+    assertThat(results, is(notNullValue()));
+    assertThat(results.results, is(notNullValue()));
+    assertThat(results.results, hasSize(2));
+    assertSearchResult(results.results.get(0), "search-app-1", "SEARCH-APP-1", "1249e25aebb15358bedd", "tomcat",
+        "tomcat-util", "5.5.23", 8);
+    assertSearchResult(results.results.get(1), "search-app-2", "SEARCH-APP-2", "1249e25aebb15358bedd", "tomcat",
+        "tomcat-util", "5.5.23", 4);
+  }
+
+  @Test
+  public void testSearchComponent_ByGavAndHash_NoIntersection() throws Exception {
+    createAppWithScan("search-app-1", Stage.ID_BUILD);
+    createAppWithScan("search-app-2", Stage.ID_BUILD);
+
+    Response response = RestAccess.get(getSearchUrl(Stage.ID_BUILD, "a397f601582e5ccd4b1a", "*", "tomcat-util", "*"));
+    assertResponseStatus(200, response);
+    SearchResults results = JsonHelpers.fromJson(response.getResponseBody(), SearchResults.class);
+    assertThat(results, is(notNullValue()));
+    assertThat(results.results, is(notNullValue()));
+    assertThat(results.results, hasSize(0));
+  }
+
+  @Test
+  public void testSearchComponent_EchoCriteria() throws Exception {
+    Response response = RestAccess.get(getSearchUrl(Stage.ID_BUILD, "1249e25aebb15358bedd", "gid", "aid", "1"));
+    assertResponseStatus(200, response);
+    SearchResults results = JsonHelpers.fromJson(response.getResponseBody(), SearchResults.class);
+    assertThat(results, is(notNullValue()));
+    assertThat(results.results, is(notNullValue()));
+    assertThat(results.results, hasSize(0));
+    assertThat(results.criteria, is(notNullValue()));
+    assertThat(results.criteria.stageId, is(Stage.ID_BUILD));
+    assertThat(results.criteria.hash, is("1249e25aebb15358bedd"));
+    assertThat(results.criteria.groupId, is("gid"));
+    assertThat(results.criteria.artifactId, is("aid"));
+    assertThat(results.criteria.version, is("1"));
+  }
+
+  @Test
+  public void testSearchComponent_NoHitsAmongAppComponents() throws Exception {
+    createAppWithScan("search-app-1", Stage.ID_BUILD);
+
+    Response response = RestAccess.get(getSearchUrl(Stage.ID_BUILD, "1249E25aEbb15358bEdf"));
+    assertResponseStatus(200, response);
+    SearchResults results = JsonHelpers.fromJson(response.getResponseBody(), SearchResults.class);
+    assertThat(results, is(notNullValue()));
+    assertThat(results.results, is(notNullValue()));
+    assertThat(results.results, hasSize(0));
+  }
+
+  @Test
+  public void testSearchComponent_AppWithoutAnyReports() throws Exception {
+    createApplication("search-app-1");
+
+    Response response = RestAccess.get(getSearchUrl(Stage.ID_BUILD, "1249E25aEbb15358bEdd"));
+    assertResponseStatus(200, response);
+    SearchResults results = JsonHelpers.fromJson(response.getResponseBody(), SearchResults.class);
+    assertThat(results, is(notNullValue()));
+    assertThat(results.results, is(notNullValue()));
+    assertThat(results.results, hasSize(0));
+  }
+}

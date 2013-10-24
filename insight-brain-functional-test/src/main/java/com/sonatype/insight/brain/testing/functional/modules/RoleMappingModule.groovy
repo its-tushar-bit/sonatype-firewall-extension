41ac7a3d2cba6ed695b71f1542c88b25d00df8f3
Matthew Piggott <mpiggott@sonatype.com>
2013-10-24 12:03:44 -0400

    Add headers to groovy files - CLM-534 CLM-1048

--- insight-brain-functional-test/src/main/java/com/sonatype/insight/brain/testing/functional/modules/RoleMappingModule.groovy
+++ insight-brain-functional-test/src/main/java/com/sonatype/insight/brain/testing/functional/modules/RoleMappingModule.groovy
@@ -1,4 +1,10 @@
+/**
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
 package com.sonatype.insight.brain.testing.functional.modules
+
 import geb.Module
 
 class RoleMappingModule extends Module {

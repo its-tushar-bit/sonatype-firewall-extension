b048c933266bb4d03f319b8bdee652153291e33e
Justin Young <jyoung@sonatype.com>
2013-10-24 15:22:26 -0700

    CLM-1126 Added empty constructor to DiffData for serialization

--- insight-brain-model/src/main/java/com/sonatype/insight/brain/model/trending/DiffData.java
+++ insight-brain-model/src/main/java/com/sonatype/insight/brain/model/trending/DiffData.java
@@ -11,6 +11,8 @@ public class DiffData
   private int violations;
   private int previousViolations;
 
+  public DiffData() {}
+
   public DiffData(String threat, int violations, int previousViolations) {
     this.threat = threat;
     this.violations = violations;

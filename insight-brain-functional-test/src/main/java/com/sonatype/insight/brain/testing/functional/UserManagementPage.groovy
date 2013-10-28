65d31cf3440b0d382f38533b62380f3167933b6f
Kelly Robinson <krobinson@sonatype.com>
2013-10-27 18:19:50 -0700

    - remove unnecessary selectors

--- insight-brain-functional-test/src/main/java/com/sonatype/insight/brain/testing/functional/UserManagementPage.groovy
+++ insight-brain-functional-test/src/main/java/com/sonatype/insight/brain/testing/functional/UserManagementPage.groovy
@@ -9,8 +9,6 @@ package com.sonatype.insight.brain.testing.functional
 import com.sonatype.insight.brain.testing.functional.modules.ValidationModule
 import geb.Page
 
-import static com.sonatype.insight.brain.testing.functional.utils.ValidationConstants.*
-
 class UserManagementPage
     extends Page
 {
@@ -23,29 +21,23 @@ class UserManagementPage
     userForm(required: false) { $('form', name: 'userForm') }
     controls(required: false) { index -> userForm.find('div.controls', index) }
 
-    firstNameControl(required: false) { controls(0) }
-    firstNameInput(required: false) { firstNameControl.find('input') }
-    firstNameValidations(required: false) { module ValidationModule, firstNameControl }
+    firstNameInput(required: false) { controls(0).find('input') }
+    firstNameValidations(required: false) { module ValidationModule, firstNameInput.parent() }
 
-    lastNameControl(required: false) { controls(1) }
-    lastNameInput(required: false) { lastNameControl.find('input') }
-    lastNameValidations(required: false) { module ValidationModule, lastNameControl }
+    lastNameInput(required: false) { controls(1).find('input') }
+    lastNameValidations(required: false) { module ValidationModule, lastNameInput.parent() }
 
-    emailControl(required: false) { controls(2) }
-    emailInput(required: false) { emailControl.find('input') }
-    emailValidations(required: false){ module ValidationModule, emailControl}
+    emailInput(required: false) { controls(2).find('input') }
+    emailValidations(required: false) { module ValidationModule, emailInput.parent() }
 
-    usernameControl(required: false) { controls(3) }
-    usernameInput(required: false) { usernameControl.find('input') }
-    usernameValidations(required:false){module ValidationModule, usernameControl}
+    usernameInput(required: false) { controls(3).find('input') }
+    usernameValidations(required: false) { module ValidationModule, usernameInput.parent() }
 
-    passwordControl(required: false) { controls(4) }
-    passwordInput(required: false) { passwordControl.find('input') }
-    passwordValidations(required:false) { module ValidationModule, passwordControl }
+    passwordInput(required: false) { controls(4).find('input') }
+    passwordValidations(required: false) { module ValidationModule, passwordInput.parent() }
 
-    passwordValidateControl(required: false) { controls(5) }
-    passwordValidateInput(required: false) { passwordValidateControl.find('input') }
-    passwordValidateValidations(required: false) { module ValidationModule, passwordValidateControl }
+    passwordValidateInput(required: false) { controls(5).find('input') }
+    passwordValidateValidations(required: false) { module ValidationModule, passwordValidateInput.parent() }
 
     save(required: false) { $('button', 'ng-click': 'saveClick(user)') }
     cancel(required: false) { $('button', 'ng-click': 'cancelClick(user)') }

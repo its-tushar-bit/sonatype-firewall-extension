4989f1ecd1cb908c260ad4e2414d64b0db1bbe73
vladt <vladt@sonatype.com>
2013-12-16 16:35:45 -0500

    Extracted the code used to send notifications for policy alerts to a separate class CLM-890 CLM-1490

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/policy/evaluator/PolicyEvaluateResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/policy/evaluator/PolicyEvaluateResource.java
@@ -6,17 +6,8 @@
 package com.sonatype.insight.brain.policy.evaluator;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -30,23 +21,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import com.sonatype.clm.dto.model.policy.Action;
 import com.sonatype.clm.dto.model.policy.PolicyAlert;
 import com.sonatype.clm.dto.model.policy.PolicyEvaluationResult;
 import com.sonatype.clm.dto.model.policy.Stage;
 import com.sonatype.insight.brain.dataaccess.ApplicationDAO;
-import com.sonatype.insight.brain.landing.UserInterfaceLinksResource;
 import com.sonatype.insight.brain.model.Application;
-import com.sonatype.insight.brain.model.policy.actions.ActionTypes;
-import com.sonatype.insight.brain.model.policy.actions.NotifyActionType;
-import com.sonatype.insight.brain.model.policy.stages.StageTypes;
-import com.sonatype.insight.brain.service.BaseUrl;
-import com.sonatype.insight.brain.service.InsightMail;
-import com.sonatype.insight.brain.utils.TemplateUtils;
 
-import org.sonatype.micromailer.Address;
-
-import freemarker.template.Template;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -58,21 +38,18 @@ public class PolicyEvaluateResource
 
   private static final Logger log = LoggerFactory.getLogger(PolicyEvaluateResource.class);
 
-  private static Template policyThreatsTemplate;
-
-  private final InsightMail mail;
-
-  private final BaseUrl baseUrl;
-
   private final PolicyEvaluationUtils policyEvaluationUtils;
+  
+  private final PolicyAlertNotifier policyAlertNotifier;
 
   private ApplicationDAO applicationDAO = new ApplicationDAO();
 
   @Inject
-  public PolicyEvaluateResource(final PolicyEvaluationUtils policyEvaluationUtils, InsightMail mail, BaseUrl baseUrl) {
+  public PolicyEvaluateResource(final PolicyEvaluationUtils policyEvaluationUtils,
+      PolicyAlertNotifier policyAlertNotifier)
+  {
     this.policyEvaluationUtils = policyEvaluationUtils;
-    this.mail = mail;
-    this.baseUrl = baseUrl;
+    this.policyAlertNotifier = policyAlertNotifier;
   }
 
   @POST
@@ -103,123 +80,11 @@ public class PolicyEvaluateResource
       }
 
       if (digest != null && sendNotifications) {
-        sendNotifications(applicationPublicId, appId, scanId, stage, digest);
+        policyAlertNotifier.sendNotifications(applicationPublicId, appId, scanId, stage, digest[0]);
       }
     }
 
     return policyEvaluationResult;
-  }
-
-  static Map<String, Object> createPolicyMailModel(final String serverUrl, final String cdnUrl,
-      final String applicationPublicId, final String scanId, final Stage stage, final List<PolicyAlert> policyAlerts)
-  {
-    MailPolicyAlertCounts counts = new MailPolicyAlertCounts(policyAlerts);
-
-    Collections.sort(policyAlerts, new Comparator<PolicyAlert>()
-    {
-      @Override
-      public int compare(PolicyAlert o1, PolicyAlert o2) {
-        int t1 = o1.getTrigger().getThreatLevel();
-        int t2 = o2.getTrigger().getThreatLevel();
-        int r = t2 - t1;
-        if (r == 0) {
-          r = String.CASE_INSENSITIVE_ORDER.compare(o1.getTrigger().getPolicyName(), o2.getTrigger().getPolicyName());
-        }
-        return r;
-      }
-    });
-
-    final Map<String, Object> model = new HashMap<String, Object>();
-
-    model.put("cdnUrl", cdnUrl);
-    model.put("detailedReportUrl", serverUrl + UserInterfaceLinksResource.getReportUrl(applicationPublicId, scanId));
-    model.put("policyAlerts", policyAlerts);
-    model.put("policyThreatStage", StageTypes.getById(stage.getStageTypeId()).getName());
-    model.put("policyThreatApp", applicationPublicId);
-    model.put("policyThreatTime", new SimpleDateFormat("MMMM dd, yyyy", Locale.ENGLISH).format(new Date()));
-    model.put("policyThreatRedCount", counts.red);
-    model.put("policyThreatOrangeCount", counts.orange);
-    model.put("policyThreatYellowCount", counts.yellow);
-    model.put("policyThreatDarkBlueCount", counts.darkBlue);
-    model.put("policyThreatBlueCount", counts.blue);
-    model.put("actionTypes", ActionTypes.getAll());
-
-    return model;
-  }
-
-  static String createPolicyMailSubject(MailPolicyAlertCounts counts) {
-    StringBuilder buffer = new StringBuilder(128);
-    buffer.append("Policy Alert: ");
-    int total = counts.red + counts.orange + counts.yellow + counts.darkBlue + counts.blue;
-    int highest = 0;
-    if (counts.red > 0) {
-      buffer.append(highest = counts.red).append(" critical");
-    }
-    else if (counts.orange > 0) {
-      buffer.append(highest = counts.orange).append(" severe");
-    }
-    else if (counts.yellow > 0) {
-      buffer.append(highest = counts.yellow).append(" moderate");
-    }
-    else if (counts.blue > 0 || counts.darkBlue > 0) {
-      buffer.append(highest = counts.blue + counts.darkBlue).append(" neutral");
-    }
-    buffer.append(" violation").append(highest != 1 ? "s" : "");
-    buffer.append(" out of ").append(total);
-    return buffer.toString();
-  }
-
-  private String summarizeThreats(final String applicationPublicId, final String appId, final String scanId,
-      final Stage stage, final List<PolicyAlert> policyAlerts) throws IOException
-  {
-    final Map<String, Object> model = createPolicyMailModel(baseUrl.get(), mail.getCdnUrl(), applicationPublicId,
-        scanId, stage, policyAlerts);
-    return TemplateUtils.render(getPolicyThreatsTemplate(), model);
-  }
-
-  private synchronized static Template getPolicyThreatsTemplate() throws IOException {
-    if (policyThreatsTemplate == null) {
-      policyThreatsTemplate = TemplateUtils.createFreemarkerConfig().getTemplate("policythreats.ftl");
-    }
-    return policyThreatsTemplate;
-  }
-
-  private void sendNotifications(final String applicationPublicId, String appId, final String scanId,
-      final Stage stage, final List<PolicyAlert>[] digest)
-  {
-    for (final Entry<String, List<PolicyAlert>> details : byRecipients(digest[0]).entrySet()) {
-      try {
-        final String mailId = "SONATYPE-CLM-" + applicationPublicId + '-' + scanId;
-        final List<Address> addresses = Arrays.asList(new Address(details.getKey()));
-        final String subject = createPolicyMailSubject(new MailPolicyAlertCounts(details.getValue()));
-        final String body = summarizeThreats(applicationPublicId, appId, scanId, stage, details.getValue());
-        mail.sendHtml(mailId, addresses, subject, body);
-      }
-      catch (final Exception e) {
-        log.error("Unable to send notification to: {}", details.getKey(), e);
-      }
-    }
-
-    // TODO: notify about cleared policy alerts...
-  }
-
-  private static Map<String, List<PolicyAlert>> byRecipients(final List<PolicyAlert> alerts) {
-    final Map<String, List<PolicyAlert>> byRecipients = new HashMap<String, List<PolicyAlert>>();
-    for (final PolicyAlert alert : alerts) {
-      for (final Action action : alert.getActions()) {
-        if (NotifyActionType.ID.equals(action.getActionTypeId())) {
-          final String address = action.getTarget();
-          List<PolicyAlert> personalAlerts = byRecipients.get(address);
-          if (personalAlerts == null) {
-            byRecipients.put(address, personalAlerts = new ArrayList<PolicyAlert>());
-          }
-          if (!personalAlerts.contains(alert)) {
-            personalAlerts.add(alert);
-          }
-        }
-      }
-    }
-    return byRecipients;
   }
 
   static class MailPolicyAlertCounts

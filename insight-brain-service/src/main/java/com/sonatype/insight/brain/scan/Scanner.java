c1ace9b496a92306b7c30f3db07720284dbd1667
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2013-12-17 14:32:28 +0100

    Introduced component to produce a scan file from a previously uploaded application bundle CLM-1470 CLM-1347 (PR #275)

---
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/scan/Scanner.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.scan;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.sonatype.clm.dto.model.ProprietaryConfig;
+import com.sonatype.insight.brain.dataaccess.ProprietaryConfigDAO;
+import com.sonatype.insight.brain.service.InsightWork;
+import com.sonatype.insight.scan.client.ClientScanRequest;
+import com.sonatype.insight.scan.client.ClientScanner;
+import com.sonatype.insight.scan.config.ScanPropertiesLoader;
+import com.sonatype.insight.scan.file.FileScanRequest;
+import com.sonatype.insight.scan.file.FileScanner;
+import com.sonatype.insight.scan.model.Scan;
+import com.sonatype.insight.scan.model.ScanConfiguration;
+import com.sonatype.insight.scan.model.io.ScanWriter;
+import com.sonatype.insight.scan.model.io.ScanWriterFactory;
+
+import org.codehaus.plexus.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Scans application bundles.
+ * 
+ * @since 1.7.1
+ */
+@Named
+class Scanner
+{
+  private static final Logger log = LoggerFactory.getLogger(Scanner.class);
+
+  private final ScanPropertiesLoader configLoader;
+
+  private final ClientScanner clientScanner;
+
+  private final FileScanner fileScanner;
+
+  private final ScanWriterFactory writerFactory;
+
+  private final ProprietaryConfigDAO proprietaryConfigDAO;
+
+  @Inject
+  public Scanner(ScanPropertiesLoader configLoader, ClientScanner clientScanner, FileScanner fileScanner,
+      ScanWriterFactory writerFactory, InsightWork work)
+  {
+    this.configLoader = configLoader;
+    this.clientScanner = clientScanner;
+    this.fileScanner = fileScanner;
+    this.writerFactory = writerFactory;
+    proprietaryConfigDAO = new ProprietaryConfigDAO(work.getDataDir());
+  }
+
+  /**
+   * Scans the specified target file and returns the resulting scan file.
+   */
+  public File scan(File target) throws IOException {
+    File scanFile = File.createTempFile("clm-scan-", ".xml.gz");
+    log.debug("Saving scan of {} to {}", target, scanFile);
+
+    try {
+      Scan scan = new Scan();
+      scan.setConfiguration(new ScanConfiguration(getScanConfigProps()));
+      try (ScanWriter writer = writerFactory.newWriter(scanFile)) {
+        writer.openScan(scan);
+        writer.writeConfiguration(scan.getConfiguration());
+        scan.getSummary().setStartTime();
+        clientScanner.scan(new ClientScanRequest(scan));
+        fileScanner.scan(new FileScanRequest(scan, null, Arrays.asList(target), writer));
+        scan.getSummary().setEndTime();
+        writer.writeSummary(scan.getSummary());
+        writer.closeScan();
+      }
+    }
+    catch (RuntimeException | IOException e) {
+      scanFile.delete();
+      throw e;
+    }
+
+    return scanFile;
+  }
+
+  private Properties getScanConfigProps() throws IOException {
+    Properties props = new Properties();
+    props.setProperty("fileIncludes", "");
+    props.setProperty("fileExcludes", "");
+    props.setProperty("ipAddresses", "false");
+    props.setProperty("hashJavaTypes", "true");
+    ProprietaryConfig proprietaryConfig = proprietaryConfigDAO.get();
+    if (proprietaryConfig != null) {
+      props.put("proprietaryPackages", StringUtils.join(proprietaryConfig.getPackages().iterator(), ","));
+    }
+    configLoader.loadDefaults(props, null);
+    configLoader.resolveAliases(props);
+    return props;
+  }
+}
---
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/scan/ScannerTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.scan;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.inject.Inject;
+
+import com.sonatype.clm.dto.model.ProprietaryConfig;
+import com.sonatype.insight.brain.dataaccess.ProprietaryConfigDAO;
+import com.sonatype.insight.brain.service.InsightConfig;
+import com.sonatype.insight.brain.service.InsightWork;
+import com.sonatype.insight.scan.model.Scan;
+import com.sonatype.insight.scan.model.ScanItem;
+import com.sonatype.insight.scan.model.io.ScanReader;
+
+import org.sonatype.guice.bean.containers.InjectedTest;
+
+import com.google.inject.Binder;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
+public class ScannerTest
+    extends InjectedTest
+{
+
+  @Rule
+  public TemporaryFolder tmpDir = new TemporaryFolder();
+
+  @Inject
+  private Scanner scanner;
+
+  @Inject
+  private ScanReader scanReader;
+
+  @Inject
+  private InsightWork work;
+
+  private ProprietaryConfigDAO proprietaryConfigDAO;
+
+  @Override
+  public void configure(Binder binder) {
+    InsightConfig config = new InsightConfig();
+    try {
+      config.setSonatypeWork(tmpDir.newFolder("work").getAbsolutePath());
+    }
+    catch (IOException e) {
+      throw new IllegalStateException(e);
+    }
+    InsightWork work = new InsightWork(config);
+    binder.bind(InsightWork.class).toInstance(work);
+  }
+
+  @Before
+  public void init() throws Exception {
+    ProprietaryConfig proprietaryConfig = new ProprietaryConfig();
+    proprietaryConfig.setPackages(Arrays.asList("com.sonatype"));
+    proprietaryConfigDAO = new ProprietaryConfigDAO(work.getDataDir());
+    proprietaryConfigDAO.update(proprietaryConfig);
+  }
+
+  @Test
+  public void testScan() throws Exception {
+    File appFile = new File("src/test/resources/ScannerTest/app01.zip");
+    File scanFile = scanner.scan(appFile);
+    assertThat(scanFile, is(notNullValue()));
+    assertThat(scanFile.isFile(), is(true));
+
+    Scan scan = scanReader.read(scanFile);
+    assertThat(scan, is(notNullValue()));
+    assertThat(scan.getItems(), hasSize(1));
+    ScanItem item = scan.getItems().get(0);
+    assertThat(item.getPath(), is("app01.zip"));
+    assertThat(item.getItems(), hasSize(1));
+    item = item.getItems().get(0);
+    assertThat(item.getPath(), is("proprietary.jar"));
+    assertThat(item.getItems(), hasSize(1));
+    item = item.getItems().get(0);
+    assertThat(item.getSha1(), is("44a17e5a5594edeebc94"));
+    assertThat(item.getSha1JA001(), is(notNullValue()));
+    assertThat(item.getSha1JB001(), is(notNullValue()));
+    assertThat(item.getSha1JC001(), is(notNullValue()));
+    assertThat(item.getSha1JD001(), is(notNullValue()));
+    assertThat(item.getPath(), is(nullValue()));
+    assertThat(item.getNoPathReason(), is("proprietaryPackages"));
+  }
+}

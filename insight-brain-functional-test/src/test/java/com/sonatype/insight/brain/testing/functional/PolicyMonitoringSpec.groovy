4244f9280a0acefea5cf4757aac6f4f59179647f
Kelly Robinson <krobinson@sonatype.com>
2014-01-07 14:39:49 -0800

    CLM-1488 forgot license headers :(

--- insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/PolicyMonitoringSpec.groovy
+++ insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/PolicyMonitoringSpec.groovy
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2011-2013 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
 package com.sonatype.insight.brain.testing.functional
 
 import com.sonatype.insight.brain.model.Application

a3aa3ddf719f5c10a51f7d81b5bb428994cbb8bb
Kelly Robinson <krobinson@sonatype.com>
2014-01-14 12:50:02 -0800

    CLM-1647 - Enable functional UI tests on all supported browsers (PR #245)

--- insight-brain-functional-test/src/test/java/GebConfig.groovy
+++ insight-brain-functional-test/src/test/java/GebConfig.groovy
@@ -11,10 +11,15 @@
 
 import geb.driver.SauceLabsDriverFactory
 import org.openqa.selenium.Dimension
+import org.openqa.selenium.Platform
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.ie.InternetExplorerDriver
+import org.openqa.selenium.phantomjs.PhantomJSDriver
+import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
+import org.openqa.selenium.safari.SafariDriver
 
 reportsDir = "target/test-reports/geb"
 // Port is not known until runtime, needs to be set in BaseSpec.groovy
@@ -38,7 +43,7 @@ if (sauceBrowser) {
     return driver
   }
   //increase default timeouts to account for remote execution
-  waiting{
+  waiting {
     timeout = 10
     retryInterval = 0.5
   }
@@ -47,6 +52,7 @@ else {
   driver = { configure(new FirefoxDriver()) }
 }
 
+Platform current = Platform.current
 environments {
 
   // run as “mvn -Dgeb.env=chrome test”
@@ -55,7 +61,38 @@ environments {
     driver = { configure(new ChromeDriver()) }
   }
 
-  // See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
+  // see https://code.google.com/p/selenium/wiki/SafariDriver
+  safari {
+    if (current.is(Platform.MAC)) {
+      driver = { configure(new SafariDriver()) }
+    }
+    else {
+      throw new IllegalStateException('Only runs on mac!')
+    }
+  }
+
+  // see https://code.google.com/p/selenium/wiki/InternetExplorerDriver
+  ie {
+    if (current.is(Platform.WINDOWS)) {
+      driver = { configure(new InternetExplorerDriver()) }
+    }
+    else {
+      throw new IllegalStateException('Only runs on windows!')
+    }
+  }
+
+  // see https://github.com/detro/ghostdriver
+  phantom {
+    String phantomJsBinary
+    new File('../insight-brain-service/target/phantomjs-maven-plugin').eachFileRecurse { File file ->
+      if (file.isFile() && file.name.matches('phantomjs(.exe)?')) {
+        phantomJsBinary = file.absolutePath
+        return false
+      }
+    }
+    def params = phantomJsBinary ? ['phantomjs.binary.path': phantomJsBinary] : [:]
+    driver = { configure(new PhantomJSDriver(new DesiredCapabilities(params))) }
+  }
 }
 
 def configure(final RemoteWebDriver driver) {

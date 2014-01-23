2adf9ec62bfdb64e18a2085b47f3c5c1fffd2a9c
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2014-01-23 15:33:12 +0100

    Formatted code

--- insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/ContactUserSpec.groovy
+++ insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/ContactUserSpec.groovy
@@ -3,10 +3,10 @@
  * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
-
 package com.sonatype.insight.brain.testing.functional
 
-class ContactUserSpec extends BaseSpec
+class ContactUserSpec
+    extends BaseSpec
 {
 
   String appName = 'TestApplication'

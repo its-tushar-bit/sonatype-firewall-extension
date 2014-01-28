339f009201f08613bd2310637bc231b07b80f77c
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2014-01-28 15:12:20 +0100

    Used proper version of Jackson

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/scan/ScanResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/scan/ScanResource.java
@@ -25,7 +25,7 @@ import com.sonatype.insight.error.ErrorResponse;
 
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
-import org.codehaus.jackson.map.ObjectMapper;
+import com.yammer.dropwizard.testing.JsonHelpers;
 
 /**
  * Accepts uploads of application binaries for the purpose of scanning them.
@@ -63,7 +63,7 @@ public class ScanResource
       ScanTicket result = scanService.scanBinary(appPublicId, is, fileDetail.getFileName(), new Stage(stageId),
           sendNotifications);
       if (noFormData) {
-        return Response.ok(new ObjectMapper().writeValueAsString(result), ErrorResponse.CONTENT_TYPE).build();
+        return Response.ok(JsonHelpers.asJson(result), ErrorResponse.CONTENT_TYPE).build();
       } else {
         return Response.ok(result, MediaType.APPLICATION_JSON).build();  
       }

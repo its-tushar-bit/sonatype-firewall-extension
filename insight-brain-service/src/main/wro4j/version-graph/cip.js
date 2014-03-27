f4c4d48f50783d504563c619c1731d4920d2ca4d
Matthew Piggott <mpiggott@sonatype.com>
2014-03-27 09:41:01 -0400

    Switch to $last in license template

--- insight-brain-service/src/main/wro4j/version-graph/cip.js
+++ insight-brain-service/src/main/wro4j/version-graph/cip.js
@@ -547,7 +547,7 @@
         licenses : '=',
         emptyText : '@'
       },
-      template : '<span ng-repeat="license in licenses" class="license">{{license.licenseName}}{{$index < licenses.length - 1 ? "," : ""}}</span>' +
+      template : '<span ng-repeat="license in licenses" class="license">{{license.licenseName}}{{!$last ? "," : ""}}</span>' +
                  '<span ng-if="licenses.length == 0">{{emptyText}}</span>'
     };
   });

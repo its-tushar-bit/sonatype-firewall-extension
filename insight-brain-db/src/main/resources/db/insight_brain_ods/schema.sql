54814b0ffa1fdb53ba7f63e930330be0bbe205cf
vladt <vladt@sonatype.com>
2014-04-01 12:30:33 -0400

    Removed invalid unique key on policy_violation CLM-1978 CLM-2112

--- insight-brain-db/src/main/resources/db/insight_brain_ods/schema.sql
+++ insight-brain-db/src/main/resources/db/insight_brain_ods/schema.sql
@@ -301,6 +301,5 @@ CREATE TABLE policy_violation (
   constraint_facts_json CLOB NOT NULL, -- the constraint facts (that caused the policy violation) stored in json format
   CONSTRAINT policy_violation_pk PRIMARY KEY (policy_violation_id),
   CONSTRAINT policy_violation_evaluation_fk FOREIGN KEY (policy_evaluation_id) REFERENCES policy_evaluation(policy_evaluation_id),
-  CONSTRAINT policy_violation_policy_fk FOREIGN KEY (policy_id) REFERENCES policy(policy_id),
-  CONSTRAINT policy_violation_uk UNIQUE KEY (policy_evaluation_id, policy_id, group_id, artifact_id, version, hash)
+  CONSTRAINT policy_violation_policy_fk FOREIGN KEY (policy_id) REFERENCES policy(policy_id)
 );
--- insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0042.sql
+++ insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0042.sql
@@ -1,9 +1,6 @@
 SET SCHEMA insight_brain_ods;
 
 ALTER TABLE policy_violation
-  ADD CONSTRAINT policy_violation_uk UNIQUE KEY (policy_evaluation_id, policy_id, group_id, artifact_id, version, hash);
-  
-ALTER TABLE policy_violation
   ADD COLUMN policy_name varchar(60) NULL;
   
 UPDATE policy_violation pv SET pv.policy_name=SELECT p.name FROM policy p WHERE p.policy_id=pv.policy_id;

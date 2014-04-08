c048d06cc86bcb89e76ac5cc2ebb8ea29921c43e
vladt <vladt@sonatype.com>
2014-04-08 15:12:53 -0400

    Re-use query in PolicyEvaluationDAO CLM-2084

--- insight-brain-data/src/main/java/com/sonatype/insight/brain/dataaccess/policy/PolicyEvaluationDAO.java
+++ insight-brain-data/src/main/java/com/sonatype/insight/brain/dataaccess/policy/PolicyEvaluationDAO.java
@@ -28,10 +28,13 @@ public class PolicyEvaluationDAO
   }
 
   public PolicyEvaluation getLastMonitoringByApplicationIdAndScanId(String appId, String scanId) {
-    String sQuery = "SELECT entity FROM PolicyEvaluation entity" + //
-        " WHERE entity.applicationId=?1 AND entity.scanId=?2 AND entity.isForMonitoring=true" + //
-        " ORDER BY entity.time DESC";
-    return createQuery(sQuery, appId, scanId).forceSingleResult().get();
+    EntityManager em = createEntityManager();
+    try {
+      return getLastMonitoringByApplicationIdAndScanId(em, appId, scanId);
+    }
+    finally {
+      close(em);
+    }
   }
 
   public PolicyEvaluation getLastMonitoringByApplicationIdAndScanId(EntityManager em, String appId, String scanId) {

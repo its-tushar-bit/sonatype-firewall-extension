05cd67be2e1f42c5c3bcb347b3fa47cad46176f5
kwitten <kevin.witten222@gmail.com>
2014-05-09 17:27:06 -0500

    CLM-2346 Print CLM version in logs (PR #598)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/service/InsightBrainService.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/service/InsightBrainService.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 import java.util.Set;
 
 import javax.inject.Named;
@@ -22,6 +23,7 @@ import com.sonatype.insight.brain.landing.IndexCacheControlFilter;
 import com.sonatype.insight.brain.saas.DefaultLicenseDataUpdater;
 import com.sonatype.insight.brain.security.CLMShiroAopModule;
 import com.sonatype.insight.brain.security.CLMShiroModule;
+import com.sonatype.insight.brain.version.VersionResource;
 import com.sonatype.insight.db.DatabaseConfig;
 import com.sonatype.insight.error.JaxRsExceptionMapper;
 
@@ -87,11 +89,22 @@ public class InsightBrainService
 
   @Override
   public void run(InsightConfig configuration, Environment environment) throws Exception {
+    printVersion();
     super.run(configuration, environment);
 
     LicenseDataUpdater.setUpdater(getInstance(DefaultLicenseDataUpdater.class));
 
     getInstance(DataMigrator.class).migrate();
+  }
+
+  private void printVersion() throws IOException {
+    Properties prop = VersionResource.get();
+    String version = prop.getProperty("version", "Unknown");
+    log.info("|------------------------------------------");
+    log.info("|");
+    log.info("| Initializing Sonatype CLM {}", version);
+    log.info("|");
+    log.info("|------------------------------------------");
   }
 
   private static boolean validateTempDir() {
--- insight-brain-service/src/main/java/com/sonatype/insight/brain/version/VersionResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/version/VersionResource.java
@@ -18,6 +18,8 @@ import javax.ws.rs.core.MediaType;
 import com.sonatype.insight.brain.product.license.UnlicensedPath;
 
 import org.codehaus.plexus.util.IOUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Named
 @Path(VersionResource.SERVICE_PATH)
@@ -26,22 +28,37 @@ public class VersionResource
 {
   public static final String SERVICE_PATH = "rest/version";
 
+  private static final Logger log = LoggerFactory.getLogger(VersionResource.class);
+
+  private static final String FILE_NAME = "version.properties";
+
+  private static Properties properties;
+
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   public Properties getVersionInfo() throws Exception {
     return get();
   }
 
-  public static Properties get() throws IOException {
-    Properties props = new Properties();
-    InputStream is = VersionResource.class.getResourceAsStream("version.properties");
-    try {
-      props.load(is);
+  public synchronized static Properties get() throws IOException {
+    if (properties == null) {
+      properties = new Properties();
+      InputStream is = VersionResource.class.getResourceAsStream(FILE_NAME);
+      if (is != null) {
+        try {
+          properties.load(is);
+        }
+        catch (IOException e) {
+          log.error(e.getMessage(), e);
+        }
+        finally {
+          IOUtil.close(is);
+        }
+      } else {
+        log.error("Missing properties file {}", FILE_NAME);
+      }
     }
-    finally {
-      IOUtil.close(is);
-    }
-    return props;
+    return properties;
 
   }
 }

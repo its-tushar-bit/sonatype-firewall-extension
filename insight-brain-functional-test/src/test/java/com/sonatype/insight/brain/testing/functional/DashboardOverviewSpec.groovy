c9a37e6774dca4db878d0deb1342d434f3fe3525
Matthew Piggott <mpiggott@sonatype.com>
2014-05-30 13:08:44 -0400

    Minor functional test for affected applications - CLM-2566 CLM-2503 Fixes #672

--- insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/DashboardOverviewSpec.groovy
+++ insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/DashboardOverviewSpec.groovy
@@ -538,6 +538,7 @@ class DashboardOverviewSpec
     then: 'Component Table Displayed'
       waitFor { componentViolationsTable.rows.size() == 1 }
       componentViolationsTable.rows[0].component.text() == "Group1 : Artifact1 : Version1"
+      componentViolationsTable.rows[0].affectedApplications.text() == "2"
       componentViolationsTable.rows[0].netRisk.text() == "15"
       componentViolationsTable.rows[0].criticalRisk.text() == "10"
       componentViolationsTable.rows[0].severeRisk.text() == "5"

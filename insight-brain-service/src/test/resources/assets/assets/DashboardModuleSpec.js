47d24e0469b7bdb6cae1f13bf0bf0a881d05b41a
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2014-06-06 23:35:16 +0200

    Fixed JS syntax errors from 3ec4546d4826aff013d77c593a6ff467bef21cc4

--- insight-brain-service/src/test/resources/assets/assets/DashboardModuleSpec.js
+++ insight-brain-service/src/test/resources/assets/assets/DashboardModuleSpec.js
@@ -1015,12 +1015,12 @@ describe('DashboardModule', function() {
 
         scope = $rootScope.$new();
         var page = angular
-                .element('<html><script type="text/ng-template" id="foo"><div>Foo</div></script>
+                .element('<html><script type="text/ng-template" id="foo"><div>Foo</div></script>'
                         + '<script type="text/ng-template" id="bar"><div>Bar</div></script>'
-                        + '<div id="divFoo" modal-help="foo">click</div>
-                        + '<div id="divBar" modal-help="bar" modal-help-trigger="mouseover">mouseover</div>
-                        + '<div id="divBaz" modal-help="bar" modal-help-trigger="mouseover" 
-                        +    'modal-help-class="test-class">mouseover</div></html>');
+                        + '<div id="divFoo" modal-help="foo">click</div>'
+                        + '<div id="divBar" modal-help="bar" modal-help-trigger="mouseover">mouseover</div>'
+                        + '<div id="divBaz" modal-help="bar" modal-help-trigger="mouseover"' 
+                        + 'modal-help-class="test-class">mouseover</div></html>');
         $compile(page)(scope);
 
         divFoo = page[2];

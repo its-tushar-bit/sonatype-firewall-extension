1da6e6134a51fd0e4dc715ab680c5ec8e5baf9a3
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2014-06-10 15:14:34 +0200

    Improved PortAllocator to avoid occasional "BindException: Address already in use" seen during CI

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/service/PortAllocator.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/service/PortAllocator.java
@@ -7,30 +7,39 @@ package com.sonatype.insight.brain.service;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @since 1.9.1
  */
 public class PortAllocator
 {
+  /**
+   * Given we close the socket to get the ephemeral port, it's possible for another call to {@link #findFreePort(int)}
+   * to yield the same port if the previously found free port has not been used just yet. To avoid such unintented port
+   * reuse, we keep a little history of ports that have been delivered and manually exclude them.
+   */
+  @SuppressWarnings("serial")
+  private static final Map<Integer, Object> usedPorts = new LinkedHashMap<Integer, Object>()
+  {
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Object> eldest) {
+      return size() >= 10;
+    };
+  };
+
   public static int findFreePort(final int defaultPort) {
     int port = defaultPort;
-    ServerSocket socket = null;
-    try {
-      socket = new ServerSocket(0);
-      port = socket.getLocalPort();
-    }
-    catch (final IOException e) {
-      e.printStackTrace();
-    }
-    finally {
-      if (socket != null) {
-        try {
-          socket.close();
-        }
-        catch (final IOException e) {
-          e.printStackTrace();
-        }
+    for (int i = 0; i < 10; i++) {
+      try (ServerSocket socket = new ServerSocket(0)) {
+        port = socket.getLocalPort();
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
+      if (usedPorts.put(port, "") == null) {
+        break;
       }
     }
     return port;

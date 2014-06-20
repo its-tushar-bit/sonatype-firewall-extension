7da56d92e5c8d984f0accc79149ada097c01a3b1
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2014-06-20 21:22:38 +0200

    Added functional test to cover workflow of adding group as role member when group search for LDAP is disabled CLM-2750 CLM-2749 (PR #747)

--- insight-brain-functional-test/src/main/java/com/sonatype/insight/brain/testing/functional/modules/RoleModule.groovy
+++ insight-brain-functional-test/src/main/java/com/sonatype/insight/brain/testing/functional/modules/RoleModule.groovy
@@ -18,18 +18,24 @@ class RoleModule
     editor(required: false) { $('div[app-security-editor] > div') }
     queryInput(required: false) { $('input[name=filter]') }
 
-    appliedMembers(required: false) { $('div[app-security-editor] .selectList-large:first-child .large-select-list-item') }
+    appliedMembersList(required: false) { $('div[app-security-editor] .selectList-large:first-child') }
+    appliedMembers(required: false) { appliedMembersList.find('.large-select-list-item') }
     appliedMemberNames(required: false) { appliedMembers.find('span:not(.large-select-list-item-detail):not(.large-select-list-item-right-detail):not(.ui-match)') }
     appliedMember(required: false) { displayName -> appliedMembers.has('span:not(.large-select-list-item-detail):not(.large-select-list-item-right-detail):not(.ui-match)', text: displayName ) }
     appliedMemberEmail(required: false) { appliedMembers.find('.large-select-list-item-detail') }
     appliedMemberRealm(required: false) { appliedMembers.find('.large-select-list-item-right-detail') }
 
-    availableMembers(required: false) { $('div[app-security-editor] .selectList-large:last-child .large-select-list-item') }
+    availableMembersList(required: false) { $('div[app-security-editor] .selectList-large:last-child') }
+    availableMembers(required: false) { availableMembersList.find('.large-select-list-item') }
     availableMemberNames(required: false) { availableMembers.find('span:not(.large-select-list-item-detail):not(.large-select-list-item-right-detail):not(.ui-match)') }
     availableMember(required: false) { displayName -> availableMembers.has('span:not(.large-select-list-item-detail):not(.large-select-list-item-right-detail):not(.ui-match)', text: displayName ) }
     availableMemberEmail(required: false) { availableMembers.find('.large-select-list-item-detail') }
     availableMemberRealm(required: false) { availableMembers.find('.large-select-list-item-right-detail') }
 
     confirmButton(required: false) { $('button.btn-primary') }
+
+    usersButton(required: false) { $('button[ng-click="mtype=null"]') }
+    groupsButton(required: false) { $('button[ng-click="mtype=\\"groups\\""]') }
+    addGroupButton(required: false) { $('button[ng-click="addGroup()"]') }
   }
 }
--- insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/AppSecurityManagementSpec.groovy
+++ insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/AppSecurityManagementSpec.groovy
@@ -5,13 +5,21 @@
  */
 package com.sonatype.insight.brain.testing.functional
 
+import com.sonatype.insight.brain.configuration.ldap.LdapUserMapping
+import com.sonatype.insight.brain.configuration.ldap.LdapGroupMappingType
+import com.sonatype.insight.brain.dataaccess.configuration.ldap.LdapUserMappingDAO
 
 class AppSecurityManagementSpec
     extends BaseSpec 
 {
+  String orgId
+  String appPublicId
+
   // assumes a license has already been installed
   // get to the organizations page
   def setup() {
+    orgId = temporaryEntity.newOrganization('Test Org').id
+    appPublicId = temporaryEntity.newApplication('Test App', 'test-app', orgId).publicId
     loginAsAdminVia()
   }
 
@@ -21,7 +29,7 @@ class AppSecurityManagementSpec
 
   def "validate organization roles"() {
     when: "Open Security Tab"
-      createOrganization('validateOrgRolesOrg');
+      to OrganizationPage, orgId
       tabs.securityTabButton.click()
 
     then: "security tab content is shown"
@@ -31,10 +39,8 @@ class AppSecurityManagementSpec
   }
 
   def "validate application roles"() {
-
-    when: "create a new application"
-      createOrganization('validateAppRolesOrg');
-      createApplication('validateAllRolesApp', 'validateAppRolesAppId', 'validateAppRolesOrg');
+    when: "Navigating to an application"
+      to ApplicationPage, appPublicId
 
     then: "see the security tab shown"
       waitFor { tabs.securityTabButton.displayed }
@@ -46,5 +52,79 @@ class AppSecurityManagementSpec
       waitFor { tabs.securityTab.displayed }
       waitFor { tabs.securityTab.role("Developer").displayed }
       waitFor { tabs.securityTab.role("Owner").displayed }
+  }
+
+  def 'Can directly enter role members when group search is disabled'() {
+    given: 'an LDAP server with group search disabled'
+      String serverId = temporaryEntity.newLdapServer('LDAP').id
+      temporaryEntity.newLdapConnection(serverId)
+      LdapUserMapping userMapping = temporaryEntity.newLdapUserMapping(serverId)
+      userMapping.setGroupMappingType(LdapGroupMappingType.DYNAMIC)
+      userMapping.setDynamicGroupSearchEnabled(false)
+      new LdapUserMappingDAO().update(userMapping)
+
+    when: 'navigating to the security tab of some application'
+      to ApplicationPage, appPublicId
+      waitFor { tabs.securityTabButton.displayed }
+      tabs.securityTabButton.click()
+
+    then: 'the security tab shows the built-in roles'
+      waitFor { tabs.securityTab.displayed }
+      waitFor { tabs.securityTab.role('Developer').displayed }
+      waitFor { tabs.securityTab.role('Owner').displayed }
+
+    when: 'clicking the edit button for a role'
+      def roleRow = tabs.securityTab.role('Developer')
+      interact {
+        moveToElement(roleRow)
+      }
+      roleRow.editButton.click()
+
+    then: 'the users & groups buttons are visible'
+      roleRow.usersButton.displayed
+      roleRow.groupsButton.displayed
+
+    and: 'only the users button is marked as active'
+      roleRow.usersButton.hasClass('active')
+      !roleRow.groupsButton.hasClass('active')
+
+    when: 'entering a username fragment'
+      roleRow.queryInput.value('admin')
+
+    then: 'the matching users are listed as usual'
+      waitFor { roleRow.availableMembers.size() == 1 }
+      roleRow.availableMemberNames*.text().sort() == ['Admin BuiltIn']
+
+    when: 'clicking the groups button'
+      roleRow.groupsButton.click()
+
+    then: 'the available members list is hidden'
+      !roleRow.availableMembersList.displayed
+
+    and: 'an add button is shown instead'
+      roleRow.addGroupButton.displayed
+
+    and: 'only the groups button is marked as active'
+      !roleRow.usersButton.hasClass('active')
+      roleRow.groupsButton.hasClass('active')
+
+    when: 'clicking the add button'
+      roleRow.addGroupButton.click()
+
+    then: 'the entered group is added to the applied list'
+      roleRow.appliedMembers.size() == 1
+      roleRow.appliedMemberNames*.text() == ['admin']
+      roleRow.appliedMemberEmail*.text() == ['']
+      roleRow.appliedMemberRealm*.text() == ['LDAP']
+
+    when: 'saving the updated role membership mapping'
+      roleRow.confirmButton.click()
+      roleRow = tabs.securityTab.role('Developer') // account for DOM refresh
+
+    then: 'the edit form is closed'
+      !roleRow.editor.displayed
+
+    and: 'the added member is shown in the list'
+      roleRow.memberNames == ['admin']
   }
 }

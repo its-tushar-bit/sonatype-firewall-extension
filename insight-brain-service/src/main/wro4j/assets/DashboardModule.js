3b1178c1f75b6e842d0395fe2ff15152933f87a8
Matthew Piggott <mpiggott@sonatype.com>
2014-06-25 12:46:54 -0400

    Adjust column widths and fix shifts when sorting! CLM-2606 PR #758

--- insight-brain-service/src/main/wro4j/assets/DashboardModule.js
+++ insight-brain-service/src/main/wro4j/assets/DashboardModule.js
@@ -723,10 +723,13 @@
       require : '^sortable',
       scope: {
         field: '@sortColumns',     // comma separated list
-        inverted: '@?sortInverted' // is the data logically inverted, i.e. AGE vs TIME
+        inverted: '@?sortInverted', // is the data logically inverted, i.e. AGE vs TIME
+        centered : '@?'
       },
       transclude : true,
-      template : '<a ng-click="setSort()"><span ng-transclude></span> <i class="sonatype-icons" ng-class="{ up : isUp(), down : isDown(), emptyIconGlyph : !isUp() && !isDown() }"></i></a>',
+      template : '<a ng-click="setSort()">' +
+                   '<i ng-if="centered" class="sonatype-icons emptyIconGlyph"></i> ' + // should help center
+                   '<span ng-transclude></span> <i class="sonatype-icons" ng-class="{ up : isUp(), down : isDown(), emptyIconGlyph : !isUp() && !isDown() }"></i></a>',
       link : function (scope, element, attrs, sortableCtrl) {
         var mainSort = scope.field.split(',')[0];
         var isInverted = scope.inverted === 'true';

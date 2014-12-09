its-tushar-bit/sonatype-firewall-extension

commit 8ab214086159556c567152bcee95863299de8505

Application public ID now allows dots, dashes, and underscores. Application public ID is now verified server side. CLM-3903 PR #1002

insight-brain-data/src/main/java/com/sonatype/insight/brain/dataaccess/ApplicationDAO.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import javax.persistence.EntityManager;
 
@@ -46,6 +47,8 @@ public class ApplicationDAO
     extends AbstractOperationalSqlDAO<Application>
 {
   private static final Logger log = LoggerFactory.getLogger(ApplicationDAO.class);
+
+  private static final Pattern WHITESPACE_PATTERN = Pattern.compile("\\s");
 
   @Override
   public Application getById(EntityManager em, String id) {
@@ -199,6 +202,7 @@ public class ApplicationDAO
   @Override
   public void insert(EntityManager em, Application application) {
     validate(application);
+    validatePublicId(application.getPublicId());
 
     if (getByName(em, application.getName()) != null) {
       throw new InvalidNameException(application.getName() + " is already used as a name.");
@@ -367,10 +371,12 @@ public class ApplicationDAO
 
   private void validate(Application application) {
     NameHelper.validate(application.getName());
+  }
 
-    final String applicationPublicId = application.getPublicId();
-    if (applicationPublicId == null || applicationPublicId.trim().isEmpty()) {
-      throw new InvalidApplicationException("A public ID is required to save an application.");
+  private void validatePublicId(String publicId) {
+    NameHelper.validate("Public ID", publicId);
+    if (WHITESPACE_PATTERN.matcher(publicId).find()) {
+      throw new InvalidApplicationException("Public ID cannot contain whitespaces.");
     }
   }
 

insight-brain-service/src/main/resources/assets/application/components/aoeditor.html
@@ -270,6 +270,7 @@
                   model-field='publicId'
                   empty-text='Enter ID'
                   whitespace-check='false'
+                  no-spaces='true'
                   invalid='$parent.$parent.aoIdEditor.$invalid'
                   duplicate-array='ao.siblings'
                   duplicate-id-field='id'>

insight-brain-service/src/main/wro4j/assets/EditorTools.js
@@ -182,7 +182,8 @@
   ]);
 
   module.directive('clmEditable', ['$parse', 'regexFactory', function ($parse, regexFactory) {
-    var alphaNumericRegex = new RegExp('[^-' + regexFactory.allLetters().source + '0-9 ]', 'i');
+    var invalidCharsRegex = new RegExp('[^-\\. _' + regexFactory.allLetters().source + '0-9]', 'i');
+    var spaceRegex = new RegExp('\\s', 'i');
     return {
       template : '<span><span ng-click="myForm.$show()"' +
           'editable-text="model[modelField]"' +
@@ -198,6 +199,7 @@
         duplicateIdField : '@',
         emptyText : '@',
         whitespaceCheck : '@',
+        noSpaces : '@',
         model : '=',
         modelField : '@',
         invalid : '=?',
@@ -232,7 +234,7 @@
         scope.check = function (val) {
           val = val || '';
           scope.invalid = true;
-          // check dupe
+          // check duplicate
           if (scope.duplicateArray) {
             var duplicate = false,
                 lowercaseVal = val.toLowerCase();
@@ -247,11 +249,15 @@
               return 'Already in use';
             }
           }
-          // check alpha
-          if (val.match(alphaNumericRegex)) {
-            return 'Name must be alpha numeric';
+          // check if spaces are not allowed
+          if(scope.noSpaces && val.match(spaceRegex)) {
+            return 'Spaces or tabs are not allowed';
           }
-          // check spaces
+          // check for invalid characters
+          if (val.match(invalidCharsRegex)) {
+            return 'Only alpha numeric,'+ (scope.noSpaces ? '' : ' spaces,')+' dash, underscore, or dot characters are allowed';
+          }
+          // check for double spaces or tabs
           if (scope.whitespaceCheck  && val.match(/^ | {2,}|\t| $/)) {
             return 'No double spaces or tabs in name';
           }

insight-brain-data/src/test/java/com/sonatype/insight/brain/dataaccess/AbstractDbDAOTest.java
@@ -25,8 +25,8 @@ public abstract class AbstractDbDAOTest
   @Before
   public void setup() {
     organization = tempEntity.newOrganization("AbstractDbDAOTest");
-    application = tempEntity
-        .newApplication("AbstractDbDAOTest-AppName", "AbstractDbDAOTest_AppPublicId", organization.getId());
+    application = tempEntity.newApplication("AbstractDbDAOTest-AppName", "AbstractDbDAOTest-AppPublicId",
+        organization.getId());
     applicationId = application.getId();
   }
 }

insight-brain-data/src/test/java/com/sonatype/insight/brain/dataaccess/ApplicationDAOTest.java
@@ -76,6 +76,9 @@ import static org.junit.Assert.fail;
 public class ApplicationDAOTest
     extends AbstractDbDAOTest
 {
+  /** Prohibited application public ID whitespace characters. */
+  public static final char[] PUBLIC_ID_WHITESPACE_CHARS = { '\t', '\n', '\u000B', '\f', '\r' };
+
   private ApplicationDAO applicationDAO = new ApplicationDAO();
 
   @Rule
@@ -197,27 +200,67 @@ public class ApplicationDAOTest
   }
 
   @Test
-  public void testValidateNullPublicId_Insert() {
-    Application app = new Application(null, "name", organization.getId());
-    try {
-      applicationDAO.insert(app);
-      fail("Expected InvalidApplicationException");
-    }
-    catch (InvalidApplicationException expected) {
-      assertEquals("A public ID is required to save an application.", expected.getMessage());
+  public void testValidatePublicIdValidChars_Insert() {
+    for (String publicId : NameHelperTest.VALID_NAMES) {
+      tempEntity.newApplication(tempEntity.uuid(), publicId.replaceAll("\\s", ""), organization.getId());
     }
   }
 
   @Test
-  public void testValidateNullPublicId_Update() {
-    application.setPublicId(" ");
-    application.setName(application.getName() + "1");
-    try {
-      applicationDAO.update(application);
-      fail("Expected InvalidApplicationException");
+  public void testValidatePublicIdInvalidChars_Insert() {
+    Application app = new Application(null, "name", organization.getId());
+    for (String publicId : NameHelperTest.INVALID_ALPHANUMERIC) {
+      app.setPublicId(publicId);
+      try {
+        applicationDAO.insert(app);
+        fail("Expected InvalidNameException");
+      }
+      catch (InvalidNameException expected) {
+        assertEquals(String.format(NameHelper.INVALID_CHAR_MESSAGE, "Public ID", publicId.charAt(0)),
+            expected.getMessage());
+      }
     }
-    catch (InvalidApplicationException expected) {
-      assertEquals("A public ID is required to save an application.", expected.getMessage());
+  }
+
+  @Test
+  public void testValidateNullPublicId_Insert() {
+    Application app = new Application(null, "name", organization.getId());
+    try {
+      applicationDAO.insert(app);
+      fail("Expected InvalidNameException");
+    }
+    catch (InvalidNameException expected) {
+      assertEquals("Public ID is required.", expected.getMessage());
+    }
+  }
+
+  @Test
+  public void testValidateEmptyPublicId_Insert() {
+    Application app = new Application("", "name", organization.getId());
+    try {
+      applicationDAO.insert(app);
+      fail("Expected InvalidNameException");
+    }
+    catch (InvalidNameException expected) {
+      assertEquals("Public ID is required.", expected.getMessage());
+    }
+  }
+
+  @Test
+  public void testValidatePublicIdWithWhitespaces_Insert() {
+    Application app = new Application(null, "name", organization.getId());
+    for (char invalidChar : PUBLIC_ID_WHITESPACE_CHARS) {
+      app.setPublicId("foo" + invalidChar + "bar");
+      try {
+        applicationDAO.insert(app);
+        fail("Expected InvalidNameException");
+      }
+      catch (InvalidApplicationException expected) {
+        assertEquals("Public ID cannot contain whitespaces.", expected.getMessage());
+      }
+      catch (InvalidNameException expected) {
+        assertEquals(String.format(NameHelper.INVALID_CHAR_MESSAGE, "Public ID", invalidChar), expected.getMessage());
+      }
     }
   }
 

insight-brain-service/src/test/resources/assets/assets/EditorToolsSpec.js
@@ -379,7 +379,11 @@ describe('EditorToolsSpec', function() {
         scope.$digest();
         expect(scope.$invalid).not.toBeTruthy();
 
-        expect(directiveScope.check('Foo&Bar')).toEqual('Name must be alpha numeric');
+        expect(directiveScope.check('._ -')).toEqual(null);
+        scope.$digest();
+        expect(scope.$invalid).not.toBeTruthy();
+        
+        expect(directiveScope.check('Foo&Bar')).toEqual('Only alpha numeric, spaces, dash, underscore, or dot characters are allowed');
         scope.$digest();
         expect(scope.$invalid).toBeTruthy();
       });
@@ -409,6 +413,20 @@ describe('EditorToolsSpec', function() {
           });
 
           expect(directiveScope.check('foo')).toEqual('Already in use');
+          scope.$digest();
+          expect(scope.$invalid).toBeTruthy();
+        });
+      });
+      
+      describe('No Spaces', function () {
+        it('Spaces Validation', function () {
+          directiveScope.noSpaces = 'true';
+          expect(directiveScope.check('f oo')).toEqual('Spaces or tabs are not allowed');
+          scope.$digest();
+          expect(scope.$invalid).toBeTruthy();
+          
+          directiveScope.noSpaces = 'true';
+          expect(directiveScope.check('Foo&Bar')).toEqual('Only alpha numeric, dash, underscore, or dot characters are allowed');
           scope.$digest();
           expect(scope.$invalid).toBeTruthy();
         });

commit bd39e8baa06e18c2ee4ee88bdef6046921013a12

8ab214086159556c567152bcee95863299de8505 caused many functional test failures, will fix on an isolated branch.

insight-brain-data/src/main/java/com/sonatype/insight/brain/dataaccess/ApplicationDAO.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 import javax.persistence.EntityManager;
 
@@ -47,8 +46,6 @@ public class ApplicationDAO
     extends AbstractOperationalSqlDAO<Application>
 {
   private static final Logger log = LoggerFactory.getLogger(ApplicationDAO.class);
-
-  private static final Pattern WHITESPACE_PATTERN = Pattern.compile("\\s");
 
   @Override
   public Application getById(EntityManager em, String id) {
@@ -202,7 +199,6 @@ public class ApplicationDAO
   @Override
   public void insert(EntityManager em, Application application) {
     validate(application);
-    validatePublicId(application.getPublicId());
 
     if (getByName(em, application.getName()) != null) {
       throw new InvalidNameException(application.getName() + " is already used as a name.");
@@ -371,12 +367,10 @@ public class ApplicationDAO
 
   private void validate(Application application) {
     NameHelper.validate(application.getName());
-  }
 
-  private void validatePublicId(String publicId) {
-    NameHelper.validate("Public ID", publicId);
-    if (WHITESPACE_PATTERN.matcher(publicId).find()) {
-      throw new InvalidApplicationException("Public ID cannot contain whitespaces.");
+    final String applicationPublicId = application.getPublicId();
+    if (applicationPublicId == null || applicationPublicId.trim().isEmpty()) {
+      throw new InvalidApplicationException("A public ID is required to save an application.");
     }
   }
 

insight-brain-service/src/main/resources/assets/application/components/aoeditor.html
@@ -270,7 +270,6 @@
                   model-field='publicId'
                   empty-text='Enter ID'
                   whitespace-check='false'
-                  no-spaces='true'
                   invalid='$parent.$parent.aoIdEditor.$invalid'
                   duplicate-array='ao.siblings'
                   duplicate-id-field='id'>

insight-brain-service/src/main/wro4j/assets/EditorTools.js
@@ -182,8 +182,7 @@
   ]);
 
   module.directive('clmEditable', ['$parse', 'regexFactory', function ($parse, regexFactory) {
-    var invalidCharsRegex = new RegExp('[^-\\. _' + regexFactory.allLetters().source + '0-9]', 'i');
-    var spaceRegex = new RegExp('\\s', 'i');
+    var alphaNumericRegex = new RegExp('[^-' + regexFactory.allLetters().source + '0-9 ]', 'i');
     return {
       template : '<span><span ng-click="myForm.$show()"' +
           'editable-text="model[modelField]"' +
@@ -199,7 +198,6 @@
         duplicateIdField : '@',
         emptyText : '@',
         whitespaceCheck : '@',
-        noSpaces : '@',
         model : '=',
         modelField : '@',
         invalid : '=?',
@@ -234,7 +232,7 @@
         scope.check = function (val) {
           val = val || '';
           scope.invalid = true;
-          // check duplicate
+          // check dupe
           if (scope.duplicateArray) {
             var duplicate = false,
                 lowercaseVal = val.toLowerCase();
@@ -249,15 +247,11 @@
               return 'Already in use';
             }
           }
-          // check if spaces are not allowed
-          if(scope.noSpaces && val.match(spaceRegex)) {
-            return 'Spaces or tabs are not allowed';
+          // check alpha
+          if (val.match(alphaNumericRegex)) {
+            return 'Name must be alpha numeric';
           }
-          // check for invalid characters
-          if (val.match(invalidCharsRegex)) {
-            return 'Only alpha numeric,'+ (scope.noSpaces ? '' : ' spaces,')+' dash, underscore, or dot characters are allowed';
-          }
-          // check for double spaces or tabs
+          // check spaces
           if (scope.whitespaceCheck  && val.match(/^ | {2,}|\t| $/)) {
             return 'No double spaces or tabs in name';
           }

insight-brain-data/src/test/java/com/sonatype/insight/brain/dataaccess/AbstractDbDAOTest.java
@@ -25,8 +25,8 @@ public abstract class AbstractDbDAOTest
   @Before
   public void setup() {
     organization = tempEntity.newOrganization("AbstractDbDAOTest");
-    application = tempEntity.newApplication("AbstractDbDAOTest-AppName", "AbstractDbDAOTest-AppPublicId",
-        organization.getId());
+    application = tempEntity
+        .newApplication("AbstractDbDAOTest-AppName", "AbstractDbDAOTest_AppPublicId", organization.getId());
     applicationId = application.getId();
   }
 }

insight-brain-data/src/test/java/com/sonatype/insight/brain/dataaccess/ApplicationDAOTest.java
@@ -76,9 +76,6 @@ import static org.junit.Assert.fail;
 public class ApplicationDAOTest
     extends AbstractDbDAOTest
 {
-  /** Prohibited application public ID whitespace characters. */
-  public static final char[] PUBLIC_ID_WHITESPACE_CHARS = { '\t', '\n', '\u000B', '\f', '\r' };
-
   private ApplicationDAO applicationDAO = new ApplicationDAO();
 
   @Rule
@@ -200,67 +197,27 @@ public class ApplicationDAOTest
   }
 
   @Test
-  public void testValidatePublicIdValidChars_Insert() {
-    for (String publicId : NameHelperTest.VALID_NAMES) {
-      tempEntity.newApplication(tempEntity.uuid(), publicId.replaceAll("\\s", ""), organization.getId());
-    }
-  }
-
-  @Test
-  public void testValidatePublicIdInvalidChars_Insert() {
-    Application app = new Application(null, "name", organization.getId());
-    for (String publicId : NameHelperTest.INVALID_ALPHANUMERIC) {
-      app.setPublicId(publicId);
-      try {
-        applicationDAO.insert(app);
-        fail("Expected InvalidNameException");
-      }
-      catch (InvalidNameException expected) {
-        assertEquals(String.format(NameHelper.INVALID_CHAR_MESSAGE, "Public ID", publicId.charAt(0)),
-            expected.getMessage());
-      }
-    }
-  }
-
-  @Test
   public void testValidateNullPublicId_Insert() {
     Application app = new Application(null, "name", organization.getId());
     try {
       applicationDAO.insert(app);
-      fail("Expected InvalidNameException");
+      fail("Expected InvalidApplicationException");
     }
-    catch (InvalidNameException expected) {
-      assertEquals("Public ID is required.", expected.getMessage());
+    catch (InvalidApplicationException expected) {
+      assertEquals("A public ID is required to save an application.", expected.getMessage());
     }
   }
 
   @Test
-  public void testValidateEmptyPublicId_Insert() {
-    Application app = new Application("", "name", organization.getId());
+  public void testValidateNullPublicId_Update() {
+    application.setPublicId(" ");
+    application.setName(application.getName() + "1");
     try {
-      applicationDAO.insert(app);
-      fail("Expected InvalidNameException");
+      applicationDAO.update(application);
+      fail("Expected InvalidApplicationException");
     }
-    catch (InvalidNameException expected) {
-      assertEquals("Public ID is required.", expected.getMessage());
-    }
-  }
-
-  @Test
-  public void testValidatePublicIdWithWhitespaces_Insert() {
-    Application app = new Application(null, "name", organization.getId());
-    for (char invalidChar : PUBLIC_ID_WHITESPACE_CHARS) {
-      app.setPublicId("foo" + invalidChar + "bar");
-      try {
-        applicationDAO.insert(app);
-        fail("Expected InvalidNameException");
-      }
-      catch (InvalidApplicationException expected) {
-        assertEquals("Public ID cannot contain whitespaces.", expected.getMessage());
-      }
-      catch (InvalidNameException expected) {
-        assertEquals(String.format(NameHelper.INVALID_CHAR_MESSAGE, "Public ID", invalidChar), expected.getMessage());
-      }
+    catch (InvalidApplicationException expected) {
+      assertEquals("A public ID is required to save an application.", expected.getMessage());
     }
   }
 

insight-brain-service/src/test/resources/assets/assets/EditorToolsSpec.js
@@ -379,11 +379,7 @@ describe('EditorToolsSpec', function() {
         scope.$digest();
         expect(scope.$invalid).not.toBeTruthy();
 
-        expect(directiveScope.check('._ -')).toEqual(null);
-        scope.$digest();
-        expect(scope.$invalid).not.toBeTruthy();
-        
-        expect(directiveScope.check('Foo&Bar')).toEqual('Only alpha numeric, spaces, dash, underscore, or dot characters are allowed');
+        expect(directiveScope.check('Foo&Bar')).toEqual('Name must be alpha numeric');
         scope.$digest();
         expect(scope.$invalid).toBeTruthy();
       });
@@ -413,20 +409,6 @@ describe('EditorToolsSpec', function() {
           });
 
           expect(directiveScope.check('foo')).toEqual('Already in use');
-          scope.$digest();
-          expect(scope.$invalid).toBeTruthy();
-        });
-      });
-      
-      describe('No Spaces', function () {
-        it('Spaces Validation', function () {
-          directiveScope.noSpaces = 'true';
-          expect(directiveScope.check('f oo')).toEqual('Spaces or tabs are not allowed');
-          scope.$digest();
-          expect(scope.$invalid).toBeTruthy();
-          
-          directiveScope.noSpaces = 'true';
-          expect(directiveScope.check('Foo&Bar')).toEqual('Only alpha numeric, dash, underscore, or dot characters are allowed');
           scope.$digest();
           expect(scope.$invalid).toBeTruthy();
         });
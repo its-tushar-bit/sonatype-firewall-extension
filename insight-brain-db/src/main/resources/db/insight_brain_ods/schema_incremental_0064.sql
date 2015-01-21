cf36edff1308839385ac09ec5f6cab1925e3f877
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2015-01-21 13:39:07 +0100

    Fixed typo in incremental SQL schema script CLM-4054

--- insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0064.sql
+++ insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0064.sql
@@ -2,4 +2,4 @@
 SET SCHEMA insight_brain_ods;
 
 alter table license_threat_group_license alter column license_id varchar(1000) not null;
-alter table license_override alter column license_id varchar(1000) not null;
+alter table license_override alter column license_id varchar(1000) null;

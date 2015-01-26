719bbea4a48cb1932f2a77812050847341c773e6
Matthew Piggott <mpiggott@sonatype.com>
2015-01-26 14:42:23 -0500

    Fix multi-select button corners & search alignment - CLM-4050 CLM-2836

--- insight-brain-service/src/main/wro4j/util/AngularCommon.js
+++ insight-brain-service/src/main/wro4j/util/AngularCommon.js
@@ -817,10 +817,10 @@ var AngularStateUtils = {
   angularCommon.directive('multiSelect', [function () {
     return {
       template : '<div class="btn-group" ng-class="{ open : open }">' +
-                   '<button class="btn" ng-click="open = !open" ng-class="{ \'btn-small\': small }" type="button">' +
+                   '<button class="btn dropdown-toggle" ng-click="open = !open" ng-class="{ \'btn-small\': small }" type="button">' +
                    '<span><div>{{getText()}}</div></span> <span class="caret"></span></button>' +
                    '<ul class="dropdown-menu multiselect-container">' +
-                     '<li ng-if="items.length > 9"><input type="text" ng-model="filter.name" style="margin:0;width:160px" placeholder="Search"></li>' +
+                     '<li ng-if="items.length > 9"><input type="text" ng-model="filter.name" style="margin:0 auto 5px auto;width:160px;display:block" placeholder="Search"></li>' +
                      '<li ng-repeat="item in items | filter: { name : filter.name }" ng-class="{ selected : selected[item.id]  }">' +
                        '<label class="checkbox" ng-class="{ \'has-owner\': item.owner }">' +
                          '<input type="checkbox" ng-model="selected[item.id]" ng-change="updateSelectedIds(item.id)">' +

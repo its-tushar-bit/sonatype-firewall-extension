1cb925dc6be534d517a249632fb385fe74fb548c
Matthew Piggott <mpiggott@sonatype.com>
2015-03-09 10:17:50 -0400

    Improved update scripts for 1.13
    Fixes #1105

--- insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0066.sql
+++ insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0066.sql
@@ -7,12 +7,6 @@ ALTER TABLE hash_component_identifier
 ALTER TABLE license_override
   ALTER COLUMN component_id_coordinates_json varchar(1000) NOT NULL;
 
-ALTER TABLE policy_violation
-  ALTER COLUMN component_id_coordinates_json varchar(1000);
-
-ALTER TABLE application_component
-  ALTER COLUMN component_id_coordinates_json varchar(1000);
-
 ALTER TABLE hash_component_identifier
   ADD CONSTRAINT hash_component_identifier_component_id_uk UNIQUE KEY (component_id_format, component_id_coordinates_json);
 

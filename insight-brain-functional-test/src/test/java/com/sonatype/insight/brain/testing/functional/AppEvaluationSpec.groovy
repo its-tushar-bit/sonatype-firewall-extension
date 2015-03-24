2c993a5b19bce4776fa01923ce73159fd4be04e8
vladt <vladt@sonatype.com>
2015-03-24 17:22:47 -0400

    Eclipse formatting

--- insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/AppEvaluationSpec.groovy
+++ insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/AppEvaluationSpec.groovy
@@ -7,11 +7,11 @@ package com.sonatype.insight.brain.testing.functional
 
 import com.sonatype.insight.brain.model.Organization
 import com.sonatype.insight.brain.model.security.Permission
+
 import org.apache.commons.io.IOUtils
 
 class AppEvaluationSpec
-    extends BaseSpec
-{
+extends BaseSpec {
   static Organization org
 
   private static final ArrayList<String> AVAILABLE_STAGES = ['Build', 'Stage Release', 'Release', 'Operate']
@@ -37,94 +37,94 @@ class AppEvaluationSpec
 
   def "validate application evaluation available from organization screen"() {
     when: 'User accesses organization'
-      OrganizationManagementPage orgManPage = to OrganizationManagementPage
-      waitFor { orgManPage.organization("AppEvaluationOrg").displayed }
-      orgManPage.organization("AppEvaluationOrg").click()
+    OrganizationManagementPage orgManPage = to OrganizationManagementPage
+    waitFor { orgManPage.organization("AppEvaluationOrg").displayed }
+    orgManPage.organization("AppEvaluationOrg").click()
 
     then: 'User at organization page and app eval button is visible'
-      OrganizationPage orgPage = at(OrganizationPage)
-      orgPage.tools.appEvalButton.displayed
+    OrganizationPage orgPage = at(OrganizationPage)
+    orgPage.tools.appEvalButton.displayed
 
     when: 'User clicks on the app eval button'
-      orgPage.tools.appEvalButton.click()
+    orgPage.tools.appEvalButton.click()
 
     then: 'User see the app eval dialog and no application is selected'
-      waitFor { orgPage.tools.appEval.dialog.displayed }
-      waitFor { orgPage.tools.appEval.application.value() == '' }
+    waitFor { orgPage.tools.appEval.dialog.displayed }
+    waitFor { orgPage.tools.appEval.application.value() == '' }
 
     and: 'Four choices are available for the stage'
     orgPage.tools.appEval.availableStages == AVAILABLE_STAGES
 
     and: 'Dialog can be canceled'
-      orgPage.tools.appEval.cancel.click()
-      waitFor { !orgPage.tools.appEval.dialog.displayed }
+    orgPage.tools.appEval.cancel.click()
+    waitFor { !orgPage.tools.appEval.dialog.displayed }
   }
 
   def "validate application evaluation available from application screen"() {
     when: 'User accesses application'
-      ApplicationManagementPage appManPage = to ApplicationManagementPage
-      waitFor { appManPage.application("AppEvaluationApp2").displayed }
-      appManPage.application("AppEvaluationApp2").click()
+    ApplicationManagementPage appManPage = to ApplicationManagementPage
+    waitFor { appManPage.application("AppEvaluationApp2").displayed }
+    appManPage.application("AppEvaluationApp2").click()
 
     then: 'User at application page and app eval button is visible'
-      ApplicationPage appPage = at(ApplicationPage)
-      appPage.tools.appEvalButton.displayed
+    ApplicationPage appPage = at(ApplicationPage)
+    appPage.tools.appEvalButton.displayed
 
     when: 'User clicks on the app eval button'
-      appPage.tools.appEvalButton.click()
+    appPage.tools.appEvalButton.click()
 
     then: 'User see the app eval dialog and an application is selected'
-      waitFor { appPage.tools.appEval.dialog.displayed }
-      appPage.tools.appEval.application.value() == '1'
-      appPage.tools.appEval.getSelectedApplicationOption() == "AppEvaluationApp2"
+    waitFor { appPage.tools.appEval.dialog.displayed }
+    appPage.tools.appEval.application.value() == '1'
+    appPage.tools.appEval.getSelectedApplicationOption() == "AppEvaluationApp2"
 
     and: 'Four choices are available for the stage'
     appPage.tools.appEval.availableStages == AVAILABLE_STAGES
 
     and: 'Dialog can be canceled'
-      appPage.tools.appEval.cancel.click()
-      waitFor { !appPage.tools.appEval.dialog.displayed }
+    appPage.tools.appEval.cancel.click()
+    waitFor { !appPage.tools.appEval.dialog.displayed }
   }
 
   def "validate upload"() {
     when: 'User accesses application and sets params for application evaluation'
-      ApplicationManagementPage appManPage = to ApplicationManagementPage
-      waitFor { appManPage.application("AppEvaluationApp2").displayed }
-      appManPage.application("AppEvaluationApp2").click()
-      ApplicationPage appPage = at(ApplicationPage)
-      appPage.tools.appEvalButton.displayed
-      appPage.tools.appEvalButton.click()
-      waitFor { appPage.tools.appEval.dialog.displayed }
-      appPage.tools.appEval.stage.value('3')
-      // integrating the file input with Angular is, interesting, populating this last to check UI responds
-      // properly/immediately
-      appPage.tools.appEval.file.value(
-          new File(getClass().getResource('/AppEvaluationSpec/some.file').toURI()).getAbsoluteFile().getAbsolutePath())
+    ApplicationManagementPage appManPage = to ApplicationManagementPage
+    waitFor { appManPage.application("AppEvaluationApp2").displayed }
+    appManPage.application("AppEvaluationApp2").click()
+    ApplicationPage appPage = at(ApplicationPage)
+    appPage.tools.appEvalButton.displayed
+    appPage.tools.appEvalButton.click()
+    waitFor { appPage.tools.appEval.dialog.displayed }
+    appPage.tools.appEval.stage.value('3')
+    // integrating the file input with Angular is, interesting, populating this last to check UI responds
+    // properly/immediately
+    appPage.tools.appEval.file.value(
+        new File(getClass().getResource('/AppEvaluationSpec/some.file').toURI()).getAbsoluteFile().getAbsolutePath())
 
     then: 'The upload button enables'
-      !appPage.tools.appEval.upload.disabled
+    !appPage.tools.appEval.upload.disabled
 
     when: 'User clicks upload'
-      appPage.tools.appEval.upload.click()
+    appPage.tools.appEval.upload.click()
 
     then: 'User sees the evaluation status screen'
-      //will be disabled initially, until processing complete
-      waitFor { appPage.tools.appEval.viewReport.@disabled || appPage.tools.appEval.status.text() == 'Done' }
-      //so just wait for that to happen
-      waitFor('slow') { !appPage.tools.appEval.viewReport.@disabled }
-      getAvailableWindows().size() == 1
+    //will be disabled initially, until processing complete
+    waitFor { appPage.tools.appEval.viewReport.@disabled || appPage.tools.appEval.status.text() == 'Done' }
+    //so just wait for that to happen
+    waitFor('slow') { !appPage.tools.appEval.viewReport.@disabled }
+    getAvailableWindows().size() == 1
 
     when: 'User clicks to view the report'
-      appPage.tools.appEval.viewReport.click()
+    appPage.tools.appEval.viewReport.click()
 
     then: 'new tab is open on the report page'
-      waitFor { getAvailableWindows().size() == 2 }
-      withWindow(close: true, availableWindows[1]) {
-        waitFor { driver.currentUrl.contains('index.html#/reports/AppEvaluationApp2') }
-      }
+    waitFor { getAvailableWindows().size() == 2 }
+    withWindow(close: true, availableWindows[1]) {
+      waitFor { driver.currentUrl.contains('index.html#/reports/AppEvaluationApp2') }
+    }
 
     and: 'Dialog can be closed'
-      appPage.tools.appEval.close.click()
-      waitFor { !appPage.tools.appEval.dialog.displayed }
+    appPage.tools.appEval.close.click()
+    waitFor { !appPage.tools.appEval.dialog.displayed }
   }
 }
--- insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/PolicyNotificationSpec.groovy
+++ insight-brain-functional-test/src/test/java/com/sonatype/insight/brain/testing/functional/PolicyNotificationSpec.groovy
@@ -9,12 +9,12 @@ import com.sonatype.insight.brain.testing.functional.modules.ActionModule
 import com.sonatype.insight.brain.testing.functional.modules.ImportPolicyModule
 import com.sonatype.insight.brain.testing.functional.modules.NotificationsModule
 import com.sonatype.insight.brain.testing.functional.modules.PolicyEditorModule
+
 import spock.lang.Stepwise
 
 @Stepwise
 class PolicyNotificationSpec
-    extends BaseSpec
-{
+extends BaseSpec {
   static PolicyEditorModule policyEditor;
   static OrganizationPage organizationPage;
 
@@ -35,102 +35,102 @@ class PolicyNotificationSpec
 
   def 'Opens a modal to edit notifications'() {
     given: 'The action table has fully loaded'
-      waitFor { !policyEditor.actions.empty }
+    waitFor { !policyEditor.actions.empty }
 
     when: 'The first stage notifications envelope is clicked'
-      ActionModule firstStage = policyEditor.actions[0]
-      firstStage.notificationButton.click()
+    ActionModule firstStage = policyEditor.actions[0]
+    firstStage.notificationButton.click()
 
     then: 'Notifications dialog is shown'
-      waitFor { notificationModal.displayed }
+    waitFor { notificationModal.displayed }
   }
 
   def 'Adds an email to the list of notified emails'() {
     when: 'An email is added'
-      NotificationsModule notificationModal = notificationModal
-      waitFor { notificationModal.emailInput.displayed } 
-      notificationModal.emailInput.value('test@sonatype.com')
-      notificationModal.addEmailButton.click()
+    NotificationsModule notificationModal = notificationModal
+    waitFor { notificationModal.emailInput.displayed }
+    notificationModal.emailInput.value('test@sonatype.com')
+    notificationModal.addEmailButton.click()
 
     then: 'Email is shown in the list of notified emails'
-      waitFor { notificationModal.selectedEmails.size() == 1 }
-      notificationModal.selectedEmails[0].text() == 'test@sonatype.com'
+    waitFor { notificationModal.selectedEmails.size() == 1 }
+    notificationModal.selectedEmails[0].text() == 'test@sonatype.com'
   }
 
   def 'It validates emails'() {
     when: 'A value that is not an email is entered'
-      NotificationsModule notificationModal = notificationModal
-      notificationModal.emailInput.value('foo')
+    NotificationsModule notificationModal = notificationModal
+    notificationModal.emailInput.value('foo')
 
     then: 'A popover should be displayed indicating an invalid email'
-      popoverText(notificationModal.emailInput) == 'Use valid format: abc@xyz.com'
-    
+    popoverText(notificationModal.emailInput) == 'Use valid format: abc@xyz.com'
+
     when: 'A duplicate email is entered'
-      notificationModal.emailInput.value('test@sonatype.com')
+    notificationModal.emailInput.value('test@sonatype.com')
 
     then: 'A popover should be displayed indicating a duplicate email'
-      popoverText(notificationModal.emailInput) == 'Enter a unique value'
+    popoverText(notificationModal.emailInput) == 'Enter a unique value'
   }
 
   def 'Adds a role to the list of roles'() {
     when: 'A role is added'
-      NotificationsModule notificationModal = notificationModal
-      waitFor { notificationModal.roleSelect.displayed }
-      notificationModal.roleSelect.value('0');
-      notificationModal.addRoleButton.click()
+    NotificationsModule notificationModal = notificationModal
+    waitFor { notificationModal.roleSelect.displayed }
+    notificationModal.roleSelect.value('0');
+    notificationModal.addRoleButton.click()
 
     then: 'Role is shown in the list of notified roles'
-      waitFor { notificationModal.selectedRoles.size() == 1 }
-      notificationModal.selectedRoles[0].text() == 'Developer'
+    waitFor { notificationModal.selectedRoles.size() == 1 }
+    notificationModal.selectedRoles[0].text() == 'Developer'
 
     and: 'Expect added role to be removed from select list'
-      notificationModal.roleOptions.size() == 2
-      notificationModal.roleOptions[0].text() == '-- Select Role --'
-      notificationModal.roleOptions[1].text() == 'Owner'
+    notificationModal.roleOptions.size() == 2
+    notificationModal.roleOptions[0].text() == '-- Select Role --'
+    notificationModal.roleOptions[1].text() == 'Owner'
   }
 
   def 'Disables role selector when there are no available roles'() {
     when: 'Last role is added'
-      NotificationsModule notificationModal = notificationModal
-      waitFor { notificationModal.roleSelect.displayed }
-      notificationModal.roleSelect.value('0');
-      notificationModal.addRoleButton.click()
+    NotificationsModule notificationModal = notificationModal
+    waitFor { notificationModal.roleSelect.displayed }
+    notificationModal.roleSelect.value('0');
+    notificationModal.addRoleButton.click()
 
     then: 'Role selector is disabled'
-      notificationModal.roleSelect.enabled == false
-      notificationModal.addRoleButton.enabled == false
+    notificationModal.roleSelect.enabled == false
+    notificationModal.addRoleButton.enabled == false
   }
 
   def 'Updates notifications on policy'() {
     when: 'Notifications are updated'
-      NotificationsModule modal = notificationModal
-      waitFor { modal.saveButton.displayed }
-      modal.saveButton.click()
+    NotificationsModule modal = notificationModal
+    waitFor { modal.saveButton.displayed }
+    modal.saveButton.click()
 
     then: 'Policy Editor shows updated notification counts'
-      policyEditor.actions[0].notificationCount == "3"
+    policyEditor.actions[0].notificationCount == "3"
 
     when: 'Policy is saved and refreshed'
-      policyEditor.buttons.save.click()
-      waitFor { !policyEditor.buttons.save.displayed }
-      driver.navigate().refresh()
-      waitFor { organizationPage.policies.findPolicyEditor(POLICY_NAME).displayed }
-      policyEditor = organizationPage.policies.findPolicyEditor(POLICY_NAME)
-      waitFor { policyEditor.displayed }
-      policyEditor.editButton.click()
+    policyEditor.buttons.save.click()
+    waitFor { !policyEditor.buttons.save.displayed }
+    driver.navigate().refresh()
+    waitFor { organizationPage.policies.findPolicyEditor(POLICY_NAME).displayed }
+    policyEditor = organizationPage.policies.findPolicyEditor(POLICY_NAME)
+    waitFor { policyEditor.displayed }
+    policyEditor.editButton.click()
 
     then: 'Policy editor still shows notification counts'
-      waitFor { policyEditor.actions[0].notificationCount == '3' }
+    waitFor { policyEditor.actions[0].notificationCount == '3' }
 
     when: 'Notifications editor is opened'
-      ActionModule firstStage = policyEditor.actions[0]
-      firstStage.notificationButton.click()
-      modal = notificationModal
-      waitFor { modal.displayed }
+    ActionModule firstStage = policyEditor.actions[0]
+    firstStage.notificationButton.click()
+    modal = notificationModal
+    waitFor { modal.displayed }
 
     then: 'Notifications show up in modal'
-      modal.selectedEmails[0].text() == 'test@sonatype.com'
-      modal.selectedRoles[0].text() == 'Developer'
-      modal.selectedRoles[1].text() == 'Owner'
+    modal.selectedEmails[0].text() == 'test@sonatype.com'
+    modal.selectedRoles[0].text() == 'Developer'
+    modal.selectedRoles[1].text() == 'Owner'
   }
 }

06a9509bdb165bd4c05baa1fedd377405006389a
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2015-04-01 13:59:37 +0200

    Used generic "present" for end year in copyright header

--- insight-brain-performance-test/runTests.sh
+++ insight-brain-performance-test/runTests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2011-2015 Sonatype, Inc. All rights reserved.
+# Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
 # Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
 # "Sonatype" is a trademark of Sonatype, Inc.
 #

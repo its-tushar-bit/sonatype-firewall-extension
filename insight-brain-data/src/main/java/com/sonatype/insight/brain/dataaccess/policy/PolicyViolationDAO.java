feb671bf653533307cd0035424d6c9c73d3d3a27
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2015-05-21 23:54:33 +0200

    Formatted code

--- insight-brain-data/src/main/java/com/sonatype/insight/brain/dataaccess/policy/PolicyViolationDAO.java
+++ insight-brain-data/src/main/java/com/sonatype/insight/brain/dataaccess/policy/PolicyViolationDAO.java
@@ -155,6 +155,4 @@ public class PolicyViolationDAO
     }
     return firstViolation;
   }
-
-
 }
--- insight-brain-data/src/test/java/com/sonatype/insight/brain/dataaccess/policy/PolicyViolationDAOTest.java
+++ insight-brain-data/src/test/java/com/sonatype/insight/brain/dataaccess/policy/PolicyViolationDAOTest.java
@@ -25,6 +25,7 @@ import com.sonatype.insight.brain.model.policy.stages.ReleaseStageType;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import org.junit.Test;
+
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;

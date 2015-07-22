cb236f06efe045ea3739f93923ea6eb809f4e648
Matthew Piggott <mpiggott@sonatype.com>
2015-07-22 12:25:26 -0400

    Create deferred - CLM-4656 CLM-4898

--- insight-brain-service/src/main/frontend/assets/components/OwnerControllers.js
+++ insight-brain-service/src/main/frontend/assets/components/OwnerControllers.js
@@ -102,6 +102,7 @@
               });
             }
             else {
+              deferred = $q.defer();
               form.find('*[upload-submit]').click();
             }
             return deferred.promise;

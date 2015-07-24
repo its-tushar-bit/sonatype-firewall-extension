e13e51f68e25a72ef0dce885253a607fd0af935b
vladt <vladt@sonatype.com>
2015-07-24 11:55:46 -0400

    Fixed @since for the Owner interface

--- insight-brain-data/src/main/java/com/sonatype/insight/brain/model/Owner.java
+++ insight-brain-data/src/main/java/com/sonatype/insight/brain/model/Owner.java
@@ -10,7 +10,7 @@ import com.sonatype.insight.model.HasStringId;
 /**
  * Interface for entities that can own other entities, like {@link Organization} and {@link Application}.
  * 
- * @since 1.16.0
+ * @since 1.17.0
  */
 public interface Owner
     extends HasStringId

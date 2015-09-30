bcb5d7ffdf38a9a531d488707a27969477f8e2eb
Justin Young <jyoung@sonatype.com>
2015-09-30 16:32:20 -0700

    CLM-5191 Label Editor Color Picker. Fixes #1448.

---
+++ insight-brain-service/src/main/frontend/utility/widgets/color.picker.directive.js
@@ -0,0 +1,33 @@
+/**
+ * @license Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+(function(angular) {
+  'use strict';
+
+  function ColorPickerController() {
+    var vm = this;
+
+    vm.colorRows = [
+      [ 'light-red', 'yellow', 'light-green', 'light-blue', 'light-purple' ],
+      [ 'dark-red', 'orange', 'dark-green', 'dark-blue', 'dark-purple' ]
+    ];
+  }
+
+  function ColorPicker() {
+    return {
+      scope: {
+        colorPicker: '='
+      },
+      templateUrl: 'utility/widgets/color.picker.directive.html',
+      controller: ColorPickerController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+  }
+
+  angular
+      .module('utility')
+      .directive('colorPicker', ColorPicker);
+}(angular));

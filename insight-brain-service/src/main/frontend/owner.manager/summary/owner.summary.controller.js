its-tushar-bit/sonatype-firewall-extension

commit ed7eaef3aef6b587e0e8b45b77f4a26be0cd3d3d

CLM-4842, CLM-5318 Create link to evaluation reports Fixes #1481

insight-brain-service/src/main/frontend/owner.manager/summary/owner.summary.controller.js
@@ -6,48 +6,86 @@
 (function(angular) {
   'use strict';
 
-  function OwnerSummaryController($scope, $state, OwnerEditor, ApplicationStore, OrganizationStore) {
-    var siblings;
+  function OwnerSummaryController($state, $q, $http, $window, OwnerEditor, ApplicationStore, OrganizationStore,
+                                  CLMLocations,
+                                  StageTypeStore)
+  {
+    var vm = this;
 
-    $scope.doLoad = function() {
-      var isApp = $state.current.name.indexOf('application') !== -1, stateIdField = isApp ? 'applicationPublicId'
-          : 'organizationId', idField = isApp ? 'publicId' : 'id';
+    vm.error = undefined;
+    vm.owner = undefined;
+    vm.stages = undefined;
+    vm.doLoad = doLoad;
+    vm.edit = edit;
+    vm.getShortTypeName = getShortTypeName;
+    vm.openReport = openReport;
 
-      $scope.type = isApp ? 'application' : 'organization';
+    var siblings,
+        isApp = $state.current.name.indexOf('application') !== -1,
+        stateIdField = isApp ? 'applicationPublicId' : 'organizationId',
+        idField = isApp ? 'publicId' : 'id',
+        type = isApp ? 'application' : 'organization';
 
-      (isApp ? ApplicationStore : OrganizationStore)[$scope.error ? 'refresh' : 'get']().then(function(candidates) {
-        siblings = candidates;
-        angular.forEach(candidates, function(candidate) {
+    vm.doLoad();
+
+    function doLoad() {
+      var promises = [
+        ( isApp ? ApplicationStore : OrganizationStore)[vm.error ? 'refresh' : 'get']()
+      ];
+
+      if (isApp) {
+        promises.push(StageTypeStore.getDashboardStages());
+        promises.push($http.get(CLMLocations.getApplicationSummaryUrl($state.params[stateIdField])));
+      }
+
+      $q.all(promises).then(function(results) {
+        siblings = results[0];
+        angular.forEach(siblings, function(candidate) {
           if (candidate[idField] === $state.params[stateIdField]) {
-            $scope.owner = candidate;
+            vm.owner = candidate;
           }
         });
 
-        if (!$scope.owner) {
-          $scope.error = 'Unable to locate ' + $scope.type;
+        if (isApp) {
+          vm.stages = results[1];
+          vm.applicationSummary = results[2].data;
+        }
+
+        if (!vm.owner) {
+          vm.error = 'Unable to locate ' + type;
         }
       }, function() {
-        $scope.error = arguments;
+        vm.error = arguments;
       });
 
-      delete $scope.error;
-    };
+      delete vm.error;
+    }
 
-    $scope.edit = function() {
-      OwnerEditor.open($scope.owner, $scope.type, siblings);
-    };
+    function edit() {
+      OwnerEditor.open(vm.owner, type, siblings);
+    }
 
-    $scope.getShortTypeName = function() {
-      return $scope.type === 'application' ? 'App' : 'Org';
-    };
+    function getShortTypeName() {
+      return type === 'application' ? 'App' : 'Org';
+    }
 
-    $scope.doLoad();
+    function openReport(stage) {
+      if (vm.applicationSummary.policyEvaluations[stage.id]) {
+        $window.open($state.href('report', {
+          publicId: vm.applicationSummary.publicId,
+          scanId: vm.applicationSummary.policyEvaluations[stage.id].scanId
+        }), '_blank');
+      }
+    }
   }
 
-  OwnerSummaryController.$inject = ['$scope', '$state', 'OwnerEditorService', 'ApplicationStore', 'OrganizationStore'];
+  OwnerSummaryController.$inject = [
+    '$state', '$q', '$http', '$window', 'OwnerEditorService', 'ApplicationStore', 'OrganizationStore', 'CLMLocations',
+    'StageTypeStore'
+  ];
 
-  angular
-    .module('owner.manager.module')
-    .controller('OwnerSummaryController', OwnerSummaryController);
+  angular//
+      .module('owner.manager.module')//
+      .controller('OwnerSummaryController', OwnerSummaryController);
 
 }(angular));

insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/AbstractSummaryViewTest.java
@@ -220,4 +220,6 @@ public abstract class AbstractSummaryViewTest
     }
     return hierarchySize;
   }
+
+  protected abstract void testReportLinks();
 }

insight-brain-java-functional-test/src/main/java/com/sonatype/clm/testing/functional/elements/ActionDropDown.java
@@ -5,8 +5,13 @@
  */
 package com.sonatype.clm.testing.functional.elements;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ActionDropDown
@@ -26,6 +31,27 @@ public class ActionDropDown
 
   public static SelenideElement actionButton() {
     return root().find("button");
+  }
+
+  public static Condition reportLinkText(String stageName) {
+    stageName = stageName.equals("Stage Release") ? "stage" : stageName;
+    return text("View " + stageName + " report");
+  }
+
+  public static Condition disabled() {
+    return cssClass("disabled");
+  }
+
+  public static String reportLinkUrl(String publicId, String scanId) {
+    return Configuration.baseUrl + "new/assets/index.html#/reports/" + publicId + "/" + scanId;
+  }
+
+  public static ElementsCollection reportLinks() {
+    return root().findAll("#app-report-link");
+  }
+
+  public static SelenideElement reportLink(int num) {
+    return reportLinks().get(num);
   }
 
 }

insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/ApplicationSummaryViewTest.java
@@ -5,9 +5,25 @@
  */
 package com.sonatype.clm.testing.functional.brain;
 
-import com.sonatype.insight.brain.model.Application;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.sonatype.clm.testing.functional.elements.ActionDropDown;
+import com.sonatype.insight.brain.model.Application;
+import com.sonatype.insight.brain.model.policy.PolicyEvaluation;
+import com.sonatype.insight.brain.model.policy.StageType;
+import com.sonatype.insight.brain.model.policy.stages.StageTypes;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Condition.visible;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class ApplicationSummaryViewTest
     extends AbstractSummaryViewTest
@@ -21,5 +37,51 @@ public class ApplicationSummaryViewTest
   public void init() {
     application = tempEntity.newApplicationWithParent(getClass().getSimpleName(), YE_OLE_APPLICATION);
     super.init(application);
+  }
+
+  @Override
+  @Test
+  public void testReportLinks() {
+    List<StageType> stages = new ArrayList<>();
+    stages.add(StageTypes.BUILD);
+    stages.add(StageTypes.STAGE_RELEASE);
+    stages.add(StageTypes.RELEASE);
+    stages.add(StageTypes.OPERATE);
+
+    ActionDropDown.actionButton().click();
+    ActionDropDown.reportLinks().shouldHaveSize(stages.size());
+
+    for (int i = 0; i < ActionDropDown.reportLinks().size(); i++) {
+      ActionDropDown.reportLink(i).shouldBe(visible, ActionDropDown.disabled())
+          .shouldHave(ActionDropDown
+              .reportLinkText(stages.get(i).getName()));
+    }
+
+    List<PolicyEvaluation> policyEvaluations = new ArrayList<>();
+
+    for (StageType stage : stages) {
+      policyEvaluations.add(
+          tempEntity.newPolicyEvaluation(application.getId(), stage.getId(), stage.getId() + "FakeScanID"));
+    }
+
+    refresh();
+
+    ActionDropDown.actionButton().click();
+    ActionDropDown.reportLinks().shouldHaveSize(policyEvaluations.size());
+
+    for (int i = 0; i < ActionDropDown.reportLinks().size(); i++) {
+      ActionDropDown.reportLink(i).shouldBe(visible).shouldNotBe(ActionDropDown.disabled()).shouldHave(
+          ActionDropDown.reportLinkText(stages.get(i).getName()));
+
+      ActionDropDown.reportLink(i).followLink();
+      Selenide.switchTo().window(1);
+
+      assertThat(WebDriverRunner.getWebDriver().getCurrentUrl(),
+          equalTo(ActionDropDown.reportLinkUrl(application.getPublicId(), policyEvaluations.get(i).getScanId())));
+
+      WebDriverRunner.getWebDriver().close();
+      Selenide.switchTo().window(0);
+      ActionDropDown.actionButton().click();
+    }
   }
 }

insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/OrganizationSummaryViewTest.java
@@ -5,9 +5,13 @@
  */
 package com.sonatype.clm.testing.functional.brain;
 
+import com.sonatype.clm.testing.functional.elements.ActionDropDown;
 import com.sonatype.insight.brain.model.Organization;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static com.codeborne.selenide.CollectionCondition.empty;
 
 public class OrganizationSummaryViewTest
     extends AbstractSummaryViewTest
@@ -21,5 +25,12 @@ public class OrganizationSummaryViewTest
   public void init() {
     organization = tempEntity.newOrganization(YE_OLE_ORGANIZATION);
     super.init(organization);
+  }
+
+  @Override
+  @Test
+  public void testReportLinks() {
+    ActionDropDown.actionButton().click();
+    ActionDropDown.reportLinks().shouldBe(empty);
   }
 }

insight-brain-service/src/main/frontend/owner.manager/summary/owner.summary.view.html
@@ -1,20 +1,26 @@
 <div class="tile-container">
-  <div class="tile summary-tile" id='owner-summary' ng-controller="OwnerSummaryController">
-    <div load-wrapper="error" loading="!owner" reload="doLoad()">
+  <div class="tile summary-tile" id='owner-summary' ng-controller="OwnerSummaryController as vm">
+    <div load-wrapper="vm.error" loading="!vm.owner" reload="vm.doLoad()">
       <div class="clm-dropdown pull-right" id="action-dropdown">
         <button class="btn btn-default dropdown-toggle btn-tertiary" type="button" id="action-menu" data-toggle="dropdown">
           Actions
           <span class="caret"></span>
         </button>
         <ul class="dropdown-menu dropdown-menu-left">
-          <li><a ng-click='edit()' id="app-org-link"><span><i class="fa fa-pencil fa-fw" />Edit {{:: getShortTypeName()}} Name / Icon</span></a></li>
-          <li><a id="eval-ahc-link"><i class="fa fa-upload fa-fw" /><span>Evaluate AhcService</span></a></li>
-          <li><a id="import-policies-link"><i class="fa fa-download fa-fw" /><span>Import Policies</span></a></li>
-          <li><a id="delete-ahc-link"><i class="fa fa-trash fa-fw" /><span>Delete AhcService</span></a></li>
+          <li><a ng-click='vm.edit()' id="app-org-link"><span><i class="fa fa-pencil fa-fw"/>Edit {{:: vm.getShortTypeName()}} Name / Icon</span></a></li>
+          <li><a id="eval-ahc-link"><i class="fa fa-upload fa-fw"/><span>Evaluate AhcService</span></a></li>
+          <li><a id="import-policies-link"><i class="fa fa-download fa-fw"/><span>Import Policies</span></a></li>
+          <li><a id="delete-ahc-link"><i class="fa fa-trash fa-fw"/><span>Delete AhcService</span></a></li>
+          <li ng-repeat="stage in ::vm.stages">
+            <a id="app-report-link" class="link"
+               ng-class="{'disabled': !vm.applicationSummary.policyEvaluations[stage.id]}"
+               ng-click="vm.openReport(stage)">
+              <i class="fa fa-external-link fa-fw"/><span>View {{::stage.name | lowercase}} report</span></a>
+          </li>
         </ul>
       </div>
-      <h1><span owner-image="owner"></span> {{owner.name}}</h1>
-      <div class="sub-header" ng-if="owner.description">{{owner.description}}</div>
+      <h1><span owner-image="owner"></span> {{vm.owner.name}}</h1>
+      <div class="sub-header" ng-if="vm.owner.description">{{vm.owner.description}}</div>
 
       <ul class="nav nav-pills">
         <li class="active"><a data-toggle="pill" data-target="#owner-pill-app-type" ng-href="">Application Types</a></li>

insight-brain-service/src/main/frontend/scss/_widgets.scss
@@ -537,6 +537,27 @@
         color: inherit;
         @extend .semi-bold;
 
+        &.link {
+          &.disabled {
+            color: $light-blue;
+            cursor: default;
+
+            &:hover {
+              background: $off-white;
+              border-bottom: 1px solid $light-blue;
+            }
+          }
+
+          &:not(.disabled) {
+            color: $link-blue;
+
+            &:hover {
+              background: $off-white;
+              border-bottom: 1px solid $link-blue;
+            }
+          }
+        }
+
         span {
           display: inline-block;
           vertical-align: middle;

insight-brain-service/src/main/frontend/util/Stores.js
@@ -54,13 +54,14 @@
       var stageTypeStore = CLMResource.getStore({
         id: 'id',
         url: CLMLocations.getActionStageUrl()
-      }), promise = stageTypeStore.get();
+      });
+
       return {
         'get': function() {
-          return promise;
+          return stageTypeStore.get();
         },
         'getDashboardStages': function() {
-          return promise.then(function(result) {
+          return stageTypeStore.get().then(function(result) {
             result = angular.copy(result);
             for (var i = 0; i < result.length; i++) {
               if (result[i].id === 'develop') {

commit ca7590ae42d267c1ddf56876439386d7b39e6ff4

Quick fix for failing build due to different error path introduced in ed7eaef.

insight-brain-service/src/main/frontend/owner.manager/summary/owner.summary.controller.js
@@ -24,7 +24,8 @@
         isApp = $state.current.name.indexOf('application') !== -1,
         stateIdField = isApp ? 'applicationPublicId' : 'organizationId',
         idField = isApp ? 'publicId' : 'id',
-        type = isApp ? 'application' : 'organization';
+        type = isApp ? 'application' : 'organization',
+        id = $state.params[stateIdField];
 
     vm.doLoad();
 
@@ -35,7 +36,7 @@
 
       if (isApp) {
         promises.push(StageTypeStore.getDashboardStages());
-        promises.push($http.get(CLMLocations.getApplicationSummaryUrl($state.params[stateIdField])));
+        promises.push($http.get(CLMLocations.getApplicationSummaryUrl(id)));
       }
 
       $q.all(promises).then(function(results) {
@@ -52,10 +53,10 @@
         }
 
         if (!vm.owner) {
-          vm.error = 'Unable to locate ' + type;
+          vm.error = 'Could not find an ' + type + ' with ID ' +  id + '.';
         }
-      }, function() {
-        vm.error = arguments;
+      }, function(error) {
+        vm.error = error;
       });
 
       delete vm.error;

insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/AbstractSummaryViewTest.java
@@ -65,7 +65,7 @@ public abstract class AbstractSummaryViewTest
 
     ErrorBox error = OwnerSummaryPage.SummaryTile.error();
     error.root().shouldBe(visible);
-    error.message().shouldHave(text("unable to locate"));
+    error.message().shouldHave(text("Could not find an " + currentOwner.getType().toString()));
     error.retryButton().shouldBe(visible, enabled);
   }
 
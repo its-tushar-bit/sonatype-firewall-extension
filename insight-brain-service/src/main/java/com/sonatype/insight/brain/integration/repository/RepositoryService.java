f386ee48321b997c5cda41231cb7299fa6f2760a
Matthew Piggott <mpiggott@sonatype.com>
2015-12-04 13:00:43 -0500

    Missing import?

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/integration/repository/RepositoryService.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/integration/repository/RepositoryService.java
@@ -8,6 +8,7 @@ package com.sonatype.insight.brain.integration.repository;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;

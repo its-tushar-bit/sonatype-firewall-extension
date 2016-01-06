92671b70b0b3e812a0d18db2cdbac1bd29ce956a
Matthew Piggott <mpiggott@sonatype.com>
2016-01-06 09:29:22 -0500

    Remove legacy ImportPolicyController from frontend
    Fixes #1670

--- insight-brain-service/src/test/frontend/assets/EditorToolsSpec.js
+++ insight-brain-service/src/test/frontend/assets/EditorToolsSpec.js
@@ -229,104 +229,6 @@ describe('EditorToolsSpec', function() {
     });
   });
 
-  describe('Policy import', function(){
-    beforeEach(inject(function ($controller, $window) {
-      $controller('ImportPolicyController', {
-        $scope : scope 
-      });
-      $window.FileReader = FakeReader;
-
-      FakeReader.prototype.readAsText = function () {};
-    }));
-
-    it('Initial State', inject(function ($timeout) {
-      var spy = spyOn(angular, 'element'),
-          selectedFiles = [];
-      expect(scope.btnDisabled).toBeTruthy();
-      expect(scope.requestActive).toBeFalsy();
-      expect(scope.error).toBeFalsy();
-
-      spy.andReturn([{ files : selectedFiles }]);
-      $timeout.flush();
-      expect(scope.btnDisabled).toBeTruthy();
-
-      // Button should enable once a file is selected
-      selectedFiles.push({});
-      $timeout.flush();
-      expect(scope.btnDisabled).toBeFalsy();
-    }));
-
-    describe('IE9', function () {
-      beforeEach(inject(function ($window) {
-        // IE9 doesn't support the FileReader API, simulate this
-        $window.FileReader = null;
-        scope.doSubmit();
-      }));
-
-      it('Error', inject(function ($window) {
-        expect(scope.requestActive).toBeTruthy();
-        expect(scope.error).toBeFalsy();
-
-        // Progress notification
-        scope.uploaded('Please wait...', false);
-        expect(scope.requestActive).toBeTruthy();
-        expect(scope.error).toBeFalsy();
-
-        scope.uploaded('Error', true);
-        expect(scope.requestActive).toBeFalsy();
-        expect(scope.error[0].msg).toEqual('Error');
-      }));
-
-      it('Successful', inject(function ($window) {
-        expect(scope.requestActive).toBeTruthy();
-        expect(scope.error).toBeFalsy();
-
-        // Progress notification
-        scope.uploaded('Please wait...', false);
-        expect(scope.requestActive).toBeTruthy();
-        expect(scope.error).toBeFalsy();
-
-        scope.uploaded('', true);
-        expect(scope.requestActive).toBeFalsy();
-        expect(scope.error).toBeFalsy();
-      }));
-    });
-
-    describe('File Selected', function () {
-      beforeEach(inject(function ($timeout) {
-        spyOn(angular, 'element').andReturn([{ files : [{}] }]);
-        $timeout.flush();
-      }));
-
-      it('Simulate Firefox exception', inject(function ($timeout) {
-        FakeReader.prototype.readAsText = function () {
-          throw new Error("Foo");
-        };
-        scope.doSubmit();
-        expect(scope.error[0].msg).toEqual('Foo');
-      }));
-
-      it('Read Failure', function () {
-        scope.doSubmit();
-        expect(scope.requestActive).toBeTruthy();
-
-        reader.error = {
-           message : 'FooBar'
-        };
-        reader.onerror();
-        expect(scope.error[0].msg).toEqual('FooBar');
-        expect(scope.requestActive).toBeFalsy();
-      });
-
-      it('Success', function () {
-        scope.doSubmit();
-        expect(scope.requestActive).toBeTruthy();
-
-        reader.result = 'FOO';
-        reader.onload();
-      });
-    });
-
     describe('clmEditable', function () {
       var scope, directiveScope, element;
 
@@ -424,5 +326,4 @@ describe('EditorToolsSpec', function() {
         });
       });
     });
-  });
 });

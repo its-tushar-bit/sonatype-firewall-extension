22c13b1b446593e7de2052ca65a5781d87a592ff
vladt <vladt@sonatype.com>
2016-01-13 15:26:07 -0500

    Fixed package for IdValidationUtilsTest

--- insight-brain-data/src/test/java/com/sonatype/insight/brain/utils/IdValidationUtilsTest.java
+++ insight-brain-data/src/test/java/com/sonatype/insight/brain/utils/IdValidationUtilsTest.java
@@ -3,7 +3,7 @@
  * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
-package utils;
+package com.sonatype.insight.brain.utils;
 
 import com.sonatype.insight.brain.utils.IdValidationUtils;
 import com.sonatype.insight.error.exception.BadRequestException;

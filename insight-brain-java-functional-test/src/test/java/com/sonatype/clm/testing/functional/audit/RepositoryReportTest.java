a814d517bf623136fbf6e709b4f9ed23e7c3d176
vladt <vladt@sonatype.com>
2016-02-25 16:27:22 -0500

    Removed bogus "throws IOException"

--- insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/audit/RepositoryReportTest.java
+++ insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/audit/RepositoryReportTest.java
@@ -67,17 +67,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static com.codeborne.selenide.CollectionCondition.texts;
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.cssClass;
-import static com.codeborne.selenide.Condition.disabled;
-import static com.codeborne.selenide.Condition.disappear;
-import static com.codeborne.selenide.Condition.enabled;
-import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Condition.present;
-import static com.codeborne.selenide.Condition.selected;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.value;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -255,7 +245,7 @@ public class RepositoryReportTest
     testUnknownComponentCIP();
   }
 
-  private void setupHDSFirewallResponse(String hash) throws IOException {
+  private void setupHDSFirewallResponse(String hash) {
     ComponentEvaluationDataList hdsResult = new ComponentEvaluationDataList();
     hdsResult.components = new ArrayList<ComponentEvaluationData>();
     ComponentEvaluationData componentEvaluationData = new ComponentEvaluationData();

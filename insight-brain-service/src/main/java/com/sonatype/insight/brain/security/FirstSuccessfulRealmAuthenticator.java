e0927e77dcc13b9932de1b8c7e7e27c479464141
vladt <vladt@sonatype.com>
2016-03-03 13:50:25 -0500

    Catch Exception instead of Throwable during auth attempts (PR #1815)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/security/FirstSuccessfulRealmAuthenticator.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/security/FirstSuccessfulRealmAuthenticator.java
@@ -57,8 +57,8 @@ class FirstSuccessfulRealmAuthenticator
           }
           log.trace("Realm [{}] returned null during a multi-realm authentication attempt.", realm);
         }
-        catch (Throwable t) {
-          log.debug("Realm [{}] threw an exception during a multi-realm authentication attempt:", realm, t);
+        catch (Exception e) {
+          log.debug("Realm [{}] threw an exception during a multi-realm authentication attempt:", realm, e);
         }
       }
       else {

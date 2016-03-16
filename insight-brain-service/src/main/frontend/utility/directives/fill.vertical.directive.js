71ed29b08fee2537ec612d7f60ffdf93d7f737b7
Matthew Piggott <mpiggott@sonatype.com>
2016-03-16 13:11:20 -0400

    Update root organization migrate styles - CLM-6154 CLM-6122
    Fixes #1865

---
+++ insight-brain-service/src/main/frontend/utility/directives/fill.vertical.directive.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+(function() {
+  'use strict';
+
+  function FillVertical($window, $timeout, maximizeHeightService) {
+    function link(scope, element) {
+      var timerId;
+
+      function updateDimensions() {
+          timerId = maximizeHeightService.updateDimensions(element, {bottomPadding: 0}) || timerId;
+      }
+
+      function dedupe() {
+        if (timerId) {
+          $timeout.cancel(timerId);
+        }
+        timerId = $timeout(updateDimensions, 20);
+      }
+
+      $timeout(updateDimensions, 100);
+      if (!$.browser.msie || $.browser.version > 8) {
+        $($window).resize(dedupe);
+      }
+
+      scope.$on('$destroy', function () {
+        $($window).unbind('resize', dedupe);
+      });
+    }
+
+    return {
+      link: link
+    };
+  }
+  FillVertical.$inject = ['$window', '$timeout', 'maximizeHeightService'];
+
+  angular.module('utility.directives').directive('fillVertical', FillVertical);
+}());

5a3811a1685184e32d67a697c80c363a711a4bb1
Mark Rekveld <mrekveld@sonatype.com>
2016-03-24 10:18:34 +0100

    CLM-6195 Fix for disappearing padding (PR #1888)

---
+++ insight-brain-service/src/main/frontend/utility/directives/calc.tile.scroll.height.directive.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+(function(angular) {
+  'use strict';
+
+  function CalcTileScrollHeight($interpolate, EventNameConstant) {
+    return {
+      restrict: 'A',
+      link: CalcTileScrollHeightLink
+    };
+
+    function CalcTileScrollHeightLink(scope, element) {
+      var selector = element.attr('calc-tile-scroll-height');
+      if (!selector) {
+        throw new Error('missing selector for calc-tile-scroll-height directive');
+      }
+
+      updateHeight($(selector).outerHeight(true));
+
+      scope.$watch(function() {
+        return $(selector).outerHeight(true);
+      }, function(val) {
+        updateHeight(val);
+      });
+
+      function updateHeight(height) {
+        element.css('height', $interpolate(
+            'calc(100% - ({{height}}px + {{paddingTop}} + {{paddingBottom}} + {{marginTop}} + {{marginBottom}}))')({
+          height: height,
+          paddingTop: element.css('padding-top'),
+          paddingBottom: element.css('padding-bottom'),
+          marginTop: element.css('margin-top'),
+          marginBottom: element.css('margin-bottom')
+        }));
+        scope.$emit(EventNameConstant.RESIZE_SCROLLABLE_AREA);
+      }
+    }
+  }
+
+  CalcTileScrollHeight.$inject = ['$interpolate', 'event.name.constant'];
+
+  angular //
+      .module('utility.directives') //
+      .directive('calcTileScrollHeight', CalcTileScrollHeight);
+
+}(angular));

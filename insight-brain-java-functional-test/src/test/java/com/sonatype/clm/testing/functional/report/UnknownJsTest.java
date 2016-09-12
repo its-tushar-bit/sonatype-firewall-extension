8857e48a1a363205aed7f06ab93b93947b743ad4
Matthew Piggott <mpiggott@sonatype.com>
2016-09-12 10:09:43 -0400

    Add functional test to verify unknown JS via a query param - CLM-6808 CLM-6794 (#2155)

---
+++ insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/report/UnknownJsTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.clm.testing.functional.report;
+
+import java.io.File;
+
+import com.sonatype.clm.testing.functional.AbstractFunctionalTest;
+import com.sonatype.clm.testing.functional.pages.ReportListPage;
+import com.sonatype.clm.testing.functional.pages.ReportPage;
+import com.sonatype.clm.testing.functional.pages.ReportPolicyPage;
+import com.sonatype.clm.testing.functional.utils.TestReportEvaluator;
+import com.sonatype.clm.testing.functional.utils.TestReportUtil;
+import com.sonatype.insight.brain.model.Application;
+import com.sonatype.insight.brain.model.component.MatchState;
+import com.sonatype.insight.brain.model.policy.Condition;
+import com.sonatype.insight.brain.model.policy.Constraint;
+import com.sonatype.insight.brain.model.policy.Policy;
+import com.sonatype.insight.brain.model.policy.conditions.MatchStateConditionType;
+import com.sonatype.insight.brain.service.InsightWork;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Selenide.open;
+
+public class UnknownJsTest
+    extends AbstractFunctionalTest
+{
+  @Rule
+  public TemporaryFolder tempFolder = new TemporaryFolder();
+
+  private static final String scanId = "306e0a923df34c64b836358182b1b902";
+
+  private static final InsightWork work = new InsightWork(testCLMServer.getCLMServer().getConfiguration());
+
+  private Application app;
+
+  private TestReportEvaluator evaluator;
+
+  @BeforeClass
+  public static void startup() {
+    open(ReportListPage.URL);
+    loginAsAdmin();
+  }
+
+  @Before
+  public void start() {
+    app = tempEntity.newApplicationWithParent(WaiverTest.class.getSimpleName());
+    createPolicy(app.getId());
+  }
+
+  @Test
+  public void testViewWaivedPolicyViolations() throws Exception {
+    File report = TestReportUtil.setupReport("/UnknownJsTest", tempFolder.newFile());
+
+    evaluator = new TestReportEvaluator(app, scanId, report.toURI().toURL(), Configuration.baseUrl, work);
+    evaluator.evaluatePolicy();
+    refreshOrOpen(ReportPage.url(app, scanId));
+
+    ReportPage.policyTabButton().click();
+    ReportPolicyPage.rows().shouldHave(texts("frontend.zip"));
+
+    refreshOrOpen(ReportPage.url(app, scanId).replaceAll("index.html", "index.html?unknownjs=true"));
+
+    ReportPage.policyTabButton().click();
+    ReportPolicyPage.rows().shouldHave(texts("frontend.zip", "ComponentDisplayModule.js"));
+  }
+
+  private void createPolicy(String ownerId) {
+    // create policy
+    Condition condition = new Condition(MatchStateConditionType.ID, "is", MatchState.UNKNOWN.getId());
+    Constraint constraint = new Constraint();
+    constraint.setName("Match State Check");
+    constraint.addCondition(condition);
+    Policy policy = new Policy();
+    policy.setName("Unmatched");
+    policy.addConstraint(constraint);
+    policy.setOwnerId(ownerId);
+
+    // add policy
+    tempEntity.newPolicy(policy);
+  }
+}

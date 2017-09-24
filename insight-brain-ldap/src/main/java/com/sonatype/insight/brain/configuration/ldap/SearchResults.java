a4d64fc128c83b5946798878fea54f976abea6d5
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2017-09-24 20:30:15 +0200

    Added debug logging to SearchResults to ease troubleshooting LDAP queries

--- insight-brain-ldap/src/main/java/com/sonatype/insight/brain/configuration/ldap/SearchResults.java
+++ insight-brain-ldap/src/main/java/com/sonatype/insight/brain/configuration/ldap/SearchResults.java
@@ -10,6 +10,8 @@ import javax.naming.NamingException;
 import javax.naming.directory.SearchResult;
 
 import org.apache.shiro.realm.ldap.LdapUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Helper to enable try-with-resources with LDAP searches.
@@ -17,7 +19,11 @@ import org.apache.shiro.realm.ldap.LdapUtils;
 class SearchResults
     implements NamingEnumeration<SearchResult>, AutoCloseable
 {
+  private static final Logger log = LoggerFactory.getLogger(SearchResults.class);
+
   private final NamingEnumeration<SearchResult> delegate;
+
+  private int counter;
 
   public SearchResults(NamingEnumeration<SearchResult> delegate) {
     this.delegate = delegate;
@@ -30,6 +36,7 @@ class SearchResults
 
   @Override
   public SearchResult nextElement() {
+    counter++;
     return delegate.nextElement();
   }
 
@@ -40,11 +47,13 @@ class SearchResults
 
   @Override
   public SearchResult next() throws NamingException {
+    counter++;
     return delegate.next();
   }
 
   @Override
   public void close() {
+    log.debug("Finished LDAP query after {} results", counter);
     LdapUtils.closeEnumeration(delegate);
   }
 }

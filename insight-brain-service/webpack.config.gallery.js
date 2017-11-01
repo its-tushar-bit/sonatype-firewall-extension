2b0f76a20b4d7b0ae106be34daae8d059bb9cad8
Ross Pokorny <rpokorny@sonatype.com>
2017-11-01 16:23:00 -0400

    Open up webpack-dev-server to all IP addresses - CLM-9044 (#2749)

--- insight-brain-service/webpack.config.gallery.js
+++ insight-brain-service/webpack.config.gallery.js
@@ -67,6 +67,7 @@ module.exports = {
     contentBase: ['src/main/component-gallery', 'src/main/frontend', 'target/classes/assets', '.tmp/scss']
         .map(subpath => path.join(__dirname, subpath)),
     publicPath,
-    port: 4040
+    port: 4040,
+    host: '0.0.0.0'
   }
 };

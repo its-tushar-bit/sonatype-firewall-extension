613fed396c59c1f4a2ced27fbff0294957ccfb55
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2018-01-02 19:47:12 +0100

    Converted LoginSpec to LoginTest

---
+++ insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/LoginTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.clm.testing.functional.brain;
+
+import com.sonatype.clm.testing.functional.AbstractFunctionalTest;
+import com.sonatype.clm.testing.functional.elements.LoginModal;
+import com.sonatype.clm.testing.functional.pages.ManagementPage;
+import com.sonatype.clm.testing.functional.pages.OwnerSummaryPage;
+import com.sonatype.clm.testing.functional.pages.ReportListPage;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.After;
+import org.junit.Test;
+
+import static com.codeborne.selenide.Condition.enabled;
+import static com.codeborne.selenide.Condition.focused;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+
+public class LoginTest
+    extends AbstractFunctionalTest
+{
+  private LoginModal loginModal = new LoginModal();
+
+  @After
+  public void clearCookies() {
+    Selenide.clearBrowserCookies();
+  }
+
+  @Test
+  public void testInitialLoginFormState() {
+    refreshOrOpen(ReportListPage.URL);
+    loginModal.shouldBe(visible);
+    loginModal.username().shouldBe(focused);
+    loginModal.loginButton().shouldBe(enabled);
+  }
+
+  @Test
+  public void testValidCredentials() {
+    refreshOrOpen(ReportListPage.URL);
+    loginModal.shouldBe(visible);
+    loginModal.username().setValue("admin");
+    loginModal.password().setValue("admin123");
+    loginModal.loginButton().shouldBe(enabled).click();
+    loginModal.shouldNotBe(visible);
+  }
+
+  @Test
+  public void testInvalidCredentials() {
+    refreshOrOpen(ReportListPage.URL);
+    loginModal.shouldBe(visible);
+    loginModal.username().setValue("unknown");
+    loginModal.password().setValue("user");
+    loginModal.loginButton().shouldBe(enabled).click();
+    loginModal.shouldBe(visible);
+    loginModal.errorMessage().shouldBe(visible).shouldHave(text("Invalid credentials"));
+  }
+
+  @Test
+  public void testAuthenticationSessionStateIsRememberedByCookie() {
+    refreshOrOpen(ManagementPage.URL);
+    loginAsAdmin();
+    OwnerSummaryPage.summaryTile().shouldBe(visible);
+    refreshOrOpen(ReportListPage.URL);
+    ReportListPage.listContainer().shouldBe(visible);
+    clearCookies();
+    refreshOrOpen(ReportListPage.URL);
+    loginModal.shouldBe(visible);
+  }
+
+  @Test
+  public void testLogout() {
+    refreshOrOpen(ReportListPage.URL);
+    loginAsAdmin();
+    logout();
+    loginModal.shouldBe(visible);
+  }
+
+  @Test
+  public void testNavigationWhileLoggedOut() {
+    refreshOrOpen(ReportListPage.URL);
+    loginModal.shouldBe(visible);
+    refreshOrOpen(ManagementPage.URL);
+    loginModal.shouldBe(visible);
+    Selenide.back();
+    loginModal.shouldBe(visible);
+    Selenide.forward();
+    loginModal.shouldBe(visible);
+  }
+}

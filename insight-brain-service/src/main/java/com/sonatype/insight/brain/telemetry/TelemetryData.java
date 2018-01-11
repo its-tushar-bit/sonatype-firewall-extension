7f18a546fa8ef51794614878064c18ec7878808e
jim wilcox <jwilcox@sonatype.com>
2018-01-11 11:44:49 -0500

    Analytics for organization hierarchy when the server starts CLM-9219 CLM-9365 (#2874)
    * Analytics Structure for the info, including names - CLM-9365
    Initial design and creation of the two data structures to support the collection of analytical data.
    
    A port from Nexus 3 code base.  The code modified for IQ.
    Ported Files:
    EventData (Data Structure)
    EventHeader (Data Structure)
    
    Design decisions:
    * Remove factories.  Use instead constructor based creation.
    * Remove sessionId or userId.  The fear of data being trackable. 
    * Change Class name prefix to telemetry.  Better describes classes.
    * Clean up style, javadocs, method signatures and instance declarations to fit iq style.
    * Removal of factories requires similar work to be done during the send task which better fits iq style.
    *Remove RollingCounter.  Not needed.
    *Remove Type.  Not needed.

---
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/telemetry/TelemetryData.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.telemetry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @since 1.43.0
+ */
+public class TelemetryData
+{
+  private final long timestamp;
+
+  private Long duration;
+
+  private Map<String, String> attributes = new HashMap<>();
+
+  public TelemetryData(final long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  public Long getDuration() {
+    return duration;
+  }
+
+  public void setDuration(final Long duration) {
+    this.duration = duration;
+  }
+
+  public Map<String, String> getAttributes() {
+    return attributes;
+  }
+
+  public void setAttributes(final Map<String, String> attributes) {
+    this.attributes = attributes;
+  }
+}
---
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/telemetry/TelemetryHeader.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.telemetry;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @since 1.43.0
+ */
+public class TelemetryHeader
+{
+  /**
+   * Format of the event stream (ex. zip-bundle/1).  Used downstream by a handler to process data associated with
+   * this header.
+   */
+  private final String format;
+
+  private final String product;
+
+  private final String telemetryId;
+
+  private final Date createTime;
+
+  private Map<String, String> attributes = new HashMap<>();
+
+  public TelemetryHeader(final String format, final String product, final Date createTime, final String telemetryId) {
+    this.format = format;
+    this.product = product;
+    this.createTime = createTime;
+    this.telemetryId = telemetryId;
+  }
+
+  public String getFormat() {
+    return format;
+  }
+
+  public String getProduct() {
+    return product;
+  }
+
+  public String getTelemetryId() {
+    return telemetryId;
+  }
+
+  public Map<String, String> getAttributes() {
+    return attributes;
+  }
+
+  public void setAttributes(final Map<String, String> attributes) {
+    this.attributes = attributes;
+  }
+
+  public Date getCreateTime() {
+    return createTime;
+  }
+}

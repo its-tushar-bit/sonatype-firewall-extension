43f89ac2f5056421c81fd9245161896bc6f8d82e
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2018-01-29 16:28:50 +0100

    Added tests for DashboardUtils.getStageTypes()

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/dashboard/DashboardUtilsTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/dashboard/DashboardUtilsTest.java
@@ -6,16 +6,22 @@
 package com.sonatype.insight.brain.dashboard;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 import javax.inject.Inject;
 
+import com.sonatype.insight.brain.TestProductLicenseManager;
 import com.sonatype.insight.brain.dashboard.filters.PolicyThreatCategoryFilter;
 import com.sonatype.insight.brain.dashboard.filters.PolicyThreatLevelFilter;
 import com.sonatype.insight.brain.model.Application;
 import com.sonatype.insight.brain.model.policy.PolicyThreatCategory;
 import com.sonatype.insight.brain.model.policy.PolicyViolation;
+import com.sonatype.insight.brain.model.policy.stages.StageTypes;
+import com.sonatype.insight.brain.product.license.CLMLicenseManager;
+import com.sonatype.insight.error.exception.BadRequestException;
+import com.sonatype.insight.license.model.ProductLicenseDetails;
 
 import com.google.common.base.Predicates;
 import com.google.common.collect.Lists;
@@ -25,13 +31,21 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class DashboardUtilsTest
     extends InjectedTest
 {
   @Inject
   private DashboardUtils dashboardUtils;
+
+  @Inject
+  private CLMLicenseManager clmLicenseManager;
+
+  @Inject
+  private TestProductLicenseManager productLicenseManager;
 
   @Test
   public void testPolicyViolationFilter() {
@@ -106,5 +120,41 @@ public class DashboardUtilsTest
     apps.add(app2);
     appIds = dashboardUtils.getApplicationIds(apps);
     assertThat(appIds, containsInAnyOrder(app1.getId(), app2.getId()));
+  }
+
+  @Test
+  public void testGetStageTypes_StageTypeIdsNull() {
+    assertThat(dashboardUtils.getStageTypes(null),
+        contains(StageTypes.BUILD, StageTypes.STAGE_RELEASE, StageTypes.RELEASE, StageTypes.OPERATE));
+  }
+
+  @Test
+  public void testGetStageTypes_StageTypeIdsEmpty() {
+    assertThat(dashboardUtils.getStageTypes(Collections.emptySet()),
+        contains(StageTypes.BUILD, StageTypes.STAGE_RELEASE, StageTypes.RELEASE, StageTypes.OPERATE));
+  }
+
+  @Test
+  public void testGetStageTypes_InvalidStageTypeId() {
+    try {
+      dashboardUtils.getStageTypes(Collections.singleton("invalid-stage-type-id"));
+      fail("Expected exception");
+    }
+    catch (BadRequestException e) {
+      assertThat(e.getMessage(), is("Invalid stage type: invalid-stage-type-id."));
+    }
+  }
+
+  @Test
+  public void testGetStageTypes_UnlicensedStageTypeId() throws Exception {
+    productLicenseManager.setProducts(ProductLicenseDetails.PRODUCT_RISK);
+    clmLicenseManager.installLicense(null);
+    try {
+      dashboardUtils.getStageTypes(Collections.singleton(StageTypes.BUILD.getId()));
+      fail("Expected exception");
+    }
+    catch (BadRequestException e) {
+      assertThat(e.getMessage(), is("Current license does not support stage type: build."));
+    }
   }
 }

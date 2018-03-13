4eca71f38533f6069a724882abe8b1e48bdea2fc
Ross Pokorny <rpokorny@sonatype.com>
2018-03-13 11:57:37 -0400

    Reference policy integration test - CLM-9636 (#2999)

---
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/service/ReferencePolicyImportIntegrationTest.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.service;
+
+import java.net.URL;
+import java.util.Collection;
+
+import com.sonatype.insight.mock.hds.HdsMockServer;
+import com.sonatype.insight.mock.hds.HdsMockServer.HdsConfigurator;
+import com.sonatype.insight.brain.dataaccess.label.LabelDAO;
+import com.sonatype.insight.brain.dataaccess.license.LicenseThreatGroupDAO;
+import com.sonatype.insight.brain.dataaccess.license.LicenseThreatGroupLicenseDAO;
+import com.sonatype.insight.brain.dataaccess.policy.PolicyDAO;
+import com.sonatype.insight.brain.dataaccess.tag.PolicyTagDAO;
+import com.sonatype.insight.brain.dataaccess.tag.TagDAO;
+import com.sonatype.insight.brain.model.label.Label;
+import com.sonatype.insight.brain.model.license.LicenseThreatGroup;
+import com.sonatype.insight.brain.model.policy.Policy;
+import com.sonatype.insight.brain.model.tag.Tag;
+import com.sonatype.insight.brain.policy.PolicyExportResult;
+import com.sonatype.insight.brain.service.TestInsightBrainService.Configurator;
+import com.sonatype.insight.json.store.JsonUtils;
+
+import org.junit.After;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * This class tests that the actual, current reference policy document can be imported successfully into IQ.
+ * It is similar to parts of NewInstancePopulatorTest, but that test uses a canned policy as test data, and in doing
+ * so, is able to verify the import contents to a greater degree of detail. With this class on the other hand, we don't
+ * want to be updating it every time the reference policy gets updated, so we only test things that don't require
+ * hard-coded assumptions about the policy file contents.  Mainly, that the number of entities in the JSON matches the
+ * number of entities in the database tables after the import.
+ */
+public class ReferencePolicyImportIntegrationTest
+    extends AbstractBrainServiceTest
+{
+  private final PolicyDAO policyDAO = new PolicyDAO();
+
+  private final LabelDAO labelDAO = new LabelDAO();
+
+  private final LicenseThreatGroupDAO licenseThreatGroupDAO = new LicenseThreatGroupDAO();
+
+  private final LicenseThreatGroupLicenseDAO licenseThreatGroupLicenseDAO = new LicenseThreatGroupLicenseDAO();
+
+  private final TagDAO tagDAO = new TagDAO();
+
+  private final PolicyTagDAO policyTagDAO = new PolicyTagDAO();
+
+  private final URL referencePolicyUrl = getClass().getResource("/reference-policies-v2.json");
+
+  @After
+  public void cleanup() {
+    Collection<Policy> policies = policyDAO.getAll();
+    Collection<Label> labels = labelDAO.getAll();
+    Collection<LicenseThreatGroup> ltgs = licenseThreatGroupDAO.getAll();
+    Collection<Tag> tags = tagDAO.getAll();
+
+    for (Policy policy : policies) {
+      policyDAO.delete(policy);
+    }
+
+    for (Label label : labels) {
+      labelDAO.delete(label);
+    }
+
+    for (LicenseThreatGroup ltg : ltgs) {
+      licenseThreatGroupDAO.delete(ltg);
+    }
+
+    for (Tag tag : tags) {
+      tagDAO.delete(tag);
+    }
+
+    // LicenseThreatGroupLicenses and PolicyTags get deleted with the LicenseThreatGroups and Policies
+  }
+
+  @Override
+  public void setUpTestLicenseThreatGroups() {
+    // noop - do not set up test LTGs for this test
+  }
+
+  @Test
+  @ManualServerInit
+  public void testImportCurrentReferencePolicies() throws Exception {
+    Configurator configurator = new Configurator()
+    {
+      @Override
+      public void configure(InsightConfig config) {
+        config.setImportRefrencePoliciesFromHDS(true);
+      }
+    };
+
+    HdsConfigurator hdsConfigurator = new HdsConfigurator()
+    {
+      @Override
+      public void configure(HdsMockServer hdsServer) {
+        hdsServer.setResponseForURI("rest/referencePolicies/v2", referencePolicyUrl, 200);
+        hdsServer.setResponseForURI("rest/license", "{}", 200);
+      }
+    };
+
+    initServer(configurator, hdsConfigurator);
+
+    PolicyExportResult importData = JsonUtils.parse(referencePolicyUrl.openStream(), PolicyExportResult.class);
+    int policyCount = importData.policies.size();
+    int ltgCount = importData.licenseThreatGroups.size();
+    int ltgLicenseCount = importData.licenseThreatGroupLicenses.size();
+    int labelCount = importData.labels.size();
+    int tagCount = importData.tags.size();
+    int policyTagCount = importData.policyTags.size();
+
+    assertThat(policyCount, is(greaterThan(0)));
+    assertThat(ltgCount, is(greaterThan(0)));
+    assertThat(ltgLicenseCount, is(greaterThan(0)));
+    assertThat(labelCount, is(greaterThan(0)));
+    assertThat(tagCount, is(greaterThan(0)));
+    assertThat(policyTagCount, is(greaterThan(0)));
+
+    assertThat(policyDAO.getAll().size(), is(policyCount));
+    assertThat(licenseThreatGroupDAO.getAll().size(), is(ltgCount));
+    assertThat(licenseThreatGroupLicenseDAO.getAll().size(), is(ltgLicenseCount));
+    assertThat(labelDAO.getAll().size(), is(labelCount));
+    assertThat(tagDAO.getAll().size(), is(tagCount));
+    assertThat(policyTagDAO.getAll().size(), is(policyTagCount));
+  }
+}

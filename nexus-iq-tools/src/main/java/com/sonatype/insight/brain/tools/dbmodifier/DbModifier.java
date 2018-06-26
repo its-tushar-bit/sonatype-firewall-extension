7e0f22c5bd0e048a82185bad282e70c7fd5c40f4
Tim Kemper <35043087+tkemperSonatype@users.noreply.github.com>
2018-06-26 08:09:57 -0600

    DB date shift initial task CLM-10349 (#3172)
    * CLM-10349 - DB date shift.

---
+++ nexus-iq-tools/src/main/java/com/sonatype/insight/brain/tools/dbmodifier/DbModifier.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.tools.dbmodifier;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.google.common.annotations.VisibleForTesting;
+
+public class DbModifier
+{
+  private Connection connection;
+
+  private static final String DEFAULT_SCHEMA = "insight_brain_ods";
+
+  private String schemaName;
+
+  @VisibleForTesting
+  static class TableAndColumns
+  {
+    String table;
+
+    final List<String> columns = new ArrayList<>();
+
+  }
+
+  public Connection createDbConnection(final String dbConnectionString,
+                                       final String username,
+                                       final String password,
+                                       final String schemaName) throws SQLException
+  {
+    this.schemaName = schemaName;
+    closeDbConnection();
+    connection = DriverManager.getConnection(dbConnectionString, username, password);
+    return connection;
+  }
+
+  public void closeDbConnection() throws SQLException {
+    if (connection != null) {
+      connection.close();
+      connection = null;
+    }
+  }
+
+  private static String getDefaultDbConnectionString(final File file) {
+    return "jdbc:h2:" + file.getAbsolutePath() +
+        ";DATABASE_TO_UPPER=FALSE;DB_CLOSE_DELAY=-1;LOCK_TIMEOUT=10000;SCHEMA=" + DEFAULT_SCHEMA;
+  }
+
+  /**
+   * @return null if no timestamp values set in db
+   */
+  public Timestamp getMinTimestamp() {
+    List<TableAndColumns> allTablesWithTimestamps = getAllTablesWithTimestampColumns();
+    return getTimestamp(allTablesWithTimestamps, this::getMinTimestampInTable, Comparator.reverseOrder());
+  }
+
+  /**
+   * @return null if no timestamp values set in db
+   */
+  public Timestamp getMaxTimestamp() {
+    List<TableAndColumns> allTablesWithTimestamps = getAllTablesWithTimestampColumns();
+    return getTimestamp(allTablesWithTimestamps, this::getMaxTimestampInTable, Comparator.naturalOrder());
+  }
+
+  private Timestamp getTimestamp(final List<TableAndColumns> tableAndColumnsList,
+                                 final Function<TableAndColumns, Timestamp> timestampEval,
+                                 final Comparator<Timestamp> sortOrder)
+  {
+    return tableAndColumnsList.stream().map(timestampEval).filter(Objects::nonNull).max(sortOrder).orElse(null);
+  }
+
+  @VisibleForTesting
+  Timestamp getMinTimestampInTable(final TableAndColumns tableAndColumns) {
+    return getTimestampInTable(tableAndColumns, false);
+  }
+
+  @VisibleForTesting
+  Timestamp getMaxTimestampInTable(final TableAndColumns tableAndColumns) {
+    return getTimestampInTable(tableAndColumns, true);
+  }
+
+  private Timestamp getTimestampInTable(final TableAndColumns tableAndColumns, boolean isMax) {
+    String maxOrMinSql = getMaxOrMinSql(tableAndColumns, isMax);
+    List<Timestamp> timestamps = retrieveTimestamps(maxOrMinSql);
+
+    Comparator<Timestamp> sortOrder = isMax ? Comparator.naturalOrder() : Comparator.reverseOrder();
+    return timestamps.stream().max(sortOrder).orElse(null);
+  }
+
+  private String getMaxOrMinSql(final TableAndColumns tableAndColumns, boolean isMax) {
+    String sql = "SELECT ";
+    Function<String, String> toColumnExpression = columnName -> (isMax ? "MAX(" : "MIN(") + columnName + ")";
+    sql += tableAndColumns.columns.stream().map(toColumnExpression).collect(Collectors.joining(", "));
+    sql += " FROM " + tableAndColumns.table;
+    return sql;
+  }
+
+  private List<Timestamp> retrieveTimestamps(final String maxOrMinSql) {
+    List<Timestamp> timestamps = new ArrayList<>();
+    try (Statement statement = connection.createStatement()) {
+      ResultSet resultSet = statement.executeQuery(maxOrMinSql);
+      int numOfColumns = resultSet.getMetaData().getColumnCount();
+      while (resultSet.next()) {
+        for (int i = 1; i <= numOfColumns; i++) {
+          Timestamp resultValue = resultSet.getTimestamp(i);
+          if (resultValue != null) {
+            timestamps.add(resultValue);
+          }
+        }
+      }
+    }
+    catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return timestamps;
+  }
+
+  private List<TableAndColumns> getAllTablesWithTimestampColumns() {
+    List<String> allTables = getAllTables();
+    return getAllTimestampColumns(allTables);
+  }
+
+  @VisibleForTesting
+  List<TableAndColumns> getAllTimestampColumns(final List<String> tableNames) {
+    List<TableAndColumns> tablesList = new ArrayList<>();
+    for (String tableName : tableNames) {
+      TableAndColumns tableAndColumns = new TableAndColumns();
+      tableAndColumns.table = tableName;
+
+      try (Statement statement = connection.createStatement()) {
+        ResultSet resultSet = statement.executeQuery(getSqlForTimestampColumns(tableName));
+        while (resultSet.next()) {
+          tableAndColumns.columns.add(resultSet.getString(1));
+        }
+      }
+      catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+      if (!tableAndColumns.columns.isEmpty()) {
+        tablesList.add(tableAndColumns);
+      }
+    }
+    return tablesList;
+  }
+
+  private String getSqlForTimestampColumns(final String tableName) {
+    return "SELECT COLUMN_NAME FROM \"INFORMATION_SCHEMA\".\"COLUMNS\" WHERE TABLE_SCHEMA = '" + schemaName +
+        "' AND TABLE_NAME = '" + tableName + "' AND TYPE_NAME = 'TIMESTAMP'";
+  }
+
+  @VisibleForTesting
+  List<String> getAllTables() {
+    List<String> tableNames = new ArrayList<>();
+    String getTablesSql =
+        "SELECT TABLE_NAME from \"INFORMATION_SCHEMA\".\"TABLES\" where TABLE_SCHEMA = '" + schemaName + "'";
+    try (Statement statement = connection.createStatement()) {
+      ResultSet resultSet = statement.executeQuery(getTablesSql);
+      while (resultSet.next()) {
+        tableNames.add(resultSet.getString(1));
+      }
+      return tableNames;
+    }
+    catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
+
---
+++ nexus-iq-tools/src/test/java/com/sonatype/insight/brain/tools/dbmodifier/DbModifierTest.java
@@ -0,0 +1,231 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.tools.dbmodifier;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
+
+import com.sonatype.insight.brain.tools.dbmodifier.DbModifier.TableAndColumns;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
+
+public class DbModifierTest
+{
+  private static final String TEST_SCHEMA = "TEST_SCHEMA";
+
+  private static final LocalDate MAX_DATE = LocalDate.of(2018, 8, 1);
+
+  private static final LocalDate MIN_DATE = LocalDate.of(2017, 2, 1);
+
+  private static final LocalDate MIN_DATE_TABLE1 = LocalDate.of(2018, 1, 1);
+
+  private static final LocalDate MAX_DATE_TABLE2 = LocalDate.of(2017, 3, 8);
+
+  private static DbModifier dbModifier;
+
+  private static final String TABLE_NAME1 = "table1";
+
+  private static final String TABLE_NAME2 = "table2";
+
+  private static final DateTimeFormatter timestampFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+  private Connection dbConnection;
+
+  @Before
+  public void init() throws Exception {
+    dbModifier = new DbModifier();
+    dbConnection = createTestDbConnection(dbModifier);
+    createTestDb(dbConnection);
+  }
+
+  @After
+  public void cleanup() throws Exception {
+    dropTestDb(dbConnection);
+    dbModifier.closeDbConnection();
+  }
+
+  @Test
+  public void testGetAllTables() {
+    List<String> allTables = dbModifier.getAllTables();
+    assertThat(allTables.size(), is(2));
+    assertThat(allTables, hasItems(TABLE_NAME1, TABLE_NAME2));
+  }
+
+  @Test
+  public void testGetMinTimestampForTable() {
+    List<TableAndColumns> allTimestampColumns = dbModifier.getAllTimestampColumns(dbModifier.getAllTables());
+    allTimestampColumns.sort(Comparator.comparing(item -> item.table));
+
+    Timestamp minTimestampInTable1 = dbModifier.getMinTimestampInTable(allTimestampColumns.get(0));
+    assertThat(minTimestampInTable1, is(getTimestamp(MIN_DATE_TABLE1)));
+
+    Timestamp minTimestampInTable2 = dbModifier.getMinTimestampInTable(allTimestampColumns.get(1));
+    assertThat(minTimestampInTable2, is(getTimestamp(MIN_DATE)));
+  }
+
+  @Test
+  public void testGetMaxTimestampForTable() {
+    List<TableAndColumns> allTimestampColumns = dbModifier.getAllTimestampColumns(dbModifier.getAllTables());
+    allTimestampColumns.sort(Comparator.comparing(item -> item.table));
+
+    Timestamp maxTimestampInTable1 = dbModifier.getMaxTimestampInTable(allTimestampColumns.get(0));
+    assertThat(maxTimestampInTable1, is(getTimestamp(MAX_DATE)));
+
+    Timestamp maxTimestampInTable2 = dbModifier.getMaxTimestampInTable(allTimestampColumns.get(1));
+    assertThat(maxTimestampInTable2, is(getTimestamp(MAX_DATE_TABLE2)));
+  }
+
+  @Test
+  public void testGetMaxTimestamp() {
+    Timestamp maxTimestamp = dbModifier.getMaxTimestamp();
+    assertThat(maxTimestamp, is(getTimestamp(MAX_DATE)));
+  }
+
+  @Test
+  public void testGetMinTimestamp() {
+    Timestamp minTimestamp = dbModifier.getMinTimestamp();
+    assertThat(minTimestamp, is(getTimestamp(MIN_DATE)));
+  }
+
+  @Test
+  public void testGetMaxTimestampWithAllNulls() throws Exception {
+    dropTestDb(dbConnection);
+    createEmptyTimestampDb(dbConnection);
+    Timestamp maxTimestamp = dbModifier.getMaxTimestamp();
+    assertThat(maxTimestamp, nullValue());
+  }
+
+  @Test
+  public void testGetMinTimestampWithAllNulls() throws Exception {
+    dropTestDb(dbConnection);
+    createEmptyTimestampDb(dbConnection);
+    Timestamp minTimestamp = dbModifier.getMinTimestamp();
+    assertThat(minTimestamp, nullValue());
+  }
+
+  private static Connection createTestDbConnection(DbModifier dbModifier) throws Exception {
+    String dbConnectionString = "jdbc:h2:mem:test" + ";DATABASE_TO_UPPER=FALSE;DB_CLOSE_DELAY=-1;LOCK_TIMEOUT=10000";
+    return dbModifier.createDbConnection(dbConnectionString, "", "", TEST_SCHEMA);
+  }
+
+  private static void dropTestDb(Connection conn) throws Exception {
+    try (Statement statement = conn.createStatement()) {
+      statement.executeUpdate("DROP TABLE IF EXISTS " + TABLE_NAME1);
+      statement.executeUpdate("DROP TABLE IF EXISTS " + TABLE_NAME2);
+      statement.executeUpdate("SET SCHEMA PUBLIC");
+      statement.executeUpdate("DROP SCHEMA IF EXISTS " + TEST_SCHEMA);
+    }
+  }
+
+  private static void createTestDb(Connection conn) throws Exception {
+
+    String tableSql1 = "CREATE TABLE " + TABLE_NAME1 + " (id varchar(5) NOT NULL, ts1 datetime NOT NULL)";
+    try (Statement statement = conn.createStatement()) {
+      statement.executeUpdate("CREATE SCHEMA " + TEST_SCHEMA);
+      statement.executeUpdate("SET SCHEMA " + TEST_SCHEMA);
+      statement.executeUpdate(tableSql1);
+      statement.executeUpdate("ALTER TABLE " + TABLE_NAME1 + " ADD PRIMARY KEY (id)");
+      int index = 1;
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, MIN_DATE_TABLE1));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, LocalDate.of(2018, 2, 1)));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, LocalDate.of(2018, 3, 1)));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, LocalDate.of(2018, 4, 1)));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, LocalDate.of(2018, 5, 1)));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, LocalDate.of(2018, 6, 1)));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, LocalDate.of(2018, 7, 1)));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, MAX_DATE));
+    }
+
+    String tableSql2 = "CREATE TABLE " + TABLE_NAME2 + " (id varchar(5) NOT NULL, ts1 datetime NOT NULL, ts2 datetime)";
+    try (Statement statement = conn.createStatement()) {
+      statement.executeUpdate(tableSql2);
+      statement.executeUpdate("ALTER TABLE " + TABLE_NAME2 + " ADD PRIMARY KEY (id)");
+      int index = 1;
+      statement.executeUpdate(getInsertString(TABLE_NAME2, index++, LocalDate.of(2017, 3, 1), MIN_DATE));
+      statement
+          .executeUpdate(getInsertString(TABLE_NAME2, index++, LocalDate.of(2017, 3, 2), LocalDate.of(2017, 2, 2)));
+      statement
+          .executeUpdate(getInsertString(TABLE_NAME2, index++, LocalDate.of(2017, 3, 3), LocalDate.of(2017, 2, 3)));
+      statement
+          .executeUpdate(getInsertString(TABLE_NAME2, index++, LocalDate.of(2017, 3, 4), LocalDate.of(2017, 2, 4)));
+      statement.executeUpdate(getInsertString(TABLE_NAME2, index++, LocalDate.of(2017, 3, 5), null));
+      statement
+          .executeUpdate(getInsertString(TABLE_NAME2, index++, LocalDate.of(2017, 3, 6), LocalDate.of(2017, 2, 6)));
+      statement
+          .executeUpdate(getInsertString(TABLE_NAME2, index++, LocalDate.of(2017, 3, 7), LocalDate.of(2017, 2, 7)));
+      statement.executeUpdate(getInsertString(TABLE_NAME2, index++, MAX_DATE_TABLE2, LocalDate.of(2017, 2, 8)));
+    }
+  }
+
+  private static void createEmptyTimestampDb(Connection conn) throws Exception {
+
+    String tableSql1 = "CREATE TABLE " + TABLE_NAME1 + " (id varchar(5) NOT NULL, ts1 datetime)";
+    try (Statement statement = conn.createStatement()) {
+      statement.executeUpdate("CREATE SCHEMA " + TEST_SCHEMA);
+      statement.executeUpdate("SET SCHEMA " + TEST_SCHEMA);
+      statement.executeUpdate(tableSql1);
+      statement.executeUpdate("ALTER TABLE " + TABLE_NAME1 + " ADD PRIMARY KEY (id)");
+      int index = 1;
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME1, index++, null));
+    }
+
+    String tableSql2 = "CREATE TABLE " + TABLE_NAME2 + " (id varchar(5) NOT NULL, ts1 datetime, ts2 datetime)";
+    try (Statement statement = conn.createStatement()) {
+      statement.executeUpdate(tableSql2);
+      statement.executeUpdate("ALTER TABLE " + TABLE_NAME2 + " ADD PRIMARY KEY (id)");
+      int index = 1;
+      statement.executeUpdate(getInsertString(TABLE_NAME2, index++, null, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME2, index++, null, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME2, index++, null, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME2, index++, null, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME2, index++, null, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME2, index++, null, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME2, index++, null, null));
+      statement.executeUpdate(getInsertString(TABLE_NAME2, index++, null, null));
+    }
+  }
+
+  private static String getInsertString(String tableName, int id, LocalDate... dates) {
+    String insertString = "INSERT into " + tableName + " values ('id" + id + "'";
+    if (dates == null) {
+      return insertString + ", null)";
+    }
+    for (LocalDate date : dates) {
+      if (date == null) {
+        insertString += ", null";
+      }
+      else {
+        String timestampString = date.format(timestampFormatter) + " 00:00:00";
+        insertString += ", '" + timestampString + "'";
+      }
+    }
+    insertString += ")";
+    return insertString;
+  }
+
+  private static Timestamp getTimestamp(final LocalDate localDate) {
+    return Timestamp.valueOf(localDate.atStartOfDay());
+  }
+}

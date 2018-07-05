c612553986124ce86b3a62e027c9ff5360104447
Vlad Tatavu <vladt@sonatype.com>
2018-07-05 12:27:38 -0400

    Simplify ApplicationAdapterTest by using real code instead of mocks (PR #3198)

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/organization/ApplicationAdapterTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/organization/ApplicationAdapterTest.java
@@ -12,41 +12,38 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
-import com.sonatype.insight.brain.dataaccess.OrganizationDAO;
+import javax.inject.Inject;
+
+import com.sonatype.insight.brain.dataaccess.TemporaryEntity;
 import com.sonatype.insight.brain.model.Application;
 import com.sonatype.insight.brain.model.Organization;
 import com.sonatype.insight.brain.model.security.MemberType;
+import com.sonatype.insight.brain.security.InternalRealm;
 import com.sonatype.insight.brain.security.Member;
 import com.sonatype.insight.brain.security.UserDirectory;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
+import org.eclipse.sisu.launch.InjectedTest;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ApplicationAdapterTest
+    extends InjectedTest
 {
-
-  private static final String REALM = "REALM";
+  @Rule
+  public TemporaryEntity tempEntity = new TemporaryEntity();
 
   private static final String USER_DIRECTORY_ERROR = "User directory query result error.";
 
   private static final String TEST_MESSAGE = "Test Exception Message";
 
+  @Inject
   private ApplicationAdapter applicationAdapter;
-
-  private UserDirectory mockUserDirectory;
-
-  private OrganizationDAO mockOrganizationDAO;
 
   // Application variables
   private String applicationId = "AppId";
@@ -55,7 +52,7 @@ public class ApplicationAdapterTest
 
   private String publicId = "publicId";
 
-  private String organizationId = "OrgId";
+  private String organizationId;
 
   private String organizationName = "My Organization";
 
@@ -67,17 +64,13 @@ public class ApplicationAdapterTest
 
   private String userEmail = "jsmith@sonatype.com";
 
+  @Override
   @Before
-  public void setUp() {
+  public void setUp() throws Exception {
+    super.setUp();
 
-    mockOrganizationDAO = Mockito.mock(OrganizationDAO.class);
-    mockUserDirectory = Mockito.mock(UserDirectory.class);
-    applicationAdapter = new ApplicationAdapter(mockUserDirectory, mockOrganizationDAO);
-
-    // Return this organization when ever the mock organization DAO getByIdNotNull method is called
-    Organization organization = new Organization(organizationName);
-    organization.setId(organizationId);
-    when(mockOrganizationDAO.getByIdNotNull(organizationId)).thenReturn(organization);
+    Organization org = tempEntity.newOrganization(organizationName);
+    organizationId = org.getId();
   }
 
   @Test
@@ -85,13 +78,10 @@ public class ApplicationAdapterTest
 
     Application application = createApplication(organizationId, applicationName, applicationId, contactInternalName);
 
-    // Return this member when ever the mock user directory get members by names is called.
-    Member member = createMember(contactInternalName, userFirstName + " " + userLastName, userEmail, REALM);
-    Set<String> userNames = Sets.newHashSet(contactInternalName);
-    setQueryResultForGetMembersByNames(mockUserDirectory, userNames, Lists.newArrayList(member), null);
+    createMember(contactInternalName, userFirstName, userLastName, userEmail);
 
     ContactDTO expectedContactDTO = createExpectedContact(contactInternalName, userFirstName + " " + userLastName,
-        REALM, userEmail);
+        InternalRealm.DISPLAY_NAME, userEmail);
     ApplicationDTO expectedApplicationDTO = createExpectedDTO(applicationName, applicationId, expectedContactDTO);
 
     ApplicationDTO actualApplicationDTO = applicationAdapter.convert(application);
@@ -104,13 +94,10 @@ public class ApplicationAdapterTest
     Application application = createApplication(organizationId, applicationName, applicationId, contactInternalName);
 
     // Member has a name that is in all upper case but searching is done in a case-insensitive manner.
-    Member member = createMember(contactInternalName.toUpperCase(Locale.ENGLISH), userFirstName + " " + userLastName,
-        userEmail, REALM);
-    Set<String> userNames = Sets.newHashSet(contactInternalName);
-    setQueryResultForGetMembersByNames(mockUserDirectory, userNames, Lists.newArrayList(member), null);
+    createMember(contactInternalName.toUpperCase(Locale.ENGLISH), userFirstName, userLastName, userEmail);
 
     ContactDTO expectedContactDTO = createExpectedContact(contactInternalName.toUpperCase(Locale.ENGLISH),
-        userFirstName + " " + userLastName, REALM, userEmail);
+        userFirstName + " " + userLastName, InternalRealm.DISPLAY_NAME, userEmail);
     ApplicationDTO expectedApplicationDTO = createExpectedDTO(applicationName, applicationId, expectedContactDTO);
 
     ApplicationDTO actualApplicationDTO = applicationAdapter.convert(application);
@@ -121,8 +108,6 @@ public class ApplicationAdapterTest
   public void testConvertApplication_WithUnfoundUser() {
 
     Application application = createApplication(organizationId, applicationName, applicationId, contactInternalName);
-    setQueryResultForGetMembersByNames(mockUserDirectory, Sets.newHashSet(contactInternalName),
-        new ArrayList<Member>(), null);
 
     ContactDTO expectedContact = createExpectedContactForNotFoundError(contactInternalName);
     ApplicationDTO expectedApplicationDTO = createExpectedDTO(applicationName, applicationId, expectedContact);
@@ -135,12 +120,16 @@ public class ApplicationAdapterTest
   public void testConvertApplication_WithUserDirectoryException() {
 
     Application application = createApplication(organizationId, applicationName, applicationId, contactInternalName);
-    setQueryResultForGetMembersByNames(mockUserDirectory, Sets.newHashSet(contactInternalName),
-        new ArrayList<Member>(), new Exception(TEST_MESSAGE));
+
+    UserDirectory.QueryResult result = new UserDirectory.QueryResult(new ArrayList<Member>(),
+        new Exception(TEST_MESSAGE));
+    UserDirectory mockUserDirectory = mock(UserDirectory.class);
+    when(mockUserDirectory.getUsersByName(Collections.singleton(contactInternalName))).thenReturn(result);
 
     ContactDTO expectedContactDTO = createExpectedContactForUserDirectoryError(contactInternalName);
     ApplicationDTO expectedApplicationDTO = createExpectedDTO(applicationName, applicationId, expectedContactDTO);
 
+    applicationAdapter = new ApplicationAdapter(mockUserDirectory);
     ApplicationDTO actualApplicationDTO = applicationAdapter.convert(application);
     assertApplication(actualApplicationDTO, expectedApplicationDTO);
   }
@@ -154,8 +143,6 @@ public class ApplicationAdapterTest
 
     ApplicationDTO actualApplicationDTO = applicationAdapter.convert(application, false);
     assertApplication(actualApplicationDTO, expectedApplicationDTO);
-
-    verifyZeroInteractions(mockUserDirectory);
   }
 
   @Test
@@ -178,17 +165,16 @@ public class ApplicationAdapterTest
       Application application = createApplication(orgId, appName, appId, contactName);
       applications.add(application);
 
-      ContactDTO expectedContactDTO = createExpectedContact(contactName, displayName, REALM, email);
+      ContactDTO expectedContactDTO = createExpectedContact(contactName, displayName, InternalRealm.DISPLAY_NAME,
+          email);
       ApplicationDTO expectedApplicationDTO = createExpectedDTO(appName, appId, expectedContactDTO);
       expectedApplicationDTOs.add(expectedApplicationDTO);
 
       // These members will be returned by the user directory.
-      Member member = createMember(contactName, firstName + " " + lastName, email, REALM);
+      Member member = createMember(contactName, firstName, lastName, email);
       members.add(member);
       memberNames.add(contactName);
     }
-
-    setQueryResultForGetMembersByNames(mockUserDirectory, memberNames, members, null);
 
     List<ApplicationDTO> actualApplicationDTOs = applicationAdapter.convert(applications);
     assertApplications(actualApplicationDTOs, expectedApplicationDTOs);
@@ -199,8 +185,6 @@ public class ApplicationAdapterTest
 
     List<ApplicationManagementSummaryDTO> expectedDTOs = new ArrayList<>();
 
-    Set<String> memberNames = new HashSet<>();
-    List<Member> members = new ArrayList<>();
     List<Application> applications = new ArrayList<>();
 
     for (int i = 1; i <= 5; i++) {
@@ -217,23 +201,16 @@ public class ApplicationAdapterTest
       Application application = createApplication(orgId, appName, appId, contactName);
       applications.add(application);
 
-      ContactDTO expectedContact = createExpectedContact(contactName, displayName, REALM, email);
+      ContactDTO expectedContact = createExpectedContact(contactName, displayName, InternalRealm.DISPLAY_NAME, email);
       expectedDTOs.add(createExpectedApplicationManagementSummaryDTO(orgId, orgName, appName, appId, expectedContact));
 
-      Member member = createMember(contactName, firstName + " " + lastName, email, REALM);
-      memberNames.add(contactName);
-      members.add(member);
+      createMember(contactName, firstName, lastName, email);
     }
-
-    setQueryResultForGetMembersByNames(mockUserDirectory, memberNames, members, null);
 
     List<ApplicationManagementSummaryDTO> actualDTOs = applicationAdapter
         .createApplicationManagementSummaries(applications);
 
     assertApplicationManagementSummaryDTOs(actualDTOs, expectedDTOs);
-
-    // Expect only one call as all applications in the test have the same organization
-    verify(mockOrganizationDAO, times(1)).getByIdNotNull(anyString());
   }
 
   @Test
@@ -260,17 +237,10 @@ public class ApplicationAdapterTest
       memberNames.add(contactName);
     }
 
-    List<Member> members = Collections.emptyList();
-
-    setQueryResultForGetMembersByNames(mockUserDirectory, memberNames, members, null);
-
     List<ApplicationManagementSummaryDTO> actualDTOs = applicationAdapter
         .createApplicationManagementSummaries(applications);
 
     assertApplicationManagementSummaryDTOs(actualDTOs, expectedDTOs);
-
-    // Expect only one call as all applications in the test have the same organization
-    verify(mockOrganizationDAO, times(1)).getByIdNotNull(anyString());
   }
 
   @Test
@@ -298,15 +268,15 @@ public class ApplicationAdapterTest
 
     List<Member> members = Collections.emptyList();
 
-    setQueryResultForGetMembersByNames(mockUserDirectory, memberNames, members, new Exception(TEST_MESSAGE));
+    UserDirectory.QueryResult result = new UserDirectory.QueryResult(members, new Exception(TEST_MESSAGE));
+    UserDirectory mockUserDirectory = mock(UserDirectory.class);
+    when(mockUserDirectory.getUsersByName(memberNames)).thenReturn(result);
 
+    applicationAdapter = new ApplicationAdapter(mockUserDirectory);
     List<ApplicationManagementSummaryDTO> actualDTOs = applicationAdapter
         .createApplicationManagementSummaries(applications);
 
     assertApplicationManagementSummaryDTOs(actualDTOs, expectedDTOs);
-
-    // Expect only one call as all applications in the test have the same organization
-    verify(mockOrganizationDAO, times(1)).getByIdNotNull(anyString());
   }
 
   @Test
@@ -327,21 +297,10 @@ public class ApplicationAdapterTest
       expectedDTOs.add(createExpectedApplicationManagementSummaryDTO(orgId, orgName, appName, appId, null));
     }
 
-    // Internally the null contact names get converted into a list of nulls, which gets converted into a set which would
-    // only contain one null.
-    Set<String> nullMemberNames = new HashSet<>();
-    nullMemberNames.add(null);
-    List<Member> members = Collections.emptyList();
-
-    setQueryResultForGetMembersByNames(mockUserDirectory, nullMemberNames, members, null);
-
     List<ApplicationManagementSummaryDTO> actualDTOs = applicationAdapter
         .createApplicationManagementSummaries(applications);
 
     assertApplicationManagementSummaryDTOs(actualDTOs, expectedDTOs);
-
-    // Expect only one call as all applications in the test have the same organization
-    verify(mockOrganizationDAO, times(1)).getByIdNotNull(anyString());
   }
 
   @Test
@@ -361,23 +320,17 @@ public class ApplicationAdapterTest
       Application application = createApplication(orgId, appName, appId, contactInternalName);
       applications.add(application);
 
-      ContactDTO expectedContact = createExpectedContact(contactInternalName, displayName, REALM, userEmail);
+      ContactDTO expectedContact = createExpectedContact(contactInternalName, displayName, InternalRealm.DISPLAY_NAME,
+          userEmail);
       expectedDTOs.add(createExpectedApplicationManagementSummaryDTO(orgId, orgName, appName, appId, expectedContact));
     }
 
-    Set<String> memberNames = Sets.newHashSet(contactInternalName);
-    List<Member> members = new ArrayList<>();
-    members.add(createMember(contactInternalName, displayName, userEmail, REALM));
-
-    setQueryResultForGetMembersByNames(mockUserDirectory, memberNames, members, null);
+    createMember(contactInternalName, userFirstName, userLastName, userEmail);
 
     List<ApplicationManagementSummaryDTO> actualDTOs = applicationAdapter
         .createApplicationManagementSummaries(applications);
 
     assertApplicationManagementSummaryDTOs(actualDTOs, expectedDTOs);
-
-    // Expect only one call as all applications in the test have the same organization
-    verify(mockOrganizationDAO, times(1)).getByIdNotNull(anyString());
   }
 
   @Test
@@ -385,8 +338,6 @@ public class ApplicationAdapterTest
 
     List<ApplicationManagementSummaryDTO> expectedDTOs = new ArrayList<>();
     List<Application> applications = new ArrayList<>();
-    Set<String> memberNames = new HashSet<>();
-    List<Member> members = new ArrayList<>();
 
     for (int i = 1; i <= 5; i++) {
       String orgId = organizationId;
@@ -405,11 +356,9 @@ public class ApplicationAdapterTest
       ContactDTO expectedContact;
 
       // All names will be passed to the user directory, but only half of them will be found.
-      memberNames.add(contactName);
       if (i % 2 == 0) {
-        Member member = createMember(contactName, displayName, email, REALM);
-        members.add(member);
-        expectedContact = createExpectedContact(contactName, displayName, REALM, email);
+        createMember(contactName, firstName, lastName, email);
+        expectedContact = createExpectedContact(contactName, displayName, InternalRealm.DISPLAY_NAME, email);
       }
       else {
         expectedContact = createExpectedContactForNotFoundError(contactName);
@@ -417,15 +366,10 @@ public class ApplicationAdapterTest
       expectedDTOs.add(createExpectedApplicationManagementSummaryDTO(orgId, orgName, appName, appId, expectedContact));
     }
 
-    setQueryResultForGetMembersByNames(mockUserDirectory, memberNames, members, null);
-
     List<ApplicationManagementSummaryDTO> actualDTOs = applicationAdapter
         .createApplicationManagementSummaries(applications);
 
     assertApplicationManagementSummaryDTOs(actualDTOs, expectedDTOs);
-
-    // Expect only one call as all applications in the test have the same organization
-    verify(mockOrganizationDAO, times(1)).getByIdNotNull(anyString());
   }
 
   private ApplicationManagementSummaryDTO createExpectedApplicationManagementSummaryDTO(String orgId,
@@ -494,12 +438,13 @@ public class ApplicationAdapterTest
     return application;
   }
 
-  private Member createMember(String internalName, String displayName, String email, String realm) {
+  private Member createMember(String username, String firstName, String lastName, String email) {
+    tempEntity.newUser(username, firstName, lastName, email);
     Member member = new Member();
-    member.setInternalName(internalName);
-    member.setDisplayName(displayName);
+    member.setInternalName(username);
+    member.setDisplayName(firstName + " " + lastName);
     member.setEmail(email);
-    member.setRealm(realm);
+    member.setRealm(InternalRealm.DISPLAY_NAME);
     member.setType(MemberType.USER);
 
     return member;
@@ -567,14 +512,4 @@ public class ApplicationAdapterTest
 
     assertContact(actual.getContact(), expected.getContact());
   }
-
-  private void setQueryResultForGetMembersByNames(UserDirectory userDirectory,
-                                                  Set<String> names,
-                                                  List<Member> members,
-                                                  Exception exception)
-  {
-    UserDirectory.QueryResult result = new UserDirectory.QueryResult(members, exception);
-    when(userDirectory.getUsersByName(names)).thenReturn(result);
-  }
-
 }

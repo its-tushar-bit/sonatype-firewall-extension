656c0fb0b6594166bf7ec278dce08c9bb4ba8cc3
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2018-10-25 12:33:18 +0200

    Reverted 84c20882e322b0df01ccaf7ec4444ab574de190e and downgraded to jetty:9.4.11 in order to restore GZIP support as needed by e.g. Eclipse plugin CLM-11151 (PR #3468)

---
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/service/GzipTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Arrays;
+import java.util.zip.GZIPOutputStream;
+
+import com.sonatype.clm.dto.model.component.ComponentIdentifier;
+import com.sonatype.insight.brain.HttpRequest;
+import com.sonatype.insight.brain.HttpResponse;
+import com.sonatype.insight.brain.api.PublicApiPaths;
+import com.sonatype.insight.brain.api.v2.dto.ApiComponentIdentifierDTOV2;
+import com.sonatype.insight.brain.api.v2.service.ApiComponentVersionsServiceV2;
+import com.sonatype.insight.json.store.JsonUtils;
+
+import com.google.common.net.HttpHeaders;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
+
+public class GzipTest
+    extends AbstractResourceTest
+{
+  private byte[] gzip(Object pojo) {
+    try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+      try (GZIPOutputStream gzip = new GZIPOutputStream(baos)) {
+        gzip.write(JsonUtils.generate(pojo));
+      }
+      return baos.toByteArray();
+    }
+    catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+  @Override
+  protected HttpRequest restRequest() {
+    return super.restRequest().header(HttpHeaders.CONTENT_ENCODING, "gzip");
+  }
+
+  @Test
+  public void testCompressedRequestBody() throws Exception {
+    setHdsResponseForURI(ApiComponentVersionsServiceV2.HDS_COMPONENT_VERSIONS_LIST_PATH, Arrays.asList("1.0"), 200);
+
+    ApiComponentIdentifierDTOV2 request = ApiComponentIdentifierDTOV2
+        .fromComponentIdentifier(ComponentIdentifier.createMavenCoordinates("g1", "a1", "v1", "c1", "e1"));
+
+    HttpResponse response = restRequest().path(PublicApiPaths.COMPONENT_VERSIONS_PATH_V2).body(gzip(request)).post();
+    assertResponseStatus(200, response);
+
+    assertThat(response.getBodyList(), contains("1.0"));
+  }
+}

c0d759dd43c0cef81e848294149f59672f622d9b
Marushkevych <marushkevych@gmail.com>
2018-11-19 12:07:14 -0500

    Gallery - multiple example boxes CLM-11128 (#3559)

--- insight-brain-frontend/src/main/component-gallery/app/gallery/itemExampleHtmlDirective/itemExampleHtmlDirective.js
+++ insight-brain-frontend/src/main/component-gallery/app/gallery/itemExampleHtmlDirective/itemExampleHtmlDirective.js
@@ -6,15 +6,25 @@
 function itemExampleHtmlDirective() {
   return {
     restrict: 'E',
+    scope: true,
     compile: function(element) {
 
       var staticHTML = element[0].innerHTML;
 
-      element.html('<div class="iq-tile"><div class="iq-tile-header"><div class="iq-tile-header__title"><h2>Example</h2></div></div>' + staticHTML + '</div>');
-      element.append('<div class="iq-tile"><div class="iq-tile-header"><div class="iq-tile-header__title"><h2>Code</h2></div></div><div hljs hljs-source="code"></div></div>');
+      element.html(`
+        <div class="iq-tile">
+          <div class="iq-tile-header">
+            <div class="iq-tile-header__title">
+              <h2>{{title}}</h2>
+            </div>
+          </div>
+          ${staticHTML}
+          <div hljs hljs-source="code"></div>
+        </div>`);
 
-      return function postLink(scope) {
+      return function postLink(scope, elm, attrs) {
         scope.code = staticHTML.replace(/^(\r\n|\r|\n)|=""|\s+$/g, '');
+        scope.title = attrs.title || 'Example';
       };
     }
   };

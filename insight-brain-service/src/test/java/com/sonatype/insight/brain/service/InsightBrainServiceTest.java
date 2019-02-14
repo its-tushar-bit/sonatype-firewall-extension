its-tushar-bit/sonatype-firewall-extension

commit ee01c9cefa23d0646ebc70fb0fd909c89ee54f06

Simplified feature handling in CLMLicenseManager

insight-brain-service/src/main/java/com/sonatype/insight/brain/product/license/CLMLicenseManager.java
@@ -29,6 +29,7 @@ import com.sonatype.insight.brain.audit.AuditData;
 import com.sonatype.insight.brain.audit.AuditEvent;
 import com.sonatype.insight.brain.audit.AuditRecorder;
 import com.sonatype.insight.brain.audit.AuditSession;
+import com.sonatype.insight.brain.features.Feature;
 import com.sonatype.insight.license.model.CLMEnforcementPoint;
 import com.sonatype.insight.license.model.ProductLicenseDetails;
 
@@ -44,24 +45,6 @@ import org.slf4j.LoggerFactory;
 @Singleton
 public class CLMLicenseManager
 {
-  private static final String FEATURE_POLICY_MONITORING = "PolicyMonitoring";
-
-  private static final String FEATURE_DASHBOARD = "DASHBOARD";
-
-  private static final String FEATURE_CLI_SCAN = "CLI_SCAN";
-
-  private static final String FEATURE_QUALITY = "QUALITY";
-
-  private static final String FEATURE_REPOSITORY_FIREWALL = "REPOSITORY_FIREWALL";
-
-  private static final String FEATURE_ENFORCEMENT = "ENFORCEMENT";
-
-  private static final String FEATURE_NOTIFICATIONS = "NOTIFICATIONS";
-
-  private static final String FEATURE_POLICY_GRANDFATHERING = "POLICY_GRANDFATHERING";
-
-  private static final String FEATURE_WEBHOOKS = "WEBHOOKS";
-
   public static final String PRODUCT_PRO_PLUS = "Pro+";
 
   public static final String PRODUCT_LIFECYCLE = "Lifecycle";
@@ -86,7 +69,7 @@ public class CLMLicenseManager
 
     private final Set<String> products;
 
-    private final Set<String> features;
+    private final Set<Feature> features;
 
     private final Set<CLMEnforcementPoint> enforcementPoints;
 
@@ -102,7 +85,7 @@ public class CLMLicenseManager
                              String contactCompany,
                              String contactEmail,
                              Set<String> products,
-                             Set<String> features,
+                             Set<Feature> features,
                              Set<CLMEnforcementPoint> enforcementPoints,
                              Integer applicationLimit,
                              Integer maxUsers,
@@ -206,64 +189,16 @@ public class CLMLicenseManager
     return licenseCache.applicationLimit;
   }
 
-  public boolean hasPolicyMonitoring() {
-    return hasFeature(FEATURE_POLICY_MONITORING);
-  }
-
-  public boolean hasDashboard() {
-    return hasFeature(FEATURE_DASHBOARD);
-  }
-
-  public boolean hasCLIScanning() {
-    return hasFeature(FEATURE_CLI_SCAN);
-  }
-
-  /**
-   * Checks to see if the license enables the quality feature
-   *
-   * @since 1.11.0
-   */
-  public boolean hasQuality() {
-    return hasFeature(FEATURE_QUALITY);
-  }
-
-  /**
-   * @since 1.17
-   */
-  public boolean hasRepositoryFirewall() {
-    return hasFeature(FEATURE_REPOSITORY_FIREWALL);
-  }
-
-  /**
-   * @since 1.59
-   */
   public boolean hasEnforcement() {
-    return hasFeature(FEATURE_ENFORCEMENT);
+    return hasFeature(Feature.ENFORCEMENT);
   }
 
-  /**
-   * @since 1.59
-   */
-  public boolean hasNotifications() {
-    return hasFeature(FEATURE_NOTIFICATIONS);
-  }
-
-  /**
-   * @since 1.59
-   */
-  public boolean hasPolicyGrandfathering() {
-    return hasFeature(FEATURE_POLICY_GRANDFATHERING);
-  }
-
-  /**
-   * @since 1.59
-   */
-  public boolean hasWebhooks() {
-    return hasFeature(FEATURE_WEBHOOKS);
-  }
-
-  private boolean hasFeature(String feature) {
+  public boolean hasFeature(Feature feature) {
     return licenseCache.features.contains(feature);
+  }
+
+  public Set<Feature> getFeatures() {
+    return EnumSet.copyOf(licenseCache.features);
   }
 
   public boolean hasProduct(String productId) {
@@ -441,48 +376,48 @@ public class CLMLicenseManager
     Set<String> products = getProducts(key);
 
     Set<CLMEnforcementPoint> enforcementPoints = EnumSet.noneOf(CLMEnforcementPoint.class);
-    Set<String> features = new LinkedHashSet<>();
+    Set<Feature> features = EnumSet.noneOf(Feature.class);
     if (products.contains(ProductLicenseDetails.PRODUCT_RISK)) {
-      features.add(FEATURE_POLICY_MONITORING);
-      features.add(FEATURE_DASHBOARD);
-      features.add(FEATURE_CLI_SCAN);
-      features.add(FEATURE_ENFORCEMENT);
-      features.add(FEATURE_NOTIFICATIONS);
-      features.add(FEATURE_POLICY_GRANDFATHERING);
-      features.add(FEATURE_WEBHOOKS);
+      features.add(Feature.POLICY_MONITORING);
+      features.add(Feature.DASHBOARD);
+      features.add(Feature.CLI_INTEGRATION);
+      features.add(Feature.ENFORCEMENT);
+      features.add(Feature.NOTIFICATIONS);
+      features.add(Feature.POLICY_GRANDFATHERING);
+      features.add(Feature.WEBHOOKS);
       enforcementPoints.add(CLMEnforcementPoint.Release);
     }
     if (products.contains(ProductLicenseDetails.PRODUCT_RISK_AND_REMEDIATION)) {
-      features.add(FEATURE_QUALITY);
-      features.add(FEATURE_POLICY_MONITORING);
-      features.add(FEATURE_DASHBOARD);
-      features.add(FEATURE_CLI_SCAN);
-      features.add(FEATURE_ENFORCEMENT);
-      features.add(FEATURE_NOTIFICATIONS);
-      features.add(FEATURE_POLICY_GRANDFATHERING);
-      features.add(FEATURE_WEBHOOKS);
+      features.add(Feature.QUALITY);
+      features.add(Feature.POLICY_MONITORING);
+      features.add(Feature.DASHBOARD);
+      features.add(Feature.CLI_INTEGRATION);
+      features.add(Feature.ENFORCEMENT);
+      features.add(Feature.NOTIFICATIONS);
+      features.add(Feature.POLICY_GRANDFATHERING);
+      features.add(Feature.WEBHOOKS);
       enforcementPoints.add(CLMEnforcementPoint.Develop);
       enforcementPoints.add(CLMEnforcementPoint.Build);
       enforcementPoints.add(CLMEnforcementPoint.StageRelease);
       enforcementPoints.add(CLMEnforcementPoint.Release);
     }
     if (products.contains(ProductLicenseDetails.PRODUCT_NEXUS)) {
-      features.add(FEATURE_ENFORCEMENT);
-      features.add(FEATURE_NOTIFICATIONS);
-      features.add(FEATURE_POLICY_GRANDFATHERING);
-      features.add(FEATURE_WEBHOOKS);
+      features.add(Feature.ENFORCEMENT);
+      features.add(Feature.NOTIFICATIONS);
+      features.add(Feature.POLICY_GRANDFATHERING);
+      features.add(Feature.WEBHOOKS);
       enforcementPoints.add(CLMEnforcementPoint.StageRelease);
       enforcementPoints.add(CLMEnforcementPoint.Release);
     }
     if (products.contains(ProductLicenseDetails.PRODUCT_FOUNDATION)) {
-      features.add(FEATURE_DASHBOARD);
-      features.add(FEATURE_CLI_SCAN);
+      features.add(Feature.DASHBOARD);
+      features.add(Feature.CLI_INTEGRATION);
       enforcementPoints.add(CLMEnforcementPoint.Build);
       enforcementPoints.add(CLMEnforcementPoint.StageRelease);
       enforcementPoints.add(CLMEnforcementPoint.Release);
     }
     if (products.contains(ProductLicenseDetails.PRODUCT_FIREWALL)) {
-      features.add(FEATURE_REPOSITORY_FIREWALL);
+      features.add(Feature.FIREWALL);
       enforcementPoints.add(CLMEnforcementPoint.StageRelease);
       enforcementPoints.add(CLMEnforcementPoint.Release);
     }

insight-brain-service/src/test/java/com/sonatype/insight/brain/product/license/CLMLicenseManagerTest.java
@@ -10,13 +10,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.inject.Inject;
 
 import com.sonatype.insight.brain.TestLicenseFingerprinter;
 import com.sonatype.insight.brain.TestProductLicenseManager;
+import com.sonatype.insight.brain.features.Feature;
 import com.sonatype.insight.brain.service.AbstractComponentTest;
 import com.sonatype.insight.license.model.CLMEnforcementPoint;
 import com.sonatype.insight.license.model.ProductLicenseDetails;
@@ -80,23 +79,23 @@ public class CLMLicenseManagerTest
   public void testLicenseCache() throws Exception {
     assertThat(clmLicenseManager.isValid()).isTrue();
     assertThat(clmLicenseManager.getApplicationCountLimit()).isEqualTo(100);
-    assertThat(clmLicenseManager.hasPolicyMonitoring()).isTrue();
-    assertThat(clmLicenseManager.hasDashboard()).isTrue();
-    assertThat(clmLicenseManager.hasQuality()).isTrue();
+    assertThat(clmLicenseManager.hasFeature(Feature.POLICY_MONITORING)).isTrue();
+    assertThat(clmLicenseManager.hasFeature(Feature.DASHBOARD)).isTrue();
+    assertThat(clmLicenseManager.hasFeature(Feature.QUALITY)).isTrue();
 
     // now change the value and make sure the cache is still stale
     licenseManager.setApplicationLimit(10);
     assertThat(clmLicenseManager.getApplicationCountLimit()).isEqualTo(100);
     licenseManager.setProducts("");
-    assertThat(clmLicenseManager.hasPolicyMonitoring()).isTrue();
-    assertThat(clmLicenseManager.hasDashboard()).isTrue();
-    assertThat(clmLicenseManager.hasQuality()).isTrue();
+    assertThat(clmLicenseManager.hasFeature(Feature.POLICY_MONITORING)).isTrue();
+    assertThat(clmLicenseManager.hasFeature(Feature.DASHBOARD)).isTrue();
+    assertThat(clmLicenseManager.hasFeature(Feature.QUALITY)).isTrue();
     licenseManager.setEnforcementPoints(CLMEnforcementPoint.StageRelease, CLMEnforcementPoint.Release);
 
     // now install the license (which causes the cache to be cleared) and make sure the cache is no longer stale
     installLicense();
     assertThat(clmLicenseManager.getApplicationCountLimit()).isEqualTo(10);
-    assertThat(clmLicenseManager.hasPolicyMonitoring()).isFalse();
+    assertThat(clmLicenseManager.hasFeature(Feature.POLICY_MONITORING)).isFalse();
   }
 
   @Test
@@ -148,103 +147,43 @@ public class CLMLicenseManagerTest
   }
 
   @Test
-  public void testHasFeatures_NexusProPlusLicense() throws Exception {
+  public void testGetFeatures_NexusProPlus() throws Exception {
     licenseManager.setProducts(ProductLicenseDetails.PRODUCT_NEXUS);
     installLicense();
-    assertThat(clmLicenseManager.hasDashboard()).isFalse();
-    assertThat(clmLicenseManager.hasPolicyMonitoring()).isFalse();
-    assertThat(clmLicenseManager.hasCLIScanning()).isFalse();
-    assertThat(clmLicenseManager.hasQuality()).isFalse();
-    assertThat(clmLicenseManager.hasRepositoryFirewall()).isFalse();
-    assertThat(clmLicenseManager.hasEnforcement()).isTrue();
-    assertThat(clmLicenseManager.hasNotifications()).isTrue();
-    assertThat(clmLicenseManager.hasPolicyGrandfathering()).isTrue();
-    assertThat(clmLicenseManager.hasWebhooks()).isTrue();
+    assertThat(clmLicenseManager.getFeatures()).containsExactlyInAnyOrder(Feature.ENFORCEMENT, Feature.NOTIFICATIONS,
+        Feature.POLICY_GRANDFATHERING, Feature.WEBHOOKS);
   }
 
   @Test
-  public void testHasFeatures_NexusAuditorLicense() throws Exception {
+  public void testGetFeatures_Auditor() throws Exception {
     licenseManager.setProducts(ProductLicenseDetails.PRODUCT_RISK);
     installLicense();
-    assertThat(clmLicenseManager.hasDashboard()).isTrue();
-    assertThat(clmLicenseManager.hasPolicyMonitoring()).isTrue();
-    assertThat(clmLicenseManager.hasCLIScanning()).isTrue();
-    assertThat(clmLicenseManager.hasQuality()).isFalse();
-    assertThat(clmLicenseManager.hasRepositoryFirewall()).isFalse();
-    assertThat(clmLicenseManager.hasEnforcement()).isTrue();
-    assertThat(clmLicenseManager.hasNotifications()).isTrue();
-    assertThat(clmLicenseManager.hasPolicyGrandfathering()).isTrue();
-    assertThat(clmLicenseManager.hasWebhooks()).isTrue();
+    assertThat(clmLicenseManager.getFeatures()).containsExactlyInAnyOrder(Feature.DASHBOARD, Feature.POLICY_MONITORING,
+        Feature.CLI_INTEGRATION, Feature.ENFORCEMENT, Feature.NOTIFICATIONS, Feature.POLICY_GRANDFATHERING,
+        Feature.WEBHOOKS);
   }
 
   @Test
-  public void testHasFeatures_NexusLifecycleLicense() throws Exception {
+  public void testGetFeatures_Lifecycle() throws Exception {
     licenseManager.setProducts(ProductLicenseDetails.PRODUCT_RISK_AND_REMEDIATION);
     installLicense();
-    assertThat(clmLicenseManager.hasDashboard()).isTrue();
-    assertThat(clmLicenseManager.hasPolicyMonitoring()).isTrue();
-    assertThat(clmLicenseManager.hasCLIScanning()).isTrue();
-    assertThat(clmLicenseManager.hasQuality()).isTrue();
-    assertThat(clmLicenseManager.hasRepositoryFirewall()).isFalse();
-    assertThat(clmLicenseManager.hasEnforcement()).isTrue();
-    assertThat(clmLicenseManager.hasNotifications()).isTrue();
-    assertThat(clmLicenseManager.hasPolicyGrandfathering()).isTrue();
-    assertThat(clmLicenseManager.hasWebhooks()).isTrue();
+    assertThat(clmLicenseManager.getFeatures()).containsExactlyInAnyOrder(Feature.DASHBOARD, Feature.POLICY_MONITORING,
+        Feature.CLI_INTEGRATION, Feature.QUALITY, Feature.ENFORCEMENT, Feature.NOTIFICATIONS,
+        Feature.POLICY_GRANDFATHERING, Feature.WEBHOOKS);
   }
 
   @Test
-  public void testHasQuality_NoNexusLifecycle() throws Exception {
-    Set<String> productSet = new HashSet<>(ProductLicenseDetails.PRODUCTS);
-    productSet.remove(ProductLicenseDetails.PRODUCT_RISK_AND_REMEDIATION);
-    String[] products = productSet.toArray(new String[ProductLicenseDetails.PRODUCTS.size()]);
-    licenseManager.setProducts(products);
-    installLicense();
-    assertThat(clmLicenseManager.hasQuality()).isFalse();
-  }
-
-  @Test
-  public void testHasFeatures_NexusFirewallLicense() throws Exception {
+  public void testGetFeatures_Firewall() throws Exception {
     licenseManager.setProducts(ProductLicenseDetails.PRODUCT_FIREWALL);
     installLicense();
-    assertThat(clmLicenseManager.hasDashboard()).isFalse();
-    assertThat(clmLicenseManager.hasPolicyMonitoring()).isFalse();
-    assertThat(clmLicenseManager.hasCLIScanning()).isFalse();
-    assertThat(clmLicenseManager.hasQuality()).isFalse();
-    assertThat(clmLicenseManager.hasRepositoryFirewall()).isTrue();
-    assertThat(clmLicenseManager.hasEnforcement()).isFalse();
-    assertThat(clmLicenseManager.hasNotifications()).isFalse();
-    assertThat(clmLicenseManager.hasPolicyGrandfathering()).isFalse();
-    assertThat(clmLicenseManager.hasWebhooks()).isFalse();
+    assertThat(clmLicenseManager.getFeatures()).containsExactlyInAnyOrder(Feature.FIREWALL);
   }
 
   @Test
-  public void testHasFeatures_FoundationLicense() throws Exception {
+  public void testGetFeatures_Foundation() throws Exception {
     licenseManager.setProducts(ProductLicenseDetails.PRODUCT_FOUNDATION);
     clmLicenseManager.installLicense(null);
-    assertThat(clmLicenseManager.hasDashboard()).isTrue();
-    assertThat(clmLicenseManager.hasPolicyMonitoring()).isFalse();
-    assertThat(clmLicenseManager.hasCLIScanning()).isTrue();
-    assertThat(clmLicenseManager.hasQuality()).isFalse();
-    assertThat(clmLicenseManager.hasRepositoryFirewall()).isFalse();
-    assertThat(clmLicenseManager.hasEnforcement()).isFalse();
-    assertThat(clmLicenseManager.hasNotifications()).isFalse();
-    assertThat(clmLicenseManager.hasPolicyGrandfathering()).isFalse();
-    assertThat(clmLicenseManager.hasWebhooks()).isFalse();
-  }
-
-  @Test
-  public void testHasFeatures_FoundationLicenseWithFirewall() throws Exception {
-    licenseManager.setProducts(ProductLicenseDetails.PRODUCT_FOUNDATION, ProductLicenseDetails.PRODUCT_FIREWALL);
-    clmLicenseManager.installLicense(null);
-    assertThat(clmLicenseManager.hasDashboard()).isTrue();
-    assertThat(clmLicenseManager.hasPolicyMonitoring()).isFalse();
-    assertThat(clmLicenseManager.hasCLIScanning()).isTrue();
-    assertThat(clmLicenseManager.hasQuality()).isFalse();
-    assertThat(clmLicenseManager.hasRepositoryFirewall()).isTrue();
-    assertThat(clmLicenseManager.hasEnforcement()).isFalse();
-    assertThat(clmLicenseManager.hasNotifications()).isFalse();
-    assertThat(clmLicenseManager.hasPolicyGrandfathering()).isFalse();
-    assertThat(clmLicenseManager.hasWebhooks()).isFalse();
+    assertThat(clmLicenseManager.getFeatures()).containsExactlyInAnyOrder(Feature.DASHBOARD, Feature.CLI_INTEGRATION);
   }
 
   @Test

insight-brain-service/src/main/java/com/sonatype/insight/brain/component/ComponentDetailService.java
@@ -22,6 +22,7 @@ import com.sonatype.insight.brain.dashboard.StageDetailDTO;
 import com.sonatype.insight.brain.dataaccess.ApplicationComponentDAO;
 import com.sonatype.insight.brain.dataaccess.policy.PolicyEvaluationDAO;
 import com.sonatype.insight.brain.dataaccess.policy.PolicyViolationDAO;
+import com.sonatype.insight.brain.features.Feature;
 import com.sonatype.insight.brain.model.Application;
 import com.sonatype.insight.brain.model.ApplicationComponent;
 import com.sonatype.insight.brain.model.policy.PolicyEvaluation;
@@ -229,7 +230,7 @@ public class ComponentDetailService
   }
 
   private void validateDashboardLicensed() {
-    if (!licenseManager.hasDashboard()) {
+    if (!licenseManager.hasFeature(Feature.DASHBOARD)) {
       throw new InvalidLicenseException();
     }
   }

insight-brain-service/src/main/java/com/sonatype/insight/brain/configuration/webhook/WebhookService.java
@@ -16,6 +16,7 @@ import javax.inject.Singleton;
 
 import com.sonatype.insight.brain.audit.AuditData;
 import com.sonatype.insight.brain.dataaccess.configuration.webhook.WebhookDAO;
+import com.sonatype.insight.brain.features.Feature;
 import com.sonatype.insight.brain.model.configuration.webhook.Webhook;
 import com.sonatype.insight.brain.model.configuration.webhook.WebhookEventType;
 import com.sonatype.insight.brain.model.security.Permission;
@@ -90,7 +91,7 @@ public class WebhookService
 
   @Authorize(permission = Permission.CONFIGURE_SYSTEM)
   public Webhook addWebhook(Webhook webhook) {
-    if (!clmLicenseManager.hasWebhooks()) {
+    if (!clmLicenseManager.hasFeature(Feature.WEBHOOKS)) {
       log.debug("Not adding Webhook, license does not support Webhooks.");
       throw new InvalidLicenseException();
     }
@@ -103,7 +104,7 @@ public class WebhookService
 
   @Authorize(permission = Permission.CONFIGURE_SYSTEM)
   public Webhook updateWebhook(Webhook webhook) {
-    if (!clmLicenseManager.hasWebhooks()) {
+    if (!clmLicenseManager.hasFeature(Feature.WEBHOOKS)) {
       log.debug("Not updating Webhook, license does not support Webhooks.");
       throw new InvalidLicenseException();
     }
@@ -123,7 +124,7 @@ public class WebhookService
 
   @Authorize(permission = Permission.CONFIGURE_SYSTEM)
   public void deleteWebhook(String webhookId) {
-    if (!clmLicenseManager.hasWebhooks()) {
+    if (!clmLicenseManager.hasFeature(Feature.WEBHOOKS)) {
       log.debug("Not deleting Webhook, license does not support Webhooks.");
       throw new InvalidLicenseException();
     }

insight-brain-service/src/main/java/com/sonatype/insight/brain/dashboard/DashboardUtils.java
@@ -20,6 +20,7 @@ import javax.inject.Named;
 import com.sonatype.insight.brain.dashboard.filters.PolicyThreatCategoryFilter;
 import com.sonatype.insight.brain.dashboard.filters.PolicyThreatLevelFilter;
 import com.sonatype.insight.brain.dashboard.filters.PolicyViolationStateFilter;
+import com.sonatype.insight.brain.features.Feature;
 import com.sonatype.insight.brain.model.policy.PolicyViolation;
 import com.sonatype.insight.brain.model.policy.StageType;
 import com.sonatype.insight.brain.model.policy.stages.StageTypes;
@@ -42,7 +43,7 @@ public class DashboardUtils
   }
 
   void validateDashboardLicensed() {
-    if (!licenseManager.hasDashboard()) {
+    if (!licenseManager.hasFeature(Feature.DASHBOARD)) {
       throw new InvalidLicenseException();
     }
   }

insight-brain-service/src/main/java/com/sonatype/insight/brain/features/Feature.java
@@ -16,8 +16,39 @@ import com.fasterxml.jackson.annotation.JsonValue;
  */
 public enum Feature
 {
-  POLICY, LABELS, RELEASE_GRAPH, POLICY_VIOLATIONS, NOTIFICATIONS, REEVALUATE_POLICY, POLICY_MONITORING, DASHBOARD,
-  ROOT_ORG, ROOT_ORG_MIGRATE, ALLOW_EXTERNAL_HYPERLINKS, WEBHOOKS, POLICY_GRANDFATHERING, FIREWALL, ENFORCEMENT;
+  ALLOW_EXTERNAL_HYPERLINKS,
+
+  CLI_INTEGRATION,
+
+  DASHBOARD,
+
+  ENFORCEMENT,
+
+  FIREWALL,
+
+  LABELS,
+
+  NOTIFICATIONS,
+
+  POLICY,
+
+  POLICY_GRANDFATHERING,
+
+  POLICY_MONITORING,
+
+  POLICY_VIOLATIONS,
+
+  QUALITY,
+
+  REEVALUATE_POLICY,
+
+  RELEASE_GRAPH,
+
+  ROOT_ORG,
+
+  ROOT_ORG_MIGRATE,
+
+  WEBHOOKS;
 
   @Override
   @JsonValue

insight-brain-service/src/main/java/com/sonatype/insight/brain/features/FeaturesService.java
@@ -75,32 +75,6 @@ public class FeaturesService
   }
 
   private void addLicenseSpecificFeatures(Set<Feature> features) {
-    if (licenseManager.hasPolicyMonitoring()) {
-      features.add(Feature.POLICY_MONITORING);
-    }
-
-    if (licenseManager.hasDashboard()) {
-      features.add(Feature.DASHBOARD);
-    }
-    
-    if (licenseManager.hasRepositoryFirewall()) {
-      features.add(Feature.FIREWALL);
-    }
-
-    if (licenseManager.hasEnforcement()) {
-      features.add(Feature.ENFORCEMENT);
-    }
-
-    if (licenseManager.hasNotifications()) {
-      features.add(Feature.NOTIFICATIONS);
-    }
-
-    if (licenseManager.hasPolicyGrandfathering()) {
-      features.add(Feature.POLICY_GRANDFATHERING);
-    }
-
-    if (licenseManager.hasWebhooks()) {
-      features.add(Feature.WEBHOOKS);
-    }
+    features.addAll(licenseManager.getFeatures());
   }
 }

insight-brain-service/src/main/java/com/sonatype/insight/brain/hds/CLIResource.java
@@ -19,6 +19,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import com.sonatype.clm.dto.model.ScanReceipt;
+import com.sonatype.insight.brain.features.Feature;
 import com.sonatype.insight.brain.product.license.CLMLicenseManager;
 import com.sonatype.insight.brain.product.license.InvalidLicenseException;
 import com.sonatype.insight.scan.model.ClientScanType;
@@ -59,7 +60,7 @@ public class CLIResource
                              @QueryParam("scanType") ClientScanType clientScanType,
                              @Context HttpServletRequest req) throws IOException
   {
-    if (!clmLicenseManager.hasCLIScanning()) {
+    if (!clmLicenseManager.hasFeature(Feature.CLI_INTEGRATION)) {
       throw new InvalidLicenseException();
     }
     return scanHandler.handle(req, applicationPublicId, clientScanType);

commit 62fad25192e287ad92b7015782986df1ebe1ccbc

Fixed regression introduced in ee01c9cefa23d0646ebc70fb0fd909c89ee54f06 that prevents startup of IQ Server if no product license is installed CLM-12011 (PR #3801)

insight-brain-service/src/main/java/com/sonatype/insight/brain/product/license/CLMLicenseManager.java
@@ -488,8 +488,8 @@ public class CLMLicenseManager
   }
 
   private void clearLicenseCache() {
-    licenseCache = new CachedLicenseData(null, 0, null, null, null, Collections.emptySet(), Collections.emptySet(), 0,
-        0, 0, 0);
+    licenseCache = new CachedLicenseData(null, 0, null, null, null, Collections.emptySet(),
+        EnumSet.noneOf(Feature.class), 0, 0, 0, 0);
     notifyListeners();
   }
 

insight-brain-service/src/test/java/com/sonatype/insight/brain/product/license/CLMLicenseManagerTest.java
@@ -50,6 +50,15 @@ public class CLMLicenseManagerTest
   }
 
   @Test
+  public void testMissingLicense_BasicLicenseInformationCanStillBeQueried() throws Exception {
+    clmLicenseManager.uninstallLicense();
+    assertThat(clmLicenseManager.getLicenseFingerprint()).isNull();
+    assertThat(clmLicenseManager.getFeatures()).isEmpty();
+    assertThat(clmLicenseManager.getLicenseSummary()).isNotNull();
+    assertThat(clmLicenseManager.getLicenseInfo()).isNotNull();
+  }
+
+  @Test
   public void testLicenseLacksClmFeatureAndFirewallFeature() throws Exception {
     clmLicenseManager.uninstallLicense();
     licenseManager.setForceVerificationFailure(true);

insight-brain-service/src/test/java/com/sonatype/insight/brain/service/InsightBrainServiceTest.java
@@ -232,4 +232,13 @@ public class InsightBrainServiceTest
     assertThat(logOutput).atInfoLevel()
         .contains("Initializing Nexus IQ Server 1 release " + new VersionService().getLogDisplayVersion());
   }
+
+  @Test
+  @ManualServerInit
+  public void testStartupWithoutLicense() throws Exception {
+    getTestProductLicenseManager().uninstallLicense();
+    // Manually initialize server with custom configurator to ensure it gets restarted if already running
+    initServer(config -> {
+    });
+  }
 }
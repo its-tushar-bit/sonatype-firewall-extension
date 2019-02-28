f075f0046130a3fdc86cc77b7f96a4ddc77e38fb
Steve Baker <sbaker@sonatype.com>
2019-02-28 14:26:38 -0500

    fix license header formatting

--- nexus-iq-tools/testsuite/i_feel_lucky.sh
+++ nexus-iq-tools/testsuite/i_feel_lucky.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#
 # Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
 # Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
 # "Sonatype" is a trademark of Sonatype, Inc.
+#
 
 IQ_LICENSE=$(locate lifecycle | egrep "*.lic$" | sort | tail -n 1)
 start=$(date +%s)

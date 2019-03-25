f2f97f16e3ba73d8eb2aad0427f9180fdc8eaa19
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2019-03-25 12:09:05 +0100

    Added background task to automatically purge obsolete policy violations CLM-12263 CLM-12247 (PR #3874)

---
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/successmetrics/SuccessMetricsPurger.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.successmetrics;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import com.sonatype.insight.brain.dataaccess.ApplicationDAO;
+import com.sonatype.insight.brain.dataaccess.OwnerDAO;
+import com.sonatype.insight.brain.dataaccess.configuration.DataRetentionPolicyDAO;
+import com.sonatype.insight.brain.dataaccess.policy.PolicyViolationDAO;
+import com.sonatype.insight.brain.model.Application;
+import com.sonatype.insight.brain.model.Owner;
+import com.sonatype.insight.brain.model.configuration.DataRetentionPolicy;
+import com.sonatype.insight.brain.security.SystemRunnable;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import io.dropwizard.lifecycle.Managed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Named
+@Singleton
+public class SuccessMetricsPurger
+    implements Managed
+{
+  private static final Logger log = LoggerFactory.getLogger(SuccessMetricsPurger.class);
+
+  private final DataRetentionPolicyDAO dataRetentionPolicyDAO;
+
+  private final ApplicationDAO applicationDAO;
+
+  private final OwnerDAO ownerDAO;
+
+  private final PolicyViolationDAO policyViolationDAO;
+
+  private ScheduledExecutorService executor;
+
+  @Inject
+  public SuccessMetricsPurger(
+      DataRetentionPolicyDAO dataRetentionPolicyDAO,
+      ApplicationDAO applicationDAO,
+      OwnerDAO ownerDAO,
+      PolicyViolationDAO policyViolationDAO)
+  {
+    this.dataRetentionPolicyDAO = dataRetentionPolicyDAO;
+    this.applicationDAO = applicationDAO;
+    this.ownerDAO = ownerDAO;
+    this.policyViolationDAO = policyViolationDAO;
+  }
+
+  private ScheduledExecutorService newExecutor() {
+    ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat(getClass().getSimpleName() + "-%d")
+        .setDaemon(true).setPriority(Thread.MIN_PRIORITY).build();
+    return new ScheduledThreadPoolExecutor(1, threadFactory);
+  }
+
+  @Override
+  public void start() {
+    executor = newExecutor();
+    // NOTE: Using daily (instead of monthly) period to ensure timely purge after enabling purging
+    Duration period = Duration.ofDays(1);
+    Duration initialDelay = Duration.between(LocalTime.now(), LocalTime.of(1, 30));
+    while (initialDelay.isNegative()) {
+      initialDelay = initialDelay.plusDays(1);
+    }
+    Runnable purgeTask = new SystemRunnable(() -> {
+      try {
+        purgeSuccessMetrics();
+      }
+      catch (RuntimeException e) {
+        log.warn("Failed to purge obsolete success metrics", e);
+      }
+    });
+    executor.scheduleAtFixedRate(purgeTask, initialDelay.toMillis(), period.toMillis(), TimeUnit.MILLISECONDS);
+    log.debug("Scheduled periodic purging of obsolete success metrics for {}", LocalDateTime.now().plus(initialDelay));
+  }
+
+  @Override
+  public void stop() {
+    if (executor != null) {
+      executor.shutdown();
+      executor = null;
+      log.debug("Stopped periodic purging of obsolete success metrics");
+    }
+  }
+
+  void purgeSuccessMetrics() {
+    List<Application> applications = applicationDAO.getAll();
+    log.debug("Purging obsolete success metrics from {} applications", applications.size());
+    int purgedApplications = 0;
+    for (Application application : applications) {
+      if (purgeSuccessMetrics(application)) {
+        purgedApplications++;
+      }
+    }
+    if (purgedApplications > 0) {
+      log.info("Purged obsolete success metrics from {} applications", purgedApplications);
+    }
+  }
+
+  private boolean purgeSuccessMetrics(Application application) {
+    Date cutoffDate = getCutoffDate(getDataRetentionPolicy(application));
+    if (cutoffDate == null) {
+      return false;
+    }
+    int deletedRows = policyViolationDAO.deleteFixedByApplicationIdAndDate(application.getId(), cutoffDate);
+    if (deletedRows > 0) {
+      log.info("Purged {} obsolete records older than {} from violation history of application {}", deletedRows,
+          cutoffDate, application.getName());
+    }
+    return deletedRows > 0;
+  }
+
+  private DataRetentionPolicy getDataRetentionPolicy(Application application) {
+    for (Owner owner : ownerDAO.walkHierarchy(application)) {
+      DataRetentionPolicy dataRetentionPolicy = dataRetentionPolicyDAO.getByOwnerIdAndContextId(owner.getId(),
+          DataRetentionPolicy.CONTEXT_ID_SUCCESS_METRICS);
+      if (dataRetentionPolicy != null) {
+        return dataRetentionPolicy;
+      }
+    }
+    return null;
+  }
+
+  private Date getCutoffDate(DataRetentionPolicy dataRetentionPolicy) {
+    if (dataRetentionPolicy == null || !dataRetentionPolicy.isPurgingEnabled()) {
+      return null;
+    }
+    int maxAgeInYears = Math.max(1, dataRetentionPolicy.getMaxAgeInDays() / 365);
+    return Date
+        .from(ZonedDateTime.now().withDayOfMonth(1).truncatedTo(ChronoUnit.DAYS).minusYears(maxAgeInYears).toInstant());
+  }
+}
---
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/successmetrics/SuccessMetricsPurgerTest.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.successmetrics;
+
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import com.sonatype.clm.dto.model.policy.Stage;
+import com.sonatype.insight.brain.dataaccess.configuration.DataRetentionPolicyDAO;
+import com.sonatype.insight.brain.dataaccess.policy.PolicyViolationDAO;
+import com.sonatype.insight.brain.model.Application;
+import com.sonatype.insight.brain.model.Organization;
+import com.sonatype.insight.brain.model.configuration.DataRetentionPolicy;
+import com.sonatype.insight.brain.model.policy.Policy;
+import com.sonatype.insight.brain.model.policy.PolicyEvaluation;
+import com.sonatype.insight.brain.model.policy.PolicyViolation;
+import com.sonatype.insight.brain.service.AbstractComponentTest;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SuccessMetricsPurgerTest
+    extends AbstractComponentTest
+{
+  @Inject
+  private SuccessMetricsPurger successMetricsPurger;
+
+  @Inject
+  private DataRetentionPolicyDAO dataRetentionPolicyDAO;
+
+  @Inject
+  private PolicyViolationDAO policyViolationDAO;
+
+  private ZonedDateTime now = ZonedDateTime.now();
+
+  private Date monthsAgo(int months) {
+    return Date.from(now.withDayOfMonth(1).truncatedTo(ChronoUnit.DAYS).minusMonths(months).toInstant());
+  }
+
+  private void fixViolation(PolicyViolation violation, PolicyEvaluation evaluation) {
+    violation.setFixTime(evaluation.getTime());
+    policyViolationDAO.update(violation);
+  }
+
+  @Test
+  public void testPurgeSuccessMetrics() {
+    Policy policy = tempEntity.newPolicy();
+
+    Organization org1 = tempEntity.newOrganization();
+    dataRetentionPolicyDAO.insert(
+        new DataRetentionPolicy(org1.getId(), DataRetentionPolicy.CONTEXT_ID_SUCCESS_METRICS, false, null, null));
+    Organization org2 = tempEntity.newOrganization();
+    dataRetentionPolicyDAO
+        .insert(new DataRetentionPolicy(org2.getId(), DataRetentionPolicy.CONTEXT_ID_SUCCESS_METRICS, true, null, 365));
+    Organization org3 = tempEntity.newOrganization();
+    dataRetentionPolicyDAO.insert(
+        new DataRetentionPolicy(org3.getId(), DataRetentionPolicy.CONTEXT_ID_SUCCESS_METRICS, true, null, 365 * 2));
+
+    Application app1 = tempEntity.newApplication(org1.getId());
+    Application app2 = tempEntity.newApplication(org2.getId());
+    Application app3 = tempEntity.newApplication(org3.getId());
+
+    Map<String, PolicyViolation> namedViolations = new HashMap<>();
+    for (Application app : Arrays.asList(app1, app2, app3)) {
+      PolicyEvaluation evaluation1 =
+          tempEntity.newPolicyEvaluation(app.getId(), Stage.ID_BUILD, "scan-1", monthsAgo(26));
+      PolicyViolation violation1 = tempEntity.newPolicyViolation(evaluation1, policy);
+      namedViolations.put(app.getId() + "-fixed1", violation1);
+      namedViolations.put(app.getId() + "-open", tempEntity.newPolicyViolation(evaluation1, policy));
+      namedViolations.put(app.getId() + "-grandfathered",
+          tempEntity.newGrandfatheredPolicyViolation(evaluation1, policy));
+      namedViolations.put(app.getId() + "-waived",
+          tempEntity.newWaivedPolicyViolation(evaluation1, policy, tempEntity.newWaiver(policy.getId(), app.getId())));
+
+      PolicyEvaluation evaluation2 =
+          tempEntity.newPolicyEvaluation(app.getId(), Stage.ID_BUILD, "scan-2", monthsAgo(25));
+      fixViolation(violation1, evaluation2);
+
+      PolicyEvaluation evaluation3 =
+          tempEntity.newPolicyEvaluation(app.getId(), Stage.ID_RELEASE, "scan-3", monthsAgo(25));
+      PolicyViolation violation2 = tempEntity.newPolicyViolation(evaluation3, policy);
+      namedViolations.put(app.getId() + "-fixed2", violation2);
+      PolicyViolation violation3 = tempEntity.newPolicyViolation(evaluation3, policy);
+      namedViolations.put(app.getId() + "-fixed3", violation3);
+
+      PolicyEvaluation evaluation4 =
+          tempEntity.newPolicyEvaluation(app.getId(), Stage.ID_RELEASE, "scan-4", monthsAgo(24));
+      fixViolation(violation2, evaluation4);
+
+      PolicyEvaluation evaluation5 =
+          tempEntity.newPolicyEvaluation(app.getId(), Stage.ID_RELEASE, "scan-5", monthsAgo(12));
+      fixViolation(violation3, evaluation5);
+      PolicyViolation violation4 = tempEntity.newPolicyViolation(evaluation5, policy);
+      namedViolations.put(app.getId() + "-fixed4", violation4);
+
+      PolicyEvaluation evaluation6 =
+          tempEntity.newPolicyEvaluation(app.getId(), Stage.ID_RELEASE, "scan-6", monthsAgo(11));
+      fixViolation(violation4, evaluation6);
+    }
+
+    successMetricsPurger.purgeSuccessMetrics();
+
+    assertThat(policyViolationDAO.getByApplicationId(app1.getId()))
+        .usingElementComparator(Comparator.comparing(PolicyViolation::getId)).containsExactlyInAnyOrder( //
+            namedViolations.get(app1.getId() + "-open"), //
+            namedViolations.get(app1.getId() + "-waived"), //
+            namedViolations.get(app1.getId() + "-grandfathered"), //
+            namedViolations.get(app1.getId() + "-fixed1"), //
+            namedViolations.get(app1.getId() + "-fixed2"), //
+            namedViolations.get(app1.getId() + "-fixed3"), //
+            namedViolations.get(app1.getId() + "-fixed4"));
+
+    assertThat(policyViolationDAO.getByApplicationId(app2.getId()))
+        .usingElementComparator(Comparator.comparing(PolicyViolation::getId)).containsExactlyInAnyOrder( //
+            namedViolations.get(app2.getId() + "-open"), //
+            namedViolations.get(app2.getId() + "-waived"), //
+            namedViolations.get(app2.getId() + "-grandfathered"), //
+            namedViolations.get(app2.getId() + "-fixed3"), //
+            namedViolations.get(app2.getId() + "-fixed4"));
+
+    assertThat(policyViolationDAO.getByApplicationId(app3.getId()))
+        .usingElementComparator(Comparator.comparing(PolicyViolation::getId)).containsExactlyInAnyOrder( //
+            namedViolations.get(app3.getId() + "-open"), //
+            namedViolations.get(app3.getId() + "-waived"), //
+            namedViolations.get(app3.getId() + "-grandfathered"), //
+            namedViolations.get(app3.getId() + "-fixed2"), //
+            namedViolations.get(app3.getId() + "-fixed3"), //
+            namedViolations.get(app3.getId() + "-fixed4"));
+  }
+}

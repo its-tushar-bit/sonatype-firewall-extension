993655bbcbe85b121ee63488104109441240aa03
vladt <vladt@sonatype.com>
2019-05-30 10:31:07 -0400

    Moved classes related to operational checks to new package: com.sonatype.insight.brain.operational.check

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/operational/check/WorkDirectoryOperationalCheck.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/operational/check/WorkDirectoryOperationalCheck.java
@@ -3,12 +3,18 @@
  * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
-package com.sonatype.insight.brain.service;
+package com.sonatype.insight.brain.operational.check;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+import com.sonatype.insight.brain.service.InsightConfig;
+
+/**
+ * Dropwizard exposes health/operational checks on:
+ * http://localhost:8071/healthcheck
+ */
 @Named
 @Singleton
 public class WorkDirectoryOperationalCheck

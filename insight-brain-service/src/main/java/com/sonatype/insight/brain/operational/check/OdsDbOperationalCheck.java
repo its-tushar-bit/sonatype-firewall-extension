11fe2374738c534a1838644066eebce6b149a9e9
Vlad Tatavu <vladt@sonatype.com>
2019-05-31 12:31:56 -0400

    Added operational/health check for db access CLM-12634 (PR #3968)

---
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/operational/check/OdsDbOperationalCheck.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.operational.check;
+
+import java.sql.Connection;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import javax.sql.DataSource;
+
+import com.sonatype.insight.brain.db.OperationalDataStoreProvider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Verifies that the process can access the ods db.
+ * 
+ * @since 1.66
+ */
+@Named
+@Singleton
+public class OdsDbOperationalCheck
+    extends AbstractOperationalCheck
+{
+  private static final Logger log = LoggerFactory.getLogger(OdsDbOperationalCheck.class);
+
+  @Inject
+  public OdsDbOperationalCheck() {
+    super("ods-database");
+  }
+
+  @Override
+  protected Result check() throws Exception {
+    DataSource dataSource = OperationalDataStoreProvider.getDataSource();
+    try (Connection connection = dataSource.getConnection()) {
+      ResultBuilder resultBuilder = Result.builder();
+
+      long start = System.currentTimeMillis();
+      boolean isValidConnection = connection.isValid(3 /* timeout in seconds */);
+      long duration = System.currentTimeMillis() - start;
+      
+      if (isValidConnection) {
+        return resultBuilder.withDetail("roundTripTimeInMs", duration).build();
+      }
+      else {
+        return Result.unhealthy("Cannot access the database. The connection timed out after " + duration + " ms.");
+      }
+    }
+    catch (Exception e) {
+      log.error(e.getMessage(), e);
+      return Result.unhealthy("Cannot access the database: " + e.getMessage());
+    }
+  }
+}
---
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/operational/check/OdsDbOperationalCheckTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.operational.check;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import com.sonatype.insight.brain.db.DataSourceFactory;
+import com.sonatype.insight.brain.db.OperationalDataStoreProvider;
+import com.sonatype.insight.test.InjectedTest;
+
+import com.codahale.metrics.health.HealthCheck.Result;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OdsDbOperationalCheckTest
+    extends InjectedTest
+{
+  @Inject
+  private OdsDbOperationalCheck odsDbOperationalCheck;
+
+  @Test
+  public void testExecute_Healthy() {
+    Result result = odsDbOperationalCheck.execute();
+    assertThat(result.isHealthy()).isTrue();
+    Map<String, Object> resultDetails = result.getDetails();
+    assertThat((long) resultDetails.get("roundTripTimeInMs")).isGreaterThanOrEqualTo(0L);
+  }
+
+  @Test
+  public void testExecute_Unhealthy() throws Exception {
+    try {
+      try (Connection connection = OperationalDataStoreProvider.getDataSource().getConnection();
+          Statement statement = connection.createStatement()) {
+        statement.execute("DROP SCHEMA " + OperationalDataStoreProvider.ID);
+      }
+      Result result = odsDbOperationalCheck.execute();
+      assertThat(result.isHealthy()).isFalse();
+      assertThat(result.getMessage()).contains("Cannot access the database: ");
+    }
+    finally {
+      DataSourceFactory.clear_ForTestsOnly();
+    }
+  }
+}

c591df19d576509f3a40c871b8ac61394abcd3ea
Koray Tugay <ktugay@sonatype.com>
2019-08-19 12:40:07 -0400

    SAML: Create a user - set attributes - logout CLM-13270 CLM-13242 (#4198)

--- keycloak-server/src/main/java/com/sonatype/insight/keycloak/KeycloakTestServer.java
+++ keycloak-server/src/main/java/com/sonatype/insight/keycloak/KeycloakTestServer.java
@@ -8,6 +8,8 @@ package com.sonatype.insight.keycloak;
 import java.net.ServerSocket;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.ws.rs.client.ClientBuilder;
@@ -18,6 +20,9 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.representations.idm.UserSessionRepresentation;
 import org.keycloak.testsuite.KeycloakServer;
 
 // Encapsulates a closable KeycloakServer started on a random port
@@ -35,6 +40,8 @@ public class KeycloakTestServer
   private String adminBearerToken;
 
   private final Set<String> createdClientIds = new HashSet<>();
+
+  private final Set<String> createdUserIds = new HashSet<>();
 
   public void start() {
     if (running) {
@@ -101,6 +108,85 @@ public class KeycloakTestServer
     }
   }
 
+  /**
+   * @return Returns the id assigned to the created user.
+   * @see <a href="https://www.keycloak.org/docs-api/6.0/rest-api/#_users_resource">Users Resource</a>
+   * @see <a href="https://www.keycloak.org/docs-api/6.0/rest-api/#_userrepresentation">User Representation</a>
+   */
+  public String createUser(UserRepresentation user) {
+    Response response = ClientBuilder.newClient().target(url).path("admin/realms/master/users").request()
+        .header("Authorization", "Bearer " + adminBearerToken)
+        .buildPost(Entity.entity(user, MediaType.APPLICATION_JSON)).invoke();
+
+    if (response.getStatus() == Status.CREATED.getStatusCode()) {
+      String id =
+          Arrays.stream(getUsers()).filter(u -> u.getUsername().equals(user.getUsername())).findAny().get().getId();
+      createdUserIds.add(id);
+      return id;
+    }
+    else {
+      throw new RuntimeException("User creation failed with status code: " + response.getStatus());
+    }
+  }
+
+  /**
+   * Convenience method to create a user from parameters directly
+   *
+   * @return Returns the id assigned to the created user.
+   */
+  public String createUser(
+      String firstName,
+      String lastName,
+      String username,
+      String email,
+      String password,
+      Map<String, List<String>> attributes)
+  {
+    UserRepresentation user = new UserRepresentation();
+    user.setUsername(username);
+    user.setEmail(email);
+    user.setEnabled(true);
+    user.setFirstName(firstName);
+    user.setLastName(lastName);
+
+    CredentialRepresentation credential = new CredentialRepresentation();
+    credential.setType(CredentialRepresentation.PASSWORD);
+    credential.setValue(password);
+    credential.setTemporary(false);
+    user.setCredentials(Arrays.asList(credential));
+
+    user.setAttributes(attributes);
+
+    return createUser(user);
+  }
+
+  /**
+   * @see <a href="https://www.keycloak.org/docs-api/6.0/rest-api/#_users_resource">Users Resource</a>
+   * @see <a href="https://www.keycloak.org/docs-api/6.0/rest-api/#_userrepresentation">User Representation</a>
+   */
+  public void updateUser(UserRepresentation user) {
+    Response response =
+        ClientBuilder.newClient().target(url).path("admin/realms/master/users").path(user.getId()).request()
+            .header("Authorization", "Bearer " + adminBearerToken)
+            .buildPut(Entity.entity(user, MediaType.APPLICATION_JSON)).invoke();
+
+    if (response.getStatus() != Status.NO_CONTENT.getStatusCode()) {
+      throw new RuntimeException("User update failed with status code: " + response.getStatus());
+    }
+  }
+
+  /**
+   * Remove all user sessions associated with the user
+   * Also send notification to all clients that have an admin URL to invalidate the sessions for the particular user
+   *
+   * @see <a href="https://www.keycloak.org/docs-api/6.0/rest-api/#_users_resource">Users Resource</a>
+   * @see <a href="https://www.keycloak.org/docs-api/6.0/rest-api/#_userrepresentation">User Representation</a>
+   */
+  public void logoutUser(String userId) {
+    ClientBuilder.newClient().target(url).path("admin/realms/master/users").path(userId).path("logout").request()
+        .header("Authorization", "Bearer " + adminBearerToken).buildPost(null).invoke();
+  }
+
   public String getUrl() {
     return url;
   }
@@ -110,11 +196,28 @@ public class KeycloakTestServer
       ClientBuilder.newClient().target(url).path("admin/realms/master/clients").path(clientId).request()
           .header("Authorization", "Bearer " + adminBearerToken).buildDelete().invoke();
     }
+    for (String userId : createdUserIds) {
+      ClientBuilder.newClient().target(url).path("admin/realms/master/users").path(userId).request()
+          .header("Authorization", "Bearer " + adminBearerToken).buildDelete().invoke();
+    }
   }
 
   ClientRepresentation[] getClients() {
     return ClientBuilder.newClient().target(url).path("admin/realms/master/clients").request()
         .header("Authorization", "Bearer " + adminBearerToken).buildGet().invoke()
         .readEntity(ClientRepresentation[].class);
+  }
+
+  UserRepresentation[] getUsers() {
+    return ClientBuilder.newClient().target(url).path("admin/realms/master/users").request()
+        .header("Authorization", "Bearer " + adminBearerToken).buildGet().invoke()
+        .readEntity(UserRepresentation[].class);
+  }
+
+  UserSessionRepresentation[] getSessionsOfUser(String userId) {
+    return ClientBuilder.newClient().target(url).path("admin/realms/master/users").path(userId).path("sessions")
+        .request()
+        .header("Authorization", "Bearer " + adminBearerToken).buildGet().invoke()
+        .readEntity(UserSessionRepresentation[].class);
   }
 }
--- keycloak-server/src/test/java/com/sonatype/insight/keycloak/KeycloakTestServerTest.java
+++ keycloak-server/src/test/java/com/sonatype/insight/keycloak/KeycloakTestServerTest.java
@@ -7,6 +7,10 @@ package com.sonatype.insight.keycloak;
 
 import java.net.ConnectException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.core.Response;
@@ -14,7 +18,11 @@ import javax.ws.rs.core.Response;
 import org.junit.Rule;
 import org.junit.Test;
 import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
 
+import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
 import static javax.ws.rs.client.ClientBuilder.newClient;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -84,16 +92,27 @@ public class KeycloakTestServerTest
   @Test
   public void testClean() {
     assertThat(keycloak.getClients()).hasSize(5);
+    assertThat(keycloak.getUsers()).extracting(UserRepresentation::getUsername).containsExactly("admin");
 
     ClientRepresentation clientRepresentation = new ClientRepresentation();
     clientRepresentation.setClientId("a-new-client");
     clientRepresentation.setProtocol("saml");
 
+    UserRepresentation userRepresentation = new UserRepresentation();
+    userRepresentation.setUsername("john.doe");
+    userRepresentation.setEmail("example@example.com");
+    userRepresentation.setEnabled(true);
+
     keycloak.createClient(clientRepresentation);
+    keycloak.createUser(userRepresentation);
+
     assertThat(keycloak.getClients()).hasSize(6);
+    assertThat(keycloak.getUsers()).hasSize(2);
 
     keycloak.clean();
+
     assertThat(keycloak.getClients()).hasSize(5);
+    assertThat(keycloak.getUsers()).extracting(UserRepresentation::getUsername).containsExactly("admin");
   }
 
   @Test
@@ -105,5 +124,111 @@ public class KeycloakTestServerTest
     keycloak.createClient(clientRepresentation);
     assertThatThrownBy(() -> keycloak.createClient(clientRepresentation)).isInstanceOf(RuntimeException.class)
         .hasMessage("Client creation failed.");
+  }
+
+  @Test
+  public void testCreateUser_UsingUserRepresentation() {
+    UserRepresentation user = new UserRepresentation();
+    user.setUsername("john.doe");
+    user.setEmail("example@example.com");
+    user.setEnabled(true);
+
+    CredentialRepresentation credential = new CredentialRepresentation();
+    credential.setType(CredentialRepresentation.PASSWORD);
+    credential.setValue("password");
+    credential.setTemporary(false);
+    user.setCredentials(asList(credential));
+
+    Map<String, List<String>> userAttributes = new HashMap<>();
+    userAttributes.put("key-01", asList("key-01-val01", "key-01-val02"));
+    userAttributes.put("key-02", asList("key-02-val01", "key-02-val02"));
+    user.setAttributes(userAttributes);
+
+    String userId = keycloak.createUser(user);
+    assertThat(userId).isNotNull();
+
+    user = stream(keycloak.getUsers()).filter(u -> u.getUsername().equals("john.doe")).findFirst().get();
+    assertThat(user.getEmail()).isEqualTo("example@example.com");
+    assertThat(user.isEnabled()).isTrue();
+    assertThat(userAttributes).isEqualTo(user.getAttributes());
+
+    // This asserts the user is enabled and the password is working
+    assertThat(keycloak.getToken("john.doe", "password")).isNotNull();
+  }
+
+  @Test
+  public void testCreateUser_UsingUserRepresentationDuplicate() {
+    testCreateUser_UsingUserRepresentation();
+    assertThatExceptionOfType(RuntimeException.class).isThrownBy(this::testCreateUser_UsingUserRepresentation)
+        .withMessage("User creation failed with status code: 409");
+  }
+
+  @Test
+  public void testCreateUser_UsingUserAttributes() {
+    String id = keycloak.createUser("john", "doe", "joanne.doe", "joanne@example.com", "pw", new HashMap<>());
+    assertThat(id).isNotNull();
+
+    UserRepresentation user =
+        stream(keycloak.getUsers()).filter(u -> u.getUsername().equals("joanne.doe")).findFirst().get();
+    assertThat(user.getEmail()).isEqualTo("joanne@example.com");
+    assertThat(user.getFirstName()).isEqualTo("john");
+    assertThat(user.getLastName()).isEqualTo("doe");
+    assertThat(user.isEnabled()).isTrue();
+    assertThat(user.getAttributes()).isNull();
+
+    // This asserts the user is enabled and the password is working
+    assertThat(keycloak.getToken("joanne.doe", "pw")).isNotNull();
+  }
+
+  @Test
+  public void testCreateUser_UsingUserAttributesDuplicate() {
+    testCreateUser_UsingUserAttributes();
+    assertThatExceptionOfType(RuntimeException.class).isThrownBy(this::testCreateUser_UsingUserAttributes)
+        .withMessage("User creation failed with status code: 409");
+  }
+
+  @Test
+  public void testUpdateUser() {
+    UserRepresentation user = new UserRepresentation();
+    user.setUsername("john.doe");
+    user.setEmail("example@example.com");
+    user.setEnabled(true);
+
+    CredentialRepresentation credential = new CredentialRepresentation();
+    credential.setType(CredentialRepresentation.PASSWORD);
+    credential.setValue("password");
+    credential.setTemporary(false);
+    user.setCredentials(asList(credential));
+
+    Map<String, List<String>> userAttributes = new HashMap<>();
+    userAttributes.put("foo", asList("bar"));
+    user.setAttributes(userAttributes);
+
+    keycloak.createUser(user);
+    user = stream(keycloak.getUsers()).filter(u -> u.getUsername().equals("john.doe")).findFirst().get();
+    assertThat(user.getAttributes().get("foo")).isEqualTo(Arrays.asList("bar"));
+
+    user.getAttributes().get("foo").set(0, "baz");
+    keycloak.updateUser(user);
+
+    user = stream(keycloak.getUsers()).filter(u -> u.getUsername().equals("john.doe")).findFirst().get();
+    assertThat(user.getAttributes().get("foo")).isEqualTo(Arrays.asList("baz"));
+  }
+
+  @Test
+  public void testUpdateUser_UserDoesNotExist() {
+    UserRepresentation user = new UserRepresentation();
+    user.setId("no-such-user");
+    assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> keycloak.updateUser(user))
+        .withMessage("User update failed with status code: 404");
+  }
+
+  @Test
+  public void testLogoutUser() {
+    String id = keycloak.createUser("john", "doe", "john.doe", "", "password", new HashMap<>());
+    keycloak.getToken("john.doe", "password"); // Creates a session for john.doe
+    assertThat(keycloak.getSessionsOfUser(id)).hasSize(1);
+    keycloak.logoutUser(id);
+    assertThat(keycloak.getSessionsOfUser(id)).isEmpty();
   }
 }

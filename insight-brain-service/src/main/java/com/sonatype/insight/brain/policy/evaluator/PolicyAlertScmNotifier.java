815ecb13cdd5695dca9e615ee98923c14cfcfa7c
Adrian Powell <adrianpowell@users.noreply.github.com>
2019-09-25 15:00:28 -0700

    INT-1939 check if remediation options exist before creating a PR (#4309)
    * check that remediation options exist
    
    * PR fixes
    
    * add additional logging checks

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/policy/evaluator/PolicyAlertScmNotifier.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/policy/evaluator/PolicyAlertScmNotifier.java
@@ -8,12 +8,20 @@ package com.sonatype.insight.brain.policy.evaluator;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
 
 import javax.inject.Inject;
 
-import com.sonatype.clm.dto.model.policy.Stage;
+import com.sonatype.clm.dto.model.component.ComponentIdentifier;
+import com.sonatype.insight.brain.api.v2.dto.ApiComponentDTOV2;
+import com.sonatype.insight.brain.api.v2.dto.ApiComponentIdentifierDTOV2;
+import com.sonatype.insight.brain.api.v2.dto.remediation.ApiComponentRemediationDTO;
+import com.sonatype.insight.brain.api.v2.dto.remediation.options.ApiVersionChangeOptionDTO;
+import com.sonatype.insight.brain.api.v2.service.ApiComponentRemediationService;
 import com.sonatype.insight.brain.git.PullRequestFeatureCheck;
 import com.sonatype.insight.brain.model.Application;
+import com.sonatype.insight.brain.model.OwnerType;
 import com.sonatype.insight.brain.model.policy.notifications.PolicyNotification;
 
 import org.slf4j.Logger;
@@ -29,24 +37,53 @@ public class PolicyAlertScmNotifier
 
   private final PullRequestFeatureCheck pullRequestFeatureCheck;
 
+  private final ApiComponentRemediationService remediationService;
+
+  private final PolicyAlertSourceCodeOrganizer policyAlertSourceCodeOrganizer;
+
   @Inject
-  public PolicyAlertScmNotifier(final PullRequestFeatureCheck pullRequestFeatureCheck) {
+  public PolicyAlertScmNotifier(final PullRequestFeatureCheck pullRequestFeatureCheck,
+                                final ApiComponentRemediationService remediationService,
+                                final PolicyAlertSourceCodeOrganizer policyAlertSourceCodeOrganizer)
+  {
     this.pullRequestFeatureCheck = pullRequestFeatureCheck;
+    this.remediationService = remediationService;
+    this.policyAlertSourceCodeOrganizer = policyAlertSourceCodeOrganizer;
   }
 
   public void sendNotifications(final Application app,
-                                final String scanId,
-                                final Stage stage,
-                                final List<PolicyNotification> policyNotifications,
-                                final int grandfatheredPolicyViolationCount,
-                                final String targetUrl)
+                                final List<PolicyNotification> policyNotifications)
       throws IOException
   {
     if (!pullRequestFeatureCheck.isPullRequestFeatureSupported(app)) {
       return;
     }
 
-    // TODO invoke PR engine
-    log.debug("Invoke PR engine to construct a PR");
+    // for each component, check to see if remediation options are available
+    String ownerId = app.getId();
+    SortedMap<ComponentIdentifier, List<PolicyNotification>> sortedComponentAlerts =
+        policyAlertSourceCodeOrganizer.getNotificationsForScm(policyNotifications);
+    for (Map.Entry<ComponentIdentifier, List<PolicyNotification>> entry : sortedComponentAlerts.entrySet()) {
+      List<ApiVersionChangeOptionDTO> remediationOptions = getRemediationList(entry.getKey(), ownerId);
+      if (remediationOptions.isEmpty()) {
+        log.debug("No remediation options found for component [{}]", entry.getKey());
+        continue;
+      }
+      // TODO invoke PR engine
+      log.debug("Invoke PR engine to construct a PR for [{}]", entry.getKey());
+    }
+
+  }
+
+  private List<ApiVersionChangeOptionDTO> getRemediationList(ComponentIdentifier componentIdentifier,
+                                 String ownerId)
+  {
+    ApiComponentDTOV2 componentDto = new ApiComponentDTOV2();
+    componentDto.componentIdentifier = ApiComponentIdentifierDTOV2.fromComponentIdentifier(componentIdentifier);
+    OwnerType ownerType = OwnerType.APPLICATION;
+    String stageId = null;
+    ApiComponentRemediationDTO remediationDTO =
+        remediationService.getSuggestedRemediationForComponent(componentDto, ownerType, ownerId, stageId);
+    return remediationDTO.remediation.versionChanges;
   }
 }
--- insight-brain-service/src/test/java/com/sonatype/insight/brain/policy/evaluator/PolicyAlertScmNotifierTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/policy/evaluator/PolicyAlertScmNotifierTest.java
@@ -5,25 +5,43 @@
  */
 package com.sonatype.insight.brain.policy.evaluator;
 
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
-import com.sonatype.clm.dto.model.policy.Stage;
+import com.sonatype.clm.dto.model.component.ComponentIdentifier;
+import com.sonatype.clm.dto.model.policy.ComponentFact;
+import com.sonatype.clm.dto.model.policy.PolicyFact;
+import com.sonatype.insight.brain.api.v2.dto.ApiComponentDTOV2;
+import com.sonatype.insight.brain.api.v2.dto.ApiComponentIdentifierDTOV2;
+import com.sonatype.insight.brain.api.v2.dto.remediation.ApiComponentRemediationDTO;
+import com.sonatype.insight.brain.api.v2.dto.remediation.actions.ApiComponentChangeActionDTO;
+import com.sonatype.insight.brain.api.v2.dto.remediation.options.ApiVersionChangeOptionDTO;
+import com.sonatype.insight.brain.api.v2.dto.remediation.options.ApiVersionChangeOptionType;
+import com.sonatype.insight.brain.api.v2.service.ApiComponentRemediationService;
 import com.sonatype.insight.brain.git.PullRequestFeatureCheck;
 import com.sonatype.insight.brain.model.Application;
+import com.sonatype.insight.brain.model.OwnerType;
+import com.sonatype.insight.brain.model.policy.notifications.Notifications;
+import com.sonatype.insight.brain.model.policy.notifications.PolicyNotification;
+import com.sonatype.insight.brain.model.policy.notifications.UserNotification;
 import com.sonatype.insight.brain.service.AbstractComponentTest;
+import com.sonatype.insight.test.LogOutput;
 
-import org.assertj.core.util.Lists;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.when;
 
 public class PolicyAlertScmNotifierTest
     extends AbstractComponentTest
 {
-  private static final Stage DEFAULT_STAGE = new Stage(Stage.ID_BUILD);
-
   private static final String PUBLIC_ID = "abc123";
 
   private static final String NAME = "reponame";
@@ -33,19 +51,92 @@ public class PolicyAlertScmNotifierTest
   @Mock
   private PullRequestFeatureCheck pullRequestFeatureCheck;
 
+  @Mock
+  private ApiComponentRemediationService remediationService;
+
   private PolicyAlertScmNotifier scmNotifier;
 
+  private PolicyAlertSourceCodeOrganizer policyAlertSourceCodeOrganizer;
+
+  private Application application;
+
+  @Rule
+  public LogOutput logOutput = new LogOutput(PolicyAlertScmNotifier.class);
+
   @Before
-  public void setup() {
-    scmNotifier = new PolicyAlertScmNotifier(pullRequestFeatureCheck);
+  public void setup() throws Exception {
+    policyAlertSourceCodeOrganizer = new PolicyAlertSourceCodeOrganizer();
+    scmNotifier =
+        new PolicyAlertScmNotifier(pullRequestFeatureCheck, remediationService, policyAlertSourceCodeOrganizer);
+    application = new Application(PUBLIC_ID, NAME, ORGANIZATION_ID);
+
+    when(pullRequestFeatureCheck.isPullRequestFeatureSupported(any(Application.class))).thenReturn(true);
   }
 
   @Test
-  public void testShouldRunPullRequestFeature() throws IOException {
-    Application application = new Application(PUBLIC_ID, NAME, ORGANIZATION_ID);
-    when(pullRequestFeatureCheck.isPullRequestFeatureSupported(application)).thenReturn(false);
-    scmNotifier.sendNotifications(application, "", DEFAULT_STAGE, Lists.emptyList(), 0, "");
+  public void test_noRemediationOptions() throws Exception {
+    ApiComponentRemediationDTO emptyRemediationDTO = new ApiComponentRemediationDTO();
+    when(remediationService
+        .getSuggestedRemediationForComponent(any(ApiComponentDTOV2.class), eq(OwnerType.APPLICATION),
+            eq(application.getId()), isNull()))
+        .thenReturn(emptyRemediationDTO);
 
-    // TODO assert no invocations on anything once we have something
+    PolicyFact policyFact1 = new PolicyFact("policyid-1", "policyname-1", 3);
+    policyFact1.addComponentFact(
+        new ComponentFact(ComponentIdentifier.createNugetCoordinates("Package1", "1.2.3"), randomString()));
+
+    Notifications notifications = new Notifications(
+        new UserNotification("foo@mail.com", "release")
+    );
+    PolicyNotification policyNotification1 = new PolicyNotification(policyFact1, notifications);
+    List<PolicyNotification> policyNotifications = Arrays.asList(policyNotification1);
+
+    scmNotifier.sendNotifications(application, policyNotifications);
+
+    assertThat(logOutput).atDebugLevel()
+        .contains("No remediation options found for component [nuget: {packageId=Package1, version=1.2.3}]");
+    assertThat(logOutput).atDebugLevel()
+        .doesNotContain("Invoke PR engine to construct a PR");
+  }
+
+  @Test
+  public void test_invokePREngine() throws Exception {
+    // construct a remediation DTO with a suggested remediation
+    ApiVersionChangeOptionDTO versionChangeOptionDTO = new ApiVersionChangeOptionDTO();
+    versionChangeOptionDTO.setType(ApiVersionChangeOptionType.NEXT_NON_FAILING);
+    ApiComponentChangeActionDTO changeActionDTO = new ApiComponentChangeActionDTO();
+    ApiComponentDTOV2 componentDTOV2 = new ApiComponentDTOV2();
+    changeActionDTO.setComponent(componentDTOV2);
+    // upgrade version
+    componentDTOV2.componentIdentifier = ApiComponentIdentifierDTOV2
+        .fromComponentIdentifier(ComponentIdentifier.createNugetCoordinates("Package1", "2.0.1"));
+    ApiComponentRemediationDTO remediationDTO = new ApiComponentRemediationDTO();
+    remediationDTO.remediation.versionChanges = Arrays.asList(versionChangeOptionDTO);
+
+    when(remediationService
+        .getSuggestedRemediationForComponent(any(ApiComponentDTOV2.class), eq(OwnerType.APPLICATION),
+            eq(application.getId()), isNull()))
+        .thenReturn(remediationDTO);
+
+    PolicyFact policyFact1 = new PolicyFact("policyid-1", "policyname-1", 3);
+    policyFact1.addComponentFact(
+        new ComponentFact(ComponentIdentifier.createNugetCoordinates("Package1", "1.2.3"), randomString()));
+
+    Notifications notifications = new Notifications(
+        new UserNotification("foo@mail.com", "release")
+    );
+    PolicyNotification policyNotification1 = new PolicyNotification(policyFact1, notifications);
+    List<PolicyNotification> policyNotifications = Arrays.asList(policyNotification1);
+
+    scmNotifier.sendNotifications(application, policyNotifications);
+
+    assertThat(logOutput).atDebugLevel()
+        .contains("Invoke PR engine to construct a PR for [nuget: {packageId=Package1, version=1.2.3}]");
+    assertThat(logOutput).atDebugLevel()
+        .doesNotContain("No remediation options found");
+  }
+
+  private String randomString() {
+    return UUID.randomUUID().toString();
   }
 }

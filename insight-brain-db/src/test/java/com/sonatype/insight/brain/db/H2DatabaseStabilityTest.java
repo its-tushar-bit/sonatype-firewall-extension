fa439d161d45fee44c36693104803726d2fed5ff
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2019-09-27 16:13:58 +0200

    Included patched version of H2's ValueTimestamp class to restore backward-compatibility with databases/timestamps written by H2 1.3.x CLM-13616 (PR #4316)

--- insight-brain-db/src/test/java/com/sonatype/insight/brain/db/H2DatabaseStabilityTest.java
+++ insight-brain-db/src/test/java/com/sonatype/insight/brain/db/H2DatabaseStabilityTest.java
@@ -8,11 +8,14 @@ package com.sonatype.insight.brain.db;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 import com.sonatype.insight.db.DatabaseConfig;
 
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class H2DatabaseStabilityTest
     extends AbstractDatabaseTest
@@ -39,6 +42,28 @@ public class H2DatabaseStabilityTest
         statement.execute("SET SCHEMA insight_brain_ods;");
         statement.executeQuery("SELECT count(*) FROM insight_brain_ods.application");
       }
+    }
+  }
+
+  /**
+   * Verifies timestamps written by 1.3.174 can be loaded and do not suffer from:
+   * https://github.com/h2database/h2database/issues/551
+   * https://github.com/h2database/h2database/issues/1284
+   */
+  @Test
+  public void testLegacyTimestampFormat() throws Exception {
+    File databaseDir = tempDir.newFolder();
+    copyDatabase(databaseDir, getClass().getSimpleName());
+    DatabaseConfig databaseConfig = getDatabaseConfig(databaseDir, "timestamp");
+    try (Connection connection = DriverManager.getConnection(databaseConfig.getUrl(), "", "");
+        Statement statement = connection.createStatement();
+        ResultSet results = statement.executeQuery("SELECT open_time FROM policy_violation")) {
+      int rows = 0;
+      while (results.next()) {
+        rows++;
+        assertThat(results.getTimestamp(1).toString()).matches("[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:59:59.999");
+      }
+      assertThat(rows).isEqualTo(24);
     }
   }
 }

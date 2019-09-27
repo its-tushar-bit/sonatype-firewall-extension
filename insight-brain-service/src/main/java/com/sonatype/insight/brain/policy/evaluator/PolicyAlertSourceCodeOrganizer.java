e41770dc605ff88f5eb76e2dab655b348ae7a329
Collin Peters <cpeters@sonatype.com>
2019-09-27 09:13:05 -0700

    INT-2133 Sort components by highest threats in the policies (#4310)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/policy/evaluator/PolicyAlertSourceCodeOrganizer.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/policy/evaluator/PolicyAlertSourceCodeOrganizer.java
@@ -7,11 +7,13 @@
 package com.sonatype.insight.brain.policy.evaluator;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import com.sonatype.clm.dto.model.component.ComponentIdentifier;
 import com.sonatype.clm.dto.model.policy.ComponentFact;
@@ -19,18 +21,24 @@ import com.sonatype.insight.brain.model.policy.notifications.PolicyNotification;
 
 public class PolicyAlertSourceCodeOrganizer
 {
+  private static final int INITIAL_DEPTH = 0;
+
+  private final Comparator<PolicyNotification> threatLevelComparator =
+      Comparator.comparing(policyNotification -> policyNotification.getPolicyFact().getThreatLevel());
+
   /**
-   * Returns a sorted Map for all notifications ready for SCM notification.
-   * Note that the keySet is sorted, and the entries in the value list are also sorted.
+   * Returns a map of all policy notifications, aggregated by component and sorted by policy threats.
+   * Note that the map itself is sorted, and also that the entries in the value list are also sorted.
    */
-  public SortedMap<ComponentIdentifier, List<PolicyNotification>> getNotificationsForScm(
-      List<PolicyNotification> policyNotifications)
+  public Map<ComponentIdentifier, List<PolicyNotification>> getNotificationsForScm(
+      final List<PolicyNotification> policyNotifications)
   {
     if (policyNotifications == null) {
       return null;
     }
 
-    SortedMap<ComponentIdentifier, List<PolicyNotification>> componentMap = new TreeMap<>();
+    // Aggregate components
+    Map<ComponentIdentifier, List<PolicyNotification>> componentMap = new HashMap<>();
     for (PolicyNotification policyNotification : policyNotifications) {
       for (ComponentFact componentFact : policyNotification.getPolicyFact().getComponentFacts()) {
         ComponentIdentifier componentIdentifier = componentFact.getComponentIdentifier();
@@ -39,13 +47,78 @@ public class PolicyAlertSourceCodeOrganizer
       }
     }
 
-    // sort all lists by priority order
-    Comparator<PolicyNotification> notificationComparator =
-        Comparator.comparing(policyNotification -> policyNotification.getPolicyFact().getThreatLevel());
+    // Within each component, sort all policy notifications by threat level
     for (List<PolicyNotification> componentPolicyNotifications : componentMap.values()) {
-      Collections.sort(componentPolicyNotifications, notificationComparator.reversed());
+      componentPolicyNotifications.sort(threatLevelComparator.reversed());
     }
 
-    return componentMap;
+    // Finally, re-sort the components by highest threats within the policies, and then the component name
+    return componentMap.entrySet()
+        .stream()
+        .sorted(this::compareEntries)
+        .collect(Collectors.toMap(Entry::getKey, Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+  }
+
+  private int compareEntries(
+      final Entry<ComponentIdentifier, List<PolicyNotification>> entry1,
+      final Entry<ComponentIdentifier, List<PolicyNotification>> entry2)
+  {
+    int listCompare = comparePolicyViolationThreats(entry1.getValue(), entry2.getValue(), INITIAL_DEPTH);
+    // If the policy notification lists are equal, fall back to a name comparison
+    return listCompare == 0 ? nameCompare(entry1.getKey(), entry2.getKey()) : listCompare;
+  }
+
+  /**
+   * Compare two policy notification lists and sort by highest threat level at each depth. i.e. A '10 10' will sort
+   * ahead of a '10 9' which will sort ahead of a '10'
+   */
+  private int comparePolicyViolationThreats(
+      final List<PolicyNotification> policyNotifications1,
+      final List<PolicyNotification> policyNotifications2,
+      final int depth)
+  {
+    // Short-circuit if the depths don't match
+    if (policyNotifications1.size() == depth && policyNotifications2.size() >= depth) {
+      return checkDepth(policyNotifications1, policyNotifications2, depth, 1);
+    }
+    if (policyNotifications2.size() <= depth && policyNotifications1.size() >= depth) {
+      return checkDepth(policyNotifications1, policyNotifications2, depth, -1);
+    }
+
+    int threatLevel1 = policyNotifications1.get(depth).getPolicyFact().getThreatLevel();
+    int threatLevel2 = policyNotifications2.get(depth).getPolicyFact().getThreatLevel();
+
+    return threatLevel1 > threatLevel2 ? -1 :
+        threatLevel1 < threatLevel2 ? 1 :
+            comparePolicyViolationThreats(policyNotifications1, policyNotifications2, depth + 1);
+  }
+
+  /**
+   * Compare by component id, but without the format
+   */
+  private int nameCompare(
+      final ComponentIdentifier componentIdentifier1,
+      final ComponentIdentifier componentIdentifier2)
+  {
+    return componentNameWithoutFormat(componentIdentifier1).compareTo(componentNameWithoutFormat(componentIdentifier2));
+  }
+
+  private String componentNameWithoutFormat(final ComponentIdentifier componentIdentifier) {
+    return String.join(":", componentIdentifier.getCoordinates().values());
+  }
+
+  private int checkDepth(
+      final List<PolicyNotification> policyNotifications1,
+      final List<PolicyNotification> policyNotifications2,
+      final int depth,
+      final int defaultValue)
+  {
+    // if both lists are the same size, they are equal
+    if (policyNotifications1.size() == depth && policyNotifications2.size() == depth) {
+      return 0;
+    }
+    else {
+      return defaultValue;
+    }
   }
 }
--- insight-brain-service/src/test/java/com/sonatype/insight/brain/policy/evaluator/PolicyAlertSourceCodeOrganizerTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/policy/evaluator/PolicyAlertSourceCodeOrganizerTest.java
@@ -6,7 +6,6 @@
 package com.sonatype.insight.brain.policy.evaluator;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -22,96 +21,190 @@ import com.sonatype.insight.brain.model.policy.notifications.UserNotification;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.sonatype.clm.dto.model.component.ComponentIdentifier.createMavenCoordinates;
+import static com.sonatype.clm.dto.model.component.ComponentIdentifier.createNugetCoordinates;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PolicyAlertSourceCodeOrganizerTest
 {
+  private static final String CRITICAL = "Severity-Critical: 9";
+
+  private static final String MEDIUM = "Severity-Medium: 5";
+
+  private static final String LOW = "Severity-Low: 2";
+
+  private static final ComponentFact EGGS = createComponentFact("Eggs", "1.2.3");
+
+  private static final ComponentFact BACON = createComponentFact("Bacon", "2.0.1");
+
+  private static final ComponentFact PANCAKES = createComponentFact("Pancakes", "0.2.1");
+
+  private static final ComponentFact TOAST = createComponentFact("Toast", "5.7.0");
+
+  private static final ComponentFact SAUSAGE = createComponentFact("Sausage", "3.2.1");
+
+  private static final ComponentFact HASH_BROWNS = createComponentFact("Hash-Browns", "2.5");
+
+  private static final ComponentFact SYRUP = createComponentFact("Syrup", "1.5.8");
+
+  private static final ComponentFact SANDWICH = createMavenComponentFact("Sandwich", "0.8.2");
+
+  private PolicyFact critical1;
+
+  private PolicyFact critical2;
+
+  private PolicyFact medium;
+
+  private PolicyFact low;
+
   private PolicyAlertSourceCodeOrganizer policyAlertSourceCodeOrganizer;
+
+  private static String getComponentId(ComponentIdentifier componentIdentifier) {
+    return String.join(":", componentIdentifier.getCoordinates().values());
+  }
+
+  private static String getComponentId(ComponentFact componentFact) {
+    return getComponentId(componentFact.getComponentIdentifier());
+  }
 
   @Before
   public void setup() {
     policyAlertSourceCodeOrganizer = new PolicyAlertSourceCodeOrganizer();
+
+    critical1 = new PolicyFact("policyid-2", "Severity-Critical", 9);
+    critical2 = new PolicyFact("policyid-2b", "Severity-Critical", 9);
+    medium = new PolicyFact("policyid-3", "Severity-Medium", 5);
+    low = new PolicyFact("policyid-1", "Severity-Low", 2);
   }
 
   @Test
-  public void testGetNotificationsForScm__basic() {
-    PolicyFact policyFactMid = new PolicyFact("policyid-1", "Mid", 5);
-    policyFactMid.addComponentFact(
-        new ComponentFact(ComponentIdentifier.createNugetCoordinates("Package1", "1.2.3"), randomString()));
-    policyFactMid.addComponentFact(
-        new ComponentFact(ComponentIdentifier.createNugetCoordinates("Package2", "1.3.4"), randomString()));
-    policyFactMid.addComponentFact(
-        new ComponentFact(ComponentIdentifier.createNugetCoordinates("AA-Package", "1.0.1"), randomString()));
+  public void testGetNotificationsForScm__singleComponent_singleViolation() {
+    medium.addComponentFact(EGGS);
 
-    PolicyFact policyFactCritical = new PolicyFact("policyid-2", "Critical", 9);
-    policyFactCritical.addComponentFact(
-        new ComponentFact(ComponentIdentifier.createNugetCoordinates("Package1-2", "1.2.3"), randomString()));
-    policyFactCritical.addComponentFact(
-        new ComponentFact(ComponentIdentifier.createNugetCoordinates("Package2-2", "1.3.4"), randomString()));
-    policyFactCritical.addComponentFact(
-        new ComponentFact(ComponentIdentifier.createNugetCoordinates("Package2", "1.3.4"), randomString()));
-    policyFactCritical.addComponentFact(
-        new ComponentFact(ComponentIdentifier.createNugetCoordinates("AA-Package-2", "1.0.1"), randomString()));
+    Map<ComponentIdentifier, List<PolicyNotification>> results = runTest();
 
-    // components appear in multiple policies with different threat levels
-    PolicyFact policyFactLow = new PolicyFact("policyid-3", "Low", 1);
-    policyFactLow.addComponentFact(
-        new ComponentFact(ComponentIdentifier.createNugetCoordinates("Package1", "1.2.3"), randomString()));
-    policyFactLow.addComponentFact(
-        new ComponentFact(ComponentIdentifier.createNugetCoordinates("AA-Package", "1.0.1"), randomString()));
+    validateComponents(results, EGGS);
+    validateComponentsPolicies(results, EGGS, new String[]{MEDIUM});
+  }
 
-    Notifications notifications = new Notifications(
-        new UserNotification("foo@mail.com", "release")
-    );
-    PolicyNotification policyNotificationMid = new PolicyNotification(policyFactMid, notifications);
-    PolicyNotification policyNotificationCritical = new PolicyNotification(policyFactCritical, notifications);
-    PolicyNotification policyNotificationLow = new PolicyNotification(policyFactLow, notifications);
+  @Test
+  public void testGetNotificationsForScm__singleComponent_multipleViolation() {
+    medium.addComponentFact(EGGS);
+    critical1.addComponentFact(EGGS);
+
+    Map<ComponentIdentifier, List<PolicyNotification>> results = runTest();
+
+    validateComponents(results, EGGS);
+    validateComponentsPolicies(results, EGGS, new String[]{CRITICAL, MEDIUM});
+  }
+
+  @Test
+  public void testGetNotificationsForScm__multipleComponents_singleViolation() {
+    medium.addComponentFact(PANCAKES);
+    medium.addComponentFact(EGGS);
+    medium.addComponentFact(BACON);
+
+    Map<ComponentIdentifier, List<PolicyNotification>> results = runTest();
+
+    validateComponents(results, BACON, EGGS, PANCAKES);
+    validateComponentsPolicies(results, EGGS, new String[]{MEDIUM});
+    validateComponentsPolicies(results, BACON, new String[]{MEDIUM});
+    validateComponentsPolicies(results, PANCAKES, new String[]{MEDIUM});
+  }
+
+  private static ComponentFact createMavenComponentFact(final String name, final String version) {
+    return new ComponentFact(createMavenCoordinates("groupid", name, version), UUID.randomUUID().toString());
+  }
+
+  @Test
+  public void testGetNotificationsForScm__multipleComponents_multipleViolations() {
+    // Bacon - critical & critical
+    // Toast - critical & low
+    // Sausage - critical
+    // Eggs - medium & low
+    // Pancakes - medium
+    // Syrup - medium (Syrup should sort alphabetically after Pancakes)
+    // Hashbrowns - low
+
+    critical1.addComponentFact(BACON);
+    critical1.addComponentFact(TOAST);
+    critical1.addComponentFact(SAUSAGE);
+
+    critical2.addComponentFact(BACON);
+
+    medium.addComponentFact(EGGS);
+    medium.addComponentFact(SYRUP);
+    medium.addComponentFact(PANCAKES);
+
+    low.addComponentFact(EGGS);
+    low.addComponentFact(TOAST);
+    low.addComponentFact(HASH_BROWNS);
+
+    Map<ComponentIdentifier, List<PolicyNotification>> results = runTest();
+
+    validateComponents(results, BACON, TOAST, SAUSAGE, EGGS, PANCAKES, SYRUP, HASH_BROWNS);
+    validateComponentsPolicies(results, BACON, new String[]{CRITICAL, CRITICAL});
+    validateComponentsPolicies(results, TOAST, new String[]{CRITICAL, LOW});
+    validateComponentsPolicies(results, SAUSAGE, new String[]{CRITICAL});
+    validateComponentsPolicies(results, EGGS, new String[]{MEDIUM, LOW});
+    validateComponentsPolicies(results, PANCAKES, new String[]{MEDIUM});
+    validateComponentsPolicies(results, SYRUP, new String[]{MEDIUM});
+    validateComponentsPolicies(results, HASH_BROWNS, new String[]{LOW});
+  }
+
+  @Test
+  public void testGetNotificationsForScm__obeyFormats() {
+    critical1.addComponentFact(EGGS); // nuget
+    critical1.addComponentFact(TOAST); // nuget
+    critical1.addComponentFact(SANDWICH); // maven
+    // so if format is ignored the resulting order should be: eggs, sandwich, toast
+
+    Map<ComponentIdentifier, List<PolicyNotification>> results = runTest();
+
+    validateComponents(results, EGGS, SANDWICH, TOAST);
+    validateComponentsPolicies(results, EGGS, new String[]{CRITICAL});
+    validateComponentsPolicies(results, SANDWICH, new String[]{CRITICAL});
+    validateComponentsPolicies(results, TOAST, new String[]{CRITICAL});
+  }
+
+  private Map<ComponentIdentifier, List<PolicyNotification>> runTest() {
+    Notifications notifications = new Notifications(new UserNotification("foo@mail.com", "release"));
+    PolicyNotification policyNotificationMid = new PolicyNotification(medium, notifications);
+    PolicyNotification policyNotificationCritical = new PolicyNotification(critical1, notifications);
+    PolicyNotification policyNotificationCritical2 = new PolicyNotification(critical2, notifications);
+    PolicyNotification policyNotificationLow = new PolicyNotification(low, notifications);
     List<PolicyNotification> policyNotifications = Arrays.asList(
-        policyNotificationMid, policyNotificationCritical, policyNotificationLow
+        policyNotificationMid, policyNotificationCritical, policyNotificationCritical2, policyNotificationLow
     );
 
-    Map<ComponentIdentifier, List<PolicyNotification>> sortedNotifications =
-        policyAlertSourceCodeOrganizer.getNotificationsForScm(policyNotifications);
-
-    assertThat(sortedNotifications.keySet()).extracting(componentIdentifier -> componentIdentifier.get("packageId"))
-        .containsExactly("AA-Package", "AA-Package-2", "Package1", "Package1-2", "Package2", "Package2-2");
-    assertThat(sortedNotifications.keySet()).extracting(componentIdentifier -> componentIdentifier.get("packageId"))
-        .isSorted();
-    Comparator<PolicyNotification> notificationComparator =
-        Comparator.comparing(policyNotification -> policyNotification.getPolicyFact().getThreatLevel());
-    for (List<PolicyNotification> resultNotificationList : sortedNotifications.values()) {
-      assertThat(resultNotificationList).isSortedAccordingTo(notificationComparator.reversed());
-    }
-
-    validateComponentsPolicies(sortedNotifications, ComponentIdentifier.createNugetCoordinates("Package1", "1.2.3"),
-        new String[]{"Mid: 5", "Low: 1"});
-    validateComponentsPolicies(sortedNotifications, ComponentIdentifier.createNugetCoordinates("Package2", "1.3.4"),
-        new String[]{"Critical: 9", "Mid: 5"});
-    validateComponentsPolicies(sortedNotifications, ComponentIdentifier.createNugetCoordinates("AA-Package", "1.0.1"),
-        new String[]{"Mid: 5", "Low: 1"});
-    validateComponentsPolicies(sortedNotifications, ComponentIdentifier.createNugetCoordinates("Package1-2", "1.2.3"),
-        new String[]{"Critical: 9"});
-    validateComponentsPolicies(sortedNotifications, ComponentIdentifier.createNugetCoordinates("Package2-2", "1.3.4"),
-        new String[]{"Critical: 9"});
-    validateComponentsPolicies(sortedNotifications, ComponentIdentifier.createNugetCoordinates("AA-Package-2", "1.0.1"),
-        new String[]{"Critical: 9"});
-
-    assertThat(sortedNotifications.get(ComponentIdentifier.createNugetCoordinates("AA-Package-2", "1.0.1")))
-        .containsExactly(policyNotificationCritical);
+    return policyAlertSourceCodeOrganizer.getNotificationsForScm(policyNotifications);
   }
 
   private void validateComponentsPolicies(Map<ComponentIdentifier, List<PolicyNotification>> sortedNotifications,
-                                          ComponentIdentifier componentIdentifier,
+                                          ComponentFact componentFact,
                                           String[] matchedPolicies)
   {
-    assertThat(sortedNotifications.get(componentIdentifier)
-        .stream().map(policyNotification -> policyNotification.getPolicyFact())
+    assertThat(sortedNotifications.get(componentFact.getComponentIdentifier())
+        .stream()
+        .map(PolicyNotification::getPolicyFact)
         .map(policyFact -> String.format("%s: %d", policyFact.getPolicyName(), policyFact.getThreatLevel()))
         .collect(Collectors.toList()))
         .containsExactly(matchedPolicies);
   }
 
-  private String randomString() {
-    return UUID.randomUUID().toString();
+  private static ComponentFact createComponentFact(final String name, final String version) {
+    return new ComponentFact(createNugetCoordinates(name, version), UUID.randomUUID().toString());
+  }
+
+  private void validateComponents(
+      final Map<ComponentIdentifier, List<PolicyNotification>> results,
+      final ComponentFact... componentFacts)
+  {
+    String[] packages = Arrays.stream(componentFacts).map(PolicyAlertSourceCodeOrganizerTest::getComponentId)
+        .toArray(String[]::new);
+
+    assertThat(results.keySet())
+        .extracting(PolicyAlertSourceCodeOrganizerTest::getComponentId)
+        .containsExactly(packages);
   }
 }

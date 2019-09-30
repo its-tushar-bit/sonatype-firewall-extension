b32ea993c7ecb5868464081727f0b71f49da399d
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2019-09-30 18:54:27 +0200

    Configured IQ Server during functional tests to use same base URL as used by the (dockerized) web browser to reach it

--- insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/AbstractFunctionalTest.java
+++ insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/AbstractFunctionalTest.java
@@ -126,23 +126,23 @@ public abstract class AbstractFunctionalTest
     jiraService = Mockito.mock(JiraService.class);
     initMocks();
 
-    String contextPath = System.getProperty("iq.contextPath", "/iq-test");
-    testCLMServer = new TestCLMServer(false /* isProxyRequiredToReachHds */, getBrainModules(), new Configurator()
-    {
-      @Override
-      public void configure(InsightConfig config) {
-        config.setBaseUrl(getBaseUrl(contextPath));
-        ((DefaultServerFactory) config.getServerFactory()).setApplicationContextPath(contextPath);
-      }
-    });
-    reverseProxyServer = new ReverseProxyServer(testCLMServer.getCLMServer().getPort());
-
     try {
-      testCLMServer.start();
-      reverseProxyServer.start();
-
+      String contextPath = System.getProperty("iq.contextPath", "/iq-test");
       Configuration.baseUrl = resolveBaseUrl(getBaseUrl(contextPath));
       Configuration.reportsFolder = "target/selenide-reports";
+
+      testCLMServer = new TestCLMServer(false /* isProxyRequiredToReachHds */, getBrainModules(), new Configurator()
+      {
+        @Override
+        public void configure(InsightConfig config) {
+          config.setBaseUrl(Configuration.baseUrl);
+          ((DefaultServerFactory) config.getServerFactory()).setApplicationContextPath(contextPath);
+        }
+      });
+      reverseProxyServer = new ReverseProxyServer(testCLMServer.getCLMServer().getPort());
+
+      testCLMServer.start();
+      reverseProxyServer.start();
     }
     catch (Throwable e) {
       e.printStackTrace();

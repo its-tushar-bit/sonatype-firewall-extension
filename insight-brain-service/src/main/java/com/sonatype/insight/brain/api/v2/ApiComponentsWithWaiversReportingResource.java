b09fd555d73c779f34243b9bbc7769d83b4d3f2a
Brandon Murray <bmurmistro@gmail.com>
2019-10-02 06:14:11 -0600

    Stub out dto resource and service for components with waivers api CLM-13652  (#4327)

---
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiComponentsWithWaiversReportingResource.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.api.v2;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.sonatype.insight.brain.api.PublicApiPaths;
+import com.sonatype.insight.brain.api.v2.dto.ApiComponentWaiversDto;
+import com.sonatype.insight.brain.api.v2.service.ApiComponentsWithWaiversReportingService;
+
+import com.codahale.metrics.annotation.Timed;
+
+/**
+ * @since 1.75
+ */
+@Named
+@Timed
+@Path(PublicApiPaths.REPORTS_RESOURCE_PATH_V2 + ApiComponentsWithWaiversReportingResource.PATH)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ApiComponentsWithWaiversReportingResource
+{
+  public static final String PATH = "/componentsWithWaivers";
+
+  private final ApiComponentsWithWaiversReportingService componentsWithWaiversReportingService;
+
+  @Inject
+  public ApiComponentsWithWaiversReportingResource(
+      ApiComponentsWithWaiversReportingService componentsWithWaiversReportingService)
+  {
+    this.componentsWithWaiversReportingService = componentsWithWaiversReportingService;
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public ApiComponentWaiversDto getComponentsWithWaivers() {
+    return componentsWithWaiversReportingService.getComponentsWithWaivers();
+  }
+}
---
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/service/ApiComponentsWithWaiversReportingService.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.api.v2.service;
+
+import javax.inject.Inject;
+
+import com.sonatype.insight.brain.api.v2.dto.ApiComponentWaiversDto;
+import com.sonatype.insight.brain.policy.evaluator.PolicyViolationLoader;
+
+/**
+ * @since 1.75
+ */
+public class ApiComponentsWithWaiversReportingService
+{
+@SuppressWarnings("PMD")
+  private final PolicyViolationLoader policyViolationLoader;
+
+  @Inject
+  public ApiComponentsWithWaiversReportingService(PolicyViolationLoader policyViolationLoader) {
+    this.policyViolationLoader = policyViolationLoader;
+  }
+
+  public ApiComponentWaiversDto getComponentsWithWaivers() {
+    return null;
+  }
+}

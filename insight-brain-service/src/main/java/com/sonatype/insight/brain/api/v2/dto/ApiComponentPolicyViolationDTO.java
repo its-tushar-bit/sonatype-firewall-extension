0b14d93eb0cc62c224153f4d2c7909e576fc8826
Dennis Warren <dwarren@sonatype.com>
2019-10-30 09:04:20 -0600

    Add DTO for API components in quarantine. CLM-13726 (#4445)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/dto/ApiComponentPolicyViolationDTO.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/dto/ApiComponentPolicyViolationDTO.java
@@ -6,14 +6,25 @@
 package com.sonatype.insight.brain.api.v2.dto;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 /**
  * @since 1.76
  */
+@JsonInclude(Include.NON_EMPTY)
 public class ApiComponentPolicyViolationDTO
 {
   public ApiComponentDTOV2 component;
 
   public List<ApiWaivedPolicyViolationDTO> waivedPolicyViolations = new ArrayList<>();
+
+  public List<ApiPolicyViolationDTOV2> policyViolations = new ArrayList<>();
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
+  public Date quarantineTime;
 }

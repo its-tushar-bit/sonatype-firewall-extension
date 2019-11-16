888caa9e6372a629037690c33ce2eea7c55a63c8
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2019-11-17 00:27:05 +0100

    Simplified InsightMailerTest

--- insight-mail/src/test/java/com/sonatype/insight/mail/InsightMailerTest.java
+++ insight-mail/src/test/java/com/sonatype/insight/mail/InsightMailerTest.java
@@ -17,13 +17,11 @@ import org.sonatype.micromailer.EMailer;
 import org.sonatype.micromailer.EmailerConfiguration;
 import org.sonatype.micromailer.MailRequest;
 
-import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.junit.GreenMailRule;
 import com.icegreen.greenmail.util.GreenMailUtil;
 import com.icegreen.greenmail.util.ServerSetup;
-import org.apache.commons.lang.StringUtils;
 import org.eclipse.sisu.launch.InjectedTest;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
@@ -34,44 +32,16 @@ import static org.mockito.Mockito.verify;
 public class InsightMailerTest
     extends InjectedTest
 {
-  private GreenMail smtpServer;
+  @Rule
+  public GreenMailRule smtpServer = new GreenMailRule(new ServerSetup(0, null, ServerSetup.PROTOCOL_SMTP));
 
   @Override
   public void configure(Properties properties) {
-    Object value = get("mail.hostname");
-    if (value == null) {
-      value = "localhost";
-    }
-    properties.put("mail.hostname", value);
-    System.setProperty("mail.hostname", value.toString());
-    value = get("mail.port");
-    if (value == null) {
-      value = 3025;
-    }
-    properties.put("mail.port", value);
-    System.setProperty("mail.port", value.toString());
-    value = get("mail.systemEmail");
-    if (value == null) {
-      value = "test@test.com";
-    }
-    properties.put("mail.systemEmail", value);
-    System.setProperty("mail.systemEmail", value.toString());
+    properties.put("mail.hostname", "localhost");
+    properties.put("mail.port", smtpServer.getSmtp().getPort());
+    properties.put("mail.systemEmail", "test@test.com");
 
     super.configure(properties);
-  }
-
-  @Before
-  public void init() {
-    smtpServer = new GreenMail(new ServerSetup(Integer.valueOf(System.getProperty("mail.port")), null,
-        ServerSetup.PROTOCOL_SMTP));
-    smtpServer.start();
-  }
-
-  @After
-  public void exit() {
-    if (smtpServer != null) {
-      smtpServer.stop();
-    }
   }
 
   @Test
@@ -84,7 +54,7 @@ public class InsightMailerTest
     MailRequest request = mailer.getDefaultMailRequest(subject, msg);
     request.setToAddresses(Arrays.asList(new Address(toAddr)));
     mailer.sendMail(request);
-    smtpServer.waitForIncomingEmail(1);
+    assertThat(smtpServer.waitForIncomingEmail(1)).isTrue();
     MimeMessage mail = smtpServer.getReceivedMessages()[0];
     System.out.println(GreenMailUtil.getHeaders(mail));
     assertThat(GreenMailUtil.getBody(mail)).contains(msg);
@@ -104,21 +74,5 @@ public class InsightMailerTest
     new InsightMailer(eMailer, mailConfig);
     verify(eMailer).configure(emailerConfigurationArgumentCaptor.capture());
     assertThat(emailerConfigurationArgumentCaptor.getValue().getMailPort()).isEqualTo(25);
-  }
-
-  public static String get(String key) {
-    String envvar = System.getenv(key.toUpperCase().replace("-", "_"));
-
-    if (!StringUtils.isEmpty(envvar)) {
-      return envvar;
-    }
-
-    String sysprop = System.getProperty(key);
-
-    if (!StringUtils.isEmpty(sysprop)) {
-      return sysprop;
-    }
-
-    return null;
   }
 }

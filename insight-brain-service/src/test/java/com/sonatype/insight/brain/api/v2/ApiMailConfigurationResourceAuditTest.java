ba208eeb4bc73f8d56c690535217779b702e6821
vladt <vladt@sonatype.com>
2019-12-18 10:51:50 -0500

    Moved ApiMailConfigurationResourceAuditTest from com.sonatype.insight.brain.api.v2.service to com.sonatype.insight.brain.api.v2

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/api/v2/ApiMailConfigurationResourceAuditTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/api/v2/ApiMailConfigurationResourceAuditTest.java
@@ -3,7 +3,7 @@
  * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
-package com.sonatype.insight.brain.api.v2.service;
+package com.sonatype.insight.brain.api.v2;
 
 import com.sonatype.insight.brain.HttpRequest;
 import com.sonatype.insight.brain.api.PublicApiPaths;

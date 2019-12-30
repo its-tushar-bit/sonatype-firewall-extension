85ee88b2af21d85143d570e49d4aa0f276348150
vladt <vladt@sonatype.com>
2019-12-30 16:40:02 -0500

    Avoid deprecated Selenide.Wait.withTimeout method

--- insight-brain-java-functional-test/src/main/java/com/sonatype/clm/testing/functional/pages/KeycloakLoginPage.java
+++ insight-brain-java-functional-test/src/main/java/com/sonatype/clm/testing/functional/pages/KeycloakLoginPage.java
@@ -5,7 +5,7 @@
  */
 package com.sonatype.clm.testing.functional.pages;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 import org.openqa.selenium.JavascriptExecutor;
 
@@ -17,7 +17,7 @@ public class KeycloakLoginPage
   public static void login(String username, String password) {
     // Keycloak's login page takes some time to load, especially the first time it is being visited by a fresh browser.
     // Default timeout, although rarely, causes flaky tests with timeouts due to document not being ready within limits.
-    Wait().withTimeout(10, TimeUnit.SECONDS)
+    Wait().withTimeout(Duration.ofSeconds(10))
         .until(wd -> ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
 
     $("#username").sendKeys(username);

7d2d742d33677ca1d2e44ea83a952d4e30da93c5
koraytugay <ktugay@sonatype.com>
2020-03-02 14:30:45 -0500

    schema_incremental_0164.sql

---
+++ insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0166.sql
@@ -0,0 +1,2 @@
+-- Since GLOBAL_SEARCH
+INSERT INTO system_configuration_property (system_configuration_property_id, name, value) VALUES ('917095b878f8cc8ab116d2647df4d597', 'FULL_TEXT_SEARCH_ENABLED', 'false');

10f945120a639a490da7d19a1fb3c4a2dc7df96d
Koray Tugay <ktugay@sonatype.com>
2020-03-04 11:41:38 -0500

    Advanced Search Page Functional Tests - CLM-15091 (#4884)
    More tests should be added to this class as we work on page itself and discover behaviour that must be verified.

--- insight-brain-java-functional-test/src/main/java/com/sonatype/clm/testing/functional/pages/AdvancedSearchPage.java
+++ insight-brain-java-functional-test/src/main/java/com/sonatype/clm/testing/functional/pages/AdvancedSearchPage.java
@@ -10,6 +10,8 @@ import com.sonatype.clm.testing.functional.utils.BaseUrl;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$;
+
 public class AdvancedSearchPage
     extends BasicElement<AdvancedSearchPage>
 {
@@ -29,5 +31,44 @@ public class AdvancedSearchPage
 
   public SelenideElement advancedSearchDisabledError() {
     return child("#advanced-search-disabled-error");
+  }
+
+  public SelenideElement searchInput() {
+    return child("#global-search-input");
+  }
+
+  public SelenideElement searchButton() {
+    return child("#global-search-button");
+  }
+
+  public SelenideElement resultCount() {
+    return child("#advanced-search-result-count");
+  }
+
+  public SelenideElement currentPageInfo() {
+    return child("#advanced-search-current-page-info");
+  }
+
+  public SelenideElement nextPageButton() {
+    return child("#advanced-search-next-page-button");
+  }
+
+  public SelenideElement previousPageButton() {
+    return child("#advanced-search-previous-page-button");
+  }
+
+  public SelenideElement firstResultResultNumber() {
+    return $("#search-results > div > div:nth-child(2) > span");
+  }
+
+  public SelenideElement lastResultResultNumber() {
+    return $("#search-results > div > div:nth-child(11) > span");
+  }
+
+  // Search results table child 1 is some icon representing what the document is
+  // Second item is the text, such as Organization or Application
+  // 3rd is the actual result (and there is an > a if it is a link to an entity)
+  public SelenideElement firstSearchResultLink() {
+    return $("#advanced-search-results-table > tbody > tr > td:nth-child(3) > a");
   }
 }
--- insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/AdvancedSearchPageTest.java
+++ insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/AdvancedSearchPageTest.java
@@ -5,21 +5,34 @@
  */
 package com.sonatype.clm.testing.functional.brain;
 
+import java.io.IOException;
+import java.util.stream.IntStream;
+
 import com.sonatype.clm.testing.functional.AbstractFunctionalTest;
+import com.sonatype.clm.testing.functional.elements.FormMask;
 import com.sonatype.clm.testing.functional.pages.AdvancedSearchPage;
+import com.sonatype.clm.testing.functional.pages.OwnerSummaryPage;
+import com.sonatype.clm.testing.functional.utils.ScrollUtil;
 import com.sonatype.insight.brain.dataaccess.configuration.SystemConfigurationPropertyDAO;
 import com.sonatype.insight.brain.model.configuration.SystemConfigurationProperty;
+import com.sonatype.insight.brain.search.index.IndexService;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static com.codeborne.selenide.Condition.empty;
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.hidden;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Condition.visible;
 import static com.sonatype.insight.brain.model.configuration.SystemConfigurationProperty.FULL_TEXT_SEARCH_ENABLED;
 
 public class AdvancedSearchPageTest
     extends AbstractFunctionalTest
 {
+  private IndexService indexService = testCLMServer.getCLMServer().getInstance(IndexService.class);
+
   private final AdvancedSearchPage page = new AdvancedSearchPage();
 
   private final SystemConfigurationPropertyDAO dao = new SystemConfigurationPropertyDAO();
@@ -38,10 +51,107 @@ public class AdvancedSearchPageTest
   }
 
   @Test
+  public void testInitialState() {
+    enableAdvancedSearch();
+    refreshOrOpen(AdvancedSearchPage.url());
+    page.searchInput().shouldBe(empty);
+    page.resultCount().shouldBe(text("0"));
+    page.currentPageInfo().shouldBe(text("Page 1 of 1"));
+  }
+
+  @Test
   public void testOptedIn_ShowsEnabledContent() {
-    dao.update(new SystemConfigurationProperty(FULL_TEXT_SEARCH_ENABLED, "true"));
+    enableAdvancedSearch();
     refreshOrOpen(AdvancedSearchPage.url());
     page.advancedSearchDisabledError().shouldBe(hidden);
     page.advancedSearchEnabledContent().shouldBe(visible);
+  }
+
+  @Test
+  public void testSearch_Results_In_SinglePage() throws IOException {
+    enableAdvancedSearch();
+    indexService.createSearchIndex();
+
+    refreshOrOpen(AdvancedSearchPage.url());
+    page.searchInput().setValue("itemType:ORGANIZATION");
+    page.searchButton().click();
+    FormMask.seeAndWaitForDismissal();
+
+    eyesWatcher.eyesCheck("Advanced Search - Single Result");
+
+    page.resultCount().shouldBe(text("1"));
+    page.currentPageInfo().shouldBe(text("Page 1 of 1"));
+    page.searchInput().should(value("itemType:ORGANIZATION"));
+  }
+
+  @Test
+  public void testSearch_Results_In_MultiplePages() throws IOException {
+    enableAdvancedSearch();
+
+    // Create 15 policies
+    IntStream.range(0, 15).forEach(i -> tempEntity.newPolicy());
+    indexService.createSearchIndex();
+
+    refreshOrOpen(AdvancedSearchPage.url());
+    page.searchInput().setValue("itemType:POLICY");
+    page.searchButton().click();
+    FormMask.seeAndWaitForDismissal();
+
+    page.resultCount().shouldBe(text("15"));
+    page.currentPageInfo().shouldBe(text("Page 1 of 2"));
+    page.firstResultResultNumber().shouldBe(text("1"));
+
+    // While we are at it lets scroll to bottom to verify we can see all search results
+    ScrollUtil.awaitEndOfScrolling(page.lastResultResultNumber().should(exist).scrollIntoView(true));
+    page.lastResultResultNumber().shouldBe(visible).shouldBe(text("10"));
+
+    page.nextPageButton().click();
+    page.currentPageInfo().shouldBe(text("Page 2 of 2"));
+    page.firstResultResultNumber().shouldBe(text("11"));
+
+    page.previousPageButton().click();
+    page.currentPageInfo().shouldBe(text("Page 1 of 2"));
+    page.firstResultResultNumber().shouldBe(text("1"));
+  }
+
+  @Test
+  public void testSearch_NoResults() throws IOException {
+    enableAdvancedSearch();
+    indexService.createSearchIndex();
+
+    refreshOrOpen(AdvancedSearchPage.url());
+    page.searchInput().setValue("gibberish");
+    page.searchButton().click();
+    FormMask.seeAndWaitForDismissal();
+
+    page.resultCount().shouldBe(text("0"));
+    page.currentPageInfo().shouldBe(text("Page 1 of 1"));
+
+    page.nextPageButton().click();
+    page.currentPageInfo().shouldBe(text("Page 1 of 1"));
+  }
+
+  @Test
+  public void testNavigationAndResultRetain() throws IOException {
+    enableAdvancedSearch();
+    indexService.createSearchIndex();
+
+    refreshOrOpen(AdvancedSearchPage.url());
+    page.searchInput().setValue("itemType:ORGANIZATION");
+    page.searchButton().click();
+    FormMask.seeAndWaitForDismissal();
+
+    page.firstSearchResultLink().click();
+    waitUntilUrl(OwnerSummaryPage.urlToRootOrg());
+
+    // Go back to Advanced Search Page and verify our search and results are retained
+    refreshOrOpen(AdvancedSearchPage.url());
+    page.resultCount().shouldBe(text("1"));
+    page.currentPageInfo().shouldBe(text("Page 1 of 1"));
+    page.searchInput().should(value("itemType:ORGANIZATION"));
+  }
+
+  private void enableAdvancedSearch() {
+    dao.update(new SystemConfigurationProperty(FULL_TEXT_SEARCH_ENABLED, "true"));
   }
 }

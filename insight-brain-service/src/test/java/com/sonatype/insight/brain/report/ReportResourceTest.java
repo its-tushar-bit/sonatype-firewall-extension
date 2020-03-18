3532914d078dbbe88aa82f0324f2f9c1051a20b8
Richard Mealing <mealingr@gmail.com>
2020-03-18 18:41:32 +0000

    Removed unused BIRT design files from test reports (#4960)

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/report/ReportResourceTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/report/ReportResourceTest.java
@@ -281,7 +281,7 @@ public class ReportResourceTest
       }
     }
 
-    assertThat(verifiedFileCount).isEqualTo(110);
+    assertThat(verifiedFileCount).isEqualTo(109);
     assertResponseStatus(200, request.subpath("/").get());
   }
 

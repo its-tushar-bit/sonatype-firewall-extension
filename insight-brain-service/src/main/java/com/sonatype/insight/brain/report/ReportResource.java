a85696cafe0d2d298212241cdf922febae59f13e
rmealing <rmealing@sonatype.com>
2020-03-28 21:30:37 +0000

    Removed BIRT dependencies and related code - CLM-14947 (#4955)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/report/ReportResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/report/ReportResource.java
@@ -64,8 +64,6 @@ import com.sonatype.insight.brain.landing.UserInterfaceLinksResource;
 import com.sonatype.insight.brain.model.Application;
 import com.sonatype.insight.brain.model.policy.PolicyEvaluation;
 import com.sonatype.insight.brain.model.security.Permission;
-import com.sonatype.insight.brain.organization.ApplicationAdapter;
-import com.sonatype.insight.brain.organization.ContactDTO;
 import com.sonatype.insight.brain.organization.ReportMetadataDTO;
 import com.sonatype.insight.brain.policy.evaluator.ScanPolicyEvaluator;
 import com.sonatype.insight.brain.releasegraph.ReleaseGraphService;
@@ -120,8 +118,6 @@ public class ReportResource
 
   private PolicyEvaluationDAO policyEvaluationDAO = new PolicyEvaluationDAO();
 
-  private ApplicationAdapter applicationAdapter;
-
   private final ReportService reportService;
 
   private final ScanPolicyEvaluator scanPolicyEvaluator;
@@ -151,7 +147,6 @@ public class ReportResource
                         final ScanPolicyEvaluator scanPolicyEvaluator,
                         InsightWork work,
                         BaseUrl baseUrl,
-                        ApplicationAdapter applicationAdapter,
                         ApiReportDataServiceV2 reportDataService,
                         ReleaseGraphService releaseGraphService,
                         ComponentDetailsLoader componentDetailsLoader,
@@ -162,7 +157,6 @@ public class ReportResource
     this.scanPolicyEvaluator = scanPolicyEvaluator;
     this.work = work;
     this.baseUrl = baseUrl;
-    this.applicationAdapter = applicationAdapter;
     this.reportDataService = reportDataService;
     this.releaseGraphService = releaseGraphService;
     this.componentDetailsLoader = componentDetailsLoader;
@@ -376,8 +370,7 @@ public class ReportResource
     int dataVersion = Integer.parseInt(templateProps.getProperty("data.version", "0"));
     String dataPath = "data/";
 
-    ContactDTO contact = applicationAdapter.getContact(app.getContactInternalName());
-    File pdfFile = Report.printPdf(reportFile, "", "", contact);
+    File pdfFile = pdfGeneratorService.generateReport(app, scanId);
 
     ApiReportRawDataDTOV2 reportData = reportDataService.getDataNoAuth(appPublicId, scanId);
     List<PolicyAlert> alerts = Arrays.asList(JsonUtils

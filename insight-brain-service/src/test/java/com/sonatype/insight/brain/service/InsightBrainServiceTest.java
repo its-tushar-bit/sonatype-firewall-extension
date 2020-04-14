58a138c2fdbe9457478cafe374e3f1b56bee87c7
Vlad Tatavu <vladt@sonatype.com>
2020-04-14 11:45:54 -0400

    Added JVM and OS info to telemetry data CLM-15411 CLM-15482 (#5043)

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/service/InsightBrainServiceTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/service/InsightBrainServiceTest.java
@@ -120,7 +120,7 @@ public class InsightBrainServiceTest
         responses.put(new ByteArrayDataSource(request.getInputStream(), "multipart/form-data"), response.getStatus());
       }).andStatus(204).atUri(TelemetrySender.RESOURCE_PATH);
     });
-    await().atMost(5, SECONDS).untilAsserted(() -> assertThat(responses).hasSize(7));
+    await().atMost(5, SECONDS).untilAsserted(() -> assertThat(responses).hasSize(8));
     Date expectedMaxCreateTime = new Date();
     Collection<TelemetryData> allTelemetryData =
         assertTelemetry(responses, expectedMinCreateTime, expectedMaxCreateTime);
@@ -164,6 +164,9 @@ public class InsightBrainServiceTest
         case ROLE_USAGE:
           assertThat(telemetryDataReceived.getAttributes()).isNotEmpty();
           break;
+        case RUNTIME_ENVIRONMENT:
+          assertThat(telemetryDataReceived.getAttributes()).isNotEmpty();
+          break;
         default:
           fail("Unexpected telemetry purpose: " + telemetryPurpose);
           break;
@@ -171,7 +174,8 @@ public class InsightBrainServiceTest
     }
     assertThat(telemetryPurposes).containsOnly(TelemetryPurpose.HIERARCHY_METRICS,
         TelemetryPurpose.POLICY_STATUS_OVERRIDE, TelemetryPurpose.DATABASE, TelemetryPurpose.CONFIGURATION_PROPERTIES,
-        TelemetryPurpose.REALM, TelemetryPurpose.SOURCE_CONTROL_METRICS, TelemetryPurpose.ROLE_USAGE);
+        TelemetryPurpose.REALM, TelemetryPurpose.SOURCE_CONTROL_METRICS, TelemetryPurpose.ROLE_USAGE,
+        TelemetryPurpose.RUNTIME_ENVIRONMENT);
   }
 
   private Collection<TelemetryData> assertTelemetry(
@@ -247,7 +251,7 @@ public class InsightBrainServiceTest
     TelemetryScheduler telemetryScheduler = getCLMServer().getInstance(TelemetryScheduler.class);
     responses.clear();
     telemetryScheduler.getTelemetryRunnable().run();
-    await().atMost(5, SECONDS).untilAsserted(() -> assertThat(responses).hasSize(8));
+    await().atMost(5, SECONDS).untilAsserted(() -> assertThat(responses).hasSize(9));
     Date expectedMaxCreateTime = new Date();
     Collection<TelemetryData> allTelemetryData =
         assertTelemetry(responses, expectedMinCreateTime, expectedMaxCreateTime);

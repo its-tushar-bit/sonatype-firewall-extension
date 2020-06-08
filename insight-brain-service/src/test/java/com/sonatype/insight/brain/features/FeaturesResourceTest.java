f19587ead8b13cf8cc8852056c6bb1a692e01eb8
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2020-06-08 13:34:22 +0200

    Renamed FeatureResourceTest and FeatureServiceTest to match naming convention for tests

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/features/FeaturesResourceTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/features/FeaturesResourceTest.java
@@ -13,7 +13,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class FeatureResourceTest
+public class FeaturesResourceTest
     extends AbstractResourceTest
 {
   @Override
--- insight-brain-service/src/test/java/com/sonatype/insight/brain/features/FeaturesServiceTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/features/FeaturesServiceTest.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class FeatureServiceTest
+public class FeaturesServiceTest
     extends AbstractComponentTest
 {
   @Inject

90f995800fe812cfe60c0106d7ece8bbec3e9863
rmealing <rmealing@sonatype.com>
2020-06-26 10:35:03 +0100

    Fixed checkstyle unused import warning

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/concurrent/SemaphorePoolTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/concurrent/SemaphorePoolTest.java
@@ -12,7 +12,6 @@ import java.util.concurrent.TimeUnit;
 import com.sonatype.insight.brain.git.VerifiableLoggingTestBase;
 
 import com.google.common.collect.ImmutableList;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

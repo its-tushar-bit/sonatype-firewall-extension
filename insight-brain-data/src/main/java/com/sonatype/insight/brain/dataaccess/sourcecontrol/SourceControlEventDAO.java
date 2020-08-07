dc2d976bee9558da1b1c4e3e082ca0a4094c7438
vladt <vladt@sonatype.com>
2020-08-07 12:28:17 -0400

    Use diamond operator in SourceControlEventDAO

--- insight-brain-data/src/main/java/com/sonatype/insight/brain/dataaccess/sourcecontrol/SourceControlEventDAO.java
+++ insight-brain-data/src/main/java/com/sonatype/insight/brain/dataaccess/sourcecontrol/SourceControlEventDAO.java
@@ -51,15 +51,14 @@ public class SourceControlEventDAO
   @VisibleForTesting
   List<SourceControlEvent> getAvailableEvents() {
     String sQuery = SELECT_ENTITY + "WHERE entity.instanceId IS NULL";
-    Query<SourceControlEvent> query = new Query<SourceControlEvent>(sQuery);
+    Query<SourceControlEvent> query = new Query<>(sQuery);
     return query.getList();
   }
 
   public List<SourceControlEvent> selectEventsForInstance(final String instanceId, final int quantity) {
     String sQuery = SELECT_ENTITY +
         "WHERE entity.instanceId = ?1 AND entity.eventStatus = ?2 ORDER BY entity.createTime";
-    Query<SourceControlEvent> query =
-        new Query<SourceControlEvent>(sQuery, instanceId, SourceControlEvent.EVENT_STATUS_NEW);
+    Query<SourceControlEvent> query = new Query<>(sQuery, instanceId, SourceControlEvent.EVENT_STATUS_NEW);
     query.setMaxResults(quantity);
     return query.getList();
   }

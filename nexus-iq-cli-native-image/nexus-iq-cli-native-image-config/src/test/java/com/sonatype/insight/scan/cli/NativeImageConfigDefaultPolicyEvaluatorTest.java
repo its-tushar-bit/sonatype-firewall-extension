4847b41a0ab717e20d2ec9fe6d4838594f030b4c
Collin Peters <cpeters@sonatype.com>
2020-10-09 10:20:15 -0700

    INT-3604 Add test for no parameters on the CLI (#5641)

--- nexus-iq-cli-native-image/nexus-iq-cli-native-image-config/src/test/java/com/sonatype/insight/scan/cli/NativeImageConfigDefaultPolicyEvaluatorTest.java
+++ nexus-iq-cli-native-image/nexus-iq-cli-native-image-config/src/test/java/com/sonatype/insight/scan/cli/NativeImageConfigDefaultPolicyEvaluatorTest.java
@@ -7,6 +7,9 @@ package com.sonatype.insight.scan.cli;
 
 import java.util.List;
 
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
 /**
  * Native image config generation for the {@link DefaultPolicyEvaluatorTest}. This extends that class and will execute
  * its tests except using the {@link NativeImageConfigGenerationTestRunner}.
@@ -20,4 +23,19 @@ public class NativeImageConfigDefaultPolicyEvaluatorTest
   protected AbstractPolicyEvaluatorTestRunner withTestRunner(final List<String> params) {
     return new NativeImageConfigGenerationTestRunner(params, environmentVariables.get(), logOutput);
   }
+
+  /**
+   * This test can only run on native image as the junit tests actually start at the {@link DefaultPolicyEvaluator} and
+   * not the calling {@link PolicyEvaluatorCli} class
+   */
+  @Test
+  public void testRun_NoParameters() throws Exception {
+    List<String> params = ImmutableList.of();
+    withTestRunner(params)
+        .expectFailExit()
+        .expectErrorLog("Usage: java -jar nexus-iq-cli.jar [options] Archives or directories to scan")
+        .expectErrorLog("The following options are required: [-s | --server-url], [-i | --application-id]")
+        .doPolicyEvaluationRun();
+  }
+
 }

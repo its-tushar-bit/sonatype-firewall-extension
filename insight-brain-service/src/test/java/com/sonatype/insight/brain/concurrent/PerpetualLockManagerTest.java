a70546b04e54ded3cd090346a4bc8f89eba4a1ab
Vlad Tatavu <vladt@sonatype.com>
2020-10-15 09:31:25 -0400

    Don't mock DAOs if not necessary in PerpetualLockManagerTest INT-3619 (#5668)

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/concurrent/PerpetualLockManagerTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/concurrent/PerpetualLockManagerTest.java
@@ -7,53 +7,46 @@ package com.sonatype.insight.brain.concurrent;
 
 import java.util.Date;
 
+import javax.inject.Inject;
 import javax.persistence.EntityExistsException;
 import javax.persistence.RollbackException;
 
 import com.sonatype.insight.brain.dataaccess.PerpetualLockDAO;
-import com.sonatype.insight.brain.git.VerifiableLoggingTestBase;
 import com.sonatype.insight.brain.model.PerpetualLock;
-import com.sonatype.insight.dataaccess.TransactionContext;
+import com.sonatype.insight.brain.service.AbstractComponentTest;
+import com.sonatype.insight.test.LogOutput;
 
+import ch.qos.logback.classic.Level;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import static java.lang.System.currentTimeMillis;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.spy;
 
 public class PerpetualLockManagerTest
-    extends VerifiableLoggingTestBase
+    extends AbstractComponentTest
 {
-  @Mock
-  private PerpetualLockDAO mockPerpetualLockDAO;
+  @Rule
+  public LogOutput logOutput = new LogOutput(PerpetualLockManager.class);
 
-  @Mock
-  private TransactionContext mockTxn;
+  @Inject
+  private PerpetualLockDAO perpetualLockDAO;
 
   // subject
   private PerpetualLockManager perpetualLockManager;
 
-  public PerpetualLockManagerTest() {
-    super(PerpetualLockManager.class);
-  }
-
   @Before
-  @Override
-  public void setup() {
-    MockitoAnnotations.openMocks(this);
-    super.setup();
-    perpetualLockManager = new PerpetualLockManager(mockPerpetualLockDAO);
-    doReturn(mockTxn).when(mockPerpetualLockDAO).createTransactionContext();
+  public void before() {
+    logOutput.setLogLevel(Level.TRACE);
+
+    perpetualLockManager = new PerpetualLockManager(perpetualLockDAO);
   }
 
   @Test
@@ -99,45 +92,39 @@ public class PerpetualLockManagerTest
     // given: DAO setup with existing lock and reserve success
     final String lockId = "test-lock-successful";
     final long reserveTime = 30;
-    doReturn(new PerpetualLock()).when(mockPerpetualLockDAO).getPerpetualLockByIdForUpdate(eq(mockTxn), eq(lockId));
-    doReturn(1).when(mockPerpetualLockDAO)
-        .reservePerpetualLock(eq(mockTxn), eq(lockId), eq("test-owner"), any(Date.class));
 
     // then: lock acquired
     assertThat(perpetualLockManager.tryAcquireLock(lockId, "test-owner", reserveTime)).isTrue();
 
     // and: expiration date in the future as expected
-    ArgumentCaptor<Date> dateCaptor = ArgumentCaptor.forClass(Date.class);
-    verify(mockPerpetualLockDAO).reservePerpetualLock(eq(mockTxn), eq(lockId), eq("test-owner"), dateCaptor.capture());
-    assertThat(dateCaptor.getValue()).isAfter(new Date(currentTimeMillis() - 1_000 * (reserveTime - 2)));
-    assertThat(dateCaptor.getValue()).isBefore(new Date(currentTimeMillis() + 1_000 * (reserveTime + 2)));
+    PerpetualLock lock = perpetualLockDAO.getPerpetualLockById(lockId);
+    assertThat(lock.getExpirationTime()).isAfter(new Date(currentTimeMillis() - 1_000 * (reserveTime - 2)));
+    assertThat(lock.getExpirationTime()).isBefore(new Date(currentTimeMillis() + 1_000 * (reserveTime + 2)));
   }
 
   @Test
   public void testTryAcquirePerpetualLock_existsAndReserveUnsuccessful() {
     // given: DAO setup with existing lock and reserve unsuccessful
     final String lockId = "test-lock-unsuccessful";
-    doReturn(new PerpetualLock()).when(mockPerpetualLockDAO).getPerpetualLockByIdForUpdate(eq(mockTxn), eq(lockId));
-    doReturn(0).when(mockPerpetualLockDAO)
-        .reservePerpetualLock(eq(mockTxn), eq(lockId), eq("test-owner"), any(Date.class));
+    assertThat(perpetualLockManager.tryAcquireLock(lockId, "test-owner", 30)).isTrue();
 
     // then:
-    assertThat(perpetualLockManager.tryAcquireLock(lockId, "test-owner", 30)).isFalse();
+    assertThat(perpetualLockManager.tryAcquireLock(lockId, "test-owner1", 30)).isFalse();
   }
 
   @Test
   public void testTryAcquirePerpetualLock_doesntExistYetAndCreateSuccessful() {
     // given: DAO setup with no existing lock and create successful
     final String lockId = "test-lock-not-exists";
-    doReturn(null).when(mockPerpetualLockDAO).getPerpetualLockByIdForUpdate(eq(mockTxn), eq(lockId));
 
     // then:
     assertThat(perpetualLockManager.tryAcquireLock(lockId, "test-owner", 30)).isTrue();
-    assertThatLogMessagesEqual(
-        trace("Trying to acquire perpetual lock test-lock-not-exists on behalf of test-owner to expire in 30 seconds."),
-        trace("Perpetual lock test-lock-not-exists does not exist yet.  Creating..."),
-        trace("Perpetual lock test-lock-not-exists created and acquired on behalf of test-owner.")
-    );
+    assertThat(logOutput).atTraceLevel().contains(
+        "Trying to acquire perpetual lock test-lock-not-exists on behalf of test-owner to expire in 30 seconds.");
+    assertThat(logOutput).atTraceLevel()
+        .contains("Perpetual lock test-lock-not-exists does not exist yet.  Creating...");
+    assertThat(logOutput).atTraceLevel()
+        .contains("Perpetual lock test-lock-not-exists created and acquired on behalf of test-owner.");
   }
 
   @Test
@@ -146,18 +133,16 @@ public class PerpetualLockManagerTest
     final String lockId = "test-lock-simultaneous";
     final String owner = "test-owner";
     final long expiration = 30;
-    doReturn(null).when(mockPerpetualLockDAO).getPerpetualLockByIdForUpdate(eq(mockTxn), eq(lockId));
-    doThrow(new RollbackException(new EntityExistsException())).when(mockPerpetualLockDAO)
+    PerpetualLockDAO spyPerpetualLockDAO = spy(perpetualLockDAO);
+    perpetualLockManager = new PerpetualLockManager(spyPerpetualLockDAO);
+    doThrow(new RollbackException(new EntityExistsException())).when(spyPerpetualLockDAO)
         .createPerpetualLock(eq(lockId), eq(owner), any(Date.class));
-    doReturn(1).when(mockPerpetualLockDAO)
-        .reservePerpetualLock(eq(lockId), eq(owner), any(Date.class));
+    doReturn(1).when(spyPerpetualLockDAO).reservePerpetualLock(eq(lockId), eq(owner), any(Date.class));
 
     // then:
     assertThat(perpetualLockManager.tryAcquireLock(lockId, owner, expiration)).isTrue();
-    assertThatLogMessagesContain(
-        trace("Perpetual lock test-lock-simultaneous already exists.  " +
-            "Will try to reserve it now on behalf of test-owner.")
-    );
+    assertThat(logOutput).atTraceLevel().contains("Perpetual lock test-lock-simultaneous already exists.  " +
+        "Will try to reserve it now on behalf of test-owner.");
   }
 
   @Test
@@ -166,26 +151,29 @@ public class PerpetualLockManagerTest
     final String lockId = "test-lock-simultaneous-reserve-fail";
     final String owner = "test-owner";
     final long expiration = 30;
-    doReturn(null).when(mockPerpetualLockDAO).getPerpetualLockByIdForUpdate(eq(mockTxn), eq(lockId));
-    doThrow(new RollbackException(new EntityExistsException())).when(mockPerpetualLockDAO)
+    PerpetualLockDAO spyPerpetualLockDAO = spy(perpetualLockDAO);
+    perpetualLockManager = new PerpetualLockManager(spyPerpetualLockDAO);
+    doThrow(new RollbackException(new EntityExistsException())).when(spyPerpetualLockDAO)
         .createPerpetualLock(eq(lockId), eq(owner), any(Date.class));
-    doReturn(0).when(mockPerpetualLockDAO)
-        .reservePerpetualLock(eq(lockId), eq(owner), any(Date.class));
 
     // then:
     assertThat(perpetualLockManager.tryAcquireLock(lockId, owner, expiration)).isFalse();
-    assertThatLogMessagesContain(
-        trace("Perpetual lock test-lock-simultaneous-reserve-fail already exists.  " +
-                "Will try to reserve it now on behalf of test-owner.")
-    );
+    assertThat(logOutput).atTraceLevel().contains("Perpetual lock test-lock-simultaneous-reserve-fail already exists.  "
+        + "Will try to reserve it now on behalf of test-owner.");
   }
 
   @Test
   public void testReleasePerpetualLock() {
+    String lockId = "test-lock-release";
+    String owner = "test-owner";
+    assertThat(perpetualLockManager.tryAcquireLock(lockId, owner, 30)).isTrue();
+
     // when:
-    perpetualLockManager.releasePerpetualLock("test-lock", "test-owner");
+    perpetualLockManager.releasePerpetualLock(lockId, owner);
 
     // then:
-    verify(mockPerpetualLockDAO, times(1)).releasePerpetualLockForOwner(eq("test-lock"), eq("test-owner"));
+    PerpetualLock lock = perpetualLockDAO.getPerpetualLockById(lockId);
+    assertThat(lock.getOwner()).isNull();
+    assertThat(lock.getExpirationTime()).isNull();
   }
 }

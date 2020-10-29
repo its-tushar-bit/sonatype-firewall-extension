952a41c2c3d1c0b878932b1a637c114b04727e5f
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2020-10-29 16:52:09 +0100

    Added patched version of CpuProfileServlet to restrict the frequency parameter to a valid range, thereby avoiding an endless loop if it was negative CLM-16983 (PR #5728)

---
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/service/AdminServletTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.service;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AdminServletTest
+    extends AbstractBrainServiceTest
+{
+  @Test(timeout = 60_000)
+  public void testCpuProfiling_NoEndlessBusyLoopOnNegativeFrequency_CLM_16983() throws Exception {
+    assertThat(adminRequest().path("pprof").query("frequency", -1).query("duration", "1").get()).isNotNull();
+  }
+}

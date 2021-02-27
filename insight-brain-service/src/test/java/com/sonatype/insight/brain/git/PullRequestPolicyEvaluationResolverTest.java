f54508413833cb1be754145083f3e21c040fd485
Russ Jackson <rjackson@sonatype.com>
2021-02-26 19:20:01 -0600

    INT-4313 checkstyle fix

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/git/PullRequestPolicyEvaluationResolverTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/git/PullRequestPolicyEvaluationResolverTest.java
@@ -23,7 +23,6 @@ import org.mockito.MockitoAnnotations;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;

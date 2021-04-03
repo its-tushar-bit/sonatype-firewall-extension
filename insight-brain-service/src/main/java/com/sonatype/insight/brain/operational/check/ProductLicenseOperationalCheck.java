a00587802ba8ea1eb070cebdd10e57852d468bfb
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2021-04-03 12:24:44 +0200

    CLM-18137 Included status of product license in operation health check results

---
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/operational/check/ProductLicenseOperationalCheck.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.operational.check;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import com.sonatype.insight.brain.product.license.ProductLicense;
+
+/**
+ * @since 1.109
+ */
+@Named
+@Singleton
+public class ProductLicenseOperationalCheck
+    extends AbstractOperationalCheck
+{
+  private final ProductLicense productLicense;
+
+  @Inject
+  public ProductLicenseOperationalCheck(ProductLicense productLicense) {
+    super("product-license");
+    this.productLicense = productLicense;
+  }
+
+  @Override
+  protected Result check() throws Exception {
+    ResultBuilder resultBuilder = Result.builder();
+    if (!productLicense.isValid()) {
+      resultBuilder.unhealthy();
+    }
+    // calculated to match similar field displayed in UI
+    int remainingDays = Math.max(0,
+        (int) ((productLicense.getExpirationTimestamp() - System.currentTimeMillis()) / 1000 / 60 / 60 / 24));
+    resultBuilder.withDetail("remainingDays", remainingDays);
+    return resultBuilder.build();
+  }
+}
---
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/operational/check/ProductLicenseOperationalCheckTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.operational.check;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import javax.inject.Inject;
+
+import com.sonatype.insight.brain.product.license.ProductLicense;
+import com.sonatype.insight.brain.service.AbstractComponentTest;
+
+import com.codahale.metrics.health.HealthCheck.Result;
+import com.google.inject.Binder;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+public class ProductLicenseOperationalCheckTest
+    extends AbstractComponentTest
+{
+  @Inject
+  private ProductLicenseOperationalCheck check;
+
+  @Mock
+  private ProductLicense productLicenseMock;
+
+  @Override
+  public void configure(Binder binder) {
+    binder.bind(ProductLicense.class).toInstance(productLicenseMock);
+    super.configure(binder);
+  }
+
+  @Test
+  public void testExecute_Healthy() {
+    doReturn(true).when(productLicenseMock).isValid();
+    doReturn(Instant.now().plus(Duration.ofHours(32)).toEpochMilli()).when(productLicenseMock).getExpirationTimestamp();
+
+    Result result = check.execute();
+    assertThat(result.isHealthy()).isTrue();
+    assertThat(result.getDetails()).containsEntry("remainingDays", 1);
+
+    doReturn(Instant.now().plus(Duration.ofHours(20)).toEpochMilli()).when(productLicenseMock).getExpirationTimestamp();
+
+    result = check.execute();
+    assertThat(result.isHealthy()).isTrue();
+    assertThat(result.getDetails()).containsEntry("remainingDays", 0);
+  }
+
+  @Test
+  public void testExecute_Unhealthy() {
+    doReturn(false).when(productLicenseMock).isValid();
+    doReturn(Instant.now().minus(Duration.ofHours(30)).toEpochMilli()).when(productLicenseMock)
+        .getExpirationTimestamp();
+
+    Result result = check.execute();
+    assertThat(result.isHealthy()).isFalse();
+    assertThat(result.getDetails()).containsEntry("remainingDays", 0);
+  }
+}

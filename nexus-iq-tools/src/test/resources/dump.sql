065184cb500711569828277ed581dad823d7e059
Andres Munoz <anfe0690@users.noreply.github.com>
2021-04-19 09:21:13 -0500

    PERF-229 - Delete Nexus IQ Tools (#6354)
    * Removed Nexus IQ Tools
    
    * Removed .gitignore lines

--- nexus-iq-tools/src/test/resources/dump.sql
+++
@@ -1,7 +0,0 @@
-CREATE SCHEMA insight_brain_ods;
-
-CREATE TABLE insight_brain_ods.test_table (
-    test_column timestamp (128) NOT NULL
-);
-
-INSERT INTO insight_brain_ods.test_table (test_column) VALUES ('2020-01-01');

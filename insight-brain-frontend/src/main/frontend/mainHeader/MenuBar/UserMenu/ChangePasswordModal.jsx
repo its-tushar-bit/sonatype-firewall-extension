519956dd7a7657d04670ccaf8778a7eee29a071f
Adam L Barrett <adamlbarrett@gmail.com>
2021-05-05 11:01:36 -0600

    CLM-18394 fix change password validation behaviour (#6445)

--- insight-brain-frontend/src/main/frontend/mainHeader/MenuBar/UserMenu/ChangePasswordModal.jsx
+++ insight-brain-frontend/src/main/frontend/mainHeader/MenuBar/UserMenu/ChangePasswordModal.jsx
@@ -5,7 +5,6 @@
  */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import {
   NxModal,
   NxForm,
@@ -13,83 +12,36 @@ import {
   NxTextInput,
   NxErrorAlert,
   combineValidationErrors,
+  hasValidationErrors,
 } from '@sonatype/react-shared-components';
 import { initialState, userInput } from '@sonatype/react-shared-components/components/NxTextInput/stateHelpers';
-
+import { validateNonEmpty } from '../../../util/validationUtil';
 import useEscapeKeyStack from '../../../react/useEscapeKeyStack';
 
-const setTextInput = (setter, validator) => (value) => {
-  setter(userInput(validator, value));
-};
-
-const useFormState = (onChangePassword) => {
-  const [originalPasswordState, setOriginalPasswordState] = useState(initialState(''));
-  let [newPasswordState, setNewPasswordState] = useState(initialState(''));
-  let [confirmPasswordState, setConfirmPasswordState] = useState(initialState(''));
-
-  const handleSubmit = () => {
-    onChangePassword({
-      oldPassword: originalPasswordState.value,
-      newPassword: newPasswordState.value,
-    });
-  };
-
-  const matches = (valueToMatch) => (value) =>
-    !valueToMatch || value === valueToMatch ? null : 'New Password and Confirmation must match';
-  const matchesConfirmation = matches(confirmPasswordState.value);
-  const matchesNewPassword = matches(newPasswordState.value);
-
-  const passwordMisatchMessage =
-    matchesConfirmation(newPasswordState.value) || matchesConfirmation(confirmPasswordState.value);
-
-  const allFieldsHaveValues =
-    originalPasswordState.value && newPasswordState.value && confirmPasswordState.value
-      ? null
-      : 'Required Fields Missing';
-
-  const validationErrors = combineValidationErrors(allFieldsHaveValues, passwordMisatchMessage);
-
-  // hack needed to keep validation in sync between confirmation and current password
-  if (!passwordMisatchMessage) {
-    newPasswordState = { ...newPasswordState, validationErrors: null };
-    confirmPasswordState = { ...confirmPasswordState, validationErrors: null };
-  }
-
-  return {
-    originalPasswordState,
-    setOriginalPasswordState,
-    newPasswordState,
-    setNewPasswordState,
-    confirmPasswordState,
-    setConfirmPasswordState,
-    matchesConfirmation,
-    matchesNewPassword,
-    handleSubmit,
-    validationErrors,
-  };
-};
-
-const submitMaskMap = {
-  pending: false,
-  success: true,
-  failure: null,
-  idle: null,
-};
+export const MISSING_FIELDS_ERROR_MESSAGE = 'Required Fields Missing';
+export const CONFIRMATION_MISMATCH_ERROR_MESSAGE = 'New Password and Confirmation must match';
 
 export const ChangePasswordModal = ({ onClose, onChangePassword, changePasswordError, changePasswordStatus }) => {
-  const {
-    originalPasswordState,
-    setOriginalPasswordState,
-    newPasswordState,
-    setNewPasswordState,
-    confirmPasswordState,
-    setConfirmPasswordState,
-    matchesConfirmation,
-    matchesNewPassword,
-    handleSubmit,
-    validationErrors,
-  } = useFormState(onChangePassword);
+  const [password, setPassword] = useState(initialState(''));
+  const [newPassword, setNewPassword] = useState(initialState(''));
+  const [confirmPassword, setConfirmPassword] = useState(initialState(''));
+
+  const requiredFieldsError =
+    password.value && newPassword.value && confirmPassword.value ? null : MISSING_FIELDS_ERROR_MESSAGE;
+  const mismatchError = newPassword.value === confirmPassword.value ? null : CONFIRMATION_MISMATCH_ERROR_MESSAGE;
+
+  const validationErrors = combineValidationErrors(requiredFieldsError, mismatchError);
+
   useEscapeKeyStack(true, onClose);
+
+  const handleSubmit = () => {
+    if (!hasValidationErrors(validationErrors)) {
+      onChangePassword({
+        oldPassword: password.value,
+        newPassword: newPassword.value,
+      });
+    }
+  };
 
   return (
     <NxModal id="change-password-modal" onClose={onClose}>
@@ -109,32 +61,34 @@ export const ChangePasswordModal = ({ onClose, onChangePassword, changePasswordE
         <div className="nx-modal-content">
           <NxFormGroup label="Current Password" isRequired>
             <NxTextInput
-              {...originalPasswordState}
+              {...password}
               id="original-password"
               type="password"
-              name="original-password"
-              onChange={setTextInput(setOriginalPasswordState)}
+              name="password"
+              validatable
+              onChange={(value) => setPassword(userInput(validateNonEmpty, value))}
             />
           </NxFormGroup>
           <hr />
           <NxFormGroup label="New Password" isRequired>
             <NxTextInput
-              {...newPasswordState}
+              {...newPassword}
               id="new-password"
               type="password"
               name="new-password"
-              onChange={setTextInput(setNewPasswordState, matchesConfirmation)}
               validatable
+              onChange={(value) => setNewPassword(userInput(validateNonEmpty, value))}
             />
           </NxFormGroup>
           <NxFormGroup label="Confirm New Password" isRequired>
             <NxTextInput
-              {...confirmPasswordState}
+              {...confirmPassword}
               id="confirm-password"
               type="password"
               name="confirm-password"
-              onChange={setTextInput(setConfirmPasswordState, matchesNewPassword)}
               validatable
+              validationErrors={validateNonEmpty(confirmPassword.value) || mismatchError}
+              onChange={(value) => setConfirmPassword(userInput(null, value))}
             />
           </NxFormGroup>
           {changePasswordError && <NxErrorAlert id="change-password-error">{changePasswordError}</NxErrorAlert>}
@@ -150,4 +104,12 @@ ChangePasswordModal.propTypes = {
   changePasswordError: PropTypes.string,
   changePasswordStatus: PropTypes.oneOf(['idle', 'pending', 'success', 'failure']),
 };
+
+const submitMaskMap = {
+  pending: false,
+  success: true,
+  failure: null,
+  idle: null,
+};
+
 export default ChangePasswordModal;
--- insight-brain-frontend/src/test/frontend/mainHeader/MenuBar/UserMenu/ChangePasswordModalSpec.jsx
+++ insight-brain-frontend/src/test/frontend/mainHeader/MenuBar/UserMenu/ChangePasswordModalSpec.jsx
@@ -4,12 +4,26 @@
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
 import * as enzymeUtils from '../../../enzymeUtils';
-import ChangePasswordModal from '../../../../../main/frontend/mainHeader/MenuBar/UserMenu/ChangePasswordModal';
-import { NxForm } from '@sonatype/react-shared-components';
+import {
+  ChangePasswordModal,
+  CONFIRMATION_MISMATCH_ERROR_MESSAGE,
+  MISSING_FIELDS_ERROR_MESSAGE,
+} from '../../../../../main/frontend/mainHeader/MenuBar/UserMenu/ChangePasswordModal';
+import { NxForm, NxTextInput } from '@sonatype/react-shared-components';
 
 describe('ChangePasswordModal', function () {
   let minProps;
-  let getMountedComponent;
+  let getShallowComponent;
+
+  function findInputById(component, id) {
+    return component.find(NxTextInput).filterWhere((component) => component.prop('id') === id);
+  }
+
+  function typeInInput(component, idOrInput, value) {
+    const input = typeof idOrInput === 'string' ? findInputById(component, idOrInput) : idOrInput;
+    input.prop('onChange')(value);
+    component.update();
+  }
 
   beforeEach(() => {
     minProps = {
@@ -18,19 +32,109 @@ describe('ChangePasswordModal', function () {
       changePasswordError: undefined,
       changePasswordStatus: 'idle',
     };
-    getMountedComponent = enzymeUtils.getMountedComponent(ChangePasswordModal, minProps);
+    getShallowComponent = enzymeUtils.getShallowComponent(ChangePasswordModal, minProps);
   });
 
-  it('calls onChangePassword when for is filled and submitted', () => {
+  it('calls onChangePassword when for is filled out and submitted', () => {
     const onChangePassword = jasmine.createSpy('onChangePassword');
-    const component = getMountedComponent({ onChangePassword });
-    component.find('input#original-password').simulate('change', { target: { value: 'myOldPassword' } });
-    component.find('input#new-password').simulate('change', { target: { value: 'myNewPassword' } });
-    component.find('input#confirm-password').simulate('change', { target: { value: 'myNewPassword' } });
-    component.update();
+    const component = getShallowComponent({ onChangePassword });
+    typeInInput(component, 'original-password', 'myOldPassword');
+    typeInInput(component, 'new-password', 'myNewPassword');
+    typeInInput(component, 'confirm-password', 'myNewPassword');
 
     component.find(NxForm).invoke('onSubmit')();
-    expect(onChangePassword).toHaveBeenCalled();
-    component.unmount();
+    expect(onChangePassword).toHaveBeenCalledWith({
+      oldPassword: 'myOldPassword',
+      newPassword: 'myNewPassword',
+    });
+  });
+
+  // So when your form is pristine, and you type into "New Password", there should be no error message.
+  // But when you start typing into "Confirm New Password", the "must match" error should appear only for "Confirm New Password" input.
+  it('shows a validation error message, only under confirm-password, when confirm does not match the new-password', () => {
+    const component = getShallowComponent();
+
+    typeInInput(component, 'original-password', 'myOldPassword');
+    const newPassword = findInputById(component, 'new-password');
+    const confirmPassword = findInputById(component, 'confirm-password');
+
+    typeInInput(component, newPassword, 'imatch');
+    typeInInput(component, confirmPassword, 'idontmatch');
+
+    expect(findInputById(component, 'new-password').prop('validationErrors')).toEqual(null);
+    expect(findInputById(component, 'confirm-password').prop('validationErrors')).toEqual(
+      CONFIRMATION_MISMATCH_ERROR_MESSAGE
+    );
+  });
+
+  it('shows validation ui as soon as the new password and confirm password match', () => {
+    const component = getShallowComponent();
+
+    typeInInput(component, 'new-password', 'match');
+    typeInInput(component, 'confirm-password', 'matc');
+
+    expect(findInputById(component, 'confirm-password').prop('validationErrors')).toEqual(
+      CONFIRMATION_MISMATCH_ERROR_MESSAGE
+    );
+    typeInInput(component, 'confirm-password', 'match');
+
+    expect(findInputById(component, 'new-password').prop('validationErrors')).toEqual(null);
+    expect(findInputById(component, 'confirm-password').prop('validationErrors')).toEqual(null);
+  });
+
+  it('shows validation errors when the new password and confirm go from match to unmatched again', () => {
+    const component = getShallowComponent();
+
+    typeInInput(component, 'new-password', 'match');
+    typeInInput(component, 'confirm-password', 'match');
+
+    expect(findInputById(component, 'new-password').prop('validationErrors')).toEqual(null);
+    expect(findInputById(component, 'confirm-password').prop('validationErrors')).toEqual(null);
+
+    typeInInput(component, 'new-password', 'nomatch');
+
+    expect(findInputById(component, 'new-password').prop('validationErrors')).toEqual(null);
+    expect(findInputById(component, 'confirm-password').prop('validationErrors')).toEqual(
+      CONFIRMATION_MISMATCH_ERROR_MESSAGE
+    );
+  });
+
+  it('does not allow the form to be submitted if there are any fields empty', () => {
+    const component = getShallowComponent();
+    expect(component.find(NxForm).prop('validationErrors')).toEqual([MISSING_FIELDS_ERROR_MESSAGE]);
+
+    typeInInput(component, 'original-password', 'mypassword');
+    typeInInput(component, 'new-password', 'match');
+    typeInInput(component, 'confirm-password', 'match');
+
+    expect(component.find(NxForm).prop('validationErrors')).toEqual([]);
+
+    typeInInput(component, 'original-password', '');
+    expect(component.find(NxForm).prop('validationErrors')).toEqual([MISSING_FIELDS_ERROR_MESSAGE]);
+  });
+
+  it('does not allow the form to be submitted if the new password and confirmation do not match (while pris', () => {
+    const component = getShallowComponent();
+    expect(component.find(NxForm).prop('validationErrors')).toEqual([MISSING_FIELDS_ERROR_MESSAGE]);
+
+    typeInInput(component, 'original-password', 'mypassword');
+    typeInInput(component, 'new-password', 'match');
+    typeInInput(component, 'confirm-password', 'no-match');
+
+    expect(component.find(NxForm).prop('validationErrors')[0]).toEqual(CONFIRMATION_MISMATCH_ERROR_MESSAGE);
+  });
+
+  it('displays a non-empty error message for current password if you enter something and then delete it', () => {
+    const component = getShallowComponent();
+
+    expect(findInputById(component, 'original-password').prop('isPristine')).toEqual(true);
+
+    typeInInput(component, 'original-password', 'originalpw');
+    expect(findInputById(component, 'original-password').prop('isPristine')).toEqual(false);
+    expect(findInputById(component, 'original-password').prop('validationErrors')).toEqual(null);
+
+    typeInInput(component, 'original-password', '');
+    component.update();
+    expect(findInputById(component, 'original-password').prop('validationErrors')).toEqual('Must be non-empty');
   });
 });

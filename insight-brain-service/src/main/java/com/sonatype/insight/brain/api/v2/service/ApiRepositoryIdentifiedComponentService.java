09f4822586dc489ded6664212f8613b76ecbd7ee
Richard Mealing <rmealing@sonatype.com>
2022-07-25 15:54:03 +0100

    Make deleteRepositoryIdentifiedComponent cluster friendly - CLM-21991 (#8241)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/service/ApiRepositoryIdentifiedComponentService.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/service/ApiRepositoryIdentifiedComponentService.java
@@ -6,6 +6,8 @@
 package com.sonatype.insight.brain.api.v2.service;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import javax.inject.Inject;
@@ -17,31 +19,51 @@ import com.sonatype.insight.brain.audit.AuditData;
 import com.sonatype.insight.brain.component.RepositoryIdentifiedComponentCache;
 import com.sonatype.insight.brain.dataaccess.component.RepositoryIdentifiedComponentDAO;
 import com.sonatype.insight.brain.model.security.Permission;
+import com.sonatype.insight.brain.scheduler.TaskScheduler;
 import com.sonatype.insight.brain.security.Authorize;
+import com.sonatype.insight.brain.security.MDCUsernameScope;
 import com.sonatype.insight.error.exception.BadRequestException;
-import com.sonatype.insight.error.exception.NotFoundException;
+import com.sonatype.insight.json.store.JsonUtils;
 import com.sonatype.insight.purl.PackageUrlIdentifier;
 
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Named
 @Singleton
 public class ApiRepositoryIdentifiedComponentService
+    implements Job
 {
   private static final Logger log = LoggerFactory.getLogger(ApiRepositoryIdentifiedComponentService.class);
+
+  // Visible for testing
+  static final String TASK_NAME = "DeleteRepositoryIdentifiedComponent";
+
+  // Visible for testing
+  static final String TASK_PARAM_HASH = "hash";
+
+  // Visible for testing
+  static final String TASK_PARAM_COMPONENT_IDENTIFIER = "componentIdentifier";
 
   private final RepositoryIdentifiedComponentDAO repositoryIdentifiedComponentDAO;
 
   private final RepositoryIdentifiedComponentCache repositoryIdentifiedComponentCache;
 
+  private final TaskScheduler taskScheduler;
+
   @Inject
   public ApiRepositoryIdentifiedComponentService(
       RepositoryIdentifiedComponentDAO repositoryIdentifiedComponentDAO,
-      RepositoryIdentifiedComponentCache repositoryIdentifiedComponentCache)
+      RepositoryIdentifiedComponentCache repositoryIdentifiedComponentCache,
+      TaskScheduler taskScheduler)
   {
     this.repositoryIdentifiedComponentDAO = repositoryIdentifiedComponentDAO;
     this.repositoryIdentifiedComponentCache = repositoryIdentifiedComponentCache;
+    this.taskScheduler = taskScheduler;
   }
 
   @Authorize(permission = Permission.CONFIGURE_SYSTEM)
@@ -57,17 +79,30 @@ public class ApiRepositoryIdentifiedComponentService
     else if (nonNulls > 1) {
       throw new BadRequestException("Only one of either hash or componentIdentifier or packageUrl must be specified.");
     }
+    AuditData.get().setData("hash", hash).setComponentIdentifier(componentIdentifier).setData("packageUrl", packageUrl);
+    if (packageUrl != null) {
+      componentIdentifier = new PackageUrlIdentifier(packageUrl).toComponentIdentifier();
+    }
+    deleteRepositoryIdentifiedComponent(hash, componentIdentifier);
+    deleteRepositoryIdentifiedComponentFromMemoryOnAllOtherClusterNodes(hash, componentIdentifier);
+  }
+
+  private void deleteRepositoryIdentifiedComponent(String hash, ComponentIdentifier componentIdentifier) {
     if (hash != null) {
-      AuditData.get().setData("hash", hash);
       deleteByHash(hash);
     }
-    else if (componentIdentifier != null) {
-      AuditData.get().setComponentIdentifier(componentIdentifier);
+    else {
       deleteByComponentIdentifier(componentIdentifier);
     }
+  }
+
+  // Visible for testing
+  void deleteRepositoryIdentifiedComponentFromMemory(String hash, ComponentIdentifier componentIdentifier) {
+    if (hash != null) {
+      deleteByHashFromMemory(hash);
+    }
     else {
-      AuditData.get().setData("packageUrl", packageUrl);
-      deleteByComponentIdentifier(new PackageUrlIdentifier(packageUrl).toComponentIdentifier());
+      deleteByComponentIdentifierFromMemory(componentIdentifier);
     }
   }
 
@@ -76,54 +111,73 @@ public class ApiRepositoryIdentifiedComponentService
   }
 
   private void deleteByHash(String hash) {
-    boolean deletedByHashFromDatabase = deleteByHashFromDatabase(hash);
-    boolean deletedByHashFromMemory = deleteByHashFromMemory(hash);
-    if (!deletedByHashFromDatabase && !deletedByHashFromMemory) {
-      throw new NotFoundException(String.format("Repository identified component with hash %s was not found.", hash));
-    }
+    deleteByHashFromDatabase(hash);
+    deleteByHashFromMemory(hash);
   }
 
-  private boolean deleteByHashFromDatabase(String hash) {
+  private void deleteByHashFromDatabase(String hash) {
     if (repositoryIdentifiedComponentDAO.deleteByHash(hash) > 0) {
       log.debug("Removed repository identified component with hash {} from the database.", hash);
-      return true;
     }
-    return false;
   }
 
-  private boolean deleteByHashFromMemory(String hash) {
+  private void deleteByHashFromMemory(String hash) {
     if (repositoryIdentifiedComponentCache.removeByHash(hash) != null) {
       log.debug("Removed repository identified component with hash {} from memory.", hash);
-      return true;
     }
-    return false;
   }
 
   private void deleteByComponentIdentifier(ComponentIdentifier componentIdentifier) {
-    boolean deletedByComponentIdentifierFromDatabase = deleteByComponentIdentifierFromDatabase(componentIdentifier);
-    boolean deletedByComponentIdentifierFromMemory = deleteByComponentIdentifierFromMemory(componentIdentifier);
-    if (!deletedByComponentIdentifierFromDatabase && !deletedByComponentIdentifierFromMemory) {
-      throw new NotFoundException(
-          String.format("Repository identified components with component identifier %s were not found.",
-              componentIdentifier));
-    }
+    deleteByComponentIdentifierFromDatabase(componentIdentifier);
+    deleteByComponentIdentifierFromMemory(componentIdentifier);
   }
 
-  private boolean deleteByComponentIdentifierFromDatabase(ComponentIdentifier componentIdentifier) {
+  private void deleteByComponentIdentifierFromDatabase(ComponentIdentifier componentIdentifier) {
     if (repositoryIdentifiedComponentDAO.deleteByComponentIdentifier(componentIdentifier) > 0) {
       log.debug("Removed repository identified components with component identifier {} from the database.",
           componentIdentifier);
-      return true;
     }
-    return false;
   }
 
-  private boolean deleteByComponentIdentifierFromMemory(ComponentIdentifier componentIdentifier) {
+  private void deleteByComponentIdentifierFromMemory(ComponentIdentifier componentIdentifier) {
     if (repositoryIdentifiedComponentCache.removeByComponentIdentifier(componentIdentifier) > 0) {
       log.debug("Removed repository identified components with component identifier {} from memory.",
           componentIdentifier);
-      return true;
     }
-    return false;
+  }
+
+  public void deleteRepositoryIdentifiedComponentFromMemoryOnAllOtherClusterNodes(
+      String hash,
+      ComponentIdentifier componentIdentifier)
+  {
+    Map<String, String> parameters = new HashMap<>();
+    parameters.put(TASK_PARAM_HASH, hash);
+    parameters.put(TASK_PARAM_COMPONENT_IDENTIFIER,
+        componentIdentifier == null ? null : JsonUtils.writeUnformatted(componentIdentifier));
+    taskScheduler.scheduleOneTimeTaskForAllOtherNodes(getClass(), TASK_NAME, parameters);
+  }
+
+  @Override
+  public void execute(JobExecutionContext context) throws JobExecutionException {
+    try (MDCUsernameScope mdcUsernameScope = MDCUsernameScope.forSystem()) {
+      JobDataMap mergedJobDataMap = context.getMergedJobDataMap();
+      String hash = mergedJobDataMap.getString(TASK_PARAM_HASH);
+      String componentIdentifierJson = mergedJobDataMap.getString(TASK_PARAM_COMPONENT_IDENTIFIER);
+      ComponentIdentifier componentIdentifier = null;
+      if (componentIdentifierJson != null) {
+        componentIdentifier = JsonUtils.parse(componentIdentifierJson, ComponentIdentifier.class);
+      }
+      deleteRepositoryIdentifiedComponentFromMemory(hash, componentIdentifier);
+    }
+    catch (Exception e) {
+      log.error("Error when deleting repository identified component: {}", e.getMessage(), e);
+    }
+    catch (Throwable t) {
+      // Try to log to stderr before trying the standard logging because the standard logging may not be operational
+      // at this point.
+      t.printStackTrace();
+      log.error(t.getMessage(), t);
+      System.exit(1);
+    }
   }
 }
--- insight-brain-service/src/test/java/com/sonatype/insight/brain/api/v2/service/ApiRepositoryIdentifiedComponentServiceTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/api/v2/service/ApiRepositoryIdentifiedComponentServiceTest.java
@@ -5,23 +5,41 @@
  */
 package com.sonatype.insight.brain.api.v2.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import com.sonatype.clm.dto.model.component.ComponentIdentifier;
 import com.sonatype.insight.brain.component.RepositoryIdentifiedComponentCache;
 import com.sonatype.insight.brain.dataaccess.component.RepositoryIdentifiedComponentDAO;
 import com.sonatype.insight.brain.model.component.RepositoryIdentifiedComponent;
+import com.sonatype.insight.brain.scheduler.TaskScheduler;
+import com.sonatype.insight.brain.security.MDCUsernameScope;
 import com.sonatype.insight.brain.service.AbstractComponentTest;
 import com.sonatype.insight.error.exception.BadRequestException;
-import com.sonatype.insight.error.exception.NotFoundException;
+import com.sonatype.insight.json.store.JsonUtils;
 import com.sonatype.insight.purl.InvalidPackageURLException;
 import com.sonatype.insight.purl.PackageUrlIdentifier;
 
+import com.google.inject.Binder;
 import org.junit.After;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.quartz.JobBuilder;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.slf4j.MDC;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
 public class ApiRepositoryIdentifiedComponentServiceTest
     extends AbstractComponentTest
@@ -34,6 +52,15 @@ public class ApiRepositoryIdentifiedComponentServiceTest
 
   @Inject
   private RepositoryIdentifiedComponentCache repositoryIdentifiedComponentCache;
+
+  @Mock
+  private TaskScheduler mockTaskScheduler;
+
+  @Override
+  public void configure(Binder binder) {
+    binder.bind(TaskScheduler.class).toInstance(mockTaskScheduler);
+    super.configure(binder);
+  }
 
   @After
   public void after() {
@@ -69,13 +96,6 @@ public class ApiRepositoryIdentifiedComponentServiceTest
   }
 
   @Test
-  public void testDeleteRepositoryIdentifiedComponent_Hash_NotFound() {
-    assertThatExceptionOfType(NotFoundException.class).isThrownBy(
-        () -> repositoryIdentifiedComponentService.deleteRepositoryIdentifiedComponent("doesNotExist", null, null))
-        .withMessageContaining("Repository identified component with hash doesNotExist was not found.");
-  }
-
-  @Test
   public void testDeleteRepositoryIdentifiedComponent_Hash_OnlyInDatabase() {
     RepositoryIdentifiedComponent repositoryIdentifiedComponent1 = tempEntity.newRepositoryIdentifiedComponent();
     RepositoryIdentifiedComponent repositoryIdentifiedComponent2 = tempEntity.newRepositoryIdentifiedComponent();
@@ -85,6 +105,7 @@ public class ApiRepositoryIdentifiedComponentServiceTest
 
     assertThat(repositoryIdentifiedComponentDAO.getAll()).map(RepositoryIdentifiedComponent::getHash)
         .containsExactly(repositoryIdentifiedComponent2.getHash());
+    verifyScheduledTask(repositoryIdentifiedComponent1.getHash(), null);
   }
 
   @Test
@@ -97,6 +118,7 @@ public class ApiRepositoryIdentifiedComponentServiceTest
     repositoryIdentifiedComponentService.deleteRepositoryIdentifiedComponent("hash1", null, null);
 
     assertThat(repositoryIdentifiedComponentCache.getLoadingCache().asMap().keySet()).containsExactly("hash2");
+    verifyScheduledTask("hash1", null);
   }
 
   @Test
@@ -113,15 +135,7 @@ public class ApiRepositoryIdentifiedComponentServiceTest
         .containsExactly(repositoryIdentifiedComponent2.getHash());
     assertThat(repositoryIdentifiedComponentCache.getLoadingCache().asMap().keySet()).containsExactly(
         repositoryIdentifiedComponent2.getHash());
-  }
-
-  @Test
-  public void testDeleteRepositoryIdentifiedComponent_ComponentIdentifier_NotFound() {
-    assertThatExceptionOfType(NotFoundException.class).isThrownBy(
-        () -> repositoryIdentifiedComponentService.deleteRepositoryIdentifiedComponent(null,
-                ComponentIdentifier.createMavenCoordinates("g", "a", "v", "c", "e"), null))
-        .withMessageContaining("Repository identified components with component identifier " +
-            "maven: {artifactId=a, classifier=c, extension=e, groupId=g, version=v} were not found.");
+    verifyScheduledTask(repositoryIdentifiedComponent1.getHash(), null);
   }
 
   @Test
@@ -135,6 +149,7 @@ public class ApiRepositoryIdentifiedComponentServiceTest
 
     assertThat(repositoryIdentifiedComponentDAO.getAll()).map(RepositoryIdentifiedComponent::getComponentIdentifier)
         .containsExactly(repositoryIdentifiedComponent3.getComponentIdentifier());
+    verifyScheduledTask(null, repositoryIdentifiedComponent1.getComponentIdentifier());
   }
 
   @Test
@@ -149,6 +164,7 @@ public class ApiRepositoryIdentifiedComponentServiceTest
 
     assertThat(repositoryIdentifiedComponentCache.getLoadingCache().asMap().values()).containsExactly(
         componentIdentifier2);
+    verifyScheduledTask(null, componentIdentifier1);
   }
 
   @Test
@@ -168,15 +184,7 @@ public class ApiRepositoryIdentifiedComponentServiceTest
         .containsExactly(repositoryIdentifiedComponent3.getComponentIdentifier());
     assertThat(repositoryIdentifiedComponentCache.getLoadingCache().asMap().values()).containsExactly(
         repositoryIdentifiedComponent3.getComponentIdentifier());
-  }
-
-  @Test
-  public void testDeleteRepositoryIdentifiedComponent_Purl_NotFound() {
-    assertThatExceptionOfType(NotFoundException.class).isThrownBy(
-        () -> repositoryIdentifiedComponentService.deleteRepositoryIdentifiedComponent(null, null,
-                PackageUrlIdentifier.toPackageUrl(ComponentIdentifier.createMavenCoordinates("g", "a", "v", "c", "e"))))
-        .withMessageContaining("Repository identified components with component identifier " +
-            "maven: {artifactId=a, classifier=c, extension=e, groupId=g, version=v} were not found.");
+    verifyScheduledTask(null, repositoryIdentifiedComponent1.getComponentIdentifier());
   }
 
   @Test
@@ -196,6 +204,7 @@ public class ApiRepositoryIdentifiedComponentServiceTest
 
     assertThat(repositoryIdentifiedComponentDAO.getAll()).map(RepositoryIdentifiedComponent::getComponentIdentifier)
         .containsExactly(repositoryIdentifiedComponent3.getComponentIdentifier());
+    verifyScheduledTask(null, repositoryIdentifiedComponent1.getComponentIdentifier());
   }
 
   @Test
@@ -211,6 +220,7 @@ public class ApiRepositoryIdentifiedComponentServiceTest
 
     assertThat(repositoryIdentifiedComponentCache.getLoadingCache().asMap().values()).containsExactly(
         componentIdentifier2);
+    verifyScheduledTask(null, componentIdentifier1);
   }
 
   @Test
@@ -230,5 +240,77 @@ public class ApiRepositoryIdentifiedComponentServiceTest
         .containsExactly(repositoryIdentifiedComponent3.getComponentIdentifier());
     assertThat(repositoryIdentifiedComponentCache.getLoadingCache().asMap().values()).containsExactly(
         repositoryIdentifiedComponent3.getComponentIdentifier());
+    verifyScheduledTask(null, repositoryIdentifiedComponent1.getComponentIdentifier());
+  }
+
+  @Test
+  public void testDisallowConcurrentExecution() {
+    assertThat(JobBuilder.newJob(ApiRepositoryIdentifiedComponentService.class).build()
+        .isConcurrentExectionDisallowed()).isFalse();
+  }
+
+  @Test
+  public void testExecute_Hash() throws Exception {
+    repositoryIdentifiedComponentCache.getLoadingCache().asMap()
+        .put("hash1", ComponentIdentifier.createMavenCoordinates("g1", "a1", "v1", "c1", "e1"));
+    repositoryIdentifiedComponentCache.getLoadingCache().asMap()
+        .put("hash2", ComponentIdentifier.createMavenCoordinates("g2", "a2", "v2", "c2", "e2"));
+    ApiRepositoryIdentifiedComponentService spyRepositoryIdentifiedComponentService =
+        spy(repositoryIdentifiedComponentService);
+    doAnswer(invocationOnMock -> {
+      assertThat(MDC.get(MDCUsernameScope.USERNAME)).isEqualTo(MDCUsernameScope.SYSTEM);
+      return invocationOnMock.callRealMethod();
+    }).when(spyRepositoryIdentifiedComponentService).deleteRepositoryIdentifiedComponentFromMemory(any(), any());
+    JobExecutionContext mockJobExecutionContext = mock(JobExecutionContext.class);
+    JobDataMap jobDataMap = new JobDataMap();
+    when(mockJobExecutionContext.getMergedJobDataMap()).thenReturn(jobDataMap);
+    jobDataMap.put(ApiRepositoryIdentifiedComponentService.TASK_PARAM_HASH, "hash1");
+
+    try (MDCUsernameScope mdcUsernameScope = MDCUsernameScope.forUser("username")) {
+      spyRepositoryIdentifiedComponentService.execute(mockJobExecutionContext);
+    }
+
+    verify(spyRepositoryIdentifiedComponentService).deleteRepositoryIdentifiedComponentFromMemory("hash1", null);
+    assertThat(repositoryIdentifiedComponentCache.getLoadingCache().asMap().keySet()).containsExactly("hash2");
+    verifyNoInteractions(mockTaskScheduler);
+  }
+
+  @Test
+  public void testExecute_ComponentIdentifier() throws Exception {
+    ComponentIdentifier componentIdentifier1 = ComponentIdentifier.createMavenCoordinates("g1", "a1", "v1", "c1", "e1");
+    repositoryIdentifiedComponentCache.getLoadingCache().asMap()
+        .put("hash1", componentIdentifier1);
+    repositoryIdentifiedComponentCache.getLoadingCache().asMap()
+        .put("hash2", ComponentIdentifier.createMavenCoordinates("g2", "a2", "v2", "c2", "e2"));
+    ApiRepositoryIdentifiedComponentService spyRepositoryIdentifiedComponentService =
+        spy(repositoryIdentifiedComponentService);
+    doAnswer(invocationOnMock -> {
+      assertThat(MDC.get(MDCUsernameScope.USERNAME)).isEqualTo(MDCUsernameScope.SYSTEM);
+      return invocationOnMock.callRealMethod();
+    }).when(spyRepositoryIdentifiedComponentService).deleteRepositoryIdentifiedComponentFromMemory(any(), any());
+    JobExecutionContext mockJobExecutionContext = mock(JobExecutionContext.class);
+    JobDataMap jobDataMap = new JobDataMap();
+    when(mockJobExecutionContext.getMergedJobDataMap()).thenReturn(jobDataMap);
+    jobDataMap.put(ApiRepositoryIdentifiedComponentService.TASK_PARAM_COMPONENT_IDENTIFIER,
+        JsonUtils.writeUnformatted(componentIdentifier1));
+
+    try (MDCUsernameScope mdcUsernameScope = MDCUsernameScope.forUser("username")) {
+      spyRepositoryIdentifiedComponentService.execute(mockJobExecutionContext);
+    }
+
+    verify(spyRepositoryIdentifiedComponentService).deleteRepositoryIdentifiedComponentFromMemory(null,
+        componentIdentifier1);
+    assertThat(repositoryIdentifiedComponentCache.getLoadingCache().asMap().keySet()).containsExactly("hash2");
+    verifyNoInteractions(mockTaskScheduler);
+  }
+
+  private void verifyScheduledTask(String hash, ComponentIdentifier componentIdentifier) {
+    Map<String, String> expectedParameters = new HashMap<>();
+    expectedParameters.put(ApiRepositoryIdentifiedComponentService.TASK_PARAM_HASH, hash);
+    expectedParameters.put(ApiRepositoryIdentifiedComponentService.TASK_PARAM_COMPONENT_IDENTIFIER,
+        componentIdentifier == null ? null : JsonUtils.writeUnformatted(componentIdentifier));
+    verify(mockTaskScheduler).scheduleOneTimeTaskForAllOtherNodes(
+        ApiRepositoryIdentifiedComponentService.class, ApiRepositoryIdentifiedComponentService.TASK_NAME,
+        expectedParameters);
   }
 }

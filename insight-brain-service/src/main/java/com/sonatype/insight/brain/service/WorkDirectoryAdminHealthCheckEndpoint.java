66b557234c4a39ddc9f6546962db98b5c25ec7c5
Alexander Alzate <aalzate@sonatype.com>
2022-09-12 16:31:26 -0500

    Add new endpoint to check sonatype work directory healthcheck CLM-22501 (#8418)

---
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/service/WorkDirectoryAdminHealthCheckEndpoint.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.service;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+@Named
+@Singleton
+public class WorkDirectoryAdminHealthCheckEndpoint
+    extends DirectoryAdminHealthCheckEndpoint
+{
+  @Inject
+  public WorkDirectoryAdminHealthCheckEndpoint(final InsightConfig insightConfig) {
+    super("Sonatype Work Directory", "/healthcheck/workDirectory", insightConfig.getSonatypeWork());
+  }
+}
---
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/service/WorkDirectoryAdminHealthCheckEndpointTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+package com.sonatype.insight.brain.service;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WorkDirectoryAdminHealthCheckEndpointTest
+    extends AbstractComponentTest
+{
+  @Inject
+  private WorkDirectoryAdminHealthCheckEndpoint workDirectoryAdminHealthCheckEndpoint;
+
+  @Test
+  public void testGetName() {
+    assertThat(workDirectoryAdminHealthCheckEndpoint.getName()).isEqualTo("Sonatype Work Directory");
+  }
+
+  @Test
+  public void testGetPath() {
+    assertThat(workDirectoryAdminHealthCheckEndpoint.getPath()).isEqualTo("/healthcheck/workDirectory");
+  }
+}

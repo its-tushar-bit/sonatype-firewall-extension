5b06f635dbbf7a88a8392c8343fa327973ad3c69
Hao Chen <haoRchen@users.noreply.github.com>
2022-11-16 09:42:36 -0500

    CLM-23025 Fix flaky root org summary test caused by grandfathering tile (#8628)

--- insight-brain-frontend/src/main/frontend/OrgsAndPolicies/policyViolationGrandfatheringSelectors.js
+++ insight-brain-frontend/src/main/frontend/OrgsAndPolicies/policyViolationGrandfatheringSelectors.js
@@ -4,7 +4,7 @@
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
 import { createSelector } from '@reduxjs/toolkit';
-import { compose, ifElse, isNil, pick, prop, propOr } from 'ramda';
+import { pick, prop } from 'ramda';
 import { selectIsRootOrganization, selectRouterSlice } from '../reduxUiRouter/routerSelectors';
 import { selectOrgsAndPoliciesSlice } from './orgsAndPoliciesSelectors';
 import { deriveEditRoute } from 'MainRoot/OrgsAndPolicies/utility/util';
@@ -50,18 +50,17 @@ export const selectPolicyViolationGrandfatheringConfig = createSelector(
     // current settings for the owner and its parents. For enabled values that are being
     // inherited, we need to adjust accordingly and null out the enabled value for this
     // particular owner (since the value is not coming from this owner but a parent).
-    config.enabled = ifElse(
-      compose(isNil, prop('inheritedFromOrganizationName')),
-      prop('enabled'),
-      () => null
-    )(policyViolationGrandfathering);
+    config.enabled = policyViolationGrandfathering.inheritedFromOrganizationName
+      ? null
+      : policyViolationGrandfathering.enabled;
+
     config.calculatedEnabled = policyViolationGrandfathering.enabled;
 
     // For the root organization, values that have not yet been set in the backend are treated
     // as false (as there's nowhere else to inherit from), so nulls need to be set to false.
     if (isRootOrg) {
-      config.enabled = propOr(false, 'enabled')(config);
-      config.calculatedEnabled = propOr(false, 'calculatedEnabled')(config);
+      config.enabled = !!config.enabled;
+      config.calculatedEnabled = !!config.calculatedEnabled;
     }
     return config;
   }
--- insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/RootOrganizationPolicyViolationGrandfatheringEditorTest.java
+++ insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/RootOrganizationPolicyViolationGrandfatheringEditorTest.java
@@ -8,7 +8,10 @@ package com.sonatype.clm.testing.functional.brain;
 import com.sonatype.insight.brain.dataaccess.OrganizationDAO;
 import com.sonatype.insight.brain.model.Organization;
 
+import org.junit.After;
 import org.junit.Before;
+
+import static com.sonatype.insight.brain.model.Organization.ROOT_ORGANIZATION_ID;
 
 public class RootOrganizationPolicyViolationGrandfatheringEditorTest
     extends AbstractPolicyViolationGrandfatheringEditorTest
@@ -19,5 +22,16 @@ public class RootOrganizationPolicyViolationGrandfatheringEditorTest
   public void init() {
     organization = new OrganizationDAO().getById(Organization.ROOT_ORGANIZATION_ID);
     super.init(organization);
+  }
+
+  // Must manually reset grandfathering props since root organization is not
+  // part of the organization collection in TemporaryEntity.
+  @After
+  public void resetRootOrganizationPolicyViolationGrandfathering() {
+    OrganizationDAO orgDAO = new OrganizationDAO();
+    Organization rootOrg = orgDAO.getByIdNotNull(ROOT_ORGANIZATION_ID);
+    rootOrg.setPolicyViolationGrandfatheringEnabled(null);
+    rootOrg.setAllowPolicyViolationGrandfatheringOverride(true);
+    orgDAO.update(rootOrg);
   }
 }

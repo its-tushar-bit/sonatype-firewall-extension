4e2a1e4d77b8318e4635d6f6a7936688865de67a
Vlad Tatavu <vladt@sonatype.com>
2022-11-17 17:00:10 -0500

    Restore the root org grandfathering config after all grandfathering editor tests (#8682)

--- insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/RootOrganizationPolicyViolationGrandfatheringEditorTest.java
+++ insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/RootOrganizationPolicyViolationGrandfatheringEditorTest.java
@@ -8,33 +8,14 @@ package com.sonatype.clm.testing.functional.brain;
 import com.sonatype.insight.brain.dataaccess.OrganizationDAO;
 import com.sonatype.insight.brain.model.Organization;
 
-import org.junit.After;
 import org.junit.Before;
 
 public class RootOrganizationPolicyViolationGrandfatheringEditorTest
     extends AbstractPolicyViolationGrandfatheringEditorTest
 {
-  private Boolean grandfatheringEnabled;
-
-  private boolean grandfatheringOverrideEnabled;
-
   @Before
   public void init() {
     Organization rootOrg = new OrganizationDAO().getById(Organization.ROOT_ORGANIZATION_ID);
-
-    // Save the root org grandfathering settings so we can restore them after the tests.
-    grandfatheringEnabled = rootOrg.isPolicyViolationGrandfatheringEnabled();
-    grandfatheringOverrideEnabled = rootOrg.isAllowPolicyViolationGrandfatheringOverride();
-
     super.init(rootOrg);
-  }
-
-  @After
-  public void restoreRootOrganizationPolicyViolationGrandfatheringSettings() {
-    OrganizationDAO orgDAO = new OrganizationDAO();
-    Organization rootOrg = orgDAO.getById(Organization.ROOT_ORGANIZATION_ID);
-    rootOrg.setPolicyViolationGrandfatheringEnabled(grandfatheringEnabled);
-    rootOrg.setAllowPolicyViolationGrandfatheringOverride(grandfatheringOverrideEnabled);
-    orgDAO.update(rootOrg);
   }
 }

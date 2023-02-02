7472543a3e6af26721d71b800ae82dab9d060b3b
vladt <vladt@sonatype.com>
2023-02-02 15:04:32 -0500

    Removed unnecessary SAML configuration update from LoginTest

--- insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/LoginTest.java
+++ insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/LoginTest.java
@@ -16,7 +16,6 @@ import com.sonatype.clm.testing.functional.pages.OwnerSummaryPage;
 import com.sonatype.clm.testing.functional.pages.ReportListPage;
 import com.sonatype.clm.testing.functional.pages.VulnerabilitySearchPage;
 import com.sonatype.insight.brain.api.v2.ApiConfigFeaturesService.SystemConfigurationPropertyFeature;
-import com.sonatype.insight.brain.dataaccess.configuration.saml.SamlConfigurationDAO;
 import com.sonatype.insight.brain.model.configuration.saml.SamlConfiguration;
 import com.sonatype.insight.brain.security.SamlDeploymentManager;
 import com.sonatype.insight.brain.service.InsightConfig;
@@ -103,7 +102,6 @@ public class LoginTest
   @Test
   public void testInitialLoginFormState_SamlSso() {
     SamlConfiguration samlConfiguration = tempEntity.newSamlConfiguration();
-    new SamlConfigurationDAO().update(samlConfiguration);
     testCLMServer.getCLMServer().getInstance(SamlDeploymentManager.class).updateFromConfiguration();
 
     refreshOrOpen(ReportListPage.url());

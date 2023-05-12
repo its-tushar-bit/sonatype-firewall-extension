a501d4e2fb23fc005ea75952866b207bb47deb62
vladt <vladt@sonatype.com>
2023-05-12 16:56:29 -0400

    Replaced usages of staticTempEntity with tempEntity in DashboardTabNavigationTest

--- insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/DashboardTabNavigationTest.java
+++ insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/DashboardTabNavigationTest.java
@@ -35,15 +35,15 @@ import static com.sonatype.clm.testing.functional.pages.DashboardPage.ACTIVE;
 public class DashboardTabNavigationTest
     extends AbstractFunctionalTest
 {
-  private static Organization org;
+  private Organization org;
 
-  private static Application app;
+  private Application app;
 
-  private static Policy policy;
+  private Policy policy;
 
   private static final List<ComponentData> COMPONENTS = new ArrayList<>();
 
-  static final List<PolicyWaiver> existingWaivers = new ArrayList<>();
+  private final List<PolicyWaiver> existingWaivers = new ArrayList<>();
 
   static {
     for (int i = 0; i < 10; i++) {
@@ -55,36 +55,36 @@ public class DashboardTabNavigationTest
 
   @BeforeClass
   public static void beforeClass() {
-    setupData();
     refreshOrOpen(DashboardPage.urlToViolations());
     loginAsAdmin();
   }
 
   @Before
   public void before() {
+    setupData();
     clearFilters();
     refreshOrOpen(DashboardPage.urlToViolations());
   }
 
-  public static void setupData() {
-    org = staticTempEntity.newOrganization("DashboardPolicySummarySpec");
-    app = staticTempEntity
+  public void setupData() {
+    org = tempEntity.newOrganization("DashboardPolicySummarySpec");
+    app = tempEntity
         .newApplication("DashboardPolicySummarySpecApp", "DashboardPolicySummarySpecApp", org.getPublicId());
-    policy = staticTempEntity.newPolicy(org);
+    policy = tempEntity.newPolicy(org);
 
     DateTime now = DateTime.now();
     for (int weeksAgo = 12; weeksAgo >= 0; weeksAgo--) {
       DateTime time = now.minusWeeks(weeksAgo).minusDays(2);
       switch (weeksAgo) {
         case 12: // introduce 3 violations outside the bounds of the 12 week delta to start with
-          PolicyEvaluation seedEval = staticTempEntity
+          PolicyEvaluation seedEval = tempEntity
               .newPolicyEvaluation(app.getId(), BuildStageType.ID, "SeedEval", time.toDate());
           createViolations(seedEval, COMPONENTS.subList(0, 3));
           break;
         case 11:
           // introduce 2 new violations
           PolicyEvaluation twelthWeekEval =
-              staticTempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "twelthWeekEval", time.toDate());
+              tempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "twelthWeekEval", time.toDate());
           createViolations(twelthWeekEval, COMPONENTS.subList(0, 5));
           break;
         case 10:
@@ -92,12 +92,12 @@ public class DashboardTabNavigationTest
           break;
         case 8: // fix an issue and introduce 2 new ones
           PolicyEvaluation ninthWeekEval =
-              staticTempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "ninthWeekEval", time.toDate());
+              tempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "ninthWeekEval", time.toDate());
           createViolations(ninthWeekEval, COMPONENTS.subList(1, 7));
           break;
         case 7: // Waive 3 violations
           PolicyEvaluation eigthWeekEval =
-              staticTempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "eightWeekEval", time.toDate());
+              tempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "eightWeekEval", time.toDate());
           createViolations(eigthWeekEval, COMPONENTS.subList(4, 7));
           createWaivedViolations(eigthWeekEval, COMPONENTS.subList(1, 4));
           break;
@@ -105,25 +105,25 @@ public class DashboardTabNavigationTest
           break;
         case 5: // Fix one waived violation
           PolicyEvaluation fifthWeekEval =
-              staticTempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "fifthWeekEval", time.toDate());
+              tempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "fifthWeekEval", time.toDate());
           createViolations(fifthWeekEval, COMPONENTS.subList(4, 7));
           createWaivedViolations(fifthWeekEval, COMPONENTS.subList(2, 4));
           break;
         case 4: // find one, fix one
           PolicyEvaluation fourthWeekEval =
-              staticTempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "fourthWeekEval", time.toDate());
+              tempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "fourthWeekEval", time.toDate());
           createViolations(fourthWeekEval, COMPONENTS.subList(5, 8));
           createWaivedViolations(fourthWeekEval, COMPONENTS.subList(2, 4));
           break;
         case 3: // fix two, fix one waived violation
           PolicyEvaluation thirdWeekEval =
-              staticTempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "thirdWeekEval", time.toDate());
+              tempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "thirdWeekEval", time.toDate());
           createViolations(thirdWeekEval, COMPONENTS.subList(7, 8));
           createWaivedViolations(thirdWeekEval, COMPONENTS.subList(3, 4));
           break;
         case 2: // find one, fix one
           PolicyEvaluation secondWeekEval =
-              staticTempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "secondWeekEval", time.toDate());
+              tempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "secondWeekEval", time.toDate());
           createViolations(secondWeekEval, COMPONENTS.subList(8, 9));
           createWaivedViolations(secondWeekEval, COMPONENTS.subList(3, 4));
           break;
@@ -131,7 +131,7 @@ public class DashboardTabNavigationTest
           break;
         case 0: // find one, fix one
           PolicyEvaluation thisWeekEval =
-              staticTempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "thisWeekEval", time.toDate());
+              tempEntity.newPolicyEvaluation(app.getId(), BuildStageType.ID, "thisWeekEval", time.toDate());
           createViolations(thisWeekEval, COMPONENTS.subList(9, 10));
           createWaivedViolations(thisWeekEval, COMPONENTS.subList(3, 4));
           break;
@@ -140,25 +140,25 @@ public class DashboardTabNavigationTest
     }
   }
 
-  private static void createViolations(PolicyEvaluation evaluation, List<ComponentData> components) {
+  private void createViolations(PolicyEvaluation evaluation, List<ComponentData> components) {
     for (ComponentData component : components) {
-      staticTempEntity.newPolicyViolation(evaluation, policy, component.componentIdentifier, component.hash, "");
+      tempEntity.newPolicyViolation(evaluation, policy, component.componentIdentifier, component.hash, "");
     }
   }
 
-  private static void createWaivedViolations(PolicyEvaluation evaluation, List<ComponentData> components) {
+  private void createWaivedViolations(PolicyEvaluation evaluation, List<ComponentData> components) {
     for (ComponentData component : components) {
       PolicyWaiver waiver = findExistingWaiver(component.hash);
       if (waiver == null) {
-        waiver = staticTempEntity.newWaiver(component.hash, policy.getId(), app.getId());
+        waiver = tempEntity.newWaiver(component.hash, policy.getId(), app.getId());
         existingWaivers.add(waiver);
       }
-      staticTempEntity.newWaivedPolicyViolation(evaluation, policy, component.componentIdentifier, component.hash,
+      tempEntity.newWaivedPolicyViolation(evaluation, policy, component.componentIdentifier, component.hash,
           waiver);
     }
   }
 
-  private static PolicyWaiver findExistingWaiver(final String hash) {
+  private PolicyWaiver findExistingWaiver(final String hash) {
     for (PolicyWaiver waiver : existingWaivers) {
       if (waiver.getHash().equals(hash)) {
         return waiver;

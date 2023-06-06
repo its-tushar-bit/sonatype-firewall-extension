2153be17debc091eae5e3b9b97097a92a0d8f514
ken-duck <30352045+ken-duck@users.noreply.github.com>
2023-06-05 23:16:24 -0300

    SDEV-268: issue tracking static content (#9637)

--- insight-brain-frontend/src/main/frontend/integrations/sections/IssueTracking.jsx
+++ insight-brain-frontend/src/main/frontend/integrations/sections/IssueTracking.jsx
@@ -4,17 +4,31 @@
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
 
-import { NxH2, NxTile } from '@sonatype/react-shared-components';
+import { NxCard, NxH2, NxP } from '@sonatype/react-shared-components';
 import React from 'react';
+import IntegrationsCard from 'MainRoot/integrations/IntegrationsCard';
+import AtlassianLogo from '../../img/third-party-logos/atlassian.png';
 
 export default function IssueTracking() {
   return (
     <div id="iq-integrations-issue-tracking-section">
-      <NxH2>Issue Tracking Integrations</NxH2>
+      <NxP className="iq-integrations__full-width-text">
+        Integrate your DevSecOps SDLC pipeline with Lifecycle using Sonatype integrations plug-ins for complete security
+        orchestration, automation and response. You can view the security risks and vulnerabilities for your
+        applications and determine steps for remediation.
+      </NxP>
 
-      <NxTile>
-        <NxTile.Content>Content...</NxTile.Content>
-      </NxTile>
+      <NxH2>Issue Management Systems</NxH2>
+
+      <NxCard.Container className="iq-integrations-card-container">
+        <IntegrationsCard
+          title="Nexus IQ for Jira"
+          imgUrl={AtlassianLogo}
+          description="Atlassian Jira Server and Datacenter plug-in creating issues in Jira for policy violations."
+          linkText="Click here for installation help."
+          linkUrl="https://links.sonatype.com/products/nxiq/doc/integrations/scm/issue-tracking/jira"
+        />
+      </NxCard.Container>
     </div>
   );
 }

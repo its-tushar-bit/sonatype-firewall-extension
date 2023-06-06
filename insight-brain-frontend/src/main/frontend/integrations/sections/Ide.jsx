564a562669fd7994e9928b5f3b2bad9ed87fd68a
ken-duck <30352045+ken-duck@users.noreply.github.com>
2023-06-06 09:32:48 -0300

    SDEV-269: IDE static content (#9654)
    Creating the static page to display ci/cd plugins based figma designs
    https://www.figma.com/file/1thiVakx04yvx3AxsQvjEC/SM-Design-Sprint--Team-Vulcan--Q2-2023?type=design&node-id=244-1442&t=zEmQHvgM7TQXBfT0-0

--- insight-brain-frontend/src/main/frontend/integrations/sections/Ide.jsx
+++ insight-brain-frontend/src/main/frontend/integrations/sections/Ide.jsx
@@ -4,17 +4,49 @@
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
 
-import { NxH2, NxTile } from '@sonatype/react-shared-components';
+import { NxH2, NxCard, NxP } from '@sonatype/react-shared-components';
 import React from 'react';
+import IntegrationsCard from 'MainRoot/integrations/IntegrationsCard';
+import IdeaLogo from '../../img/third-party-logos/idea.png';
+import EclipseLogo from '../../img/third-party-logos/eclipse.png';
+import VisualStudioLogo from '../../img/third-party-logos/VisualStudio.png';
 
 export default function Ide() {
   return (
     <div id="iq-integrations-ide-section">
-      <NxH2>IDE Plugins</NxH2>
+      <NxP className="iq-integrations__full-width-text">
+        Integrate your DevSecOps SDLC pipeline with Lifecycle using Sonatype integrations plug-ins for complete security
+        orchestration, automation and response. You can view the security risks and vulnerabilities for your
+        applications and determine steps for remediation.
+      </NxP>
 
-      <NxTile>
-        <NxTile.Content>Content...</NxTile.Content>
-      </NxTile>
+      <NxH2>IDEs</NxH2>
+
+      <NxCard.Container className="iq-integrations-card-container">
+        <IntegrationsCard
+          title="IQ for IDEA"
+          imgUrl={IdeaLogo}
+          description="Provides Nexus IQ evaluation of project dependencies right inside IntelliJ IDEA."
+          linkText="Click here for installation help."
+          linkUrl="https://links.sonatype.com/products/nxiq/doc/integrations/scm/ides/idea"
+        />
+
+        <IntegrationsCard
+          title="IQ for Eclipse"
+          imgUrl={EclipseLogo}
+          description="Provides Nexus IQ evaluation of project dependencies right inside the Eclipse IDE."
+          linkText="Click here for installation help."
+          linkUrl="https://links.sonatype.com/products/nxiq/doc/integrations/scm/ides/eclipse"
+        />
+
+        <IntegrationsCard
+          title="IQ for Visual Studio"
+          imgUrl={VisualStudioLogo}
+          description="Visual Studio is a full-featured IDE. IQ for Visual Studio provides component analysis for both the Community, Professional, and Enterprise versions of Visual Studio."
+          linkText="Click here for installation help."
+          linkUrl="https://links.sonatype.com/products/nxiq/doc/integrations/scm/ides/visual-studio"
+        />
+      </NxCard.Container>
     </div>
   );
 }

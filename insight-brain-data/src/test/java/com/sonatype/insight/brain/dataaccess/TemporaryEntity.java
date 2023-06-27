17bf5c00d51275effb6d2995dca8ee99b43f107f
vladt <vladt@sonatype.com>
2023-06-27 11:03:17 -0400

    Delete SourceControlPullRequestComment test instances before Application test instances in TemporaryEntity.after()

--- insight-brain-data/src/test/java/com/sonatype/insight/brain/dataaccess/TemporaryEntity.java
+++ insight-brain-data/src/test/java/com/sonatype/insight/brain/dataaccess/TemporaryEntity.java
@@ -659,7 +659,6 @@ public class TemporaryEntity
     // - RepositoryMigration: cascaded from Repository
     // - RepositoryPolicyViolation: cascaded from Repository
     // - SourceControlDefaultBranchCommitHistory: cascaded from Application
-    // - SourceControlPullRequestComment: cascaded from PolicyEvaluation
     // - SourceControlPullRequestResult: cascaded from Application
     // - SuccessMetricsReportData: cascaded from SuccessMetricsReport
     // - VulnerabilityCustomCvssSeverityTag: cascaded from VulnerabilityCustomCvssSeverity
@@ -673,6 +672,7 @@ public class TemporaryEntity
     delete(membershipMappings, membershipMappingDAO);
     delete(dashboardFilterDAO.getAll(), dashboardFilterDAO);
     delete(userFilterDAO.getAll(), userFilterDAO);
+    delete(sourceControlPullRequestCommentDAO.getAll(), sourceControlPullRequestCommentDAO);
     delete(sourceControlOrganizationImportEventDAO.getAll(), sourceControlOrganizationImportEventDAO);
     delete(policyDAO.getAll(), entity -> policyDAO.getById(entity.getId()), policyDAO::delete);
     List<Organization> orgs = orgDAO.getAll().stream()

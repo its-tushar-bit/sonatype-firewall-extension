bb07a1eaf2862d3cbc33f3aa6a172cc38ba34da1
Anastasia Van Riet <110420624+anastasia-vanriet@users.noreply.github.com>
2023-08-08 15:42:06 -0300

    SDEV-428: add permission check to IDE usage data (#9956)

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/api/v2/service/ApiThirdPartyScanServiceAuthzTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/api/v2/service/ApiThirdPartyScanServiceAuthzTest.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
 
 import javax.inject.Inject;
 
+import com.sonatype.insight.brain.model.Organization;
 import com.sonatype.insight.brain.service.AbstractServiceAuthzTest;
 import com.sonatype.insight.brain.thirdparty.ThirdPartyUtils.SbomFormat;
 import com.sonatype.insight.error.exception.NotFoundException;
@@ -24,6 +25,8 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 public class ApiThirdPartyScanServiceAuthzTest
     extends AbstractServiceAuthzTest
 {
+  private static final long SINCE_UTC_TIMESTAMP = 1621220400000L;
+
   @Inject
   private ApiThirdPartyScanService apiThirdPartyEvaluationService;
 
@@ -64,6 +67,23 @@ public class ApiThirdPartyScanServiceAuthzTest
         .isThrownBy(() -> apiThirdPartyEvaluationService.getScanStatus(app.getId(), "scanRequestId"))
         .withMessage("Policy evaluation status with id %s for public application id %s was not found.",
             "scanRequestId", app.getPublicId());
+  }
+
+  @Test(expected = UnauthenticatedException.class)
+  public void testGetIdeUsersOverview_Unauthenticated() {
+    apiThirdPartyEvaluationService.getIdeUsersOverview(SINCE_UTC_TIMESTAMP);
+  }
+
+  @Test(expected = UnauthorizedException.class)
+  public void testGetIdeUsersOverview_Unauthorized() {
+    login();
+    apiThirdPartyEvaluationService.getIdeUsersOverview(SINCE_UTC_TIMESTAMP);
+  }
+
+  @Test
+  public void testGetIdeUsersOverview_Authorized() {
+    grantReadPermission(Organization.ROOT_ORGANIZATION_ID);
+    apiThirdPartyEvaluationService.getIdeUsersOverview(SINCE_UTC_TIMESTAMP);
   }
 
   private String getBomFile(String path) throws Exception {

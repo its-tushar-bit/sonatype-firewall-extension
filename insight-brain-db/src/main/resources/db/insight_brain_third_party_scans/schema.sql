6e54e49e8100c67bbb7b272f66f0f3da5eaadfd1
Koray Tugay <ktugay@sonatype.com>
2023-08-22 15:43:39 -0400

    CLM-25921 - Ensure "Evaluate a File" UI feature works with SPDX files (#10031)

--- insight-brain-db/src/main/resources/db/insight_brain_third_party_scans/schema.sql
+++ insight-brain-db/src/main/resources/db/insight_brain_third_party_scans/schema.sql
@@ -60,8 +60,8 @@ CREATE TABLE coordinate_security (
 CREATE TABLE coordinate_license (
   coordinate_license_id VARCHAR(50) NOT NULL,
   file_coordinate_id VARCHAR(50) NOT NULL,
-  license_id VARCHAR(50) NOT NULL,
-  name VARCHAR(50) NULL,
+  license_id VARCHAR(1000) NOT NULL,
+  name VARCHAR(1000) NULL,
   url VARCHAR(200) NULL,
   CONSTRAINT license_coordinate_pk PRIMARY KEY (coordinate_license_id),
   CONSTRAINT license_coordinate_uk UNIQUE (license_id, file_coordinate_id),

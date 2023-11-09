490c144434c0476b7e56602d2c2f81bafc40606e
Juan Esteban López <62036085+jlopez-sonatype@users.noreply.github.com>
2023-11-09 14:17:44 -0700

    Iday remove frontend gallery (#10576)

--- insight-brain-frontend/src/main/component-gallery/components/iqPolicyThreatLevelSlider/module.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
- * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
- * "Sonatype" is a trademark of Sonatype, Inc.
- */
-import iqPolicyThreatLevelSliderController from './iqPolicyThreatLevelSliderController';
-
-export default angular.module('iqPolicyThreatLevelSlider', [])
-    .controller('iqPolicyThreatLevelSliderController', iqPolicyThreatLevelSliderController);
--- insight-brain-frontend/src/main/component-gallery/styles/iq-modal/module.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
- * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
- * "Sonatype" is a trademark of Sonatype, Inc.
- */
-import IqModalExampleController from './IqModalExampleController';
-
-export default angular.module('iq-modal', [])
-    .controller('IqModalExampleController', IqModalExampleController);

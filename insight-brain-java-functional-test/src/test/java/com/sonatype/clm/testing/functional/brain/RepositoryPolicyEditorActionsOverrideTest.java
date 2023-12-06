eb935fd04ddf775a43ff4bd1ba15e5058b68e05f
Christian Fernau <cfernau@sonatype.com>
2023-12-06 17:41:07 +0000

    CLM-28722 inheritance section tests (#10746)

--- insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/RepositoryPolicyEditorActionsOverrideTest.java
+++ insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/RepositoryPolicyEditorActionsOverrideTest.java
@@ -7,20 +7,26 @@ package com.sonatype.clm.testing.functional.brain;
 
 import com.sonatype.clm.testing.functional.AbstractFunctionalTest;
 import com.sonatype.clm.testing.functional.elements.OwnerDetailSidebar;
+import com.sonatype.clm.testing.functional.pages.OwnerSummaryPage;
 import com.sonatype.clm.testing.functional.pages.PolicyEditorPage;
 import com.sonatype.clm.testing.functional.pages.RepositoriesSummaryPage;
+import com.sonatype.insight.brain.model.repository.RepositoryManager;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Condition.checked;
+import static com.codeborne.selenide.Condition.disabled;
+import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 
 public class RepositoryPolicyEditorActionsOverrideTest
     extends AbstractFunctionalTest
 {
+  private RepositoryManager repositoryManager;
+
   @BeforeClass
   public static void boot() {
     refreshOrOpen(RepositoriesSummaryPage.url());
@@ -29,28 +35,20 @@ public class RepositoryPolicyEditorActionsOverrideTest
 
   @Before
   public void init() {
-    tempEntity.newRepositoryManager();
+    repositoryManager = tempEntity.newRepositoryManager();
   }
 
   @Test
   public void testOverrideAddAndEdit_RepositoryContainer() {
-    refreshOrOpen(RepositoriesSummaryPage.url());
+    goToRepositoryContainerSummaryPage();
+    createTrivialPolicy(true);
+    PolicyEditorPage.savePolicy();
 
-    RepositoriesSummaryPage.summaryTile().name().shouldHave(text("Repository Managers"));
-    RepositoriesSummaryPage.policyTile().addPolicyButton().click();
-    PolicyEditorPage.title().shouldHave(text("New Policy"));
     PolicyEditorPage.inheritanceSection().policyActionsOverrideCheckbox().input().shouldNotBe(checked);
     PolicyEditorPage.inheritanceSection().policyActionsOverrideCheckbox().label()
         .shouldHave(text("Allow action overrides at repository manager and repository levels"));
     PolicyEditorPage.inheritanceSection().allChildrenInheritRadio().shouldNotBe(visible);
     PolicyEditorPage.inheritanceSection().specifiedChildrenInheritRadio().shouldNotBe(visible);
-
-    PolicyEditorPage.summarySection().policyName().input().setValue("Test Policy");
-    PolicyEditorPage.constraintSection().constraintEditor(0).name().setValue("Test Constraint");
-    PolicyEditorPage.constraintSection().constraintEditor(0).ageCondition(0).value().age().setValue("1");
-    PolicyEditorPage.inheritanceSection().policyActionsOverrideCheckbox().click();
-    PolicyEditorPage.savePolicy();
-
     OwnerDetailSidebar.policyGroup().entryItems().shouldHaveSize(1);
     OwnerDetailSidebar.policyGroup().entryItems().get(0).click();
 
@@ -58,5 +56,107 @@ public class RepositoryPolicyEditorActionsOverrideTest
     PolicyEditorPage.inheritanceSection().policyActionsOverrideCheckbox().input().shouldBe(checked);
     PolicyEditorPage.inheritanceSection().policyActionsOverrideCheckbox().label()
         .shouldHave(text("Allow action overrides at repository manager and repository levels"));
+  }
+
+  @Test
+  public void testOverride_inheritsFromContainerPolicyTrue() {
+    goToRepositoryContainerSummaryPage();
+    testOverride_inheritsFromPolicy(true, 1);
+  }
+
+  @Test
+  public void testOverride_inheritsFromContainerPolicyFalse() {
+    goToRepositoryContainerSummaryPage();
+    testOverride_inheritsFromPolicy(false, 1);
+  }
+
+  @Test
+  public void testOverride_inheritsFromRootPolicyTrue() {
+    refreshOrOpen(OwnerSummaryPage.urlToRootOrg());
+    testOverride_inheritsFromPolicy(true, 2);
+  }
+
+  @Test
+  public void testOverride_inheritsFromRootPolicyFalse() {
+    refreshOrOpen(OwnerSummaryPage.urlToRootOrg());
+    testOverride_inheritsFromPolicy(false, 2);
+  }
+
+  /**
+   * Tests that the enableOverride checkbox enables the UI in the child to override the parent policy actions.
+   *
+   * @param enableOverride  whether to enable the override checkbox
+   * @param policyListIndex 0: local policy, 1: inherited policy from container, 2: inherited policy from root org
+   */
+  private void testOverride_inheritsFromPolicy(boolean enableOverride, int policyListIndex) {
+    createTrivialPolicy(enableOverride);
+    PolicyEditorPage.savePolicy();
+
+    goToRepositoryManagerSummaryPage(repositoryManager);
+    RepositoriesSummaryPage.policyTile().policyLists().get(policyListIndex).shouldHave(text("Test Policy"));
+    RepositoriesSummaryPage.policyTile().policyLists().get(policyListIndex).click();
+
+    PolicyEditorPage.title().shouldHave(text("View Policy"));
+    PolicyEditorPage.actionsSection().overrideParentActions().shouldBe(enableOverride ? enabled : disabled);
+    PolicyEditorPage.actionsSection().proxy().noActionRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().proxy().warnRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().proxy().failRadio().shouldBe(disabled);
+
+    if (enableOverride) {
+      PolicyEditorPage.actionsSection().overrideParentActions().click();
+      PolicyEditorPage.actionsSection().proxy().noActionRadio().shouldBe(enabled);
+      PolicyEditorPage.actionsSection().proxy().warnRadio().shouldBe(enabled);
+      PolicyEditorPage.actionsSection().proxy().failRadio().shouldBe(enabled);
+      PolicyEditorPage.actionsSection().proxy().failRadio().click();
+      PolicyEditorPage.actionsSection().proxy().failRadio().input().shouldBe(checked);
+      PolicyEditorPage.savePolicy();
+      refresh();
+      PolicyEditorPage.actionsSection().proxy().failRadio().input().shouldBe(checked);
+      PolicyEditorPage.actionsSection().proxy().noActionRadio().shouldBe(enabled);
+      PolicyEditorPage.actionsSection().proxy().warnRadio().shouldBe(enabled);
+      PolicyEditorPage.actionsSection().proxy().failRadio().shouldBe(enabled);
+    }
+    else {
+      PolicyEditorPage.actionsSection().proxy().noActionRadio().shouldBe(disabled);
+      PolicyEditorPage.actionsSection().proxy().warnRadio().shouldBe(disabled);
+      PolicyEditorPage.actionsSection().proxy().failRadio().shouldBe(disabled);
+    }
+
+    PolicyEditorPage.actionsSection().build().noActionRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().build().warnRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().build().failRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().operate().noActionRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().operate().warnRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().operate().failRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().develop().noActionRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().develop().warnRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().develop().failRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().release().noActionRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().release().warnRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().release().failRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().source().noActionRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().source().warnRadio().shouldBe(disabled);
+    PolicyEditorPage.actionsSection().source().failRadio().shouldBe(disabled);
+  }
+
+  private static void createTrivialPolicy(boolean enableOverride) {
+    RepositoriesSummaryPage.policyTile().addPolicyButton().click();
+    PolicyEditorPage.title().shouldHave(text("New Policy"));
+    PolicyEditorPage.summarySection().policyName().input().setValue("Test Policy");
+    PolicyEditorPage.constraintSection().constraintEditor(0).name().setValue("Test Constraint");
+    PolicyEditorPage.constraintSection().constraintEditor(0).ageCondition(0).value().age().setValue("1");
+    if (enableOverride) {
+      PolicyEditorPage.inheritanceSection().policyActionsOverrideCheckbox().click();
+    }
+  }
+
+  private void goToRepositoryContainerSummaryPage() {
+    refreshOrOpen(RepositoriesSummaryPage.url());
+    RepositoriesSummaryPage.summaryTile().name().shouldHave(text("Repository Managers"));
+  }
+
+  private void goToRepositoryManagerSummaryPage(RepositoryManager repositoryManager) {
+    refreshOrOpen(RepositoriesSummaryPage.repositoryManagerUrl(repositoryManager.getId()));
+    RepositoriesSummaryPage.summaryTile().name().shouldHave(text(repositoryManager.getName()));
   }
 }
--- insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/RepositoryPolicyEditorNotificationsOverrideTest.java
+++ insight-brain-java-functional-test/src/test/java/com/sonatype/clm/testing/functional/brain/RepositoryPolicyEditorNotificationsOverrideTest.java
@@ -6,21 +6,27 @@
 package com.sonatype.clm.testing.functional.brain;
 
 import com.sonatype.clm.testing.functional.AbstractFunctionalTest;
+import com.sonatype.clm.testing.functional.elements.NotificationsSection;
 import com.sonatype.clm.testing.functional.elements.OwnerDetailSidebar;
+import com.sonatype.clm.testing.functional.pages.OwnerSummaryPage;
 import com.sonatype.clm.testing.functional.pages.PolicyEditorPage;
 import com.sonatype.clm.testing.functional.pages.RepositoriesSummaryPage;
+import com.sonatype.insight.brain.model.repository.RepositoryManager;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Condition.checked;
+import static com.codeborne.selenide.Condition.disabled;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 
 public class RepositoryPolicyEditorNotificationsOverrideTest
     extends AbstractFunctionalTest
 {
+  private RepositoryManager repositoryManager;
+
   @BeforeClass
   public static void boot() {
     refreshOrOpen(RepositoriesSummaryPage.url());
@@ -29,28 +35,20 @@ public class RepositoryPolicyEditorNotificationsOverrideTest
 
   @Before
   public void init() {
-    tempEntity.newRepositoryManager();
+    repositoryManager = tempEntity.newRepositoryManager();
   }
 
   @Test
   public void testOverrideAddAndEdit_RepositoryContainer() {
-    refreshOrOpen(RepositoriesSummaryPage.url());
+    goToRepositoryContainerSummaryPage();
+    createTrivialPolicy(true);
+    PolicyEditorPage.savePolicy();
 
-    RepositoriesSummaryPage.summaryTile().name().shouldHave(text("Repository Managers"));
-    RepositoriesSummaryPage.policyTile().addPolicyButton().click();
-    PolicyEditorPage.title().shouldHave(text("New Policy"));
     PolicyEditorPage.inheritanceSection().policyNotificationsOverrideCheckbox().input().shouldNotBe(checked);
     PolicyEditorPage.inheritanceSection().policyNotificationsOverrideCheckbox().label()
         .shouldHave(text("Allow notification overrides at repository manager and repository levels"));
     PolicyEditorPage.inheritanceSection().allChildrenInheritRadio().shouldNotBe(visible);
     PolicyEditorPage.inheritanceSection().specifiedChildrenInheritRadio().shouldNotBe(visible);
-
-    PolicyEditorPage.summarySection().policyName().input().setValue("Test Policy");
-    PolicyEditorPage.constraintSection().constraintEditor(0).name().setValue("Test Constraint");
-    PolicyEditorPage.constraintSection().constraintEditor(0).ageCondition(0).value().age().setValue("1");
-    PolicyEditorPage.inheritanceSection().policyNotificationsOverrideCheckbox().click();
-    PolicyEditorPage.savePolicy();
-
     OwnerDetailSidebar.policyGroup().entryItems().shouldHaveSize(1);
     OwnerDetailSidebar.policyGroup().entryItems().get(0).click();
 
@@ -58,5 +56,99 @@ public class RepositoryPolicyEditorNotificationsOverrideTest
     PolicyEditorPage.inheritanceSection().policyNotificationsOverrideCheckbox().input().shouldBe(checked);
     PolicyEditorPage.inheritanceSection().policyNotificationsOverrideCheckbox().label()
         .shouldHave(text("Allow notification overrides at repository manager and repository levels"));
+  }
+
+  @Test
+  public void testOverride_inheritsFromContainerPolicyTrue() {
+    goToRepositoryContainerSummaryPage();
+    testOverride_inheritsFromPolicy(true, 1);
+  }
+
+  @Test
+  public void testOverride_inheritsFromContainerPolicyFalse() {
+    goToRepositoryContainerSummaryPage();
+    testOverride_inheritsFromPolicy(false, 1);
+  }
+
+  @Test
+  public void testOverride_inheritsFromRootPolicyTrue() {
+    refreshOrOpen(OwnerSummaryPage.urlToRootOrg());
+    testOverride_inheritsFromPolicy(true, 2);
+  }
+
+  @Test
+  public void testOverride_inheritsFromRootPolicyFalse() {
+    refreshOrOpen(OwnerSummaryPage.urlToRootOrg());
+    testOverride_inheritsFromPolicy(false, 2);
+  }
+
+  /**
+   * Tests that the enableOverride checkbox enables the UI in the child to override the parent policy actions.
+   *
+   * @param enableOverride  whether to enable the override checkbox
+   * @param policyListIndex 0: local policy, 1: inherited policy from container, 2: inherited policy from root org
+   */
+  private void testOverride_inheritsFromPolicy(boolean enableOverride, int policyListIndex) {
+    createTrivialPolicy(enableOverride);
+    createEmailNotification("parent@example.com");
+    PolicyEditorPage.savePolicy();
+
+    goToRepositoryManagerSummaryPage(repositoryManager);
+    RepositoriesSummaryPage.policyTile().policyLists().get(policyListIndex).shouldHave(text("Test Policy"));
+    RepositoriesSummaryPage.policyTile().policyLists().get(policyListIndex).click();
+
+    PolicyEditorPage.title().shouldHave(text("View Policy"));
+
+    NotificationsSection.notificationFor("parent@example.com").proxy().input().shouldBe(checked);
+    NotificationsSection.notificationFor("parent@example.com").proxy().shouldBe(disabled);
+
+    if (enableOverride) {
+      PolicyEditorPage.notificationsSection().overrideParentNotifications().click();
+      NotificationsSection.notificationFor("parent@example.com").proxy().input().shouldBe(checked);
+      NotificationsSection.notificationFor("parent@example.com").proxy().shouldNotBe(disabled);
+      createEmailNotification("user@example.com");
+      PolicyEditorPage.savePolicy();
+      NotificationsSection.notifications().shouldHaveSize(2);
+      NotificationsSection.notificationFor("user@example.com").proxy().input().shouldBe(checked);
+      NotificationsSection.notificationFor("user@example.com").proxy().shouldNotBe(disabled);
+    }
+    else {
+      PolicyEditorPage.notificationsSection().overrideParentNotifications().shouldBe(disabled);
+      NotificationsSection.notificationFor("parent@example.com").proxy().shouldBe(disabled);
+    }
+
+    NotificationsSection.notificationFor("parent@example.com").build().shouldBe(disabled);
+    NotificationsSection.notificationFor("parent@example.com").operate().shouldBe(disabled);
+    NotificationsSection.notificationFor("parent@example.com").develop().shouldBe(disabled);
+    NotificationsSection.notificationFor("parent@example.com").release().shouldBe(disabled);
+    NotificationsSection.notificationFor("parent@example.com").continuousMonitoring().shouldBe(disabled);
+    NotificationsSection.notificationFor("parent@example.com").source().shouldBe(disabled);
+  }
+
+  private void createEmailNotification(String email) {
+    NotificationsSection.addNotification().email().setValue(email);
+    NotificationsSection.addNotification().addButton().click();
+    NotificationsSection.notificationFor(email).proxy().click();
+  }
+
+  private static void createTrivialPolicy(boolean enableOverride) {
+    RepositoriesSummaryPage.policyTile().addPolicyButton().click();
+    PolicyEditorPage.title().shouldHave(text("New Policy"));
+    PolicyEditorPage.summarySection().policyName().input().setValue("Test Policy");
+    PolicyEditorPage.constraintSection().constraintEditor(0).name().setValue("Test Constraint");
+    PolicyEditorPage.constraintSection().constraintEditor(0).ageCondition(0).value().age().setValue("1");
+    if (enableOverride) {
+      PolicyEditorPage.inheritanceSection().policyNotificationsOverrideCheckbox().click();
+    }
+  }
+
+  private void goToRepositoryContainerSummaryPage() {
+    refreshOrOpen(RepositoriesSummaryPage.url());
+    RepositoriesSummaryPage.summaryTile().name().shouldHave(text("Repository Managers"));
+  }
+
+  private void goToRepositoryManagerSummaryPage(RepositoryManager repositoryManager) {
+    refreshOrOpen(RepositoriesSummaryPage.repositoryManagerUrl(repositoryManager.getId()));
+    RepositoriesSummaryPage.summaryTile().name().shouldHave(text(repositoryManager.getName()));
   }
 }

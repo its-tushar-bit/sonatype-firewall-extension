0c13b7c03ee75bff5cc0f715b7a74d10c870b73c
Jawad Shaik <55854941+jawadshaikst@users.noreply.github.com>
2024-03-01 12:53:17 -0500

    CLM-29597 - Step 1 of renaming applicationVersion column to sbomVersion - Add nullable column sbom_version (#11084)

--- insight-brain-db/src/main/resources/db/insight_brain_third_party_scans/schema.sql
+++ insight-brain-db/src/main/resources/db/insight_brain_third_party_scans/schema.sql
@@ -120,6 +120,7 @@ CREATE TABLE sbom_metadata (
     status VARCHAR(20) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     metadata_json TEXT,
+    sbom_version VARCHAR(200) NULL,
     CONSTRAINT sbom_metadata_pk PRIMARY KEY (sbom_metadata_id),
     CONSTRAINT sbom_metadata_third_party_file_fk FOREIGN KEY (third_party_file_id)
        REFERENCES third_party_file (third_party_file_id),

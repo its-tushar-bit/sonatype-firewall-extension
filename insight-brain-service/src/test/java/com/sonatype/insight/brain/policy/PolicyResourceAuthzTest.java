96880e76404cf6369fe1dabd51c9b9d5106352e7
vladt <vladt@sonatype.com>
2024-03-20 16:27:57 -0400

    Removed unused method from PolicyResourceAuthzTest

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/policy/PolicyResourceAuthzTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/policy/PolicyResourceAuthzTest.java
@@ -16,8 +16,6 @@ import com.sonatype.insight.brain.model.policy.Condition;
 import com.sonatype.insight.brain.model.policy.Constraint;
 import com.sonatype.insight.brain.model.policy.LogicalOperator;
 import com.sonatype.insight.brain.model.policy.Policy;
-import com.sonatype.insight.brain.model.policy.conditions.ProprietaryNameConflictConditionType;
-import com.sonatype.insight.brain.model.policy.conditions.SecurityVulnerabilityCategoryConditionType;
 import com.sonatype.insight.brain.model.policy.conditions.SecurityVulnerabilitySeverityConditionType;
 import com.sonatype.insight.brain.model.repository.RepositoryContainer;
 import com.sonatype.insight.brain.service.AbstractResourceAuthzTest;
@@ -259,29 +257,5 @@ public class PolicyResourceAuthzTest
     policyDAO.update(policy);
     testAuthzPut(
         request.parameter(OwnerType.REPOSITORY_CONTAINER, RepositoryContainer.REPOSITORY_CONTAINER_ID, policy.getId()));
-  }
-
-  private void createTestPolicyWithCondition(
-      String name,
-      final String ownerId,
-      boolean withSecurityVulnerabilityCategoryMaliciousCodeCondition,
-      boolean withProprietaryNameConflictCondition,
-      boolean withSecurityVulnerabilitySeverityCondition)
-  {
-    Policy policy = new Policy(name, name);
-    policy.setOwnerId(ownerId);
-    Constraint constraint = new Constraint("test-constraint", "Test Constraint", LogicalOperator.OR);
-
-    if (withSecurityVulnerabilitySeverityCondition) {
-      constraint.addCondition(new Condition(SecurityVulnerabilitySeverityConditionType.ID, ">=", "0"));
-    }
-    if (withSecurityVulnerabilityCategoryMaliciousCodeCondition) {
-      constraint.addCondition(new Condition(SecurityVulnerabilityCategoryConditionType.ID, "is", "malicious_code"));
-    }
-    if (withProprietaryNameConflictCondition) {
-      constraint.addCondition(new Condition(ProprietaryNameConflictConditionType.ID, "is present"));
-    }
-    policy.addConstraint(constraint);
-    policyDAO.insert(policy);
   }
 }

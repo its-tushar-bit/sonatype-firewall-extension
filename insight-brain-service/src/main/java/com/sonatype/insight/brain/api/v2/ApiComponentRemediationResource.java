579e707f617695498117bcc299b5bd140df8e504
skatre <38050374+skatre@users.noreply.github.com>
2024-05-20 10:49:30 -0500

    Add component remediation swagger doc details - CLM-26191 (#11256)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiComponentRemediationResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiComponentRemediationResource.java
@@ -22,6 +22,9 @@ import com.sonatype.insight.brain.api.v2.service.ApiComponentRemediationService;
 import com.sonatype.insight.brain.model.OwnerType;
 
 import com.codahale.metrics.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 /**
  * @since 1.64
@@ -41,12 +44,43 @@ public class ApiComponentRemediationResource
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
+  @Operation(description = "Use this method to obtain remediation suggestions for policy violations on " +
+      "a component basis. Remediations obtained from this method are same as those appearing on " +
+      "the Component Details Page in the UI.")
+  @ApiResponse(
+      responseCode = "200", description = "The response returns details for components that can be used for " +
+      "remediation. Details for the recommended component are grouped under type in the response." +
+      "<ul>" +
+      "<li>Type <i>next-no-violations</i> indicates that the component version has no violations.</li>" +
+      "<li>Type <i>next-non-failing</i> indicates that the component version does not fail policy violations. " +
+      "The response will contain this type only if stageId is provided in the method call.</li>" +
+      "<li>Type <i>next-no-violations-with-dependencies</i> indicates that the component, along-with its " +
+      "dependencies does not any violate any policies.</li>" +
+      "<li>Type <i>next-non-failing-with-dependencies</i> indicates that the component and its dependencies " +
+      "will not fail a build for the stageId provided.</li>" +
+      "</ul>" +
+      "<p>" +
+      "Hash values returned here are truncated and are not intended to be used as checksums. " +
+      "They can be used as identifiers to pass to other REST API calls. "
+
+  )
   public ApiComponentRemediationDTO getSuggestedRemediationForComponent(
       ApiComponentDTOV2 component,
+      @Parameter(description = "Possible values: application, organization, repository. ")
       @PathParam("ownerType") final OwnerType ownerType,
+      @Parameter(description = "Possible values: applicationId, organizationId or repositoryId.")
       @PathParam("ownerId") final String ownerId,
+      @Parameter(description = "Enter the stageId to obtain next-non-failing and next-non-failing-with-dependencies " +
+          "remediation types in the response. Possible values are develop, build, stage-release, release " +
+          "and operate.")
       @QueryParam("stageId") String stageId,
+      @Parameter(description = "Enter the identification source if you want the remediation result based" +
+          " on third-party scan information (non-Sonatype). " +
+          "The identification source can be obtained from the Component Details" +
+          " Page in the UI.")
       @QueryParam("identificationSource") String identificationSource,
+      @Parameter(description = "Enter the scanId (reportId) if you want the remediation result based on" +
+          " third-party scan information (non-Sonatype).")
       @QueryParam("scanId") String scanId)
   {
     return remediationService

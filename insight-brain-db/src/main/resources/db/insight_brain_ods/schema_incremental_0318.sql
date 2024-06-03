c73b1effc5d424223c1c3800829becb507431f59
Collin Peters <cpeters@sonatype.com>
2024-06-03 12:52:59 -0700

    CLM-30336 Update migration 316 to be more on-prem friendly (#11579)

--- insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0318.sql
+++ insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0318.sql
@@ -1,5 +1,10 @@
 -- Since 1.170
 -- SaaS Compatible
+
+/*
+-- See comment in schema_incremental_0316.sql
+-- With that updated migration this one is no longer applicable
+
 ALTER TABLE organization DROP COLUMN allow_policy_violation_grandfathering_override;
 ALTER TABLE organization DROP COLUMN policy_violation_grandfathering_enabled;
 
@@ -9,3 +14,4 @@ ALTER TABLE policy DROP COLUMN policy_violation_grandfathering_allowed;
 
 ALTER TABLE policy_violation DROP COLUMN grandfather_time;
 ALTER TABLE policy_violation DROP COLUMN grandfather_applied;
+*/

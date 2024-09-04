de1ed2a5c6365672a49821654ce07e151d1cf991
Yizhao Lang <langyizhao@gmail.com>
2024-09-04 13:05:24 -0400

    Version scoring HDS client and sorting (#12108)

---
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/hds/VersionScoringService.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+
+package com.sonatype.insight.brain.hds;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.sonatype.clm.dto.model.component.ComponentIdentifier;
+import com.sonatype.clm.dto.model.remediation.VersionScoringDTO;
+import com.sonatype.clm.dto.model.remediation.VersionScoringDTO.ToVersionData;
+
+@Named
+public class VersionScoringService
+{
+  static final String HDS_BULK_SCORE_VERSIONING_PATH = "rest/component/version-scoring/list";
+
+  private final HdsClient hdsClient;
+
+  @Inject
+  public VersionScoringService(HdsClient hdsClient) {
+    this.hdsClient = hdsClient;
+  }
+
+  /**
+   * @param componentIdentifiers a list of components with "from-versions"
+   * @return a map whose keys are each of the "from-version" from input,
+   * values are their corresponding "to-versions", sorted by scores, from highest to lowest.
+   */
+  public Map<ComponentIdentifier, List<String>> getSortedNonBreakingVersionsNoAuth(
+      Collection<ComponentIdentifier> componentIdentifiers)
+  {
+    Map<ComponentIdentifier, List<String>> result = new HashMap<>();
+    for (VersionScoringDTO versionScoringDTO : getVersionScoringNoAuth(componentIdentifiers)) {
+      Map<String, ToVersionData> toVersionsNonBreaking = versionScoringDTO.getToVersionsNonBreaking();
+      List<String> versionSortedByScore = toVersionsNonBreaking.entrySet().parallelStream()
+          .sorted(Entry.comparingByValue(Comparator.comparingInt(ToVersionData::getToComponentVersionScore).reversed()))
+          .map(Entry::getKey)
+          .toList();
+      result.put(versionScoringDTO.getComponentIdentifier(), versionSortedByScore);
+    }
+    return result;
+  }
+
+  private Collection<VersionScoringDTO> getVersionScoringNoAuth(Collection<ComponentIdentifier> componentIdentifiers) {
+    return List.of(
+        hdsClient.post(VersionScoringDTO[].class, HDS_BULK_SCORE_VERSIONING_PATH, componentIdentifiers)
+    );
+  }
+}

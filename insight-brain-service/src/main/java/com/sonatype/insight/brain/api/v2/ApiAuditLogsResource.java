1408e3ef07b90a0ea00b83195e476ed5cb0c506b
skatre <38050374+skatre@users.noreply.github.com>
2024-09-17 13:28:20 -0500

    CLM-26191 swagger audit logs (#12235)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiAuditLogsResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiAuditLogsResource.java
@@ -19,12 +19,21 @@ import com.sonatype.insight.brain.api.v2.service.ApiAuditLogsService;
 import com.sonatype.insight.brain.audit.AuditEvent;
 import com.sonatype.insight.brain.audit.Audited;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 /**
  * curl -u admin:admin123 "http://localhost:8070/api/v2/auditLogs?startUtcDate=2024-03-10&endUtcDate=2024-03-13"
  * >audit.log
  */
 @Named
 @Path(PublicApiPaths.AUDIT_LOGS_RESOURCE_PATH)
+@Tag(name = "Audit Logs",
+    description = "Use this REST API to access the audit logs.")
+
 public class ApiAuditLogsResource
 {
   private final ApiAuditLogsService apiAuditLogsService;
@@ -36,9 +45,26 @@ public class ApiAuditLogsResource
 
   @GET
   @Produces(MediaType.TEXT_PLAIN)
+  @Operation(description =
+      "Use this method to retrieve the audit events for the specified time period." +
+          "/n" +
+          "/n" +
+          "Permissions required: Access Audit Log",
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "The response text contains lines of audit events in chronologically ascending order.",
+              content =
+                  {
+                      @Content(mediaType = MediaType.TEXT_PLAIN)
+                  })
+      }
+  )
+
   @Audited(AuditEvent.EXPORT_AUDIT_LOG)
   public StreamingOutput getAuditLogs(
+      @Parameter(description = "Enter the start UTC date in the format (yyyy-mm-dd).")
       @QueryParam("startUtcDate") final String startUtcDate,
+      @Parameter(description = "Enter the end UTC date in the format (yyyy-mm-dd).")
       @QueryParam("endUtcDate") final String endUtcDate)
   {
     return apiAuditLogsService.getAuditLogs(startUtcDate, endUtcDate);

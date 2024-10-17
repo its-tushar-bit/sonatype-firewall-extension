4cb61e5be14d1b15da43189f93a2142a0556b6f0
skatre <38050374+skatre@users.noreply.github.com>
2024-10-17 11:18:58 -0500

    CLM- 26191 Role membership swagger (#12444)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiRoleMembershipResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiRoleMembershipResource.java
@@ -24,12 +24,22 @@ import com.sonatype.insight.brain.model.security.MemberType;
 import com.sonatype.insight.brain.security.MembershipMappingService;
 
 import com.codahale.metrics.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * @since 1.70
  */
 @Named
 @Timed
+@Tag(name = "Role Memberships",
+    description = "Use this REST API to manage authorizations for users or user groups." +
+        "\n" +
+        "\n" +
+        "You can view existing role assignments and " +
+        "grant or revoke user authorization on organizations, applications and repositories.")
 @Path(PublicApiPaths.ROLE_MEMBERSHIP_PATH_V2)
 public class ApiRoleMembershipResource
 {
@@ -49,11 +59,33 @@ public class ApiRoleMembershipResource
   @PUT
   @Path(APPLICATION_OR_ORGANIZATION)
   @Audited(AuditEvent.GRANT_ROLE_MEMBERSHIP)
+  @Operation(description = "Use this method to grant a role to a user or user group for the specified application or " +
+      "organization." +
+      "\n" +
+      "\n" +
+      "Permissions required: Edit access control")
+  @ApiResponse(
+      responseCode = "200",
+      description = "The specified roleId has been has been granted to the user or user group for the requested " +
+          "context.",
+      useReturnTypeSchema = true
+  )
   public void grantRoleMembershipApplicationOrOrganization(
+      @Parameter(description = "Enter the value for the ownerType for which you want to grant the role.",
+          required = true)
       @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the value for the internalId associated with the ownerType specified above.",
+          required = true)
       @PathParam("internalOwnerId") String internalOwnerId,
+      @Parameter(description = "Enter the roleId for the role to be granted." +
+          "\n" +
+          "\n" +
+          "Use the Roles REST API for roleIds and descriptions.", required = true)
       @PathParam("roleId") String roleId,
+      @Parameter(description = "Enter the value for memberType, to specify a user or a user group.", required = true)
       @PathParam("memberType") MemberType memberType,
+      @Parameter(description = "Enter the value for memberName. This can be a username or group name depending upon " +
+          "the value of memberType above.", required = true)
       @PathParam("memberName") String memberName)
   {
     membershipMappingService.grantRoleMembership(ownerType, internalOwnerId, roleId, memberType, memberName);
@@ -62,10 +94,31 @@ public class ApiRoleMembershipResource
   @PUT
   @Path(GLOBAL_OR_REPOSITORY_CONTAINER)
   @Audited(AuditEvent.GRANT_ROLE_MEMBERSHIP)
+  @Operation(description = "Use this method to grant a role to a user or user group globally or on all " +
+      "repositories." +
+      "\n" +
+      "\n" +
+      "Permissions required: Edit system configuration and users for a global context or edit access control for a " +
+      "non-global context")
+  @ApiResponse(
+      responseCode = "200",
+      description =
+          "The specified role has been granted to the users or user groups on the given context.",
+      useReturnTypeSchema = true
+  )
   public void grantRoleMembershipGlobalOrRepositoryContainer(
+      @Parameter(description = "Enter the value for the ownerType for which you want to grant the role.",
+          required = true)
       @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the roleId for the role to be granted." +
+          "\n" +
+          "\n" +
+          "Use the Roles REST API for roleIds and descriptions.", required = true)
       @PathParam("roleId") String roleId,
+      @Parameter(description = "Enter the value for memberType, to specify a user or a user group.", required = true)
       @PathParam("memberType") MemberType memberType,
+      @Parameter(description = "Enter the value for memberName. This can be a username or group name depending upon " +
+          "the value of memberType above.", required = true)
       @PathParam("memberName") String memberName)
   {
     membershipMappingService.grantRoleMembership(ownerType, null, roleId, memberType, memberName);
@@ -74,8 +127,23 @@ public class ApiRoleMembershipResource
   @GET
   @Path("{ownerType: application|organization}/{internalOwnerId}")
   @Produces(MediaType.APPLICATION_JSON)
+  @Operation(description = "Use this method to retrieve the users, user groups and the corresponding role Ids." +
+      "\n" +
+      "\n" +
+      "Permissions required: Edit system configuration and users for a global context or view IQ elements for a " +
+      "non-global context")
+  @ApiResponse(
+      responseCode = "200",
+      description = "The response contains the assigned role Ids, users and user groups for the application or " +
+          "organization requested. It also includes members who inherit a role based on the " +
+          "organization hierarchy.", useReturnTypeSchema = true)
+
   public ApiRoleMemberMappingListDTO getRoleMembershipsApplicationOrOrganization(
+      @Parameter(description = "Enter the ownerType for which you want to retrieve users and their role Ids.",
+          required = true)
       @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the corresponding id for the ownerType specified above.",
+          required = true)
       @PathParam("internalOwnerId") String internalOwnerId)
   {
     return membershipMappingService.getRoleMembershipsOmitEmpty(ownerType, internalOwnerId);
@@ -84,7 +152,22 @@ public class ApiRoleMembershipResource
   @GET
   @Path("{ownerType: global|repository_container}")
   @Produces(MediaType.APPLICATION_JSON)
+  @Operation(description = "Use this method to retrieve all users and roles globally or for all repositories." +
+      "\n" +
+      "\n" +
+      "Permissions required: Edit system configuration and users for a global context or view IQ elements for a " +
+      "non-global context")
+  @ApiResponse(
+      responseCode = "200",
+      description =
+          "The response contains all role Ids and the corresponding users/user groups assigned to them, for " +
+              "the ownerType specified. It also includes members who inherit a role based on the organization" +
+              " hierarchy.",
+      useReturnTypeSchema = true
+  )
   public ApiRoleMemberMappingListDTO getRoleMembershipsGlobalOrRepositoryContainer(
+      @Parameter(description = "Enter the value for ownerType. Using `global` will return the users and groups who " +
+          "have been assigned the administrator role.", required = true)
       @PathParam("ownerType") OwnerType ownerType)
   {
     return membershipMappingService.getRoleMembershipsOmitEmpty(ownerType, null);
@@ -93,11 +176,28 @@ public class ApiRoleMembershipResource
   @DELETE
   @Path(APPLICATION_OR_ORGANIZATION)
   @Audited(AuditEvent.REVOKE_ROLE_MEMBERSHIP)
+  @Operation(description = "Use this method to revoke a role from a user or user group, on a specific application or " +
+      "organization." +
+      "\n" +
+      "\n" +
+      "Permissions required: Edit access control")
+  @ApiResponse(
+      responseCode = "204",
+      description = "The specified role has been revoked from the user or user group",
+      useReturnTypeSchema = true
+  )
   public void revokeRoleMembershipApplicationOrOrganization(
+      @Parameter(description = "Enter the value for the ownerType for which you want to revoke the role. Using " +
+          "`global` will revoke the administrator role.", required = true)
       @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the internalId associated with the ownerType specified above.", required = true)
       @PathParam("internalOwnerId") String internalOwnerId,
+      @Parameter(description = "Enter the roleId for the role to be revoked.", required = true)
       @PathParam("roleId") String roleId,
+      @Parameter(description = "Enter the value for memberType, to specify a user or a user group.", required = true)
       @PathParam("memberType") MemberType memberType,
+      @Parameter(description = "Enter the value for memberName. This can be a username or group name depending upon " +
+          "the value of memberType above.", required = true)
       @PathParam("memberName") String memberName)
   {
     membershipMappingService.revokeRoleMembership(ownerType, internalOwnerId, roleId, memberType, memberName);
@@ -106,10 +206,26 @@ public class ApiRoleMembershipResource
   @DELETE
   @Path(GLOBAL_OR_REPOSITORY_CONTAINER)
   @Audited(AuditEvent.REVOKE_ROLE_MEMBERSHIP)
+  @Operation(description = "Use this method to revoke roles globally or on all repositories." +
+      "\n" +
+      "\n" +
+      "Permissions required: Edit system configuration and users for a global context or view IQ elements for a " +
+      "non-global context")
+  @ApiResponse(
+      responseCode = "204",
+      description = "The specified role has been revoked from the user or user group.",
+      useReturnTypeSchema = true
+  )
   public void revokeRoleMembershipGlobalOrRepositoryContainer(
+      @Parameter(description = "Enter the value for ownerType. Using " +
+          "`global` will revoke the administrator role.", required = true)
       @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the roleId for the role to be revoked.", required = true)
       @PathParam("roleId") String roleId,
+      @Parameter(description = "Enter the value for memberType, to specify a user or a user group.", required = true)
       @PathParam("memberType") MemberType memberType,
+      @Parameter(description = "Enter the value for memberName. This can be a username or group name depending upon " +
+          "the value of memberType above.", required = true)
       @PathParam("memberName") String memberName)
   {
     membershipMappingService.revokeRoleMembership(ownerType, null, roleId, memberType, memberName);
--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiRoleResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiRoleResource.java
@@ -17,12 +17,26 @@ import com.sonatype.insight.brain.api.v2.dto.ApiRoleListDTO;
 import com.sonatype.insight.brain.security.RoleService;
 
 import com.codahale.metrics.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * @since 1.70
  */
 @Named
 @Timed
+@Tag(name = "Roles",
+    description = "Roles provide sets of permissions that grant access to the functionality in the user interface," +
+        "through integrations, and when using REST APIs." +
+        "\n" +
+        "\n" +
+        "Permissions are granted by assigning users or groups to the " +
+        "system roles or at the various levels in the organizational hierarchy: root organization, " +
+        "repository managers, and applications and organizations." +
+        "\n" +
+        "\n" +
+        "Use this REST API to retrieve role IDs, role names and descriptions.")
 @Path(value = PublicApiPaths.ROLE_RESOURCE_PATH_V2)
 public class ApiRoleResource
 {
@@ -35,6 +49,13 @@ public class ApiRoleResource
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
+  @Operation(description = "Use this method to view the role IDs, role names and descriptions." +
+      "\n" +
+      "\n" +
+      "Permissions required: View All Roles")
+  @ApiResponse(responseCode = "200",
+      description = "The response contains the role IDs, role names and descriptions.",
+      useReturnTypeSchema = true)
   public ApiRoleListDTO getRoles() {
     return roleService.getRolesAsApiRoleListDTO();
   }

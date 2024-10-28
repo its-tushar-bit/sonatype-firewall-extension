74dad5859540633c548882e9740473831e82bfc7
Omar Barra <obarra@sonatype.com>
2024-10-28 08:33:54 -0300

    CLM-31695 new SCM telemetry attributes (#12449)
    Co-authored-by: Collin Peters <cpeters@sonatype.com>
    Co-authored-by: vladt <vladt@sonatype.com>

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/git/ScmRepoVisibilityService.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/git/ScmRepoVisibilityService.java
@@ -61,7 +61,7 @@ public class ScmRepoVisibilityService
     return gitRepositoryInfo.provider.isScmSecured(gitRepositoryInfo.normalizedRepositoryUrl);
   }
 
-  private boolean isPrivateRepository(final GitRepositoryInfo gitRepositoryInfo) {
+  public boolean isPrivateRepository(final GitRepositoryInfo gitRepositoryInfo) {
     GitApiClient client = gitClientFactory.createApiClient(gitRepositoryInfo);
     try {
       return client.isRepositoryPrivate();
--- insight-brain-service/src/main/java/com/sonatype/insight/brain/git/ScmUserMatchingService.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/git/ScmUserMatchingService.java
@@ -38,7 +38,7 @@ import org.slf4j.LoggerFactory;
 @Singleton
 public class ScmUserMatchingService
 {
-  private static final Logger log = LoggerFactory.getLogger(SourceControlScanService.class);
+  private static final Logger log = LoggerFactory.getLogger(ScmUserMatchingService.class);
 
   private final GitClientFactory gitClientFactory;
 

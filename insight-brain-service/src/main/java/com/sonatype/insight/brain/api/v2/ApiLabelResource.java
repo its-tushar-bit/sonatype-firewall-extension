7962ec556b901f11514755c9431c7f6d31eb90dc
skatre <38050374+skatre@users.noreply.github.com>
2025-01-13 10:38:25 -0600

    CLM-26191 Component Labels Swagger (#13023)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiLabelResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiLabelResource.java
@@ -6,7 +6,6 @@
 package com.sonatype.insight.brain.api.v2;
 
 import java.util.List;
-
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.Consumes;
@@ -33,6 +32,11 @@ import com.sonatype.insight.brain.product.license.ProductLicenseEnforcementPoint
 import com.sonatype.insight.license.model.LicensedFeature;
 
 import com.codahale.metrics.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * @since 1.90
@@ -41,6 +45,12 @@ import com.codahale.metrics.annotation.Timed;
 @Timed
 @Path(PublicApiPaths.LABEL_RESOURCE_PATH)
 @ProductLicenseEnforcementPoint(LicensedFeature.COMPONENT_LABELS)
+@Tag(name = "Component Labels",
+    description = "Use this REST API to manage component labels for applications, organizations and repositories." +
+        "\n" +
+        "\n" +
+        "Component Labels can be used as attributes of a component at the time of creating policies. " +
+        "A policy violation can be triggered based on the component label.")
 public class ApiLabelResource
 {
   private final LabelService labelService;
@@ -51,14 +61,43 @@ public class ApiLabelResource
   }
 
   /**
-   * @param inherit boolean if {@code true} the returned list will include labels inherited from organization
-   *          hierarchy, default is {@code false}
+   * @param inherit boolean if {@code true} the returned list will include labels inherited from organization hierarchy,
+   *                default is {@code false}
    */
   @GET
   @Produces(MediaType.APPLICATION_JSON)
+  @Operation(description = "Use this method to retrieve the details for component labels for an application, " +
+      "organization or a repository." +
+      "\n" +
+      "\n" +
+      "Permissions required: View IQ Elements",
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "The response contains an array of component label descriptions for the application, " +
+                  "organization or repository, as selected in the request. Each label description contains:" +
+                  "<ul>" +
+                  "<li>`id` is the internal identifier assigned to the label.</li>" +
+                  "<li>`label` is the identifying name of the label, for e.g. 'Architecture-Deprecated'.</li>" +
+                  "<li>`description` is additional information describing the label.</li>" +
+                  "<li>`color` is the color assigned to the component label.</li>" +
+                  "<li>`ownerId` is the identifier for the ownerType selected in the request.</li>" +
+                  "<li>`ownerType` indicates if the label is for the application, organization or repository,  " +
+                  "as selected in the request.</li>" +
+                  "</ul>" +
+                  "If the request parameter `inherit` is set to `true` the response contains a description " +
+                  "of component labels that are inherited from the parent. The inherited labels can be identified " +
+                  "by the value of `ownerId` and `ownerType`.",
+              useReturnTypeSchema = true)
+      })
+
   public List<ApiLabelDTO> getLabels(
+      @Parameter(description = "Select the `ownerType` for which you want to retrieve the component label " +
+          "information.",
+          required = true)
       @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the id of the application, organization or the repository.", required = true)
       @PathParam("ownerId") String ownerId,
+      @Parameter(description = "Set to `true` to retrieve inherited component labels.")
       @QueryParam("inherit") @DefaultValue("false") boolean inherit)
   {
     return labelService.getLabels(ownerType, ownerId, inherit);
@@ -71,8 +110,41 @@ public class ApiLabelResource
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("applicable")
-  public ApplicableLabels getApplicableLabels(@PathParam("ownerType") OwnerType ownerType,
-                                              @PathParam("ownerId") String ownerId)
+  @Operation(description = "Use this method to retrieve all component labels that are applicable to the specified " +
+      "application, organization or repository." +
+      "\n" +
+      "\n" +
+      "Permissions required: View IQ Elements",
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description =
+                  "The response contains descriptions for all component labels that are applicable to the specified " +
+                      "owner. These include all component labels that are assigned and inherited. " +
+                      "The response includes:" +
+                      "<ul>" +
+                      "<li>`ownerId` is the identifier for the owner.</li>" +
+                      "<li>`ownerName` is the name for the owner.</li>" +
+                      "<li>`ownerType` indicates if the labels are for an application, organization or " +
+                      "repository.</li> " +
+                      "<li>`labels` is the component labels for this owner.</li>" +
+                      "</ul>" +
+                      "Each label includes " +
+                      "<ul>" +
+                      "<li>`id` is the internal identifier assigned to the label.</li>" +
+                      "<li>`label` is the identifying name of the label, for e.g. 'Architecture-Deprecated'.</li>" +
+                      "<li>`description` is additional information describing the label.</li>" +
+                      "<li>`color` is the color assigned to the component label.</li>" +
+                      "<li>`ownerId` is the identifier for the ownerType selected in the request.</li>" +
+                      "<li>`ownerType` indicates if the label is for the application, organization or repository, " +
+                      "as selected in the request.</li>" +
+                      "</ul>",
+              useReturnTypeSchema = true)
+      })
+  public ApplicableLabels getApplicableLabels(
+      @Parameter(description = "Select the ownerType to retrieve the component label information for.", required = true)
+      @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the id for the application, organization or repository", required = true)
+      @PathParam("ownerId") String ownerId)
   {
     return labelService.getApplicableLabels(ownerType, ownerId);
   }
@@ -83,9 +155,28 @@ public class ApiLabelResource
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("applicable/context/{labelId}")
-  public ApplicableContext getApplicableContexts(@PathParam("ownerType") OwnerType ownerType,
-                                                 @PathParam("ownerId") String ownerId,
-                                                 @PathParam("labelId") String labelId)
+  @Operation(description = "Use this method to retrieve the hierarchy of owners (applications, organizations, " +
+      "repositories) in which the label can be applied." +
+      "\n" +
+      "\n" +
+      "Permissions required: Edit IQ Elements",
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "The response contains:" +
+                  "<ul>" +
+                  "<li>`id` is the id of the selected owner.</li>" +
+                  "<li>`name` is the name of the selected owner.</li>" +
+                  "<li>`type` is the type of the selected owner e.g. application, organization or repository.</li>" +
+                  "<li>`children` is an array of the child owners in the hierarchy.</li>",
+              useReturnTypeSchema = true)
+      })
+  public ApplicableContext getApplicableContexts(
+      @Parameter(description = "Select the ownerType.")
+      @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the ownerId")
+      @PathParam("ownerId") String ownerId,
+      @Parameter(description = "Enter the labelId")
+      @PathParam("labelId") String labelId)
   {
     return labelService.getApplicableContexts(ownerType, ownerId, labelId);
   }
@@ -94,9 +185,25 @@ public class ApiLabelResource
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   @Audited(AuditEvent.CREATE_LABEL)
+  @Operation(description = "Use this method to create and assign a component label to an application, organization " +
+      "or repository." +
+      "\n" +
+      "\n" +
+      "Permissions required: Edit IQ Elements",
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "The response contains label details sent in the request and the `id` for the label " +
+                  "created.",
+              useReturnTypeSchema = true)
+      })
   public ApiLabelDTO addLabel(
+      @Parameter(description = "Select the ownerType to which the label will be assigned.")
       @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the id for the selected ownerType.")
       @PathParam("ownerId") String ownerId,
+      @RequestBody(description = "Specify a label name, description and color for the label. Valid values for " +
+          "color are `light-red` , `light-green` , `light-blue` , `light-purple`, `dark-red` , `dark-green` , " +
+          "`dark-blue` , `dark-purple` , `orange` , `yellow`. Do not enter value for the `id` field.")
       ApiLabelDTO labelDTO)
   {
     return labelService.addLabel(ownerType, ownerId, labelDTO);
@@ -106,9 +213,26 @@ public class ApiLabelResource
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   @Audited(AuditEvent.UPDATE_LABEL)
+  @Operation(description = "Use this method to update an existing component label for an application, organization " +
+      "or repository." +
+      "\n" +
+      "\n" +
+      "Permissions required: Edit IQ Elements",
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "The response contains the label details sent in the update request.",
+              useReturnTypeSchema = true)
+      }
+  )
   public ApiLabelDTO updateLabel(
+      @Parameter(description = "Select the ownerType for which the label will be updated.")
       @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the id for the selected ownerType.")
       @PathParam("ownerId") String ownerId,
+      @RequestBody(description = "Specify the new values for label name, description, color and the corresponding " +
+          "label id for the component label to be updated. Valid values for color are `light-red` , " +
+          "`light-green` , `light-blue` , `light--purple`, `dark-red` , `dark-green` ,`dark-blue` , `dark-purple` ," +
+          "`orange` , `yellow`.")
       ApiLabelDTO labelDTO)
   {
     return labelService.updateLabel(ownerType, ownerId, labelDTO);
@@ -117,9 +241,21 @@ public class ApiLabelResource
   @DELETE
   @Path("{labelId}")
   @Audited(AuditEvent.DELETE_LABEL)
-  public void deleteLabel(@PathParam("ownerType") OwnerType ownerType,
-                          @PathParam("ownerId") String ownerId,
-                          @PathParam("labelId") String labelId)
+  @Operation(description = "Use this method to delete an existing component label." +
+      "\n" +
+      "\n" +
+      "Permissions required: Edit IQ Elements",
+      responses = {
+          @ApiResponse(responseCode = "204",
+              description = "Component label deleted successfully.")
+      })
+  public void deleteLabel(
+      @Parameter(description = "Select the ownerType for which the label will be deleted.")
+      @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the id for the selected ownerType.")
+      @PathParam("ownerId") String ownerId,
+      @Parameter(description = "Enter the id for the label to be deleted.")
+      @PathParam("labelId") String labelId)
   {
     labelService.deleteLabel(ownerType, ownerId, labelId);
   }

64f78271c097eacdec5ba908c076e5d894a844bd
skatre <38050374+skatre@users.noreply.github.com>
2025-01-17 14:19:27 -0600

    CLM-26191 Policies Swagger (#13064)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiPolicyResourceV2.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiPolicyResourceV2.java
@@ -19,15 +19,19 @@ import com.sonatype.insight.brain.product.license.ProductLicenseEnforcementPoint
 import com.sonatype.insight.license.model.LicensedFeature;
 
 import com.codahale.metrics.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
- *
  * @since 1.12.0
  */
 @Named
 @Timed
 @Path(PublicApiPaths.POLICY_RESOURCE_PATH)
 @ProductLicenseEnforcementPoint(LicensedFeature.POLICY_MANAGEMENT)
+@Tag(name = "Policies",
+    description = "Use this REST API to retrieve details on all existing policies in your instance of Lifecycle.")
 public class ApiPolicyResourceV2
 {
   private final ApiPolicyService apiPolicyService;
@@ -39,6 +43,24 @@ public class ApiPolicyResourceV2
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
+  @Operation(description = "Use this method to retrieve all existing policies." +
+      "\n" +
+      "\n" +
+      "Permissions required: View IQ Elements",
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "The response contains a `policies` object which contains a list of:" +
+                  "<ul>" +
+                  "<li>`id` is the policyId. It can be used in the GET method for endpoint /api/v2/policyViolations " +
+                  "to retrieve policy violations for the policy, and other similar operations.</li>" +
+                  "<li>`name` is the name of the policy.</li>" +
+                  "<li>`ownerType` is the ownerType.</li>" +
+                  "<li>`ownerId` is the internal id associated with the ownerType.</li>" +
+                  "<li>`threatLevel` is the threat level that is set for this policy.</li>" +
+                  "<li>`policyType` indicates the type for the policy. Values can be `Security`, `License`, " +
+                  "`Quality` or `Other`.</li>",
+              useReturnTypeSchema = true)
+      })
   public ApiPolicyListDTO getPolicies() {
     return apiPolicyService.getPolicies();
   }

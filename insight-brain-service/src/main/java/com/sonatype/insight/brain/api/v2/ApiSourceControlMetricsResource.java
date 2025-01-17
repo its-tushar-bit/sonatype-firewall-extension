6414b8b9066da9ead8732f1d398443d6a1f1923e
skatre <38050374+skatre@users.noreply.github.com>
2025-01-17 14:19:49 -0600

    CLM-26191 Source Control Metrics swagger (#13066)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiSourceControlMetricsResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiSourceControlMetricsResource.java
@@ -22,6 +22,10 @@ import com.sonatype.insight.brain.product.license.ProductLicenseEnforcementPoint
 import com.sonatype.insight.license.model.LicensedFeature;
 
 import com.codahale.metrics.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * @since 1.97
@@ -30,6 +34,8 @@ import com.codahale.metrics.annotation.Timed;
 @Timed
 @Path(value = PublicApiPaths.SOURCE_CONTROL_METRICS_PATH_V2)
 @ProductLicenseEnforcementPoint(LicensedFeature.SOURCE_CONTROL)
+@Tag(name = "Source Control Metrics",
+    description = "Use this REST API to view the response times of a source control evaluation.")
 public class ApiSourceControlMetricsResource
 {
   private static final String OWNER_TYPE = "{ownerType:application|organization}";
@@ -50,8 +56,27 @@ public class ApiSourceControlMetricsResource
   @Produces(MediaType.APPLICATION_JSON)
   @Path(BY_OWNER)
   @HasFeature(SystemConfigurationPropertyFeature.SAAS_LIFECYCLE_SCM_ENABLED)
+  @Operation(description = "Use this method to view the source control pull request metrics." +
+      "\n" +
+      "\n" +
+      "Permissions required: View IQ Elements",
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "The response contains `results` which is a list of elements, each including: " +
+                  "<ul>" +
+                  "<li>`startTime` indicates the start time of the pull request.</li>" +
+                  "<li>`title` indicates the title of the pull request.</li>" +
+                  "<li>`exceptionThrown` indicates if the pull request caused an exception.</li>" +
+                  "<li>`successful` indicates if the pull request was successful.</li>" +
+                  "<li>`totalTime` indicates the total time taken to complete the pull request.</li>" +
+                  "<li>`reasoning` indicates the summary of the outcome of the pull request.</li>" +
+                  "</ul>",
+              useReturnTypeSchema = true)
+      })
   public ApiPullRequestResults getSourceControl(
+      @Parameter(description = "Select the ownerType for the pull requests.")
       @PathParam("ownerType") OwnerType ownerType,
+      @Parameter(description = "Enter the ownerId corresponding to the ownerType.")
       @PathParam("internalOwnerId") String internalOwnerId)
   {
     return sourceControlService.getSourceControlMetricsForApplication(ownerType, internalOwnerId);

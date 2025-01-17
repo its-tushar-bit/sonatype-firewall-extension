7290bced8044719b900576202ec25f9f2d9a1af6
skatre <38050374+skatre@users.noreply.github.com>
2025-01-17 12:27:34 -0600

    CLM-26191 SPDX SBOM swagger (#13061)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiSpdxResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiSpdxResource.java
@@ -25,6 +25,9 @@ import com.sonatype.insight.brain.product.license.ProductLicenseEnforcementPoint
 import com.sonatype.insight.license.model.LicensedFeature;
 
 import com.codahale.metrics.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
@@ -35,7 +38,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Singleton
 @Path(PublicApiPaths.SPDX_RESOURCE_PATH)
 @ProductLicenseEnforcementPoint(LicensedFeature.SBOM_REPORTS)
-@Tag(name = "SPDX")
+@Tag(name = "SPDX",
+    description = "Use this REST API to generate SPDX SBOMs in XML or JSON formats.")
 public class ApiSpdxResource
 {
   static final String GET_BY_STAGE_PATH = "{applicationId}/stages/{stageId}";
@@ -57,11 +61,30 @@ public class ApiSpdxResource
   @Path(GET_BY_STAGE_PATH)
   @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM})
   @Audited(AuditEvent.EXPORT_APPLICATION_COMPOSITION_REPORT)
+  @Operation(description = "Use this method to generate SBOM(s) based on the latest application evaluation " +
+      "report at the specified stage." +
+      "\n" +
+      "\n" +
+      "Permissions required: View IQ Elements",
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "The requested SBOM(s)."
+          )
+      }
+  )
   public Response getLatestForStage(
+      @Parameter(description = "Enter the applicationId for the application you want to generate the SBOM(s).",
+          required = true)
       @PathParam("applicationId") String applicationId,
+      @Parameter(description = "Specify the stageId for the application evaluation. Allowed values are `develop`, " +
+          "`build`, `stage-release`, `release` and `operate`.")
       @PathParam("stageId") String stageId,
+      @Parameter(description = "Enter the format for the SBOM(s) to be generated.")
       @DefaultValue(DEFAULT_SPDX_FORMAT) @QueryParam("format") String format,
+      @Parameter(description = "Set to `true` to generate an equivalent CycloneDx SBOM. Both SBOMs will be combined " +
+          "as a tar.gz archive.")
       @DefaultValue("false") @QueryParam("generateCycloneDx") boolean generateCycloneDx,
+      @Parameter(description = "Enter the desired SPDX version.")
       @DefaultValue(DEFAULT_SPDX_VERSION) @QueryParam("spdxVersion") String spdxVersion)
   {
     return apiSpdxService.getLatestForStage(applicationId, stageId, format, generateCycloneDx, spdxVersion);
@@ -71,11 +94,27 @@ public class ApiSpdxResource
   @Path(GET_BY_REPORT_PATH)
   @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM})
   @Audited(AuditEvent.EXPORT_APPLICATION_COMPOSITION_REPORT)
+  @Operation(description = "Use this method to generate SBOM(s) based on a specific application scan." +
+      "\n" +
+      "\n" +
+      "Permissions required: View IQ Elemets",
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "The requested SBOM(s).")
+      })
   public Response getByScanId(
+      @Parameter(description = "Enter the applicationId for the application you want to generate the SBOM(s).",
+          required = true)
       @PathParam("applicationId") String applicationId,
+      @Parameter(description = "Enter the scanId of the application scan.",
+          required = true)
       @PathParam("scanId") String scanId,
+      @Parameter(description = "Enter the format for the SBOM(s) to be generated.")
       @DefaultValue(DEFAULT_SPDX_FORMAT) @QueryParam("format") String format,
+      @Parameter(description = "Set to `true` to generate an equivalent CycloneDx SBOM. Both SBOMs will be combined " +
+          "as a tar.gz archive.")
       @DefaultValue("false") @QueryParam("generateCycloneDx") boolean generateCycloneDx,
+      @Parameter(description = "Enter the desired SPDX version.")
       @DefaultValue(DEFAULT_SPDX_VERSION) @QueryParam("spdxVersion") String spdxVersion)
   {
     return apiSpdxService.getByScanId(applicationId, scanId, format, generateCycloneDx, spdxVersion);

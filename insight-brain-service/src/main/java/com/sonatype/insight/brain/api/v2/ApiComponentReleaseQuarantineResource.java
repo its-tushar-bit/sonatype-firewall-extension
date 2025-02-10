896bfcff00aeeb3bbf0ff99e524fe0c9f3700574
skatre <38050374+skatre@users.noreply.github.com>
2025-02-10 11:24:37 -0600

    CLM-26191 Repositories Swagger (#13172)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiComponentReleaseQuarantineResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiComponentReleaseQuarantineResource.java
@@ -23,6 +23,11 @@ import com.sonatype.insight.brain.product.license.ProductLicenseEnforcementPoint
 import com.sonatype.insight.license.model.LicensedFeature;
 
 import com.codahale.metrics.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * @since 1.78
@@ -30,6 +35,8 @@ import com.codahale.metrics.annotation.Timed;
 @Named
 @Timed
 @Path(PublicApiPaths.COMPONENT_QUARANTINE_RELEASE_PATH_V2)
+@Tag(name = "Repositories",
+    description = "Use this REST API to manage quarantined components.")
 @ProductLicenseEnforcementPoint(LicensedFeature.FIREWALL)
 public class ApiComponentReleaseQuarantineResource
 {
@@ -46,9 +53,28 @@ public class ApiComponentReleaseQuarantineResource
   @Consumes(MediaType.TEXT_PLAIN)
   @Produces(MediaType.APPLICATION_JSON)
   @Audited(AuditEvent.RELEASE_QUARANTINE)
+  @Operation(description = "Use this method to release a component from quarantine by providing the `quarantineId`." +
+      "\n" +
+      "\n" +
+      "Use the GET method of the Reports REST API to retrieve the `quarantineId` for the quarantined component." +
+      "\n" +
+      "\n" +
+      "Permissions required: Edit IQ Elements",
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "The response contains:" +
+                  "<ul>" +
+                  "<li>The quarantined component details for the component being released.</li>" +
+                  "<li>The quarantine and release times.</li>" +
+                  "<li>A list of policy violations that were waived to release the component from quarantine.</li>" +
+                  "<ul>",
+              useReturnTypeSchema = true)
+      })
   public ApiComponentReleasedFromQuarantineDTO releaseQuarantineWithoutReEval(
+      @Parameter(description = "Enter the component `quarantineId`.")
       @PathParam("quarantineId") final String quarantineId,
-      final String comment)
+      @RequestBody(description = "Enter a waiver comment for releasing the component from quarantine.",
+          required = true) final String comment)
   {
     return componentReleaseQuarantineServiceV2.releaseQuarantineWithoutReEval(quarantineId, comment);
   }
--- insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiRepositoryPathResource.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/api/v2/ApiRepositoryPathResource.java
@@ -6,7 +6,6 @@
 package com.sonatype.insight.brain.api.v2;
 
 import java.util.List;
-
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.Consumes;
@@ -23,12 +22,15 @@ import com.sonatype.insight.brain.product.license.ProductLicenseEnforcementPoint
 import com.sonatype.insight.license.model.LicensedFeature;
 
 import com.codahale.metrics.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 /**
- * At the time of this writing, there is no public API documentation for this.  The main
- * purpose of this public api endpoint is to be used by NXRM npm audit.  This could be
- * publicly documented at a later date, if there was customer interest and the endpoint
- * isn't going to be abused.
+ * At the time of this writing, there is no public API documentation for this.  The main purpose of this public api
+ * endpoint is to be used by NXRM npm audit.  This could be publicly documented at a later date, if there was customer
+ * interest and the endpoint isn't going to be abused.
  *
  * @since 1.125
  */
@@ -55,9 +57,24 @@ public class ApiRepositoryPathResource
   @Path(PATHNAMES_PATH)
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
+  @Operation(description = "Use this method to retrieve the pathnames of the repository components and the " +
+      "corresponding quarantine status." +
+      "\n" +
+      "\n" +
+      "Permissions required: View IQ Elements",
+      responses = {
+          @ApiResponse(
+              responseCode = "200",
+              description = "The response contains a list of repository component paths and their quarantine status.",
+              useReturnTypeSchema = true
+          )
+      })
   public ApiRepositoryPathResponseDTO getQuarantinedByPath(
+      @Parameter(description = "Enter the repository manager instance ID.")
       @PathParam("repositoryManagerInstanceId") String repositoryManagerInstanceId,
+      @Parameter(description = "Enter the repository public ID.")
       @PathParam("repositoryPublicId") String repositoryPublicId,
+      @RequestBody(description = "Specify the pathnames.", required = true)
       List<String> pathnames)
   {
     return repositoryPathService.getQuarantinedByPathnames(repositoryManagerInstanceId, repositoryPublicId, pathnames);

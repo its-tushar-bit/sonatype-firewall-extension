679389518c1aae0ee6b1298c68e01909deaa665e
Anjali Arora <116115595+anjaliarora06@users.noreply.github.com>
2025-08-05 15:47:40 -0400

    SDEV-1915 added new line at EOF to schema_incremental_0405.sql (#14348)

--- insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0405.sql
+++ insight-brain-db/src/main/resources/db/insight_brain_ods/schema_incremental_0405.sql
@@ -4,3 +4,4 @@
 
 ALTER TABLE application_tag DROP CONSTRAINT application_tag_app_fk;
 ALTER TABLE application_tag ADD CONSTRAINT application_tag_app_fk FOREIGN KEY (application_id) REFERENCES application (application_id) ON DELETE CASCADE;
+

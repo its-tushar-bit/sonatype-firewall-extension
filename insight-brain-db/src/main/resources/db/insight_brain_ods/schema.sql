0bd54abf84da055641b7866484deef656062650d
Alex Chen <alexanderfschen@gmail.com>
2025-09-05 09:00:20 -0700

    NEXUS-46998: Create tables to keep track of components being evaluated (#14402)

--- insight-brain-db/src/main/resources/db/insight_brain_ods/schema.sql
+++ insight-brain-db/src/main/resources/db/insight_brain_ods/schema.sql
@@ -975,6 +975,30 @@ CREATE INDEX source_control_event_instance_id_idx ON source_control_event(instan
 CREATE INDEX source_control_event_create_time_idx ON source_control_event(create_time);
 CREATE INDEX source_control_event_event_status_idx ON source_control_event(event_status);
 
+CREATE TABLE reevaluate_cascade_request
+(
+  reevaluate_cascade_request_id varchar(50) NOT NULL,
+  component_reference_hash varchar(50) NOT NULL,
+  created_at timestamp DEFAULT now() NOT NULL,
+  created_by_username varchar(255) NOT NULL,
+  owner_id varchar(50) NULL,
+  CONSTRAINT reevaluate_cascade_request_pk PRIMARY KEY (reevaluate_cascade_request_id)
+);
+
+CREATE TABLE reevaluate_cascade_progress
+(
+  reevaluate_cascade_progress_id varchar(50) NOT NULL,
+  reevaluate_cascade_request_id varchar(50) NOT NULL,
+  repository_id varchar(50) NOT NULL,
+  repository_component_id varchar(50) NOT NULL,
+  quarantined boolean NOT NULL DEFAULT FALSE,
+  status varchar(20) NOT NULL,
+  CONSTRAINT reevaluate_cascade_progress_pk PRIMARY KEY (reevaluate_cascade_progress_id),
+  CONSTRAINT reevaluate_cascade_request_id_fk FOREIGN KEY (reevaluate_cascade_request_id) REFERENCES reevaluate_cascade_request(reevaluate_cascade_request_id) ON DELETE CASCADE,
+  CONSTRAINT repository_id_fk FOREIGN KEY (repository_id) REFERENCES repository(repository_id) ON DELETE CASCADE,
+  CONSTRAINT repository_component_id_fk FOREIGN KEY (repository_component_id) REFERENCES repository_component(repository_component_id) ON DELETE CASCADE
+);
+
 -- Since 1.114
 CREATE TABLE source_control_pull_request
 (

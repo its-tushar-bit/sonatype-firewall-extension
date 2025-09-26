a1054ae641b5e7cb3f89e47da01f291aadad5924
Richard Mealing <rmealing@sonatype.com>
2025-09-26 11:33:37 +0100

    Let the admin app handle iterating storage copying - CLM-36241 (#14591)

--- insight-brain-service/src/main/java/com/sonatype/insight/brain/service/CopyStorageTask.java
+++ insight-brain-service/src/main/java/com/sonatype/insight/brain/service/CopyStorageTask.java
@@ -10,22 +10,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.regex.Pattern;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor.AbortPolicy;
+import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import com.sonatype.insight.brain.scheduler.TaskScheduler;
 import com.sonatype.insight.brain.service.config.StorageConfig.DataStoreType;
-import com.sonatype.insight.brain.tenancy.AllTenantsJob;
-import com.sonatype.insight.brain.tenancy.Tenant;
-import com.sonatype.insight.brain.tenancy.TenantThreadLocal;
-import com.sonatype.insight.brain.tenancy.TenantUtil;
+import com.sonatype.insight.brain.tenancy.MtiqBatchJob;
+import com.sonatype.insight.brain.tenancy.TenantThreadPoolExecutor;
 import com.sonatype.insight.error.exception.BadRequestException;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.dropwizard.servlets.tasks.Task;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,13 +36,11 @@ import org.slf4j.LoggerFactory;
 @DisallowConcurrentExecution
 public class CopyStorageTask
     extends Task
-    implements InsightJob, AllTenantsJob
+    implements InsightJob, MtiqBatchJob
 {
   private static final Logger log = LoggerFactory.getLogger(CopyStorageTask.class);
 
   private static final String PATH = "copyStorage";
-
-  private static final String TENANT_SLUG_REGEX_PARAMETER = "tenant";
 
   private static final String FROM_PARAMETER = "from";
 
@@ -52,33 +52,28 @@ public class CopyStorageTask
 
   private final CopyStorageService copyStorageService;
 
-  private final TenantUtil tenantUtil;
+  private final TenantThreadPoolExecutor tenantThreadPoolExecutor;
 
   @Inject
   public CopyStorageTask(
       final TaskScheduler taskScheduler,
-      final CopyStorageService copyStorageService,
-      final TenantUtil tenantUtil)
+      final CopyStorageService copyStorageService)
   {
     super(PATH);
     this.taskScheduler = taskScheduler;
     this.copyStorageService = copyStorageService;
-    this.tenantUtil = tenantUtil;
+    tenantThreadPoolExecutor = new TenantThreadPoolExecutor(1, 1, 5L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(),
+        new ThreadFactoryBuilder().setNameFormat("CopyStorage-%d").build(), new AbortPolicy(), "copy_storage",
+        getClass().getSimpleName());
   }
 
   @Override
   public void execute(final Map<String, List<String>> map, final PrintWriter printWriter) throws Exception {
-    boolean isMultiTenant = tenantUtil.isMultiTenant();
     String from = getFirstNotNull(map, FROM_PARAMETER).toUpperCase(Locale.ROOT);
     String to = getFirstNotNull(map, TO_PARAMETER).toUpperCase(Locale.ROOT);
 
     // Although CopyStorageService does these checks internally,
     // we repeat them here to avoid scheduling the job if we don't need to
-    String tenantSlugRegex = null;
-    if (isMultiTenant) {
-      tenantSlugRegex = getTenantSlugRegex(map);
-      Pattern.compile(tenantSlugRegex);
-    }
     DataStoreType fromDataStoreType = DataStoreType.valueOf(from);
     DataStoreType toDataStoreType = DataStoreType.valueOf(to);
     copyStorageService.checkSupported(fromDataStoreType);
@@ -87,31 +82,10 @@ public class CopyStorageTask
     copyStorageService.checkFromAndToAreDifferent(fromDataStoreType, toDataStoreType);
 
     Map<String, String> parameters = new HashMap<>();
-    if (isMultiTenant) {
-      parameters.put(TENANT_SLUG_REGEX_PARAMETER, tenantSlugRegex);
-    }
     parameters.put(FROM_PARAMETER, from);
     parameters.put(TO_PARAMETER, to);
 
     taskScheduler.scheduleOneTimeTask(this, parameters);
-  }
-
-  private String getTenantSlugRegex(final Map<String, List<String>> map) {
-    String tenantSlugRegex = getFirst(map, TENANT_SLUG_REGEX_PARAMETER);
-    if (tenantSlugRegex != null) {
-      return tenantSlugRegex;
-    }
-    Tenant tenant = TenantThreadLocal.getTenant();
-    if (Tenant.GLOBAL_TENANT.equals(tenant)) {
-      return ".*";
-    }
-    return tenant.tenantSlug;
-  }
-
-  private String getFirst(final Map<String, List<String>> map, final String key) {
-    return map.getOrDefault(key, List.of()).stream()
-        .findFirst()
-        .orElse(null);
   }
 
   private String getFirstNotNull(final Map<String, List<String>> map, final String key) {
@@ -122,39 +96,19 @@ public class CopyStorageTask
     return value;
   }
 
+  private String getFirst(final Map<String, List<String>> map, final String key) {
+    return map.getOrDefault(key, List.of()).stream()
+        .findFirst()
+        .orElse(null);
+  }
+
   @Override
-  public void executeForTenant(final JobExecutionContext context, final Tenant tenant) {
-    if (tenantUtil.isMultiTenant()) {
-      executeForMultiTenantMode(context, tenant);
-    }
-    else {
-      executeForSingleTenantMode(context);
-    }
-  }
-
-  private void executeForMultiTenantMode(final JobExecutionContext context, final Tenant tenant) {
-    String tenantSlugRegex = context.getMergedJobDataMap().getString(TENANT_SLUG_REGEX_PARAMETER);
-    Pattern tenantSlugPattern = Pattern.compile(tenantSlugRegex);
-
-    if (tenantSlugPattern.matcher(tenant.tenantSlug).matches()) {
-      log.info("Running '{}' job for tenant '{}' as their slug matches '{}'.", JOB_NAME, tenant.tenantSlug,
-          tenantSlugRegex);
-      doExecute(context);
-    }
-    else {
-      log.info("Skipping '{}' job for tenant '{}' as their slug does not match '{}'.", JOB_NAME, tenant.tenantSlug,
-          tenantSlugRegex);
-    }
-    log.info("Completed '{}' job for tenant '{}'.", JOB_NAME, tenant.tenantSlug);
-  }
-
-  private void executeForSingleTenantMode(final JobExecutionContext context) {
-    log.info("Running '{}' job.", JOB_NAME);
-    doExecute(context);
-    log.info("Completed '{}' job.", JOB_NAME);
+  public void execute(final JobExecutionContext context) throws JobExecutionException {
+    tenantThreadPoolExecutor.submit(() -> doExecute(context));
   }
 
   private void doExecute(final JobExecutionContext context) {
+    log.info("Running '{}' job.", JOB_NAME);
     String from = context.getMergedJobDataMap().getString(FROM_PARAMETER);
     String to = context.getMergedJobDataMap().getString(TO_PARAMETER);
 
@@ -162,6 +116,7 @@ public class CopyStorageTask
     DataStoreType toDataStoreType = DataStoreType.valueOf(to);
 
     copyStorageService.execute(fromDataStoreType, toDataStoreType);
+    log.info("Completed '{}' job.", JOB_NAME);
   }
 
   @Override
--- insight-brain-service/src/test/java/com/sonatype/insight/brain/service/CopyStorageTaskTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/service/CopyStorageTaskTest.java
@@ -7,36 +7,24 @@ package com.sonatype.insight.brain.service;
 
 import java.util.List;
 import java.util.Map;
-import java.util.regex.PatternSyntaxException;
 
 import com.sonatype.insight.brain.scheduler.TaskScheduler;
 import com.sonatype.insight.brain.service.config.StorageConfig.DataStoreType;
-import com.sonatype.insight.brain.tenancy.AllTenantsJob;
-import com.sonatype.insight.brain.tenancy.Tenant;
-import com.sonatype.insight.brain.tenancy.TenantUtil;
+import com.sonatype.insight.brain.tenancy.MtiqBatchJob;
 import com.sonatype.insight.error.exception.BadRequestException;
 
 import com.google.inject.Binder;
 import com.google.inject.Inject;
 import io.dropwizard.servlets.tasks.Task;
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.quartz.JobBuilder;
-import org.quartz.JobDataMap;
-import org.quartz.JobExecutionContext;
 
-import static com.sonatype.insight.brain.tenancy.TenantTestHelper.testAsNewTenant;
-import static com.sonatype.insight.brain.tenancy.TenantTestHelper.testAsTenant;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
 
 public class CopyStorageTaskTest
     extends AbstractComponentTest
@@ -47,9 +35,6 @@ public class CopyStorageTaskTest
   @Mock
   private CopyStorageService mockCopyStorageService;
 
-  @Mock
-  private TenantUtil mockTenantUtil;
-
   @Inject
   private CopyStorageTask copyStorageTask;
 
@@ -57,59 +42,19 @@ public class CopyStorageTaskTest
   public void configure(Binder binder) {
     binder.bind(TaskScheduler.class).toInstance(mockTaskScheduler);
     binder.bind(CopyStorageService.class).toInstance(mockCopyStorageService);
-    binder.bind(TenantUtil.class).toInstance(mockTenantUtil);
     super.configure(binder);
-  }
-
-  @Before
-  public void before() {
-    lenient().when(mockTenantUtil.isMultiTenant()).thenReturn(true);
   }
 
   @Test
   public void testCopyStorageTask() {
     assertThat(copyStorageTask).isInstanceOf(Task.class);
     assertThat(copyStorageTask).isInstanceOf(InsightJob.class);
-    assertThat(copyStorageTask).isInstanceOf(AllTenantsJob.class);
+    assertThat(copyStorageTask).isInstanceOf(MtiqBatchJob.class);
   }
 
   @Test
   public void testDisallowConcurrentExecution() {
     assertThat(JobBuilder.newJob(CopyStorageTask.class).build().isConcurrentExectionDisallowed()).isTrue();
-  }
-
-  @Test
-  public void testExecute_TenantNotSpecified_Global() {
-    Map<String, List<String>> map = Map.of(
-        "from", List.of(DataStoreType.FILE.name()),
-        "to", List.of(DataStoreType.S3.name())
-    );
-
-    testAsTenant(Tenant.GLOBAL_TENANT, t -> copyStorageTask.execute(map, null));
-
-    ArgumentCaptor<Map<String, String>> argumentCaptor = ArgumentCaptor.forClass(Map.class);
-    verify(mockTaskScheduler).scheduleOneTimeTask(eq(copyStorageTask), argumentCaptor.capture());
-    Map<String, String> value = argumentCaptor.getValue();
-    assertThat(value).containsEntry("tenant", ".*");
-    assertThat(value).containsEntry("from", DataStoreType.FILE.name());
-    assertThat(value).containsEntry("to", DataStoreType.S3.name());
-  }
-
-  @Test
-  public void testExecute_TenantNotSpecified_NotGlobal() {
-    Map<String, List<String>> map = Map.of(
-        "from", List.of(DataStoreType.FILE.name()),
-        "to", List.of(DataStoreType.S3.name())
-    );
-
-    testAsNewTenant("some-tenant", t -> copyStorageTask.execute(map, null));
-
-    ArgumentCaptor<Map<String, String>> argumentCaptor = ArgumentCaptor.forClass(Map.class);
-    verify(mockTaskScheduler).scheduleOneTimeTask(eq(copyStorageTask), argumentCaptor.capture());
-    Map<String, String> value = argumentCaptor.getValue();
-    assertThat(value).containsEntry("tenant", "some-tenant");
-    assertThat(value).containsEntry("from", DataStoreType.FILE.name());
-    assertThat(value).containsEntry("to", DataStoreType.S3.name());
   }
 
   @Test
@@ -163,7 +108,6 @@ public class CopyStorageTaskTest
   @Test
   public void testExecute_ChecksFromAndTo() throws Exception {
     Map<String, List<String>> map = Map.of(
-        "tenant", List.of("some-tenant"),
         "from", List.of(DataStoreType.FILE.name()),
         "to", List.of(DataStoreType.S3.name())
     );
@@ -177,21 +121,8 @@ public class CopyStorageTaskTest
   }
 
   @Test
-  public void testExecute_ChecksTheTenantPattern() {
-    Map<String, List<String>> map = Map.of(
-        "tenant", List.of("("),
-        "from", List.of(DataStoreType.FILE.name()),
-        "to", List.of(DataStoreType.S3.name())
-    );
-
-    assertThatExceptionOfType(PatternSyntaxException.class)
-        .isThrownBy(() -> copyStorageTask.execute(map, null));
-  }
-
-  @Test
   public void testExecute_TriggersTheJob() throws Exception {
     Map<String, List<String>> map = Map.of(
-        "tenant", List.of("some-tenant"),
         "from", List.of(DataStoreType.FILE.name()),
         "to", List.of(DataStoreType.S3.name())
     );
@@ -201,79 +132,8 @@ public class CopyStorageTaskTest
     ArgumentCaptor<Map<String, String>> argumentCaptor = ArgumentCaptor.forClass(Map.class);
     verify(mockTaskScheduler).scheduleOneTimeTask(eq(copyStorageTask), argumentCaptor.capture());
     Map<String, String> value = argumentCaptor.getValue();
-    assertThat(value).containsEntry("tenant", "some-tenant");
     assertThat(value).containsEntry("from", DataStoreType.FILE.name());
     assertThat(value).containsEntry("to", DataStoreType.S3.name());
-  }
-
-  @Test
-  public void testExecuteForTenant_SlugDoesNotMatch() {
-    JobExecutionContext mockJobExecutionContext = mock(JobExecutionContext.class);
-    JobDataMap jobDataMap = new JobDataMap();
-    jobDataMap.put("tenant", "doesNotMatch");
-    jobDataMap.put("from", DataStoreType.FILE.name());
-    jobDataMap.put("to", DataStoreType.S3.name());
-    when(mockJobExecutionContext.getMergedJobDataMap()).thenReturn(jobDataMap);
-
-    testAsNewTenant("some-tenant", t -> copyStorageTask.executeForTenant(mockJobExecutionContext, t));
-
-    verifyNoInteractions(mockCopyStorageService);
-  }
-
-  @Test
-  public void testExecuteForTenant_SlugWildcardDoesNotMatch() {
-    JobExecutionContext mockJobExecutionContext = mock(JobExecutionContext.class);
-    JobDataMap jobDataMap = new JobDataMap();
-    jobDataMap.put("tenant", "does.*");
-    jobDataMap.put("from", DataStoreType.FILE.name());
-    jobDataMap.put("to", DataStoreType.S3.name());
-    when(mockJobExecutionContext.getMergedJobDataMap()).thenReturn(jobDataMap);
-
-    testAsNewTenant("some-tenant", t -> copyStorageTask.executeForTenant(mockJobExecutionContext, t));
-
-    verifyNoInteractions(mockCopyStorageService);
-  }
-
-  @Test
-  public void testExecuteForTenant_SlugExactMatch() {
-    JobExecutionContext mockJobExecutionContext = mock(JobExecutionContext.class);
-    JobDataMap jobDataMap = new JobDataMap();
-    jobDataMap.put("tenant", "some-tenant");
-    jobDataMap.put("from", DataStoreType.FILE.name());
-    jobDataMap.put("to", DataStoreType.S3.name());
-    when(mockJobExecutionContext.getMergedJobDataMap()).thenReturn(jobDataMap);
-
-    testAsNewTenant("some-tenant", t -> copyStorageTask.executeForTenant(mockJobExecutionContext, t));
-
-    verify(mockCopyStorageService).execute(DataStoreType.FILE, DataStoreType.S3);
-  }
-
-  @Test
-  public void testExecuteForTenant_SlugWildcardMatch() {
-    JobExecutionContext mockJobExecutionContext = mock(JobExecutionContext.class);
-    JobDataMap jobDataMap = new JobDataMap();
-    jobDataMap.put("tenant", "some.*");
-    jobDataMap.put("from", DataStoreType.FILE.name());
-    jobDataMap.put("to", DataStoreType.S3.name());
-    when(mockJobExecutionContext.getMergedJobDataMap()).thenReturn(jobDataMap);
-
-    testAsNewTenant("some-tenant", t -> copyStorageTask.executeForTenant(mockJobExecutionContext, t));
-
-    verify(mockCopyStorageService).execute(DataStoreType.FILE, DataStoreType.S3);
-  }
-
-  @Test
-  public void testExecuteForTenant_SingleTenant() {
-    when(mockTenantUtil.isMultiTenant()).thenReturn(false);
-    JobExecutionContext mockJobExecutionContext = mock(JobExecutionContext.class);
-    JobDataMap jobDataMap = new JobDataMap();
-    jobDataMap.put("from", DataStoreType.FILE.name());
-    jobDataMap.put("to", DataStoreType.S3.name());
-    when(mockJobExecutionContext.getMergedJobDataMap()).thenReturn(jobDataMap);
-
-    copyStorageTask.executeForTenant(mockJobExecutionContext, Tenant.SINGLE_TENANT);
-
-    verify(mockCopyStorageService).execute(DataStoreType.FILE, DataStoreType.S3);
   }
 
   @Test

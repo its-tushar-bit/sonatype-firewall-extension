f381446ec494378c806dcd29923b84928e39563c
shanmukhhvs <shanmukha.vadrevu@sonatype.com>
2026-05-25 22:57:24 +0530

    EI-1245: hide legacy for EnterpriseReporting  success-metrics card at IQ v204+                                                                                             (#16100)

--- insight-brain-frontend/src/main/frontend/enterpriseReporting/enterpriseReportingLandingPageSelectors.js
+++ insight-brain-frontend/src/main/frontend/enterpriseReporting/enterpriseReportingLandingPageSelectors.js
@@ -4,7 +4,7 @@
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
 import { createSelector } from '@reduxjs/toolkit';
-import { filter, prop, propEq, unless } from 'ramda';
+import { filter, prop, propEq, reject, unless } from 'ramda';
 import { isNilOrEmpty } from 'MainRoot/util/jsUtil';
 import { combineDashboards } from './utils';
 
@@ -15,21 +15,39 @@ export const selectCombinedDashboards = createSelector(selectDashboardsData, (da
   unless(isNilOrEmpty, combineDashboards)(dashboards)
 );
 
-export const selectEnterpriseDashboards = createSelector(selectCombinedDashboards, (dashboards) => {
+export const selectIqVersion = createSelector(selectEnterpriseReportingLandingPage, prop('iqVersion'));
+
+// Hide the legacy success-metrics card once IQ version >= 204 (replaced by the success_metrics_group split)
+export const selectVisibleDashboards = createSelector(
+  selectCombinedDashboards,
+  selectIqVersion,
+  (dashboards, iqVersion) => {
+    if (isNilOrEmpty(dashboards)) {
+      return [];
+    }
+    const minorVersion = iqVersion ? parseInt(iqVersion.split('.')[1]) : 0;
+    if (minorVersion >= 204) {
+      return reject(propEq('dashboardId', 'success-metrics'), dashboards);
+    }
+    return dashboards;
+  }
+);
+
+export const selectEnterpriseDashboards = createSelector(selectVisibleDashboards, (dashboards) => {
   if (isNilOrEmpty(dashboards)) {
     return [];
   }
   return filter(propEq('category', 'enterprise'), dashboards);
 });
 
-export const selectDataInsightsDashboards = createSelector(selectCombinedDashboards, (dashboards) => {
+export const selectDataInsightsDashboards = createSelector(selectVisibleDashboards, (dashboards) => {
   if (isNilOrEmpty(dashboards)) {
     return [];
   }
   return filter(propEq('category', 'dataInsight'), dashboards);
 });
 
-export const selectPartnerDashboards = createSelector(selectCombinedDashboards, (dashboards) => {
+export const selectPartnerDashboards = createSelector(selectVisibleDashboards, (dashboards) => {
   if (isNilOrEmpty(dashboards)) {
     return [];
   }

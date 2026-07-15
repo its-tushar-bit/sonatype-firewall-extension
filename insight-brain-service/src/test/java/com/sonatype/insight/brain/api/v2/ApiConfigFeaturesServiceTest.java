cbdcb556b5b96b55a51bc828131ba804e965e237
Anastasia Van Riet <110420624+anastasia-vanriet@users.noreply.github.com>
2026-07-15 18:02:40 -0300

    Fix main build: add GLOBAL_SEARCH to expected feature map (CLM-41642) (#16609)

--- insight-brain-service/src/test/java/com/sonatype/insight/brain/api/v2/ApiConfigFeaturesServiceTest.java
+++ insight-brain-service/src/test/java/com/sonatype/insight/brain/api/v2/ApiConfigFeaturesServiceTest.java
@@ -1449,6 +1449,7 @@ public class ApiConfigFeaturesServiceTest
 
     expectedFeatureConfigMap.put("ADVANCED_SEARCH_CONFIGURATION", true);
     expectedFeatureConfigMap.put("ADVANCED_SEARCH_ENABLED", false);
+    expectedFeatureConfigMap.put("GLOBAL_SEARCH", false);
     expectedFeatureConfigMap.put("alpForSbomManager", false);
     expectedFeatureConfigMap.put("API_PAGE", true);
     expectedFeatureConfigMap.put("AUTOMATIC_APPLICATION_CONFIGURATION", true);

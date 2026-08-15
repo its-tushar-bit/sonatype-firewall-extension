f48a7d5730dc538f7ba3fa4d3d912030cfdf9644
Joe Stephens <13700083+j-s-3@users.noreply.github.com>
2026-08-15 13:14:23 +0000

    test: restore SourceControl repositoryUrl assertions (#17081)
    Co-authored-by: Joseph Stephens <jstephens@sonatype.com>

--- insight-brain-testing/insight-brain-variant-test-legacy/src/test/java/com/sonatype/insight/brain/api/v2/ApiSourceControlResourceTest.java
+++ insight-brain-testing/insight-brain-variant-test-legacy/src/test/java/com/sonatype/insight/brain/api/v2/ApiSourceControlResourceTest.java
@@ -387,8 +387,7 @@ public class ApiSourceControlResourceTest
         .post();
 
     assertResponseStatus(400, response);
-    assertThat(response.getBodyText())
-        .startsWith("Repository URL contains characters that are not allowed: whitespace");
+    assertThat(response.getBodyText()).startsWith("SourceControl repositoryUrl is invalid:");
   }
 
   @Test
@@ -405,8 +404,7 @@ public class ApiSourceControlResourceTest
         .post();
 
     assertResponseStatus(400, response);
-    assertThat(response.getBodyText())
-        .startsWith("Repository URL contains characters that are not allowed: whitespace");
+    assertThat(response.getBodyText()).startsWith("Cannot validate SourceControl repositoryUrl");
   }
 
   @Test
